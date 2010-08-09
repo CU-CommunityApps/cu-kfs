745f731b4d189dc60215a5c363268fcc23cb1c5e
kwk43 <kwk43@febce4ab-9070-0410-80a8-fd1c76bc7499>
2010-08-09 19:30:57 +0000

    kiti-713 Developed Asset Service Rate Indicator extended attribute

---
+++ src/main/java/edu/cornell/kfs/module/cam/businessobject/AssetExtension.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package edu.cornell.kfs.module.cam.businessobject;
+
+import org.kuali.rice.kns.bo.PersistableBusinessObjectExtensionBase;
+
+/**
+ * @author kwk43
+ *
+ */
+public class AssetExtension extends PersistableBusinessObjectExtensionBase {
+
+    private Long capitalAssetNumber;
+    private boolean serviceRateIndicator;
+	/**
+	 * @return the capitalAssetNumber
+	 */
+	public Long getCapitalAssetNumber() {
+		return capitalAssetNumber;
+	}
+	/**
+	 * @param capitalAssetNumber the capitalAssetNumber to set
+	 */
+	public void setCapitalAssetNumber(Long capitalAssetNumber) {
+		this.capitalAssetNumber = capitalAssetNumber;
+	}
+	/**
+	 * @return the serviceRateIndicator
+	 */
+	public boolean isServiceRateIndicator() {
+		return serviceRateIndicator;
+	}
+	/**
+	 * @param serviceRateIndicator the serviceRateIndicator to set
+	 */
+	public void setServiceRateIndicator(boolean serviceRateIndicator) {
+		this.serviceRateIndicator = serviceRateIndicator;
+	}
+    
+    
+	
+}
