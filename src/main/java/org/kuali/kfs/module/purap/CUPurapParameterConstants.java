915ce56f148d5b64b0a794250ae14677db873427
vrk4 <vrk4@febce4ab-9070-0410-80a8-fd1c76bc7499>
2010-12-07 20:31:56 +0000

    KITI-763
    PUR-2:  Defined constants used to route Requisition documents, if the requisition is for a B2B venodr

---
+++ src/main/java/org/kuali/kfs/module/purap/CUPurapParameterConstants.java
@@ -0,0 +1,5 @@
+package org.kuali.kfs.module.purap;
+
+public class CUPurapParameterConstants extends PurapParameterConstants{
+	public static final String B2B_TOTAL_AMOUNT_FOR_AUTO_PO = "B2B_TOTAL_AMOUNT_FOR_AUTO_PO";
+}
---
+++ src/main/java/org/kuali/kfs/module/purap/CUPurapWorkflowConstants.java
@@ -0,0 +1,7 @@
+package org.kuali.kfs.module.purap;
+
+public class CUPurapWorkflowConstants extends PurapWorkflowConstants{
+
+	  public static final String B2B_AUTO_PURCHASE_ORDER = "B2BAutoPurchaseOrder";
+	  
+}
