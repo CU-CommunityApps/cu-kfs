cd7e685f730e8e090ba336ae74e34befa1953b49
kwk43 <kwk43@febce4ab-9070-0410-80a8-fd1c76bc7499>
2011-04-19 17:12:35 +0000

    KITI-1969 KFSMI-6643

---
+++ src/main/java/org/kuali/kfs/module/purap/businessobject/PaymentRequestView.java
@@ -0,0 +1,186 @@
+/*
+ * Copyright 2007 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl2.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.purap.businessobject;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.kuali.kfs.module.purap.PurapPropertyConstants;
+import org.kuali.kfs.module.purap.document.PaymentRequestDocument;
+import org.kuali.kfs.sys.businessobject.FinancialSystemDocumentHeader;
+import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.rice.kns.bo.Note;
+import org.kuali.rice.kns.service.DataDictionaryService;
+import org.kuali.rice.kns.util.ObjectUtils;
+import org.kuali.rice.kns.web.format.CurrencyFormatter;
+import org.kuali.rice.kns.web.format.DateFormatter;
+
+/**
+ * Payment Request View Business Object.
+ */
+public class PaymentRequestView extends AbstractRelatedView {
+
+    private String invoiceNumber;
+    private Integer purchaseOrderIdentifier;
+    private String statusCode;
+    private boolean paymentHoldIndicator;
+    private boolean paymentRequestedCancelIndicator;
+    private String vendorName;
+    private String vendorCustomerNumber;
+    private Date paymentRequestPayDate;
+    private Timestamp paymentExtractedTimestamp;
+    private Timestamp paymentPaidTimestamp;
+    
+    // REFERENCE OBJECTS
+    private Status status;
+    private FinancialSystemDocumentHeader documentHeader;
+
+
+    // GETTERS & SETTERS
+    public Object getTotalAmount() {
+        return (new CurrencyFormatter()).format(documentHeader.getFinancialDocumentTotalAmount());
+    }
+
+    public String getInvoiceNumber() {
+        return invoiceNumber;
+    }
+
+    public void setInvoiceNumber(String invoiceNumber) {
+        this.invoiceNumber = invoiceNumber;
+    }
+
+    public Timestamp getPaymentExtractedTimestamp() {
+        return paymentExtractedTimestamp;
+    }
+
+    public void setPaymentExtractedTimestamp(Timestamp paymentExtractedTimestamp) {
+        this.paymentExtractedTimestamp = paymentExtractedTimestamp;
+    }
+
+    public boolean isPaymentHoldIndicator() {
+        return paymentHoldIndicator;
+    }
+
+    public void setPaymentHoldIndicator(boolean paymentHoldIndicator) {
+        this.paymentHoldIndicator = paymentHoldIndicator;
+    }
+        
+    public Timestamp getPaymentPaidTimestamp() {
+        return paymentPaidTimestamp;
+    }
+
+    public void setPaymentPaidTimestamp(Timestamp paymentPaidTimestamp) {
+        this.paymentPaidTimestamp = paymentPaidTimestamp;
+    }
+
+    public boolean isPaymentRequestedCancelIndicator() {
+        return paymentRequestedCancelIndicator;
+    }
+
+    public void setPaymentRequestedCancelIndicator(boolean paymentRequestedCancelIndicator) {
+        this.paymentRequestedCancelIndicator = paymentRequestedCancelIndicator;
+    }
+
+    public Object getPaymentRequestPayDate() {
+        return new DateFormatter().format(paymentRequestPayDate);
+    }
+
+    public void setPaymentRequestPayDate(Date paymentRequestPayDate) {
+        this.paymentRequestPayDate = paymentRequestPayDate;
+    }
+
+    public Integer getPurchaseOrderIdentifier() {
+        return purchaseOrderIdentifier;
+    }
+
+    public void setPurchaseOrderIdentifier(Integer purchaseOrderIdentifier) {
+        this.purchaseOrderIdentifier = purchaseOrderIdentifier;
+    }
+
+    public String getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+    }
+    
+    public Status getStatus() {
+        if (ObjectUtils.isNull(this.status) && StringUtils.isNotEmpty(this.getStatusCode())) {
+            this.refreshReferenceObject(PurapPropertyConstants.STATUS);
+        }
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getVendorCustomerNumber() {
+        return vendorCustomerNumber;
+    }
+
+    public void setVendorCustomerNumber(String vendorCustomerNumber) {
+        this.vendorCustomerNumber = vendorCustomerNumber;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+    
+    /**
+     * @return workflow document type for the PaymentRequestDocument
+     */
+    public String getDocumentType() {
+        return SpringContext.getBean(DataDictionaryService.class).getDocumentTypeNameByClass(PaymentRequestDocument.class);
+    }
+
+    /**
+     * The next three methods are overridden but shouldnt be! If they arent overridden, they dont show up in the tag, not sure why
+     * at this point! (AAP)
+     */
+    @Override
+    public Integer getPurapDocumentIdentifier() {
+        return super.getPurapDocumentIdentifier();
+    }
+
+    @Override
+    public String getDocumentIdentifierString() {
+        return super.getDocumentIdentifierString();
+    }
+
+    /**
+     * @see org.kuali.kfs.module.purap.businessobject.AbstractRelatedView#getNotes()
+     */
+    @Override
+    public List<Note> getNotes() {
+        return super.getNotes();
+    }
+
+    /**
+     * @see org.kuali.kfs.module.purap.businessobject.AbstractRelatedView#getUrl()
+     */
+    @Override
+    public String getUrl() {
+        return super.getUrl();
+    }
+}
