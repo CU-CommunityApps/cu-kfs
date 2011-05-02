67d2b9f0af6961787d38bbe3bbcdc9d4d1a182ab
dwf5 <dwf5@febce4ab-9070-0410-80a8-fd1c76bc7499>
2011-05-02 14:24:14 +0000

    KITI-2025 : Added a null value check to the state retrieval method.

---
+++ src/main/java/org/kuali/kfs/vnd/businessobject/VendorAddress.java
@@ -0,0 +1,328 @@
+/*
+ * Copyright 2007 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl2.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kuali.kfs.vnd.businessobject;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.log4j.Logger;
+import org.kuali.rice.kns.bo.Country;
+import org.kuali.rice.kns.bo.State;
+import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.rice.kns.service.CountryService;
+import org.kuali.rice.kns.service.StateService;
+import org.kuali.kfs.vnd.document.service.VendorService;
+import org.kuali.rice.kns.bo.Inactivateable;
+import org.kuali.rice.kns.bo.PersistableBusinessObjectBase;
+import org.kuali.rice.kns.util.ObjectUtils;
+import org.kuali.rice.kns.util.TypedArrayList;
+
+/**
+ * Address to be associated with a particular Vendor.
+ */
+public class VendorAddress extends PersistableBusinessObjectBase implements VendorRoutingComparable, Inactivateable {
+    private static Logger LOG = Logger.getLogger(VendorAddress.class);
+
+    private Integer vendorAddressGeneratedIdentifier;
+    private Integer vendorHeaderGeneratedIdentifier;
+    private Integer vendorDetailAssignedIdentifier;
+    private String vendorAddressTypeCode;
+    private String vendorLine1Address;
+    private String vendorLine2Address;
+    private String vendorCityName;
+    private String vendorStateCode;
+    private String vendorZipCode;
+    private String vendorCountryCode;
+    private String vendorAttentionName;
+    private String vendorAddressInternationalProvinceName;
+    private String vendorAddressEmailAddress;
+    private String vendorBusinessToBusinessUrlAddress;
+    private String vendorFaxNumber;
+    private boolean vendorDefaultAddressIndicator;
+    private boolean active;
+
+    private List<VendorDefaultAddress> vendorDefaultAddresses;
+
+    private VendorDetail vendorDetail;
+    private AddressType vendorAddressType;
+    private State vendorState;
+    private Country vendorCountry;
+
+    /**
+     * Default constructor.
+     */
+    public VendorAddress() {
+        vendorDefaultAddresses = new TypedArrayList(VendorDefaultAddress.class);
+    }
+
+    public Integer getVendorAddressGeneratedIdentifier() {
+        return vendorAddressGeneratedIdentifier;
+    }
+
+    public void setVendorAddressGeneratedIdentifier(Integer vendorAddressGeneratedIdentifier) {
+        this.vendorAddressGeneratedIdentifier = vendorAddressGeneratedIdentifier;
+    }
+
+    public Integer getVendorHeaderGeneratedIdentifier() {
+        return vendorHeaderGeneratedIdentifier;
+    }
+
+    public void setVendorHeaderGeneratedIdentifier(Integer vendorHeaderGeneratedIdentifier) {
+        this.vendorHeaderGeneratedIdentifier = vendorHeaderGeneratedIdentifier;
+    }
+
+    public Integer getVendorDetailAssignedIdentifier() {
+        return vendorDetailAssignedIdentifier;
+    }
+
+    public void setVendorDetailAssignedIdentifier(Integer vendorDetailAssignedIdentifier) {
+        this.vendorDetailAssignedIdentifier = vendorDetailAssignedIdentifier;
+    }
+
+    public String getVendorAddressInternationalProvinceName() {
+        return vendorAddressInternationalProvinceName;
+    }
+
+    public void setVendorAddressInternationalProvinceName(String vendorAddressInternationalProvinceName) {
+        this.vendorAddressInternationalProvinceName = vendorAddressInternationalProvinceName;
+    }
+
+    public String getVendorAddressEmailAddress() {
+        return vendorAddressEmailAddress;
+    }
+
+    public void setVendorAddressEmailAddress(String vendorAddressEmailAddress) {
+        this.vendorAddressEmailAddress = vendorAddressEmailAddress;
+    }
+
+    public String getVendorAddressTypeCode() {
+        return vendorAddressTypeCode;
+    }
+
+    public void setVendorAddressTypeCode(String vendorAddressTypeCode) {
+        this.vendorAddressTypeCode = vendorAddressTypeCode;
+    }
+
+    public String getVendorLine1Address() {
+        return vendorLine1Address;
+    }
+
+    public void setVendorLine1Address(String vendorLine1Address) {
+        this.vendorLine1Address = vendorLine1Address;
+    }
+
+    public String getVendorLine2Address() {
+        return vendorLine2Address;
+    }
+
+    public void setVendorLine2Address(String vendorLine2Address) {
+        this.vendorLine2Address = vendorLine2Address;
+    }
+
+    public String getVendorCityName() {
+        return vendorCityName;
+    }
+
+    public void setVendorCityName(String vendorCityName) {
+        this.vendorCityName = vendorCityName;
+    }
+
+    public String getVendorStateCode() {
+        return vendorStateCode;
+    }
+
+    public void setVendorStateCode(String vendorStateCode) {
+        this.vendorStateCode = vendorStateCode;
+    }
+
+    public String getVendorZipCode() {
+        return vendorZipCode;
+    }
+
+    public void setVendorZipCode(String vendorZipCode) {
+        this.vendorZipCode = vendorZipCode;
+    }
+
+    public String getVendorCountryCode() {
+        return vendorCountryCode;
+    }
+
+    public void setVendorCountryCode(String vendorCountryCode) {
+        this.vendorCountryCode = vendorCountryCode;
+    }
+
+    public String getVendorAttentionName() {
+        return vendorAttentionName;
+    }
+
+    public void setVendorAttentionName(String vendorAttentionName) {
+        this.vendorAttentionName = vendorAttentionName;
+    }
+
+    public String getVendorBusinessToBusinessUrlAddress() {
+        return vendorBusinessToBusinessUrlAddress;
+    }
+
+    public void setVendorBusinessToBusinessUrlAddress(String vendorBusinessToBusinessUrlAddress) {
+        this.vendorBusinessToBusinessUrlAddress = vendorBusinessToBusinessUrlAddress;
+    }
+
+    public VendorDetail getVendorDetail() {
+        return vendorDetail;
+    }
+
+    /**
+     * Sets the vendorDetail attribute.
+     * 
+     * @param vendorDetail The vendorDetail to set.
+     * @deprecated
+     */
+    public void setVendorDetail(VendorDetail vendorDetail) {
+        this.vendorDetail = vendorDetail;
+    }
+
+    public AddressType getVendorAddressType() {
+        return vendorAddressType;
+    }
+
+    /**
+     * Sets the vendorAddressType attribute.
+     * 
+     * @param vendorAddressType The vendorAddressType to set.
+     * @deprecated
+     */
+    public void setVendorAddressType(AddressType vendorAddressType) {
+        this.vendorAddressType = vendorAddressType;
+    }
+
+    public State getVendorState() {
+    	if(vendorStateCode!=null) {
+    		vendorState = SpringContext.getBean(StateService.class).getByPrimaryIdIfNecessary(vendorCountryCode, vendorStateCode, vendorState);
+    	}
+    	return vendorState;
+        
+    }
+
+    /**
+     * Sets the vendorState attribute.
+     * 
+     * @param vendorState The vendorState to set.
+     * @deprecated
+     */
+    public void setVendorState(State vendorState) {
+        this.vendorState = vendorState;
+    }
+
+    public Country getVendorCountry() {
+        vendorCountry = SpringContext.getBean(CountryService.class).getByPrimaryIdIfNecessary(vendorCountryCode, vendorCountry);
+        return vendorCountry;
+    }
+
+    /**
+     * Sets the vendorCountry attribute.
+     * 
+     * @param vendorCountry The vendorCountry to set.
+     * @deprecated
+     */
+    public void setVendorCountry(Country vendorCountry) {
+        this.vendorCountry = vendorCountry;
+    }
+
+    /**
+     * Gets the vendorFaxNumber attribute.
+     * 
+     * @return Returns the vendorFaxNumber.
+     */
+    public String getVendorFaxNumber() {
+        return vendorFaxNumber;
+    }
+
+    /**
+     * Sets the vendorFaxNumber attribute value.
+     * 
+     * @param vendorFaxNumber The vendorFaxNumber to set.
+     */
+    public void setVendorFaxNumber(String vendorFaxNumber) {
+        this.vendorFaxNumber = vendorFaxNumber;
+    }
+
+    /**
+     * Gets the vendorDefaultAddressIndicator attribute.
+     * 
+     * @return Returns the vendorDefaultAddressIndicator.
+     */
+    public boolean isVendorDefaultAddressIndicator() {
+        return vendorDefaultAddressIndicator;
+    }
+
+    /**
+     * Sets the vendorDefaultAddressIndicator attribute value.
+     * 
+     * @param vendorDefaultAddressIndicator The vendorDefaultAddressIndicator to set.
+     */
+    public void setVendorDefaultAddressIndicator(boolean vendorDefaultAddressIndicator) {
+        this.vendorDefaultAddressIndicator = vendorDefaultAddressIndicator;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public List<VendorDefaultAddress> getVendorDefaultAddresses() {
+        return vendorDefaultAddresses;
+    }
+
+    public void setVendorDefaultAddresses(List<VendorDefaultAddress> vendorDefaultAddresses) {
+        this.vendorDefaultAddresses = vendorDefaultAddresses;
+    }
+
+    /**
+     * @see org.kuali.kfs.vnd.document.routing.VendorRoutingComparable#isEqualForRouting(java.lang.Object)
+     */
+    public boolean isEqualForRouting(Object toCompare) {
+        LOG.debug("Entering isEqualForRouting.");
+        if ((ObjectUtils.isNull(toCompare)) || !(toCompare instanceof VendorAddress)) {
+            LOG.debug("Exiting isEqualForRouting");
+            return false;
+        }
+        else {
+            VendorAddress va = (VendorAddress) toCompare;
+            boolean eq = new EqualsBuilder().append(this.getVendorAddressGeneratedIdentifier(), va.getVendorAddressGeneratedIdentifier()).append(this.getVendorHeaderGeneratedIdentifier(), va.getVendorHeaderGeneratedIdentifier()).append(this.getVendorDetailAssignedIdentifier(), va.getVendorDetailAssignedIdentifier()).append(this.getVendorAddressTypeCode(), va.getVendorAddressTypeCode()).append(this.getVendorLine1Address(), va.getVendorLine1Address()).append(this.getVendorLine2Address(), va.getVendorLine2Address()).append(this.getVendorCityName(), va.getVendorCityName()).append(this.getVendorStateCode(), va.getVendorStateCode()).append(this.getVendorZipCode(), va.getVendorZipCode()).append(this.getVendorCountryCode(), va.getVendorCountryCode()).append(this.getVendorAttentionName(), va.getVendorAttentionName()).append(this.getVendorAddressInternationalProvinceName(), va.getVendorAddressInternationalProvinceName()).append(this.getVendorAddressEmailAddress(),
+                    va.getVendorAddressEmailAddress()).append(this.getVendorBusinessToBusinessUrlAddress(), va.getVendorBusinessToBusinessUrlAddress()).append(this.getVendorFaxNumber(), va.getVendorFaxNumber()).append(this.isVendorDefaultAddressIndicator(), va.isVendorDefaultAddressIndicator()).isEquals();
+            eq &= SpringContext.getBean(VendorService.class).equalMemberLists(this.getVendorDefaultAddresses(), va.getVendorDefaultAddresses());
+            LOG.debug("Exiting isEqualForRouting.");
+            return eq;
+        }
+    }
+
+    /**
+     * @see org.kuali.rice.kns.bo.BusinessObjectBase#toStringMapper()
+     */
+    protected LinkedHashMap toStringMapper() {
+        LinkedHashMap m = new LinkedHashMap();
+        if (this.vendorAddressGeneratedIdentifier != null) {
+            m.put("vendorAddressGeneratedIdentifier", this.vendorAddressGeneratedIdentifier.toString());
+        }
+        return m;
+    }
+
+}
