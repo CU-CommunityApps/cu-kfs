5a3e658ba0345e5421d954080d254e82c75b985a
kwk43 <kwk43@febce4ab-9070-0410-80a8-fd1c76bc7499>
2011-07-19 18:05:00 +0000

    Removing sleep for 5 mins while debugging job

--- src/main/java/org/kuali/kfs/fp/batch/ProcurementCardRouteDocumentsStep.java
+++ src/main/java/org/kuali/kfs/fp/batch/ProcurementCardRouteDocumentsStep.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package edu.cornell.kfs.fp.batch;
+package org.kuali.kfs.fp.batch;
 
 import java.util.Date;
 
