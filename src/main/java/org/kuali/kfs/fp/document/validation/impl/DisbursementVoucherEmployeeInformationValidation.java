32313f98ebe5cd8a330f03280802844d29f57216
kwk43 <kwk43@febce4ab-9070-0410-80a8-fd1c76bc7499>
2011-07-27 20:29:08 +0000

    KITI-2386 Setting payee id to the principal id if empl id is null.

---
+++ src/main/java/org/kuali/kfs/fp/document/validation/impl/DisbursementVoucherEmployeeInformationValidation.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl2.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.fp.document.validation.impl;
+
+import org.kuali.kfs.fp.businessobject.DisbursementVoucherPayeeDetail;
+import org.kuali.kfs.fp.document.DisbursementVoucherDocument;
+import org.kuali.kfs.sys.KFSConstants;
+import org.kuali.kfs.sys.KFSKeyConstants;
+import org.kuali.kfs.sys.KFSPropertyConstants;
+import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.kfs.sys.document.AccountingDocument;
+import org.kuali.kfs.sys.document.validation.GenericValidation;
+import org.kuali.kfs.sys.document.validation.event.AttributedDocumentEvent;
+import org.kuali.rice.kim.bo.Person;
+import org.kuali.rice.kim.service.PersonService;
+import org.kuali.rice.kns.service.DataDictionaryService;
+import org.kuali.rice.kns.util.GlobalVariables;
+import org.kuali.rice.kns.util.MessageMap;
+import org.kuali.rice.kns.util.ObjectUtils;
+
+public class DisbursementVoucherEmployeeInformationValidation extends GenericValidation {
+    private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(DisbursementVoucherEmployeeInformationValidation.class);
+
+    private AccountingDocument accountingDocumentForValidation;
+    
+    public static final String DV_PAYEE_ID_NUMBER_PROPERTY_PATH = KFSPropertyConstants.DV_PAYEE_DETAIL + "." + KFSPropertyConstants.DISB_VCHR_PAYEE_ID_NUMBER;
+
+    /**
+     * @see org.kuali.kfs.sys.document.validation.Validation#validate(org.kuali.kfs.sys.document.validation.event.AttributedDocumentEvent)
+     */
+    public boolean validate(AttributedDocumentEvent event) {
+        LOG.debug("validate start");
+        boolean isValid = true;
+        
+        DisbursementVoucherDocument document = (DisbursementVoucherDocument) accountingDocumentForValidation;
+        DisbursementVoucherPayeeDetail payeeDetail = document.getDvPayeeDetail();
+        
+        if(!payeeDetail.isEmployee() || payeeDetail.isVendor()) {
+            return true;
+        }
+        
+        String employeeId = payeeDetail.getDisbVchrPayeeIdNumber();
+        Person employee = SpringContext.getBean(PersonService.class).getPersonByEmployeeId(employeeId);
+        if (ObjectUtils.isNull(employee)) {
+            employee = SpringContext.getBean(PersonService.class).getPerson(employeeId);
+        }
+        
+        MessageMap errors = GlobalVariables.getMessageMap();
+        errors.addToErrorPath(KFSPropertyConstants.DOCUMENT);
+
+        // check existence of employee
+        if (employee == null) { // If employee is not found, report existence error
+            String label = SpringContext.getBean(DataDictionaryService.class).getAttributeLabel(DisbursementVoucherPayeeDetail.class, KFSPropertyConstants.DISB_VCHR_PAYEE_ID_NUMBER);
+            errors.putError(DV_PAYEE_ID_NUMBER_PROPERTY_PATH, KFSKeyConstants.ERROR_EXISTENCE, label);
+            isValid = false;
+        } 
+        else if(!KFSConstants.EMPLOYEE_ACTIVE_STATUS.equals(employee.getEmployeeStatusCode())) {
+            // If employee is found, then check that employee is active
+            String label = SpringContext.getBean(DataDictionaryService.class).getAttributeLabel(DisbursementVoucherPayeeDetail.class, KFSPropertyConstants.DISB_VCHR_PAYEE_ID_NUMBER);
+            errors.putError(DV_PAYEE_ID_NUMBER_PROPERTY_PATH, KFSKeyConstants.ERROR_INACTIVE, label);
+            isValid = false;
+        }
+        
+        errors.removeFromErrorPath(KFSPropertyConstants.DOCUMENT); 
+
+        return isValid;
+    }
+
+    /**
+     * Gets the accountingDocumentForValidation attribute. 
+     * @return Returns the accountingDocumentForValidation.
+     */
+    public AccountingDocument getAccountingDocumentForValidation() {
+        return accountingDocumentForValidation;
+    }
+
+    /**
+     * Sets the accountingDocumentForValidation attribute value.
+     * 
+     * @param accountingDocumentForValidation The accountingDocumentForValidation to set.
+     */
+    public void setAccountingDocumentForValidation(AccountingDocument accountingDocumentForValidation) {
+        this.accountingDocumentForValidation = accountingDocumentForValidation;
+    }
+}
+
