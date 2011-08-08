f643ab5ce5917be6a338762552f6e2c5300c185d
kwk43 <kwk43@febce4ab-9070-0410-80a8-fd1c76bc7499>
2011-08-08 16:35:51 +0000

    KITI-2426 added Inquiry Presentation Controller for VendorDetail.

--- src/main/java/edu/cornell/kfs/vnd/businessobject/authorization/VendorDetailInquiryPresentationController.java
+++ src/main/java/edu/cornell/kfs/vnd/businessobject/authorization/VendorDetailInquiryPresentationController.java
@@ -4,9 +4,11 @@
 package edu.cornell.kfs.vnd.businessobject.authorization;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.kfs.vnd.businessobject.VendorDetail;
 import org.kuali.rice.kim.service.IdentityManagementService;
 import org.kuali.rice.kim.util.KimConstants;
 import org.kuali.rice.kns.UserSession;
@@ -30,12 +32,19 @@ public class VendorDetailInquiryPresentationController extends InquiryPresentati
 	public Set<String> getConditionallyHiddenPropertyNames(BusinessObject businessObject) {
 		Set<String> retVal = new HashSet<String>();
 		
+		
+		
 		IdentityManagementService idService = SpringContext.getBean(IdentityManagementService.class);
 		UserSession uSession = GlobalVariables.getUserSession();
 		
 		boolean canViewAttachments = idService.isAuthorizedByTemplateName(uSession.getPrincipalId(), KNSConstants.KNS_NAMESPACE, KimConstants.PermissionTemplateNames.VIEW_NOTE_ATTACHMENT, null, null);
 		if (!canViewAttachments) {
-			retVal.add("boNotes.attachmentLink");
+			
+			VendorDetail detail = (VendorDetail)businessObject;
+
+			List boNotes = detail.getBoNotes();
+			for (int i = 0; i < boNotes.size(); i++)
+				retVal.add("boNotes["+i+"].attachmentLink");
 			
 		}
 		return retVal;
