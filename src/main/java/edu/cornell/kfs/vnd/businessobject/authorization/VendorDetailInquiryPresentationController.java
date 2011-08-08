81b37fdf09f499b23b21f6ba8ae3ea3a1a8d334c
kwk43 <kwk43@febce4ab-9070-0410-80a8-fd1c76bc7499>
2011-08-08 15:44:01 +0000

    KITI-2426 added Inquiry Presentation Controller for VendorDetail.

---
+++ src/main/java/edu/cornell/kfs/vnd/businessobject/authorization/VendorDetailInquiryPresentationController.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package edu.cornell.kfs.vnd.businessobject.authorization;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.rice.kim.service.IdentityManagementService;
+import org.kuali.rice.kim.util.KimConstants;
+import org.kuali.rice.kns.UserSession;
+import org.kuali.rice.kns.bo.BusinessObject;
+import org.kuali.rice.kns.inquiry.InquiryPresentationControllerBase;
+import org.kuali.rice.kns.util.GlobalVariables;
+import org.kuali.rice.kns.util.KNSConstants;
+
+/**
+ * @author kwk43
+ *
+ */
+public class VendorDetailInquiryPresentationController extends InquiryPresentationControllerBase {
+
+	/**
+	 * Implement this method to hide fields based on specific data in the record being inquired into
+	 * 
+	 * @return Set of property names that should be hidden
+	 */
+	@Override
+	public Set<String> getConditionallyHiddenPropertyNames(BusinessObject businessObject) {
+		Set<String> retVal = new HashSet<String>();
+		
+		IdentityManagementService idService = SpringContext.getBean(IdentityManagementService.class);
+		UserSession uSession = GlobalVariables.getUserSession();
+		
+		boolean canViewAttachments = idService.isAuthorizedByTemplateName(uSession.getPrincipalId(), KNSConstants.KNS_NAMESPACE, KimConstants.PermissionTemplateNames.VIEW_NOTE_ATTACHMENT, null, null);
+		if (!canViewAttachments) {
+			retVal.add("boNotes.attachmentLink");
+			
+		}
+		return retVal;
+	}
+	
+}
