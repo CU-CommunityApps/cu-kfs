956a6a3d3c05d63a85ea86aac99e06d250dd5863
kwk43 <kwk43@febce4ab-9070-0410-80a8-fd1c76bc7499>
2011-09-07 17:03:51 +0000

    KITI-2444 Refactored to pull reject processing from the transactional block.

---
+++ src/main/java/org/kuali/kfs/module/purap/batch/ElectronicInvoiceStep.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2007-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl2.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.purap.batch;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.kuali.kfs.module.purap.businessobject.ElectronicInvoiceLoad;
+import org.kuali.kfs.module.purap.service.ElectronicInvoiceHelperService;
+import org.kuali.kfs.sys.batch.AbstractStep;
+import org.kuali.kfs.sys.batch.BatchInputFileType;
+import org.kuali.kfs.sys.batch.service.BatchInputFileService;
+
+public class ElectronicInvoiceStep extends AbstractStep {
+    
+    private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(ElectronicInvoiceStep.class);
+
+    private ElectronicInvoiceHelperService electronicInvoiceHelperService;
+
+    public boolean execute(String jobName, Date jobRunDate) {
+        ElectronicInvoiceLoad load = electronicInvoiceHelperService.loadElectronicInvoices();
+        electronicInvoiceHelperService.sendLoadSummary(load);
+        return load.containsRejects();
+    }
+
+    public void setElectronicInvoiceHelperService(ElectronicInvoiceHelperService electronicInvoiceHelperService) {
+        this.electronicInvoiceHelperService = electronicInvoiceHelperService;
+    }
+
+}
---
+++ src/main/java/org/kuali/kfs/module/purap/service/ElectronicInvoiceHelperService.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl2.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.purap.service;
+
+import java.util.List;
+
+import org.kuali.kfs.module.purap.businessobject.ElectronicInvoice;
+import org.kuali.kfs.module.purap.businessobject.ElectronicInvoiceLoad;
+import org.kuali.kfs.module.purap.businessobject.ElectronicInvoiceLoadSummary;
+import org.kuali.kfs.module.purap.businessobject.ElectronicInvoiceOrder;
+import org.kuali.kfs.module.purap.businessobject.ElectronicInvoiceRejectReason;
+import org.kuali.kfs.module.purap.document.ElectronicInvoiceRejectDocument;
+
+public interface ElectronicInvoiceHelperService {
+
+    public ElectronicInvoiceLoad loadElectronicInvoices();
+    
+    public void sendLoadSummary(ElectronicInvoiceLoad eInvoiceLoad);
+    
+    public boolean doMatchingProcess(ElectronicInvoiceRejectDocument rejectDocument);
+    
+    public boolean createPaymentRequest(ElectronicInvoiceRejectDocument rejectDocument);
+    
+}
