1759fe35f05901d96b3ecdb5d9dfcd9016f553c9
kwk43 <kwk43@febce4ab-9070-0410-80a8-fd1c76bc7499>
2011-09-27 17:43:39 +0000

    KITI-2444 Refactored to add a separate step to route the docs created by the first step. Followed the PCard job paradigm.

--- src/main/java/org/kuali/kfs/module/purap/service/ElectronicInvoiceHelperService.java
+++ src/main/java/org/kuali/kfs/module/purap/service/ElectronicInvoiceHelperService.java
@@ -34,4 +34,6 @@ public interface ElectronicInvoiceHelperService {
     
     public boolean createPaymentRequest(ElectronicInvoiceRejectDocument rejectDocument);
     
+    public boolean routeDocuments();
+    
 }
