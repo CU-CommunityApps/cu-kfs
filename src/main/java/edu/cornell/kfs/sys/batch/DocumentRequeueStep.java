b4caa243fd679c259cf08dc580d63c5b9456427d
kwk43 <kwk43@febce4ab-9070-0410-80a8-fd1c76bc7499>
2011-10-04 22:28:04 +0000

    KITI-2589 So that did not work.. we only want to requeue docs that are enroute. The ActionRequestService gets all action requests regardless of doc status.

--- src/main/java/edu/cornell/kfs/sys/batch/DocumentRequeueStep.java
+++ src/main/java/edu/cornell/kfs/sys/batch/DocumentRequeueStep.java
@@ -3,62 +3,62 @@
  */
 package edu.cornell.kfs.sys.batch;
 
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.util.Collection;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import org.kuali.kfs.sys.batch.AbstractStep;
 import org.kuali.kfs.sys.context.SpringContext;
-import org.kuali.rice.kew.actionrequest.service.ActionRequestService;
+import org.kuali.rice.kew.actionrequest.service.DocumentRequeuerService;
+import org.kuali.rice.kew.routeheader.DocumentRouteHeaderValue;
+import org.kuali.rice.kew.routeheader.service.RouteHeaderService;
+import org.kuali.rice.kns.service.DateTimeService;
+import org.kuali.rice.kns.service.ParameterService;
 
 /**
  * @author kwk43
  *
  */
 public class DocumentRequeueStep extends AbstractStep {
-	
-	public boolean execute(String jobName, Date jobRunDate) throws InterruptedException {
-		Set respIds = new HashSet();
-		respIds.add(new Long(29));
-		ActionRequestService ars = SpringContext.getBean(ActionRequestService.class);
-		ars.updateActionRequestsForResponsibilityChange(respIds);
-		
-		return true;
-	}
-	
-	
-	
-/**
+
 	ParameterService parameterService = SpringContext.getBean(ParameterService.class);
-	static String BEFORE_DATE = "REQUEUE_BEFORE_DATE";
-	static String AFTER_DATE = "REQUEUE_AFTER_DATE";
-	static String RESPONSIBILITY_IDS = "RESPONSIBILITY_IDS";
+	//static String BEFORE_DATE = "REQUEUE_BEFORE_DATE";
+	//static String AFTER_DATE = "REQUEUE_AFTER_DATE";
+	//static String RESPONSIBILITY_IDS = "RESPONSIBILITY_IDS";
 	
 	/* (non-Javadoc)
 	 * @see org.kuali.kfs.sys.batch.Step#execute(java.lang.String, java.util.Date)
-	 
+	 */
 	public boolean execute(String jobName, Date jobRunDate) throws InterruptedException {
 		
 		RouteHeaderService routeHeaderService = SpringContext.getBean(RouteHeaderService.class);
 		DocumentRequeuerService requeuer = SpringContext.getBean(DocumentRequeuerService.class);
 
 		Set respIds = new HashSet();
-		List<String> respList = parameterService.getParameterValues("KFS-SYS", "BATCH", RESPONSIBILITY_IDS);
-		for (String resp : respList) {
-			respIds.add(resp);
-		}
+	//	List<String> respList = parameterService.getParameterValues("KFS-SYS", "BATCH", RESPONSIBILITY_IDS);
+		//for (String resp : respList) {
+			respIds.add(new Long(29));
+		//}
 		
-		Date before = getParameterDate(BEFORE_DATE);
-		Date after = getParameterDate(AFTER_DATE);
+	//	Date before = getParameterDate(BEFORE_DATE);
+	//	Date after = getParameterDate(AFTER_DATE);
 		
 		Collection docIds = routeHeaderService.findPendingByResponsibilityIds(respIds);
 		
 		for (Iterator it = docIds.iterator(); it.hasNext(); ) {
 			Long id = (Long)it.next();
 			DocumentRouteHeaderValue header = routeHeaderService.getRouteHeader(id);
-			Timestamp createDate = header.getCreateDate();
-			if (createDate.after(before) && createDate.before(after))
-				requeuer.requeueDocument(id);
+			if (header.isEnroute()) {
+		//		Timestamp createDate = header.getCreateDate();
+		//		if (createDate.after(before) && createDate.before(after)) {
+					requeuer.requeueDocument(id);
+	//			}
+			}
 		}
 		return true;
 	}
@@ -77,5 +77,5 @@ public class DocumentRequeueStep extends AbstractStep {
 		}
 		return date;
 	}
-*/
+
 }
