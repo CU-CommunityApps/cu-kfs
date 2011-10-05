625a43d593ee4e1b9f30d796c874aed5581dd26a
kwk43 <kwk43@febce4ab-9070-0410-80a8-fd1c76bc7499>
2011-10-05 13:39:46 +0000

    KITI-2589 Rewritten to read input file of doc numbers

--- src/main/java/edu/cornell/kfs/sys/batch/DocumentRequeueStep.java
+++ src/main/java/edu/cornell/kfs/sys/batch/DocumentRequeueStep.java
@@ -3,22 +3,19 @@
  */
 package edu.cornell.kfs.sys.batch;
 
-import java.sql.Timestamp;
-import java.text.ParseException;
-import java.util.Collection;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
 
 import org.kuali.kfs.sys.batch.AbstractStep;
 import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.rice.kew.actionrequest.service.DocumentRequeuerService;
 import org.kuali.rice.kew.routeheader.DocumentRouteHeaderValue;
 import org.kuali.rice.kew.routeheader.service.RouteHeaderService;
-import org.kuali.rice.kns.service.DateTimeService;
-import org.kuali.rice.kns.service.ParameterService;
 
 /**
  * @author kwk43
@@ -26,56 +23,40 @@ import org.kuali.rice.kns.service.ParameterService;
  */
 public class DocumentRequeueStep extends AbstractStep {
 
-	//ParameterService parameterService = SpringContext.getBean(ParameterService.class);
-	//static String BEFORE_DATE = "REQUEUE_BEFORE_DATE";
-	//static String AFTER_DATE = "REQUEUE_AFTER_DATE";
-	//static String RESPONSIBILITY_IDS = "RESPONSIBILITY_IDS";
+	private String stagingDirectory;
+	private String fileName = "documentRequeue.txt";
+	
+	
 	
 	/* (non-Javadoc)
 	 * @see org.kuali.kfs.sys.batch.Step#execute(java.lang.String, java.util.Date)
 	 */
 	public boolean execute(String jobName, Date jobRunDate) throws InterruptedException {
 		
-		RouteHeaderService routeHeaderService = SpringContext.getBean(RouteHeaderService.class);
 		DocumentRequeuerService requeuer = SpringContext.getBean(DocumentRequeuerService.class);
 
-		Set respIds = new HashSet();
-	//	List<String> respList = parameterService.getParameterValues("KFS-SYS", "BATCH", RESPONSIBILITY_IDS);
-		//for (String resp : respList) {
-			respIds.add(new Long(4));
-		//}
-		
-	//	Date before = getParameterDate(BEFORE_DATE);
-	//	Date after = getParameterDate(AFTER_DATE);
-		
-		Collection docIds = routeHeaderService.findPendingByResponsibilityIds(respIds);
-		
-		for (Iterator it = docIds.iterator(); it.hasNext(); ) {
-			Long id = (Long)it.next();
-			DocumentRouteHeaderValue header = routeHeaderService.getRouteHeader(id);
-			if (header.isEnroute()) {
-		//		Timestamp createDate = header.getCreateDate();
-		//		if (createDate.after(before) && createDate.before(after)) {
-					requeuer.requeueDocument(id);
-	//			}
-			}
+		File f = new File(stagingDirectory+File.separator+fileName);
+	    ArrayList<String> docIds = new ArrayList<String>();
+
+	    try {
+	    	BufferedReader reader = new BufferedReader(new FileReader(f));
+
+	    	String line = null;
+	    	while ((line=reader.readLine()) != null) {
+	    		docIds.add(line);   	
+	    	}
+	    } catch (IOException ioe) {
+	    	ioe.printStackTrace();
+	    	return false;
+	    }
+	    
+		for (Iterator<String> it = docIds.iterator(); it.hasNext(); ) {
+			Long id = new Long(it.next());
+			requeuer.requeueDocument(id);
 		}
 		return true;
 	}
 	
 	
-//	private Date getParameterDate(String paramName) {
-//		String dateString = parameterService.getParameterValue("KFS-SYS", "Batch", paramName);
-//		DateTimeService dtService = SpringContext.getBean(DateTimeService.class);
-//		java.sql.Date date = null;
-//		if (dateString !=null) {
-//			try {
-//				date = dtService.convertToSqlDate(dateString);
-//			} catch (ParseException e1) {
-//				e1.printStackTrace();
-//			}
-//		}
-//		return date;
-//	}
 
 }
