04ba5cffadb7cd409c8974031414554bc654bf2f
kwk43 <kwk43@febce4ab-9070-0410-80a8-fd1c76bc7499>
2011-10-05 13:42:43 +0000

    KITI-2589 Rewritten to read input file of doc numbers

--- src/main/java/edu/cornell/kfs/sys/batch/DocumentRequeueStep.java
+++ src/main/java/edu/cornell/kfs/sys/batch/DocumentRequeueStep.java
@@ -56,6 +56,12 @@ public class DocumentRequeueStep extends AbstractStep {
 		}
 		return true;
 	}
+
+
+
+	public void setStagingDirectory(String stagingDirectory) {
+		this.stagingDirectory = stagingDirectory;
+	}
 	
 	
 
