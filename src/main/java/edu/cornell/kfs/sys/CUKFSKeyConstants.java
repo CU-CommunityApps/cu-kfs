2124a8e8d8ea5d2b6afd2faa60308f1c47fa137f
sdm86 <sdm86@febce4ab-9070-0410-80a8-fd1c76bc7499>
2011-12-01 15:11:54 +0000

    KITI-2744 KITI-2813
    Implemented another validation class to check for unit cost & accounting line as well as valid percentages for the account distributions

--- src/main/java/edu/cornell/kfs/sys/CUKFSKeyConstants.java
+++ src/main/java/edu/cornell/kfs/sys/CUKFSKeyConstants.java
@@ -9,4 +9,7 @@ public class CUKFSKeyConstants extends KFSKeyConstants{
 	public static final String ERROR_DOCUMENT_ACCMAINT_APPROP_ACCT_NOT_GROUP_CODE = "error.document.accountMaintenance.appropAcctNotAssociatedWithGroupCode";
 	public static final String ERROR_DOCUMENT_VNDMAINT_W9RECEIVED_NOT_POPULATED = "error.document.vendor.w9ReceivedNotPopulatedButIndicatorIsTrue";
 	public static final String ERROR_DOCUMENT_VNDMAINT_W9RECEIVED_POPULATED_W_O_IND = "error.document.vendor.w9ReceivedPopulatedButIndicatorIsNull";
+	public static final String ERROR_NO_UNIT_COST_WITH_ACCOUNTS = "error.no.unit.cost.with.accounts";
+	public static final String ERROR_UNIT_COST_W_O_ACCOUNT = "error.unit.cost.with.out.accounts";
+	public static final String ERROR_NO_ZERO_PERCENT_ACCOUNT_LINES_ALLOWED = "error.no.zero.percent.account.lines.allowed";
 }
