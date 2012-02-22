8910f98c1d5ae494a05980273fdcd5a90d7099aa
dwf5 <dwf5@febce4ab-9070-0410-80a8-fd1c76bc7499>
2012-02-22 16:12:54 +0000

    KFSPTS-735 : Recommitting DV negative amounts code to continue testing.

---
+++ src/main/java/edu/cornell/kfs/fp/document/service/impl/DebitDeterminerServiceImpl.java
@@ -0,0 +1,30 @@
+package edu.cornell.kfs.fp.document.service.impl;
+
+import org.kuali.kfs.fp.document.DisbursementVoucherDocument;
+import org.kuali.kfs.fp.document.NonCheckDisbursementDocument;
+import org.kuali.kfs.sys.businessobject.GeneralLedgerPendingEntrySourceDetail;
+import org.kuali.kfs.sys.document.GeneralLedgerPendingEntrySource;
+import org.kuali.rice.kns.util.KualiDecimal;
+
+public class DebitDeterminerServiceImpl extends	org.kuali.kfs.sys.document.service.impl.DebitDeterminerServiceImpl {
+
+	/**
+	 * Override to check for negatives on DVs
+	 * 
+	 * @see org.kuali.kfs.sys.document.service.impl.DebitDeterminerServiceImpl#isDebitConsideringNothingPositiveOnly(org.kuali.kfs.sys.document.GeneralLedgerPendingEntrySource, org.kuali.kfs.sys.businessobject.GeneralLedgerPendingEntrySourceDetail)
+	 */
+	@Override
+	public boolean isDebitConsideringNothingPositiveOnly(	GeneralLedgerPendingEntrySource poster,	GeneralLedgerPendingEntrySourceDetail postable) {
+        KualiDecimal amount = postable.getAmount();
+        if (!amount.isPositive() && poster instanceof DisbursementVoucherDocument) {
+        	return false;
+        }
+
+        if (amount.isNegative() && poster instanceof NonCheckDisbursementDocument) {
+        	return false;
+        }
+		
+		return super.isDebitConsideringNothingPositiveOnly(poster, postable);
+	}
+
+}
