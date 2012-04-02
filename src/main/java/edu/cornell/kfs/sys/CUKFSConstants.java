379d98a13352385e240770bf4dd8b3866b00b67d
sdm86 <sdm86@febce4ab-9070-0410-80a8-fd1c76bc7499>
2012-04-02 17:28:59 +0000

    KFSPTS-376 Pulling PE customization back into branch

---
+++ src/main/java/edu/cornell/kfs/sys/CUKFSConstants.java
@@ -0,0 +1,23 @@
+package edu.cornell.kfs.sys;
+
+import org.kuali.kfs.sys.ParameterKeyConstants;
+import org.kuali.rice.core.util.JSTLConstants;
+
+public class CUKFSConstants extends JSTLConstants implements ParameterKeyConstants {
+	
+	public static class PreEncumbranceDocumentConstants {
+		public static final String BIWEEKLY = "biWeekly";
+		public static final String CUSTOM = "custom";
+		public static final String SEMIMONTHLY = "semiMonthly";
+		public static final String MONTHLY = "monthly";
+	}
+	
+	public static class PreEncumbranceSourceAccountingLineConstants {
+		public static final String END_DATE = "endDate";
+		public static final String START_DATE = "startDate";
+		public static final String AUTO_DISENCUMBER_TYPE = "autoDisEncumberType";
+		public static final String PARTIAL_TRANSACTION_COUNT = "partialTransactionCount";
+		public static final String PARTIAL_AMOUNT = "partialAmount";
+	}
+	
+}
