2806573917a0804bf9c6054ccefb62ce4efcb765
mrw225 <mrw225@febce4ab-9070-0410-80a8-fd1c76bc7499>
2012-07-02 20:25:55 +0000

    KFSPTS-1293 - Added code to push the emplid for the IncumbentSalarySettingAction.java class and positionNumber for the positionSalarySettingAction.java class into their respective forms from the request object as the request object does contain the correct emplid / position number for IncumbentSalarySettingAction.java / positionSalarySettingAction.java whereas the forms themselves do not.

---
+++ src/main/java/org/kuali/kfs/module/bc/document/web/struts/IncumbentSalarySettingAction.java
@@ -0,0 +1,174 @@
+/*
+ * Copyright 2007 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl2.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.bc.document.web.struts;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.kuali.kfs.module.bc.BCConstants;
+import org.kuali.kfs.module.bc.BCKeyConstants;
+import org.kuali.kfs.module.bc.BCPropertyConstants;
+import org.kuali.kfs.module.bc.businessobject.BudgetConstructionIntendedIncumbent;
+import org.kuali.kfs.module.bc.businessobject.PendingBudgetConstructionAppointmentFunding;
+import org.kuali.kfs.module.bc.service.BudgetConstructionIntendedIncumbentService;
+import org.kuali.kfs.sys.KFSConstants;
+import org.kuali.kfs.sys.KFSPropertyConstants;
+import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.rice.kns.service.BusinessObjectService;
+import org.kuali.rice.kns.util.GlobalVariables;
+import org.kuali.rice.kns.util.MessageList;
+import org.kuali.rice.kns.util.MessageMap;
+
+/**
+ * the struts action for the salary setting for incumbent
+ */
+public class IncumbentSalarySettingAction extends DetailSalarySettingAction {
+    private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(IncumbentSalarySettingAction.class);
+
+    private BusinessObjectService businessObjectService = SpringContext.getBean(BusinessObjectService.class);
+    private BudgetConstructionIntendedIncumbentService budgetConstructionIntendedIncumbentService = SpringContext.getBean(BudgetConstructionIntendedIncumbentService.class);
+
+    /**
+     * @see org.kuali.kfs.module.bc.document.web.struts.SalarySettingBaseAction#loadExpansionScreen(org.apache.struts.action.ActionMapping,
+     *      org.apache.struts.action.ActionForm, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     */
+    @Override
+    public ActionForward loadExpansionScreen(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        IncumbentSalarySettingForm incumbentSalarySettingForm = (IncumbentSalarySettingForm) form;
+        // KFSPTS-1293 - (See also PositionSalarySetting.java - loadExpansionScreen method) This issue is that after selecting a row from the list, any subsequent
+        //   selections from that list display only the data from the first selection and not the one just selected.  This resolution is a workaround and as such
+        //   is noted as obviously as possible here.  The specific issue is that data coming in from the list is not being propagated to this form class however
+        //   the request object does contain the updated URL parameters and other form data.
+        // BEGINNING OF WORKAROUND
+        incumbentSalarySettingForm.setEmplid(request.getParameterValues("emplid")[0]);   // ********
+        // END OF WORKAROUND
+        MessageMap errorMap;
+        if (incumbentSalarySettingForm.isBudgetByAccountMode()){
+            errorMap = incumbentSalarySettingForm.getCallBackErrors();
+        }
+        else {
+            errorMap = GlobalVariables.getMessageMap();
+        }
+
+        // update incumbent record if required
+        if (incumbentSalarySettingForm.isRefreshIncumbentBeforeSalarySetting()) {
+            budgetConstructionIntendedIncumbentService.refreshIncumbentFromExternal(incumbentSalarySettingForm.getEmplid());
+        }
+
+        // use the passed url parms to get the record from DB
+        Map<String, Object> fieldValues = incumbentSalarySettingForm.getKeyMapOfSalarySettingItem();
+        BudgetConstructionIntendedIncumbent budgetConstructionIntendedIncumbent = (BudgetConstructionIntendedIncumbent) businessObjectService.findByPrimaryKey(BudgetConstructionIntendedIncumbent.class, fieldValues);
+
+        if (budgetConstructionIntendedIncumbent == null) {
+            String emplid = (String) fieldValues.get(KFSPropertyConstants.EMPLID);
+            
+            errorMap.putError(KFSConstants.GLOBAL_MESSAGES, BCKeyConstants.ERROR_INCUMBENT_NOT_FOUND, emplid);
+            if (incumbentSalarySettingForm.isBudgetByAccountMode()){
+                return this.returnToCaller(mapping, form, request, response);
+            }
+            else {
+                this.cleanupAnySessionForm(mapping, request);
+                return mapping.findForward(BCConstants.MAPPING_ORGANIZATION_SALARY_SETTING_RETURNING);
+            }
+        }
+
+        incumbentSalarySettingForm.setBudgetConstructionIntendedIncumbent(budgetConstructionIntendedIncumbent);
+        if (incumbentSalarySettingForm.isSingleAccountMode()) {
+            incumbentSalarySettingForm.pickAppointmentFundingsForSingleAccount();
+        }
+
+        // acquire position and funding locks for the associated funding lines
+        if (!incumbentSalarySettingForm.isViewOnlyEntry()) {
+            incumbentSalarySettingForm.postProcessBCAFLines();
+            incumbentSalarySettingForm.setNewBCAFLine(incumbentSalarySettingForm.createNewAppointmentFundingLine());
+            
+            boolean accessModeUpdated = incumbentSalarySettingForm.updateAccessMode(errorMap);
+            if (!accessModeUpdated) {
+                if (incumbentSalarySettingForm.isBudgetByAccountMode()){
+                    return this.returnToCaller(mapping, form, request, response);
+                }
+                else {
+                    this.cleanupAnySessionForm(mapping, request);
+                    return mapping.findForward(BCConstants.MAPPING_ORGANIZATION_SALARY_SETTING_RETURNING);
+                }
+            }
+
+            boolean gotLocks = incumbentSalarySettingForm.acquirePositionAndFundingLocks(errorMap);
+            if (!gotLocks) {
+                if (incumbentSalarySettingForm.isBudgetByAccountMode()){
+                    return this.returnToCaller(mapping, form, request, response);
+                }
+                else {
+                    this.cleanupAnySessionForm(mapping, request);
+                    return mapping.findForward(BCConstants.MAPPING_ORGANIZATION_SALARY_SETTING_RETURNING);
+                }
+            }
+        }
+
+        return mapping.findForward(KFSConstants.MAPPING_BASIC);
+    }
+
+    /**
+     * @see org.kuali.kfs.module.bc.document.web.struts.DetailSalarySettingAction#save(org.apache.struts.action.ActionMapping, org.apache.struts.action.ActionForm, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     */
+    @Override
+    public ActionForward save(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        ActionForward saveAction =  super.save(mapping, form, request, response);
+
+        IncumbentSalarySettingForm incumbentSalarySettingForm = (IncumbentSalarySettingForm) form;
+        this.sendWarnings(incumbentSalarySettingForm, GlobalVariables.getMessageList());
+        
+        return saveAction;
+    }
+
+    /**
+     * send warning messsages back to the caller
+     * 
+     * @param incumbentSalarySettingForm
+     * @param warnings
+     */
+    public void sendWarnings(IncumbentSalarySettingForm incumbentSalarySettingForm, MessageList warnings){
+        List<PendingBudgetConstructionAppointmentFunding> activeAppointmentFundings = incumbentSalarySettingForm.getActiveFundingLines();
+        if (activeAppointmentFundings == null || activeAppointmentFundings.isEmpty()) {
+            return;
+        }
+        BigDecimal requestedFteQuantityTotal = incumbentSalarySettingForm.getAppointmentRequestedFteQuantityTotal();
+        
+        // check for an active line that is LWPA or LWPF
+        boolean hasFundingLineInvolvedLeaveWithoutPay = this.hasFundingLineInvolvedLeaveWithoutPay(activeAppointmentFundings);
+
+        if (!hasFundingLineInvolvedLeaveWithoutPay && requestedFteQuantityTotal.compareTo(BigDecimal.ONE) != 0) {
+            warnings.add(BCKeyConstants.WARNING_FTE_NOT_ONE);
+        }
+
+    }
+
+    /**
+     * @see org.kuali.kfs.module.bc.document.web.struts.SalarySettingBaseAction#getFundingAwareObjectName()
+     */
+    @Override
+    protected String getFundingAwareObjectName() {
+        return BCPropertyConstants.BUDGET_CONSTRUCTION_INTENDED_INCUMBENT;
+    }
+}
---
+++ src/main/java/org/kuali/kfs/module/bc/document/web/struts/PositionSalarySettingAction.java
@@ -0,0 +1,329 @@
+/*
+ * Copyright 2007 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl2.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.bc.document.web.struts;
+
+import static org.kuali.kfs.module.bc.BCConstants.AppointmentFundingDurationCodes.NONE;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.kuali.kfs.module.bc.BCConstants;
+import org.kuali.kfs.module.bc.BCKeyConstants;
+import org.kuali.kfs.module.bc.BCPropertyConstants;
+import org.kuali.kfs.module.bc.BCConstants.LockStatus;
+import org.kuali.kfs.module.bc.businessobject.BudgetConstructionLockStatus;
+import org.kuali.kfs.module.bc.businessobject.BudgetConstructionPosition;
+import org.kuali.kfs.module.bc.businessobject.PendingBudgetConstructionAppointmentFunding;
+import org.kuali.kfs.module.bc.document.service.LockService;
+import org.kuali.kfs.module.bc.document.service.SalarySettingService;
+import org.kuali.kfs.module.bc.service.BudgetConstructionPositionService;
+import org.kuali.kfs.sys.KFSConstants;
+import org.kuali.kfs.sys.KFSPropertyConstants;
+import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.rice.kns.service.BusinessObjectService;
+import org.kuali.rice.kns.util.GlobalVariables;
+import org.kuali.rice.kns.util.MessageList;
+import org.kuali.rice.kns.util.MessageMap;
+
+/**
+ * the struts action for the salary setting for position
+ */
+public class PositionSalarySettingAction extends DetailSalarySettingAction {
+    private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(PositionSalarySettingAction.class);
+
+    private SalarySettingService salarySettingService = SpringContext.getBean(SalarySettingService.class);
+    private BusinessObjectService businessObjectService = SpringContext.getBean(BusinessObjectService.class);
+    private BudgetConstructionPositionService budgetConstructionPositionService = SpringContext.getBean(BudgetConstructionPositionService.class);
+
+    /**
+     * @see org.kuali.kfs.module.bc.document.web.struts.SalarySettingBaseAction#loadExpansionScreen(org.apache.struts.action.ActionMapping,
+     *      org.apache.struts.action.ActionForm, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     */
+    @Override
+    public ActionForward loadExpansionScreen(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        PositionSalarySettingForm positionSalarySettingForm = (PositionSalarySettingForm) form;
+        // KFSPTS-1293 - (See also IncumbentSalarySetting.java - loadExpansionScreen method) This issue is that after selecting a row from the list, any subsequent
+        //   selections from that list display only the data from the first selection and not the one just selected.  This resolution is a workaround and as such
+        //   is noted as obviously as possible here.  The specific issue is that data coming in from the list is not being propagated to this form class however
+        //   the request object does contain the updated URL parameters and other form data.
+        // BEGINNING OF WORKAROUND
+        positionSalarySettingForm.setPositionNumber(request.getParameterValues("positionNumber")[0]);   // ********
+        // END OF WORKAROUND
+        MessageMap errorMap;
+        if (positionSalarySettingForm.isBudgetByAccountMode()) {
+            errorMap = positionSalarySettingForm.getCallBackErrors();
+        }
+        else {
+            errorMap = GlobalVariables.getMessageMap();
+        }
+
+        // update the position record if required
+        ActionForward resyncAction = this.resyncPositionBeforeSalarySetting(mapping, form, request, response);
+        if (resyncAction != null) {
+            return resyncAction;
+        }
+
+        Map<String, Object> fieldValues = positionSalarySettingForm.getKeyMapOfSalarySettingItem();
+        BudgetConstructionPosition budgetConstructionPosition = (BudgetConstructionPosition) businessObjectService.findByPrimaryKey(BudgetConstructionPosition.class, fieldValues);
+        if (budgetConstructionPosition == null) {
+            String positionNumber = (String) fieldValues.get(KFSPropertyConstants.POSITION_NUMBER);
+            String fiscalYear = (String) fieldValues.get(KFSPropertyConstants.UNIVERSITY_FISCAL_YEAR);
+            errorMap.putError(KFSConstants.GLOBAL_MESSAGES, BCKeyConstants.ERROR_POSITION_NOT_FOUND, positionNumber, fiscalYear);
+
+            if (positionSalarySettingForm.isBudgetByAccountMode()) {
+                return this.returnToCaller(mapping, form, request, response);
+            }
+            else {
+                this.cleanupAnySessionForm(mapping, request);
+                return mapping.findForward(BCConstants.MAPPING_ORGANIZATION_SALARY_SETTING_RETURNING);
+            }
+        }
+
+        // Lock the position even if there are no current funding lines attached
+        Integer universityFiscalYear = budgetConstructionPosition.getUniversityFiscalYear();
+        String positionNumber = budgetConstructionPosition.getPositionNumber();
+        String principalId = GlobalVariables.getUserSession().getPerson().getPrincipalId();
+
+        // attempt to lock position
+        BudgetConstructionLockStatus bcLockStatus = SpringContext.getBean(LockService.class).lockPosition(positionNumber, universityFiscalYear, principalId);
+        if (!bcLockStatus.getLockStatus().equals(BCConstants.LockStatus.SUCCESS)) {
+            errorMap.putError(KFSConstants.GLOBAL_ERRORS, BCKeyConstants.ERROR_FAIL_TO_LOCK_POSITION, budgetConstructionPosition.toString());
+            if (positionSalarySettingForm.isBudgetByAccountMode()) {
+                return this.returnToCaller(mapping, form, request, response);
+            }
+            else {
+                this.cleanupAnySessionForm(mapping, request);
+                return mapping.findForward(BCConstants.MAPPING_ORGANIZATION_SALARY_SETTING_RETURNING);
+            }
+        }
+
+
+        positionSalarySettingForm.setBudgetConstructionPosition(budgetConstructionPosition);
+        if (positionSalarySettingForm.isSingleAccountMode()) {
+            positionSalarySettingForm.pickAppointmentFundingsForSingleAccount();
+        }
+
+        // acquire position and funding locks for the associated funding lines
+        if (!positionSalarySettingForm.isViewOnlyEntry()) {
+            positionSalarySettingForm.postProcessBCAFLines();
+            positionSalarySettingForm.setNewBCAFLine(positionSalarySettingForm.createNewAppointmentFundingLine());
+
+            boolean accessModeUpdated = positionSalarySettingForm.updateAccessMode(errorMap);
+            if (!accessModeUpdated) {
+                this.unlockPositionOnly(positionSalarySettingForm);
+                if (positionSalarySettingForm.isBudgetByAccountMode()) {
+                    return this.returnToCaller(mapping, form, request, response);
+                }
+                else {
+                    this.cleanupAnySessionForm(mapping, request);
+                    return mapping.findForward(BCConstants.MAPPING_ORGANIZATION_SALARY_SETTING_RETURNING);
+                }
+            }
+
+            boolean gotLocks = positionSalarySettingForm.acquirePositionAndFundingLocks(errorMap);
+            if (!gotLocks) {
+                this.unlockPositionOnly(positionSalarySettingForm);
+                if (positionSalarySettingForm.isBudgetByAccountMode()) {
+                    return this.returnToCaller(mapping, form, request, response);
+                }
+                else {
+                    this.cleanupAnySessionForm(mapping, request);
+                    return mapping.findForward(BCConstants.MAPPING_ORGANIZATION_SALARY_SETTING_RETURNING);
+                }
+            }
+        }
+
+        return mapping.findForward(KFSConstants.MAPPING_BASIC);
+    }
+
+    /**
+     * enable to send warning after saving
+     * 
+     * @see org.kuali.kfs.module.bc.document.web.struts.DetailSalarySettingAction#save(org.apache.struts.action.ActionMapping,
+     *      org.apache.struts.action.ActionForm, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     */
+    @Override
+    public ActionForward save(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ActionForward saveAction = super.save(mapping, form, request, response);
+
+        PositionSalarySettingForm positionSalarySettingForm = (PositionSalarySettingForm) form;
+        this.sendWarnings(positionSalarySettingForm, GlobalVariables.getMessageList());
+
+        return saveAction;
+    }
+
+    /**
+     * send warning messsages back to the caller
+     * 
+     * @param positionSalarySettingForm the given position salary setting form
+     * @param warnings the warning list that can hold the warning messages if any
+     */
+    public void sendWarnings(PositionSalarySettingForm positionSalarySettingForm, MessageList warnings) {
+        List<PendingBudgetConstructionAppointmentFunding> activeAppointmentFundings = positionSalarySettingForm.getActiveFundingLines();
+        if (activeAppointmentFundings == null || activeAppointmentFundings.isEmpty()) {
+            return;
+        }
+
+        boolean hasFundingLineInvolveLeave = this.hasFundingLineInvolvedLeave(activeAppointmentFundings);
+        BudgetConstructionPosition budgetConstructionPosition = positionSalarySettingForm.getBudgetConstructionPosition();
+
+        BigDecimal positionFte = budgetConstructionPosition.getPositionFullTimeEquivalency().setScale(2, BigDecimal.ROUND_HALF_UP);
+        BigDecimal requestedFteQuantityTotal = positionSalarySettingForm.getAppointmentRequestedFteQuantityTotal().setScale(2, BigDecimal.ROUND_HALF_UP);
+        if (!hasFundingLineInvolveLeave && requestedFteQuantityTotal.compareTo(positionFte) != 0) {
+            warnings.add(BCKeyConstants.WARNING_FTE_NOT_EQUAL);
+        }
+
+        BigDecimal positionStandardHours = budgetConstructionPosition.getPositionStandardHoursDefault().setScale(2, BigDecimal.ROUND_HALF_UP);
+        BigDecimal requestedStandardHoursTotal = positionSalarySettingForm.getAppointmentRequestedStandardHoursTotal().setScale(2, BigDecimal.ROUND_HALF_UP);
+        if (!hasFundingLineInvolveLeave && requestedStandardHoursTotal.compareTo(positionStandardHours) != 0) {
+            warnings.add(BCKeyConstants.WARNING_WORKING_HOUR_NOT_EQUAL);
+        }
+
+        if (positionSalarySettingForm.isPendingPositionSalaryChange()) {
+            warnings.add(BCKeyConstants.WARNING_RECALCULATE_NEEDED);
+        }
+    }
+
+    /**
+     * Recalculates all rows where the position change flags are set and the row is edit-able and active. Sets funding months, FTE,
+     * CSF FTE, and normalizes biweekly request amounts, where appropriate This action is called from the global calculate button.
+     * 
+     * @param mapping
+     * @param form
+     * @param request
+     * @param response
+     * @return
+     * @throws Exception
+     */
+    public ActionForward recalculateAllSalarySettingLines(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        PositionSalarySettingForm positionSalarySettingForm = (PositionSalarySettingForm) form;
+
+        List<PendingBudgetConstructionAppointmentFunding> appointmentFundings = positionSalarySettingForm.getActiveFundingLines();
+        for (PendingBudgetConstructionAppointmentFunding appointmentFunding : appointmentFundings) {
+            if (!appointmentFunding.isDisplayOnlyMode()) {
+                this.recalculateSalarySettingLine(appointmentFunding);
+            }
+        }
+        return mapping.findForward(KFSConstants.MAPPING_BASIC);
+    }
+
+    /**
+     * Recalculates a single row where the position change flags are set and the row is edit-able and active. Sets funding months,
+     * FTE, CSF FTE, and normalizes biweekly request amounts, where appropriate This action is called from the row action calculate
+     * button.
+     * 
+     * @param mapping
+     * @param form
+     * @param request
+     * @param response
+     * @return
+     * @throws Exception
+     */
+    public ActionForward recalculateSalarySettingLine(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        PositionSalarySettingForm positionSalarySettingForm = (PositionSalarySettingForm) form;
+        PendingBudgetConstructionAppointmentFunding appointmentFunding = this.getSelectedFundingLine(request, positionSalarySettingForm);
+
+        this.recalculateSalarySettingLine(appointmentFunding);
+
+        return mapping.findForward(KFSConstants.MAPPING_BASIC);
+    }
+
+    /**
+     * Recalculates a PendingBudgetConstructionAppointmentFunding. Sets funding months, FTE, CSF FTE, and normalizes biweekly
+     * request amounts, where appropriate
+     * 
+     * @param appointmentFunding
+     */
+    protected void recalculateSalarySettingLine(PendingBudgetConstructionAppointmentFunding appointmentFunding) {
+
+        // do the recalc on the passed line and reset the indicator
+        // there are no rule checks involved in this operation
+        if (appointmentFunding.isPositionSalaryChangeIndicator()) {
+            appointmentFunding.setPositionSalaryChangeIndicator(Boolean.FALSE);
+
+            // check if months needs reset
+            if (appointmentFunding.getAppointmentFundingDurationCode().equals(NONE.durationCode)) {
+                if (!appointmentFunding.getAppointmentFundingMonth().equals(appointmentFunding.getBudgetConstructionPosition().getIuNormalWorkMonths())) {
+                    appointmentFunding.setAppointmentFundingMonth(appointmentFunding.getBudgetConstructionPosition().getIuNormalWorkMonths());
+                }
+            }
+
+            // recalc request fte and if hourly, normalize request amount and hourly rate
+            salarySettingService.recalculateDerivedInformation(appointmentFunding);
+
+        }
+
+
+        // doing this just to do cleanup of the default object change flag
+        // the rules will force the user to mark the line delete
+        // if the default object doesn't match with the line object when saving
+        if (appointmentFunding.isPositionObjectChangeIndicator()) {
+            appointmentFunding.setPositionObjectChangeIndicator(Boolean.FALSE);
+        }
+
+    }
+
+    /**
+     * @see org.kuali.kfs.module.bc.document.web.struts.SalarySettingBaseAction#getFundingAwareObjectName()
+     */
+    @Override
+    protected String getFundingAwareObjectName() {
+        return BCPropertyConstants.BUDGET_CONSTRUCTION_POSITION;
+    }
+
+    /**
+     * resyn position brefore performing salary setting
+     */
+    private ActionForward resyncPositionBeforeSalarySetting(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        PositionSalarySettingForm positionSalarySettingForm = (PositionSalarySettingForm) form;
+
+        if (!positionSalarySettingForm.isRefreshPositionBeforeSalarySetting()) {
+            return null;
+        }
+
+        Integer universityFiscalYear = positionSalarySettingForm.getUniversityFiscalYear();
+        String positionNumber = positionSalarySettingForm.getPositionNumber();
+        String principalId = GlobalVariables.getUserSession().getPerson().getPrincipalId();
+
+        // attempt to lock position and associated funding
+        BudgetConstructionLockStatus bcLockStatus = SpringContext.getBean(LockService.class).lockPositionAndActiveFunding(universityFiscalYear, positionNumber, principalId);
+        if (!bcLockStatus.getLockStatus().equals(BCConstants.LockStatus.SUCCESS)) {
+            GlobalVariables.getMessageMap().putError(KFSConstants.GLOBAL_ERRORS, BCKeyConstants.ERROR_POSITION_LOCK_NOT_OBTAINED, new String[] { universityFiscalYear.toString(), positionNumber });
+            return this.returnToCaller(mapping, form, request, response);
+        }
+
+        try {
+            budgetConstructionPositionService.refreshPositionFromExternal(universityFiscalYear, positionNumber);
+        }
+        finally {
+            // release locks
+            LockStatus lockStatus = SpringContext.getBean(LockService.class).unlockPositionAndActiveFunding(universityFiscalYear, positionNumber, principalId);
+            if (!lockStatus.equals(BCConstants.LockStatus.SUCCESS)) {
+                LOG.error(String.format("unable to unlock position and active funding records: %s, %s, %s", universityFiscalYear, positionNumber, principalId));
+                throw new RuntimeException(String.format("unable to unlock position and active funding records: %s, %s, %s", universityFiscalYear, positionNumber, principalId));
+            }
+        }
+
+        return null;
+    }
+}
