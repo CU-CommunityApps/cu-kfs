45d06586a8d5ca9a933284706578e09b68e97cde
dwf5 <dwf5@febce4ab-9070-0410-80a8-fd1c76bc7499>
2012-07-23 16:59:30 +0000

    KFSPTS-918, KFSPTS-1240 : Added constants and messages to be displayed by the referenced JIRAs.

--- src/main/java/edu/cornell/kfs/sys/CUKFSKeyConstants.java
+++ src/main/java/edu/cornell/kfs/sys/CUKFSKeyConstants.java
@@ -4,6 +4,8 @@ import org.kuali.kfs.sys.KFSKeyConstants;
 
 public class CUKFSKeyConstants extends KFSKeyConstants{
 	
+    public static final String MESSAGE_BATCH_UPLOAD_TITLE_COMMODITY_CODE = "message.batchUpload.title.commodityCode";
+
     public static final String ERROR_DOCUMENT_ACCMAINT_PROGRAM_CODE_NOT_GROUP_CODE = "error.document.accountMaintenance.programCodeNotAssociatedWithGroupCode";
     public static final String ERROR_DOCUMENT_ACCMAINT_PROGRAM_CODE_CANNOT_BE_BLANK_FOR_GROUP_CODE ="error.document.accountMaintenance.programCodeCannotBeBlank";
 	public static final String ERROR_DOCUMENT_ACCMAINT_APPROP_ACCT_NOT_GROUP_CODE = "error.document.accountMaintenance.appropAcctNotAssociatedWithGroupCode";	
@@ -15,4 +17,10 @@ public class CUKFSKeyConstants extends KFSKeyConstants{
 	public static final String ERROR_UNIT_COST_W_O_ACCOUNT = "error.unit.cost.with.out.accounts";
 	public static final String ERROR_NO_ZERO_PERCENT_ACCOUNT_LINES_ALLOWED = "error.no.zero.percent.account.lines.allowed";
 		
+    public static final String MESSAGE_REPORT_NEW_COMMODITY_CODE_TITLE_LINE = "message.report.commodityCode.new.titleLine";
+    public static final String MESSAGE_REPORT_UPDATE_COMMODITY_CODE_TITLE_LINE = "message.report.commodityCode.update.titleLine";
+    public static final String MESSAGE_REPORT_INACTIVE_COMMODITY_CODE_TITLE_LINE = "message.report.commodityCode.inactive.titleLine";
+	
+    public static final String WARNING_DV_PAYEE_MUST_BE_ACTIVE = "warning.dv.payee.mustBeActive";
+	
 }
