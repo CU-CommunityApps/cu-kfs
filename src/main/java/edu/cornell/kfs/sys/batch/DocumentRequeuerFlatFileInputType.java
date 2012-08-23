11d967b7b21226b22f9ad1095135ee08c1774804
kwk43 <kwk43@febce4ab-9070-0410-80a8-fd1c76bc7499>
2012-08-23 14:30:33 +0000

    KFSPTS-1366 Adding document requeuer file upload.

---
+++ src/main/java/edu/cornell/kfs/sys/batch/DocumentRequeuerFlatFileInputType.java
@@ -0,0 +1,48 @@
+package edu.cornell.kfs.sys.batch;
+
+import java.io.File;
+
+import org.kuali.kfs.sys.batch.BatchInputFileTypeBase;
+import org.kuali.kfs.sys.exception.ParseException;
+
+public class DocumentRequeuerFlatFileInputType extends BatchInputFileTypeBase {
+
+	 private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(DocumentRequeuerFlatFileInputType.class);
+
+	    private static final String FILE_NAME_PREFIX = "documentRequeue";
+	
+	public String getFileTypeIdentifer() {
+		return "documentRequeuerFlatFileInputType";
+	}
+
+	public String getFileName(String principalName, Object parsedFileContents, String fileUserIdentifuer) {
+		return FILE_NAME_PREFIX;
+	}
+
+	public Object parse(byte[] fileByteContent) throws ParseException {
+		return null;
+	}
+
+	public boolean validate(Object parsedFileContents) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	public void process(String fileName, Object parsedFileContents) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getAuthorPrincipalName(File file) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getTitleKey() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
