bedd300f5c157cf500c63e2703f5796b1cd77bd6
dwf5 <dwf5@febce4ab-9070-0410-80a8-fd1c76bc7499>
2012-10-17 18:09:13 +0000

    KFSPTS-273 KFSPTS-1488 : Added new constants for parameters that will be added to support new batch functionality.

--- src/main/java/edu/cornell/kfs/sys/CUKFSParameterKeyConstants.java
+++ src/main/java/edu/cornell/kfs/sys/CUKFSParameterKeyConstants.java
@@ -7,4 +7,10 @@ public class CUKFSParameterKeyConstants implements ParameterKeyConstants {
     public static final String DEFAULT_CHART_CODE_METHOD = "DEFAULT_CHART_CODE_METHOD";
     public static final String DEFAULT_CHART_CODE = "DEFAULT_CHART_CODE";
 	
+    public static final String AGED_FYI_ACK_DOC_TYPES_INCLUDED = "AGED_FYI_ACK_DOC_TYPES_INCLUDED";
+    public static final String AUTO_CANCEL_DOC_TYPES_PARAMETER = "AUTO_CANCEL_DOC_TYPES";   
+    public static final String DAYS_TO_AUTO_CANCEL_PARAMETER = "DAYS_TO_AUTO_CANCEL";
+
+    public static final String NON_REQUEUABLE_DOCUMENT_TYPES = "NON_REQUEUABLE_DOCUMENT_TYPES";
+    public static final String REQUEUABLE_ROLES = "REQUEUABLE_ROLES";
 }
