81fed581d82a8303aee78b21af7e05f1e6ed0da7
dwf5 <dwf5@febce4ab-9070-0410-80a8-fd1c76bc7499>
2012-11-28 18:14:58 +0000

    KFSPTS-1594 : Initial implementation of the Travel to KFS webservice.

---
+++ src/main/java/edu/cornell/kfs/fp/service/SubmitTripWebService.java
@@ -0,0 +1,34 @@
+package edu.cornell.kfs.fp.service;
+
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+/**
+ *
+ * <p>Title: SubmitTripWebService</p>
+ * <p>Description: Describes the functions that need to be implemented in the trip submission web service</p>
+ * <p>Copyright: Copyright (c) 2012</p>
+ * <p>Company: Cornell University: Kuali Financial Systems</p>
+ * @author Dennis Friends
+ * @author Sandy Eccleston
+ * @version 1.0
+ */
+
+@WebService 
+public interface SubmitTripWebService {
+
+  /**
+   * 
+   * @return
+   * @throws Exception
+   */
+  public String submitTrip(
+		  @WebParam(name = "dvDescription")String dvDescription,
+		  @WebParam(name = "dvExplanation")String dvExplanation,
+		  @WebParam(name = "travelerNetId")String travelerNetId,
+		  @WebParam(name = "initiatorNetId")String initiatorNetId,
+		  @WebParam(name = "totalAmount")double totalAmount,
+		  @WebParam(name = "checkStubText")String checkStubText
+		  ) throws Exception;
+  
+}
---
+++ src/main/java/edu/cornell/kfs/fp/service/impl/SubmitTripWebServiceImpl.java
@@ -0,0 +1,105 @@
+package edu.cornell.kfs.fp.service.impl;
+
+import javax.jws.WebService;
+
+import org.kuali.kfs.coa.businessobject.Account;
+import org.kuali.kfs.coa.businessobject.ObjectCode;
+import org.kuali.kfs.coa.businessobject.ProjectCode;
+import org.kuali.kfs.coa.businessobject.SubAccount;
+import org.kuali.kfs.coa.businessobject.SubObjectCode;
+import org.kuali.kfs.coa.service.AccountService;
+import org.kuali.kfs.coa.service.ObjectCodeService;
+import org.kuali.kfs.coa.service.ProjectCodeService;
+import org.kuali.kfs.coa.service.SubAccountService;
+import org.kuali.kfs.coa.service.SubObjectCodeService;
+import org.kuali.kfs.fp.businessobject.DisbursementPayee;
+import org.kuali.kfs.fp.document.DisbursementVoucherConstants;
+import org.kuali.kfs.fp.document.DisbursementVoucherDocument;
+import org.kuali.kfs.sys.KFSConstants;
+import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.rice.kew.exception.WorkflowException;
+import org.kuali.rice.kim.bo.Person;
+import org.kuali.rice.kim.service.PersonService;
+import org.kuali.rice.kns.UserSession;
+import org.kuali.rice.kns.service.DocumentService;
+import org.kuali.rice.kns.util.ErrorMap;
+import org.kuali.rice.kns.util.GlobalVariables;
+import org.kuali.rice.kns.util.KualiDecimal;
+import org.kuali.rice.kns.util.MessageMap;
+import org.kuali.rice.kns.workflow.service.KualiWorkflowDocument;
+import org.kuali.rice.kns.workflow.service.WorkflowDocumentService;
+
+import edu.cornell.kfs.coa.service.AccountVerificationWebService;
+import edu.cornell.kfs.fp.service.SubmitTripWebService;
+
+
+/**
+ *
+ * <p>Title: SubmitTripWebServiceImpl</p>
+ * <p>Description: Implements the webservice for travel's trip submission</p>
+ * <p>Copyright: Copyright (c) 2005</p>
+ * <p>Company: Cornell University: Kuali Financial Systems</p>
+ * @author Dennis Friends
+ * @author Sandy Eccleston
+ * @version 1.0
+ */
+@WebService(endpointInterface = "edu.cornell.kfs.fp.service.SubmitTripWebService")
+public class SubmitTripWebServiceImpl implements SubmitTripWebService {
+
+	/**
+	 * 
+	 */
+	public String submitTrip(String dvDescription, String dvExplanation, String travelerNetId, String initiatorNetId, double totalAmount, String checkStubText) throws Exception {
+        UserSession actualUserSession = GlobalVariables.getUserSession();
+        MessageMap globalErrorMap = GlobalVariables.getMessageMap();
+
+        try {
+	        // create and route doc as system user
+	        // create and route doc as system user
+	        GlobalVariables.setUserSession(new UserSession(initiatorNetId));
+	
+	        MessageMap documentErrorMap = new MessageMap();
+	        GlobalVariables.setMessageMap(documentErrorMap);
+	
+	        // Create document with description provided
+			DisbursementVoucherDocument dvDoc = null;
+			
+	        try {
+	            dvDoc = (DisbursementVoucherDocument) SpringContext.getBean(DocumentService.class).getNewDocument(DisbursementVoucherDocument.class);
+	        }
+	        catch (WorkflowException e) {
+	            throw new RuntimeException("Error creating new disbursement voucher document: " + e.getMessage(), e);
+	        }
+		  
+	        if(dvDoc != null) {
+				dvDoc.getDocumentHeader().setDocumentDescription(dvDescription);
+				dvDoc.getDocumentHeader().setExplanation(dvExplanation);
+				
+				dvDoc.initiateDocument();
+
+				// Set vendor to traveler using netID provided
+				Person traveler = SpringContext.getBean(PersonService.class).getPersonByPrincipalName(travelerNetId);
+				dvDoc.templateEmployee(traveler);
+				dvDoc.setPayeeAssigned(true);
+				
+		        dvDoc.getDvPayeeDetail().setDisbVchrPaymentReasonCode("J");
+				
+				dvDoc.setDisbVchrCheckTotalAmount(new KualiDecimal(totalAmount));
+				dvDoc.setDisbVchrPaymentMethodCode("P");
+
+				dvDoc.setDisbVchrCheckStubText(checkStubText);
+				
+				// Persist document
+				SpringContext.getBean(DocumentService.class).saveDocument(dvDoc);
+				
+				return dvDoc.getDocumentNumber();
+	        } else {
+	        	return "";
+	        }
+		} finally {
+            GlobalVariables.setUserSession(actualUserSession);
+            GlobalVariables.setMessageMap(globalErrorMap);
+		}
+	}
+  
+}
