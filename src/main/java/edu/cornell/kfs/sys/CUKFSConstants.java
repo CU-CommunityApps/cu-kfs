936cea7b6f9ebb5a3a0ab57608f50c57c59578f7
db483 <db483@febce4ab-9070-0410-80a8-fd1c76bc7499>
2013-01-16 21:25:08 +0000

    KFSPTS-1674 back out changes, not needed for now

--- src/main/java/edu/cornell/kfs/sys/CUKFSConstants.java
+++ src/main/java/edu/cornell/kfs/sys/CUKFSConstants.java
@@ -4,7 +4,4 @@ public class CUKFSConstants {
 
     public static final String COMMODITY_CODE_FILE_TYPE_INDENTIFIER = "commodityCodeInputFileType";
     
-    //FYM system parameters
-    public static final String FISCAL_YEAR_MAKER_DELETE_UNIVERSITY_DATE_IND = "DELETE_UNIVERITY_DATE_TABLE_IND";
-
 }
