d1d541bc5c99c1dfe50d493756a049e59024c4e4
sdm86 <sdm86@febce4ab-9070-0410-80a8-fd1c76bc7499>
2013-03-04 14:39:23 +0000

    KFSPTS-376 Backout of changes for PreEncumbrance auto-disencumber feature

--- src/main/java/edu/cornell/kfs/sys/CUKFSConstants.java
+++ src/main/java/edu/cornell/kfs/sys/CUKFSConstants.java
@@ -1,25 +1,7 @@
 package edu.cornell.kfs.sys;
 
-import org.kuali.kfs.sys.ParameterKeyConstants;
-import org.kuali.rice.core.util.JSTLConstants;
+public class CUKFSConstants {
 
-public class CUKFSConstants extends JSTLConstants implements ParameterKeyConstants {
-        
-        public static final String COMMODITY_CODE_FILE_TYPE_INDENTIFIER = "commodityCodeInputFileType";
-        
-        public static class PreEncumbranceDocumentConstants {
-                public static final String BIWEEKLY = "biWeekly";
-                public static final String CUSTOM = "custom";
-                public static final String SEMIMONTHLY = "semiMonthly";
-                public static final String MONTHLY = "monthly";
-        }
-        
-        public static class PreEncumbranceSourceAccountingLineConstants {
-                public static final String END_DATE = "endDate";
-                public static final String START_DATE = "startDate";
-                public static final String AUTO_DISENCUMBER_TYPE = "autoDisEncumberType";
-                public static final String PARTIAL_TRANSACTION_COUNT = "partialTransactionCount";
-                public static final String PARTIAL_AMOUNT = "partialAmount";
-        }
-        
+    public static final String COMMODITY_CODE_FILE_TYPE_INDENTIFIER = "commodityCodeInputFileType";
+    
 }
