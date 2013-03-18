fe8565e4c98a95fb3756aba99fca0f4abb6c23e0
nkk4 <nkk4@febce4ab-9070-0410-80a8-fd1c76bc7499>
2013-03-18 20:23:31 +0000

    KFSPTS-1740: Split-node routing to Award node added. XML needed to be ingested. Security needed to be changed to have Permission 60 added to Role 54 to have eDocs function correctly.

---
+++ src/main/java/org/kuali/kfs/coa/document/SubAccountMaintainableImpl.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright 2009 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl2.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.coa.document;
+
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.kuali.kfs.coa.businessobject.A21SubAccount;
+import org.kuali.kfs.coa.businessobject.Account;
+import org.kuali.kfs.coa.businessobject.SubAccount;
+import org.kuali.kfs.coa.service.A21SubAccountService;
+import org.kuali.kfs.coa.service.AccountService;
+import org.kuali.kfs.sys.KFSConstants;
+import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.kfs.sys.document.FinancialSystemMaintainable;
+import org.kuali.rice.kim.bo.Person;
+import org.kuali.rice.kns.document.MaintenanceDocument;
+import org.kuali.rice.kns.document.authorization.MaintenanceDocumentRestrictions;
+import org.kuali.rice.kns.service.ParameterService;
+import org.kuali.rice.kns.util.GlobalVariables;
+import org.kuali.rice.kns.util.KNSConstants;
+import org.kuali.rice.kns.util.ObjectUtils;
+
+/**
+ * This class...
+ */
+public class SubAccountMaintainableImpl extends FinancialSystemMaintainable {
+	
+	 private static final String REQUIRES_CG_APPROVAL_NODE = "RequiresCGResponsibilityApproval";  //KFSPTS-1740
+	 private AccountService accountService;                       //KFSPTS-1740
+	 private A21SubAccountService a21SubAccountService;           //KFSPTS-1740
+	 
+
+    /**
+     * @see org.kuali.rice.kns.maintenance.KualiMaintainableImpl#refresh(java.lang.String, java.util.Map,
+     *      org.kuali.rice.kns.document.MaintenanceDocument)
+     */
+    @Override
+    public void refresh(String refreshCaller, Map fieldValues, MaintenanceDocument document) {
+        super.refresh(refreshCaller, fieldValues, document);
+
+        Person person = GlobalVariables.getUserSession().getPerson();
+        MaintenanceDocumentRestrictions restrictions = getBusinessObjectAuthorizationService().getMaintenanceDocumentRestrictions(document, person);
+        boolean canEdit = !restrictions.isHiddenSectionId(KFSConstants.SUB_ACCOUNT_EDIT_CG_ICR_SECTION_ID) && !restrictions.isReadOnlySectionId(KFSConstants.SUB_ACCOUNT_EDIT_CG_ICR_SECTION_ID);
+        
+        // after account lookup, refresh the CG ICR account fields
+        if (StringUtils.equals(refreshCaller, "accountLookupable") && fieldValues.containsKey("document.newMaintainableObject.accountNumber") && canEdit) {
+            SubAccount subAccount = (SubAccount) this.getBusinessObject();
+            this.populateCGIcrFields(subAccount);
+        }
+    }
+
+    // populate the CG ICR fields if any
+    private void populateCGIcrFields(SubAccount subAccount) {
+        A21SubAccount a21SubAccount = subAccount.getA21SubAccount();
+        String chartOfAccountsCode = subAccount.getChartOfAccountsCode();
+        String accountNumber = subAccount.getAccountNumber();
+        
+        if (ObjectUtils.isNotNull(a21SubAccount) && (!StringUtils.equals(chartOfAccountsCode, a21SubAccount.getChartOfAccountsCode()) || !StringUtils.equals(accountNumber, a21SubAccount.getAccountNumber()))) {                  
+            A21SubAccountService a21SubAccountService = SpringContext.getBean(A21SubAccountService.class);
+            a21SubAccountService.populateCgIcrAccount(a21SubAccount, chartOfAccountsCode, accountNumber);
+        }
+    }
+    
+    /**
+     * KFSPTS-1740 : New split node routing
+     * 
+     * Enforce routing to Award split-node based on account.contractsAndGrantsAccountResponsibilityId
+     * value when subAccount type code is specified as Cost share (CS).
+     * 
+     * @see org.kuali.kfs.sys.document.FinancialSystemMaintainable#answerSplitNodeQuestion(java.lang.String)
+     */
+    @Override
+    public boolean answerSplitNodeQuestion(String nodeName) throws UnsupportedOperationException {
+        
+    	if (nodeName.equals(REQUIRES_CG_APPROVAL_NODE)) {
+    		return isCAndGReviewRequired();
+    	}    	
+    	//this is not a node we recognize
+    	throw new UnsupportedOperationException("SubAccountMaintainableImpl.answerSplitNodeQuestion cannot answer split node question for the node called('" + nodeName + "')");
+        
+    }
+    
+    //KFSPTS-1740
+    /**
+     * Answers true for the following conditions:
+     * a) New or Copy of SubAccount with a CS SubAccount type enforce route to Award split-node.
+     * b) Any Edit of an existing SubAccount type code to change it from-or-to a cost share enforces split-node route to Award (i.e. old=EX to new=CS OR old=CS to new=EX OR old=CS to new=CS).
+     * 
+     * @return
+     */
+    private boolean isCAndGReviewRequired (){
+    	//route for C&G approval when subAcount type is CS on edit or create
+    	String maintAction = super.getMaintenanceAction();    	
+    	if ( (maintAction.equalsIgnoreCase(KNSConstants.MAINTENANCE_NEW_ACTION)) || 
+    		 (maintAction.equalsIgnoreCase(KNSConstants.MAINTENANCE_COPY_ACTION)) ) {
+    		     		
+    		//need "new" bo for data comparisons
+    		SubAccount subAccount = (SubAccount)super.getBusinessObject();
+    		    		
+    		if (subAccount.getA21SubAccount().getSubAccountTypeCode().equals(KFSConstants.SubAccountType.COST_SHARE)) {    			
+    			//retrieve data we need for split-node route to work, contractsAndGrantsAccountResponsibilityId is not a sub-account attribute and we must populate the account attribute on sub-account
+    			subAccount.setAccount(getAccountService().getByPrimaryIdWithCaching(subAccount.getChartOfAccountsCode(), subAccount.getAccountNumber()));
+    			return true;
+    		}
+    	}  
+    	else if ((maintAction.equalsIgnoreCase(KNSConstants.MAINTENANCE_EDIT_ACTION)) ) {
+    		
+    		//need "new" bo for data comparisons
+    		SubAccount subAccount = (SubAccount)super.getBusinessObject();
+    		//need "old" bo for data comparisons
+    		A21SubAccount oldSubAccount = this.getA21SubAccountService().getByPrimaryKey(subAccount.getChartOfAccountsCode(), subAccount.getAccountNumber(), subAccount.getSubAccountNumber());
+    		
+    		if ( (subAccount.getA21SubAccount().getSubAccountTypeCode().equals(KFSConstants.SubAccountType.COST_SHARE)) || 
+    			 (oldSubAccount.getSubAccountTypeCode().equals(KFSConstants.SubAccountType.COST_SHARE)) ) {
+    			//retrieve data we need for split-node route to work, contractsAndGrantsAccountResponsibilityId is not a sub-account attribute and we must populate the account attribute on sub-account
+    			subAccount.setAccount(getAccountService().getByPrimaryIdWithCaching(subAccount.getChartOfAccountsCode(), subAccount.getAccountNumber()));
+    			return true;    			
+    		}
+    	}
+    	return false; 
+    }
+    
+
+    //KFSPTS-1740
+    public AccountService getAccountService() {
+    	if (accountService == null) {
+    		accountService = SpringContext.getBean(AccountService.class);    	
+    	}
+        return accountService;
+    }
+    
+    
+    //KFSPTS-1740
+    public A21SubAccountService getA21SubAccountService() {
+    	if (a21SubAccountService == null) {
+    		a21SubAccountService = SpringContext.getBean(A21SubAccountService.class);    	
+    	}
+        return a21SubAccountService;
+    }
+    
+}
+
