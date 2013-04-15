633d3709d1bc3c2fc8a4c5228184ceb00868ab39
ls693 <ls693@febce4ab-9070-0410-80a8-fd1c76bc7499>
2013-04-15 17:33:06 +0000

    KFSPTS-1719 : remove commented out code

--- src/main/java/org/kuali/kfs/module/purap/document/web/struts/PurchasingActionBase.java
+++ src/main/java/org/kuali/kfs/module/purap/document/web/struts/PurchasingActionBase.java
@@ -186,26 +186,7 @@ public class PurchasingActionBase extends PurchasingAccountsPayableActionBase {
 
         }
         
-        // KFSPTS-1719 : noqty is allowed for e-invoice.  when implementing 1719, this rule will be removed.
-//        if (StringUtils.equals(refreshCaller, VendorConstants.VENDOR_LOOKUPABLE_IMPL) && document.getVendorDetailAssignedIdentifier() != null && document.getVendorHeaderGeneratedIdentifier() != null) {
-//            // Check that item isn't a non-qty item on an e-invoice vendor order
-//        	VendorDetail vendor = document.getVendorDetail();
-//            if(ObjectUtils.isNotNull(vendor) && ((VendorDetailExtension)vendor.getExtension()).isEinvoiceVendorIndicator()) {
-//	            // Check that there aren't any req items that already have non-qty values entered
-//	            List<PurApItem> reqItems = document.getItems();
-//	            if(!reqItems.isEmpty()) {
-//	                for(PurApItem item : reqItems) {
-//	                    if(PurapConstants.ItemTypeCodes.ITEM_TYPE_SERVICE_CODE.equalsIgnoreCase(item.getItemTypeCode())) {
-//	                        // Throw error that the non-qty items are not allowed if the vendor is an einvoice vendor
-//	                        LOG.error("Requisition Item contains non-qty item code: Item #"+item.getItemLineNumber()+".");
-//	                        messageMap.addToErrorPath(PurapPropertyConstants.NEW_PURCHASING_ITEM_LINE);
-//	                        messageMap.putError(PurapPropertyConstants.ITEM_TYPE, CUPurapKeyConstants.PURAP_ITEM_NONQTY, item.getItemLineNumber().toString(), document.getVendorName()); // Add error message
-//	                        messageMap.removeFromErrorPath(PurapPropertyConstants.NEW_PURCHASING_ITEM_LINE);
-//	                    }
-//	                }
-//	            }
-//            }
-//        }
+        // KFSPTS-1719 : noqty is allowed for e-invoice.  the rule is removed.
         // Refreshing the fields after returning from a contract lookup in the vendor tab
         if (StringUtils.equals(refreshCaller, VendorConstants.VENDOR_CONTRACT_LOOKUPABLE_IMPL)) {
             if (StringUtils.isNotEmpty(request.getParameter(KFSPropertyConstants.DOCUMENT + "." + PurapPropertyConstants.VENDOR_CONTRACT_ID))) {
