65521229bd06852c7f34c36c924d2ea2939ab7fa
cah292 <cah292@febce4ab-9070-0410-80a8-fd1c76bc7499>
2013-04-24 18:57:22 +0000

    KFSPTS-2008: Added custom unmatched totals validation class that adds error messages to the message map instead of the message list.

---
+++ src/main/java/edu/cornell/kfs/module/purap/document/validation/impl/CUVendorCreditMemoTotalMatchesVendorAmountValidation.java
@@ -0,0 +1,38 @@
+package edu.cornell.kfs.module.purap.document.validation.impl;
+
+import org.kuali.kfs.module.purap.PurapKeyConstants;
+import org.kuali.kfs.module.purap.PurapPropertyConstants;
+import org.kuali.kfs.module.purap.document.VendorCreditMemoDocument;
+import org.kuali.kfs.sys.KFSPropertyConstants;
+import org.kuali.kfs.sys.document.validation.GenericValidation;
+import org.kuali.kfs.sys.document.validation.event.AttributedDocumentEvent;
+import org.kuali.rice.kns.util.GlobalVariables;
+
+/**
+ * ====
+ * CU Customization (KFSPTS-2008):
+ * Added this custom validation class to assist in displaying a proper error message when
+ * unmatched totals are encountered on a credit memo doc at routing time. This was copied from
+ * the VendorCreditMemoTotalMatchesVendorAmountValidation class in KFS, except that it has
+ * been modified to add the error message to the message map instead of the message list.
+ * ====
+ */
+public class CUVendorCreditMemoTotalMatchesVendorAmountValidation extends GenericValidation {
+
+    public boolean validate(AttributedDocumentEvent event) {
+        boolean valid = true;
+        VendorCreditMemoDocument cmDocument = (VendorCreditMemoDocument)event.getDocument();
+        GlobalVariables.getMessageMap().clearErrorPath();
+        GlobalVariables.getMessageMap().addToErrorPath(KFSPropertyConstants.DOCUMENT);
+        
+        if (cmDocument.getGrandTotal().compareTo(cmDocument.getCreditMemoAmount()) != 0 && !cmDocument.isUnmatchedOverride()) {
+            GlobalVariables.getMessageMap().putError(PurapPropertyConstants.GRAND_TOTAL, PurapKeyConstants.ERROR_CREDIT_MEMO_INVOICE_AMOUNT_NONMATCH);
+            valid = false;
+        }
+        
+        GlobalVariables.getMessageMap().clearErrorPath();
+
+        return valid;
+    }
+
+}
