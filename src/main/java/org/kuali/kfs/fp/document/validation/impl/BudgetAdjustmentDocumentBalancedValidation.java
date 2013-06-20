7d4c9ad51682575654162117d89d495f19552f80
db483 <db483@febce4ab-9070-0410-80a8-fd1c76bc7499>
2013-06-20 09:18:42 +0000

    KFSPTS-2077 Commit base KFS file

---
+++ src/main/java/org/kuali/kfs/fp/document/validation/impl/BudgetAdjustmentDocumentBalancedValidation.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl2.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.fp.document.validation.impl;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import org.kuali.kfs.fp.document.BudgetAdjustmentDocument;
+import org.kuali.kfs.sys.KFSConstants;
+import org.kuali.kfs.sys.KFSKeyConstants;
+import org.kuali.kfs.sys.document.validation.GenericValidation;
+import org.kuali.kfs.sys.document.validation.event.AttributedDocumentEvent;
+import org.kuali.rice.kns.util.GlobalVariables;
+import org.kuali.rice.kns.util.KualiDecimal;
+import org.kuali.rice.kns.util.KualiInteger;
+import org.kuali.rice.kns.util.MessageMap;
+
+/**
+ * A validation which checks if a Budget Adjustment document is balanced before heading to routing
+ */
+public class BudgetAdjustmentDocumentBalancedValidation extends GenericValidation {
+    public BudgetAdjustmentDocument accountingDocumentForValidation;
+
+    /**
+     * Validates that the budget adjustment document is balanced, based on whether the source base amount equals the target base amount
+     * and that the income stream balance map has no non-zero values.
+     * @see org.kuali.kfs.sys.document.validation.Validation#validate(org.kuali.kfs.sys.document.validation.event.AttributedDocumentEvent)
+     */
+    public boolean validate(AttributedDocumentEvent event) {
+        MessageMap errors = GlobalVariables.getMessageMap();
+
+        boolean balanced = true;
+
+        // check base amounts are equal
+        //KFSMI-3036
+        KualiInteger sourceBaseBudgetTotal = getAccountingDocumentForValidation().getSourceBaseBudgetIncomeTotal().subtract( getAccountingDocumentForValidation().getSourceBaseBudgetExpenseTotal());
+        KualiInteger targetBaseBudgetTotal = getAccountingDocumentForValidation().getTargetBaseBudgetIncomeTotal().subtract( getAccountingDocumentForValidation().getTargetBaseBudgetExpenseTotal());
+        if (sourceBaseBudgetTotal.compareTo(targetBaseBudgetTotal) != 0) {
+            GlobalVariables.getMessageMap().putError(KFSConstants.ACCOUNTING_LINE_ERRORS, KFSKeyConstants.ERROR_DOCUMENT_BA_BASE_AMOUNTS_BALANCED);
+            balanced = false;
+        }
+
+        // check current amounts balance, income stream balance Map should add to 0
+        Map incomeStreamMap = getAccountingDocumentForValidation().buildIncomeStreamBalanceMapForDocumentBalance();
+        KualiDecimal totalCurrentAmount = new KualiDecimal(0);
+        for (Iterator iter = incomeStreamMap.values().iterator(); iter.hasNext();) {
+            KualiDecimal streamAmount = (KualiDecimal) iter.next();
+            totalCurrentAmount = totalCurrentAmount.add(streamAmount);
+        }
+
+        if (totalCurrentAmount.isNonZero()) {
+            GlobalVariables.getMessageMap().putError(KFSConstants.ACCOUNTING_LINE_ERRORS, KFSKeyConstants.ERROR_DOCUMENT_BA_CURRENT_AMOUNTS_BALANCED);
+            balanced = false;
+        }
+
+        return balanced;
+    }
+
+    /**
+     * Gets the accountingDocumentForValidation attribute. 
+     * @return Returns the accountingDocumentForValidation.
+     */
+    public BudgetAdjustmentDocument getAccountingDocumentForValidation() {
+        return accountingDocumentForValidation;
+    }
+
+    /**
+     * Sets the accountingDocumentForValidation attribute value.
+     * @param accountingDocumentForValidation The accountingDocumentForValidation to set.
+     */
+    public void setAccountingDocumentForValidation(BudgetAdjustmentDocument accountingDocumentForValidation) {
+        this.accountingDocumentForValidation = accountingDocumentForValidation;
+    }
+}
