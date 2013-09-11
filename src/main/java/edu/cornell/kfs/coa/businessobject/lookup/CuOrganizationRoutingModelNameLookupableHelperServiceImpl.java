3019b93ac509db6a091a46c9d817fa15b700d7e8
Chad Bower <chad.bower379@gmail.com>
2013-09-11 16:53:07 -0400

    KFSUPGRADE-116 Account Delegate Doc Master Issue

---
+++ src/main/java/edu/cornell/kfs/coa/businessobject/lookup/CuOrganizationRoutingModelNameLookupableHelperServiceImpl.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2007 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.cornell.kfs.coa.businessobject.lookup;
+
+import edu.cornell.kfs.coa.businessobject.CuAccountDelegateGlobal;
+
+import java.util.List;
+import java.util.Properties;
+import org.kuali.kfs.coa.businessobject.lookup.OrganizationRoutingModelNameLookupableHelperServiceImpl;
+import org.kuali.kfs.sys.KFSConstants;
+import org.kuali.rice.kns.document.authorization.BusinessObjectRestrictions;
+import org.kuali.rice.kns.lookup.HtmlData;
+import org.kuali.rice.kns.lookup.HtmlData.AnchorHtmlData;
+import org.kuali.rice.kns.web.struts.form.LookupForm;
+import org.kuali.rice.krad.bo.BusinessObject;
+
+
+
+/**
+ * This class overrides the getBackLocation, getReturnUrl, setFieldConversions and getActionUrls for
+ * {@link OrganizationRoutingModelName}
+ */
+public class CuOrganizationRoutingModelNameLookupableHelperServiceImpl extends OrganizationRoutingModelNameLookupableHelperServiceImpl {
+    
+    @Override
+    public HtmlData getReturnUrl(BusinessObject businessObject, LookupForm lookupForm, List returnKeys, BusinessObjectRestrictions businessObjectRestrictions) {
+        String originalBackLocation = this.backLocation;
+        Properties parameters = getParameters(businessObject, lookupForm.getFieldConversions(), lookupForm.getLookupableImplServiceName(), returnKeys);
+        parameters.put(KFSConstants.DISPATCH_REQUEST_PARAMETER, KFSConstants.MAINTENANCE_NEWWITHEXISTING_ACTION);
+        parameters.put(KFSConstants.BUSINESS_OBJECT_CLASS_ATTRIBUTE, CuAccountDelegateGlobal.class.getName());
+        parameters.put(KFSConstants.OVERRIDE_KEYS, "modelName" + KFSConstants.FIELD_CONVERSIONS_SEPERATOR + "modelChartOfAccountsCode" 
+            + KFSConstants.FIELD_CONVERSIONS_SEPERATOR + "modelOrganizationCode");
+        setBackLocation(KFSConstants.MAINTENANCE_ACTION);
+        AnchorHtmlData htmlData = (AnchorHtmlData)getReturnAnchorHtmlData(businessObject, parameters, lookupForm, returnKeys, businessObjectRestrictions);
+        setBackLocation(originalBackLocation); //set this to prevent breaking Account Delegate Model returnLocation
+        return htmlData;
+    }
+
+    
+}
---
+++ src/main/java/edu/cornell/kfs/coa/document/CuAccountDelegateGlobalMaintainableImpl.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2007 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl2.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.cornell.kfs.coa.document;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.lang.StringUtils;
+import org.kuali.kfs.coa.businessobject.AccountDelegate;
+import org.kuali.kfs.coa.businessobject.AccountDelegateGlobal;
+import org.kuali.kfs.coa.businessobject.AccountDelegateGlobalDetail;
+import org.kuali.kfs.coa.businessobject.AccountDelegateModel;
+import org.kuali.kfs.coa.businessobject.AccountDelegateModelDetail;
+import org.kuali.kfs.coa.businessobject.AccountGlobalDetail;
+import org.kuali.kfs.coa.document.AccountDelegateGlobalMaintainableImpl;
+import org.kuali.kfs.coa.service.AccountDelegateService;
+import org.kuali.kfs.sys.KFSConstants;
+import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.kfs.sys.document.FinancialSystemGlobalMaintainable;
+import org.kuali.rice.kns.document.MaintenanceDocument;
+import org.kuali.rice.krad.bo.PersistableBusinessObject;
+import org.kuali.rice.krad.maintenance.MaintenanceLock;
+import org.kuali.rice.krad.service.BusinessObjectService;
+import org.kuali.rice.krad.util.ObjectUtils;
+
+/**
+ * This class overrides the base {@link FinancialSystemGlobalMaintainable} to generate the specific maintenance locks for Global delegates
+ * and to help with using delegate models
+ * 
+ * @see OrganizationRoutingModelName
+ */
+public class CuAccountDelegateGlobalMaintainableImpl extends AccountDelegateGlobalMaintainableImpl {
+    
+    /**
+     * This creates the particular locking representation for this global document.
+     * 
+     * @see org.kuali.rice.kns.maintenance.Maintainable#generateMaintenanceLocks()
+     */
+    @Override
+    public List<MaintenanceLock> generateMaintenanceLocks() {
+        // create locking rep for each combination of account and object code
+        List<MaintenanceLock> maintenanceLocks = new ArrayList();
+        
+        return maintenanceLocks;
+    }    
+}
