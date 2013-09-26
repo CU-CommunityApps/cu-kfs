6b757d39016e207c905ceb7155d9e7420c51417e
sbower <shawn.bower@gmail.com>
2013-09-25 21:15:23 -0400

    KFSUPGRADE-143: removing single sub fund validation

---
+++ src/main/java/edu/cornell/kfs/fp/document/validation/impl/CuAuxiliaryVoucherSingleSubFundValidation.java
@@ -0,0 +1,26 @@
+package edu.cornell.kfs.fp.document.validation.impl;
+
+import org.kuali.kfs.fp.document.AuxiliaryVoucherDocument;
+import org.kuali.kfs.fp.document.validation.impl.AuxiliaryVoucherSingleSubFundValidation;
+import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.kfs.sys.document.validation.event.AttributedDocumentEvent;
+import org.kuali.rice.coreservice.framework.parameter.ParameterService;
+
+import edu.cornell.kfs.sys.CUKFSParameterKeyConstants;
+
+public class CuAuxiliaryVoucherSingleSubFundValidation extends AuxiliaryVoucherSingleSubFundValidation {
+    
+    @Override
+    public boolean validate(AttributedDocumentEvent event) {
+        boolean allowMultiple = SpringContext.getBean(ParameterService.class)
+                .getParameterValueAsBoolean(AuxiliaryVoucherDocument.class, 
+                        CUKFSParameterKeyConstants.FpParameterConstants.FP_ALLOW_MULTIPLE_SUBFUNDS);
+        
+        if (!allowMultiple) {
+            return super.validate(event);
+        }
+        
+        return true;
+    }
+
+}
