e171668a09205d1303236ea4f4993a4c876f6293
lshyu <lshyu@vivantech.com>
2013-10-04 11:51:01 -0400

    KFSUPGRADE-238 : merge to kfs-5.0.3

---
+++ src/main/java/edu/cornell/kfs/vnd/dataaccess/CuVendorDao.java
@@ -0,0 +1,13 @@
+package edu.cornell.kfs.vnd.dataaccess;
+
+import java.util.List;
+import java.util.Map;
+
+import org.kuali.kfs.vnd.dataaccess.VendorDao;
+import org.kuali.rice.krad.bo.BusinessObject;
+
+public interface CuVendorDao extends VendorDao {
+
+    public List<BusinessObject> getSearchResults(Map<String,String> fieldValues);
+
+}
