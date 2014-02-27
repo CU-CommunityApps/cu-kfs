3163daaedfd11a2e2c9a2dae6e9951744340c98b
Shawn Bower <shawn.bower@gmail.com>
2014-02-26 21:12:54 -0500

    KFSUPGRADE-653 #comment After futher review the issue Susan is seeing as that we cant generate future actions for some PREQs becuase alternate pay method has not been implemented.  I will add a patch as a work around until alternate pay gets remediated.

--- src/main/java/edu/cornell/kfs/module/purap/document/CuPaymentRequestDocument.java
+++ src/main/java/edu/cornell/kfs/module/purap/document/CuPaymentRequestDocument.java
@@ -2,12 +2,14 @@ package edu.cornell.kfs.module.purap.document;
 
 import java.util.List;
 
+import org.kuali.kfs.module.purap.PurapWorkflowConstants;
 import org.kuali.kfs.module.purap.businessobject.PaymentRequestItem;
 import org.kuali.kfs.module.purap.document.PaymentRequestDocument;
 import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.rice.krad.rules.rule.event.KualiDocumentEvent;
 import org.kuali.rice.krad.service.SequenceAccessorService;
 
+import edu.cornell.kfs.module.purap.CUPurapWorkflowConstants;
 import edu.cornell.kfs.module.purap.businessobject.CuPaymentRequestItemExtension;
 
 public class CuPaymentRequestDocument extends PaymentRequestDocument {
@@ -25,5 +27,24 @@ public class CuPaymentRequestDocument extends PaymentRequestDocument {
                 ((CuPaymentRequestItemExtension)item.getExtension()).setItemIdentifier(generatedItemId);
             }
         }
+    }
+    
+    @Override
+    public boolean answerSplitNodeQuestion(String nodeName) throws UnsupportedOperationException {
+        if (nodeName.equals(PurapWorkflowConstants.REQUIRES_IMAGE_ATTACHMENT)) {
+            return requiresAccountsPayableReviewRouting();
+        }
+        if (nodeName.equals(PurapWorkflowConstants.PURCHASE_WAS_RECEIVED)) {
+            return shouldWaitForReceiving();
+        }
+        if (nodeName.equals(PurapWorkflowConstants.VENDOR_IS_EMPLOYEE_OR_NON_RESIDENT_ALIEN)) {
+            return isVendorEmployeeOrNonResidentAlien();
+        }
+        //need to stub out for now until 1891 is fully implemented
+        if (nodeName.equals(CUPurapWorkflowConstants.TREASURY_MANAGER)) {
+        	return false;
+        }
+        
+        throw new UnsupportedOperationException("Cannot answer split question for this node you call \"" + nodeName + "\"");
     }
 }
