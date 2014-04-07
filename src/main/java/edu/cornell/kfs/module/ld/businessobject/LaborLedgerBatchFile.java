b9edc15665da93534e3122d098bde162a51135a9
sbower <shawn.bower@gmai.com>
2014-04-06 21:14:36 -0400

    couple quick fixes

--- src/main/java/edu/cornell/kfs/module/ld/businessobject/LaborLedgerBatchFile.java
+++ src/main/java/edu/cornell/kfs/module/ld/businessobject/LaborLedgerBatchFile.java
@@ -1,7 +1,10 @@
 package edu.cornell.kfs.module.ld.businessobject;
 
+import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.kfs.sys.batch.BatchFile;
+import org.kuali.rice.coreservice.framework.parameter.ParameterConstants.NAMESPACE;
 
+@NAMESPACE(namespace = KFSConstants.OptionalModuleNamespaces.LABOR_DISTRIBUTION)
 public class LaborLedgerBatchFile extends BatchFile {
     private static final long serialVersionUID = 1L;
 
