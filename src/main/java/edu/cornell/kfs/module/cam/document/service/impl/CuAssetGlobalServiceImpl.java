01e7a0886b1210968255198b5ef1ab60e1e752e4
lshyu <lshyu@vivantech.com>
2014-06-02 15:15:09 -0400

    KFSUPGRADE-929 : merge to kfs-5.1 - perliminary fix to asset accumulated depr not separated.  need further check if payment change has any adverse effect.

--- src/main/java/edu/cornell/kfs/module/cam/document/service/impl/CuAssetGlobalServiceImpl.java
+++ src/main/java/edu/cornell/kfs/module/cam/document/service/impl/CuAssetGlobalServiceImpl.java
@@ -1,9 +1,14 @@
 package edu.cornell.kfs.module.cam.document.service.impl;
 
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
 import org.kuali.kfs.module.cam.businessobject.Asset;
 import org.kuali.kfs.module.cam.businessobject.AssetGlobal;
 import org.kuali.kfs.module.cam.businessobject.AssetGlobalDetail;
+import org.kuali.kfs.module.cam.businessobject.AssetPayment;
 import org.kuali.kfs.module.cam.document.service.impl.AssetGlobalServiceImpl;
+import org.kuali.rice.krad.bo.PersistableBusinessObject;
 
 import edu.cornell.kfs.module.cam.businessobject.AssetExtension;
 
@@ -21,5 +26,17 @@ public class CuAssetGlobalServiceImpl extends AssetGlobalServiceImpl {
 
 	}
 
-	
+    @Override
+    public List<PersistableBusinessObject> getSeparateAssets(AssetGlobal assetGlobal) {
+        List<PersistableBusinessObject> assets = super.getSeparateAssets(assetGlobal);
+        for (PersistableBusinessObject pbo : assets) {
+            for (AssetPayment payment : ((Asset)pbo).getAssetPayments()) {
+                if (StringUtils.equals(payment.getDocumentNumber(), assetGlobal.getDocumentNumber())) {
+                    payment.setPrimaryDepreciationBaseAmount(payment.getAccountChargeAmount());
+                    payment.setAccumulatedPrimaryDepreciationAmount(payment.getAccountChargeAmount());
+                }                
+            }           
+        }
+        return assets;
+    }	
 }
