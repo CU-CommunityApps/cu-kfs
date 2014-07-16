5a52f90654d289f3e891bc420512f26cd2417daa
Shawn Bower <shawn.bower@gmail.com>
2014-07-16 14:16:45 -0400

    KFSUPGRADE-996 adding test for AccountReversionServiceImpl

---
+++ src/test/java/edu/cornell/kfs/coa/fixture/AccountReversionFixture.java
@@ -0,0 +1,49 @@
+package edu.cornell.kfs.coa.fixture;
+
+import org.kuali.rice.krad.service.BusinessObjectService;
+
+import edu.cornell.kfs.coa.businessobject.AccountReversion;
+
+public enum AccountReversionFixture {
+
+	ACCOUNT_REVERSION_GOOD("G254700", 2014, "IT", "IT", "G104895", false, "IT", "G104895");
+	
+    public final String accountNumber;
+    public final Integer universityFiscalYear;
+    public final String chartOfAccountsCode;
+    public final String budgetReversionChartOfAccountsCode;
+    public final String budgetReversionAccountNumber;
+    public final boolean carryForwardByObjectCodeIndicator;
+    public final String cashReversionFinancialChartOfAccountsCode;
+    public final String cashReversionAccountNumber;
+
+	private AccountReversionFixture(String accountNumber, Integer universityFiscalYear, String chartOfAccountsCode, 
+			String budgetReversionChartOfAccountsCode, String budgetReversionAccountNumber, boolean carryForwardByObjectCodeIndicator,
+			String cashReversionFinancialChartOfAccountsCode, String cashReversionAccountNumber) {
+        this.accountNumber = accountNumber;
+        this.universityFiscalYear = universityFiscalYear;
+        this.chartOfAccountsCode = chartOfAccountsCode;
+        this.budgetReversionChartOfAccountsCode = budgetReversionChartOfAccountsCode;
+        this.budgetReversionAccountNumber = budgetReversionAccountNumber;
+        this.carryForwardByObjectCodeIndicator = carryForwardByObjectCodeIndicator;
+        this.cashReversionFinancialChartOfAccountsCode = cashReversionFinancialChartOfAccountsCode;
+        this.cashReversionAccountNumber = cashReversionAccountNumber;
+	}
+	
+   public AccountReversion createAccountReversion() {
+	   AccountReversion accountReversion = new AccountReversion();
+	   accountReversion.setAccountNumber(this.accountNumber);
+	   accountReversion.setUniversityFiscalYear(this.universityFiscalYear);
+	   accountReversion.setChartOfAccountsCode(this.chartOfAccountsCode);
+	   accountReversion.setBudgetReversionChartOfAccountsCode(this.budgetReversionChartOfAccountsCode);
+	   accountReversion.setBudgetReversionAccountNumber(this.budgetReversionAccountNumber);
+	   accountReversion.setCarryForwardByObjectCodeIndicator(this.carryForwardByObjectCodeIndicator);
+	   accountReversion.setCashReversionFinancialChartOfAccountsCode(this.cashReversionFinancialChartOfAccountsCode);
+	   accountReversion.setCashReversionAccountNumber(this.cashReversionAccountNumber);
+       return accountReversion;
+    }
+ 
+    public AccountReversion createAccountReversion(BusinessObjectService businessObjectService) {
+        return (AccountReversion) businessObjectService.retrieve(this.createAccountReversion());
+    }
+}
---
+++ src/test/java/edu/cornell/kfs/coa/fixture/ReversionCategoryFixture.java
@@ -0,0 +1,35 @@
+package edu.cornell.kfs.coa.fixture;
+
+import org.kuali.rice.krad.service.BusinessObjectService;
+
+import edu.cornell.kfs.coa.businessobject.ReversionCategory;
+
+public enum ReversionCategoryFixture {
+	A1_CATEGORY("A1", "Reversion", "A1", true),
+	BOGUS_CATEGORY("A2", "BOUGS", "A2", true);
+	
+    public final String reversionCategoryCode;
+    public final String reversionCategoryName;
+    public final String reversionSortCode;
+    public final boolean active;
+
+	private ReversionCategoryFixture(String reversionCategoryCode, String reversionCategoryName, String reversionSortCode,  boolean active) {
+        this.reversionCategoryCode = reversionCategoryCode;
+        this.reversionCategoryName = reversionCategoryName;
+        this.reversionSortCode = reversionSortCode;
+        this.active = active;
+	}
+	
+   public ReversionCategory createReversionCategory() {
+	   ReversionCategory reversionCategory = new ReversionCategory();
+	   reversionCategory.setReversionCategoryCode(this.reversionCategoryCode);
+	   reversionCategory.setReversionCategoryName(this.reversionCategoryName);
+	   reversionCategory.setReversionSortCode(this.reversionSortCode);
+	   reversionCategory.setActive(this.active);
+       return reversionCategory;
+    }
+ 
+    public ReversionCategory createAccountReversion(BusinessObjectService businessObjectService) {
+        return (ReversionCategory) businessObjectService.retrieve(this.createReversionCategory());
+    }
+}
