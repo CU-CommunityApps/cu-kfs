ccddee0f116e85a54c91d9754ea57d93a685a1d0
sdm86 <sdm86@cornell.edu>
2014-07-17 11:45:13 -0400

    Squashed commit of the following:
    commit 11a39e6c518b0f6faf54d2430995f9b982dfd181
    Author: sdm86 <sdm86@cornell.edu>
    Date:   Thu Jul 17 11:00:56 2014 -0400
    
        code cleanup, SQL refactoring
    
    commit 78ad44ac19b292d966125dddae4b0befca64b48a
    Author: sdm86 <sdm86@cornell.edu>
    Date:   Wed Jul 16 16:14:15 2014 -0400
    
        optimization of SQL to use fiscal year
    
    commit 48543950f6ad494c7120f31ef7dbce33a3864655
    Author: sdm86 <sdm86@cornell.edu>
    Date:   Wed Jul 16 13:45:38 2014 -0400
    
        comment and code cleanup
    
    commit f806a28557bd804543f4781a767a177616e28d68
    Merge: 625f963 869e774
    Author: sdm86 <sdm86@cornell.edu>
    Date:   Wed Jul 16 13:15:43 2014 -0400
    
        Merge branch 'kfs-5.2.1' of https://github.com/CU-CommunityApps/cu-kfs into KFSUPGRADE-981
    
    commit 625f96369df64ffcafd294450f7f674ab6991956
    Author: sdm86 <sdm86@cornell.edu>
    Date:   Wed Jul 16 12:25:09 2014 -0400
    
        checking this in
    
    commit 0f8258ae8eb0e7bd0cc0b68311e1ccb51df21af2
    Author: sdm86 <sdm86@cornell.edu>
    Date:   Wed Jul 16 12:22:41 2014 -0400
    
        modified sql
    
    commit 47ee4681bf943ac5e0722fd05befb375441b2313
    Author: sdm86 <sdm86@cornell.edu>
    Date:   Tue Jul 15 10:15:28 2014 -0400
    
        initial implementation of gl_entry_stats_t table
    
    commit a2de3c47522bd9c34523c2c55861ac6aa34cb171
    Author: sdm86 <sdm86@cornell.edu>
    Date:   Wed Jul 2 12:51:29 2014 -0400
    
        doing this so I can switch back to master, not sure how this will go
    
    commit 52f00379ff7eb2436c27c0bb573b7722020589be
    Author: sdm86 <sdm86@cornell.edu>
    Date:   Wed Jul 2 12:36:32 2014 -0400
    
        creation of custom batch sql
    
    final commit for KFSUPGRADE-981

---
+++ src/main/java/edu/cornell/kfs/gl/batch/dataaccess/impl/CuLedgerEntryBalanceCachingDaoJdbc.java
@@ -0,0 +1,106 @@
+package edu.cornell.kfs.gl.batch.dataaccess.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.kuali.kfs.gl.batch.dataaccess.LedgerEntryBalanceCachingDao;
+import org.kuali.kfs.gl.batch.dataaccess.impl.LedgerEntryBalanceCachingDaoJdbc;
+
+import edu.cornell.kfs.gl.batch.dataaccess.CuLedgerEntryBalanceCachingDao;
+
+public class CuLedgerEntryBalanceCachingDaoJdbc extends LedgerEntryBalanceCachingDaoJdbc implements CuLedgerEntryBalanceCachingDao {
+
+	@Override
+    public List compareEntryHistory(String entryTable, String historyTable, int fiscalYear) {
+        List<Map<String, Object>> data = null;
+                
+        StringBuilder queryBuilder = new StringBuilder();
+        String drop = "delete from GL_ENTRY_STATS_T";
+                
+        StringBuilder populate = new StringBuilder();
+        
+        populate.append("insert into gl_entry_stats_t ");
+        populate.append("SELECT UNIV_FISCAL_YR, FIN_COA_CD, FIN_OBJECT_CD, FIN_BALANCE_TYP_CD, UNIV_FISCAL_PRD_CD, TRN_DEBIT_CRDT_CD, count(*) as entry_row_cnt, sum(TRN_LDGR_ENTR_AMT) as entry_amt ");
+        populate.append("FROM GL_ENTRY_T where GL_ENTRY_T.UNIV_FISCAL_YR='");
+        populate.append(fiscalYear);
+        populate.append("' ");
+        populate.append("GROUP BY UNIV_FISCAL_YR, FIN_COA_CD, FIN_OBJECT_CD, FIN_BALANCE_TYP_CD, UNIV_FISCAL_PRD_CD, TRN_DEBIT_CRDT_CD");
+        
+                
+        queryBuilder.append("select eh.* ");
+        queryBuilder.append("from " + historyTable + " eh ");
+        queryBuilder.append("left join ");
+        queryBuilder.append("GL_ENTRY_STATS_T e ");
+        queryBuilder.append("on eh.univ_fiscal_yr = e.univ_fiscal_yr and eh.fin_coa_cd = e.fin_coa_cd and eh.fin_object_cd = e.fin_object_cd and ");
+        queryBuilder.append("eh.fin_balance_typ_cd = e.fin_balance_typ_cd and eh.univ_fiscal_prd_cd = e.univ_fiscal_prd_cd and eh.trn_debit_crdt_cd = e.trn_debit_crdt_cd ");
+        queryBuilder.append("where e.univ_fiscal_yr >= " + fiscalYear + " and (eh.row_cnt <> e.entry_row_cnt or eh.trn_ldgr_entr_amt <> e.entry_amt or e.entry_row_cnt is null) ");
+        
+	  	getJdbcTemplate().execute(drop.toString());
+	  	getJdbcTemplate().execute(populate.toString());        
+      
+        data = getSimpleJdbcTemplate().queryForList(queryBuilder.toString());
+        
+        return data;
+
+    }
+    
+    public List compareBalanceHistory(String balanceTable, String historyTable, int fiscalYear) {
+        List<Map<String, Object>> data = null;
+        StringBuilder queryBuilder = new StringBuilder();
+
+        queryBuilder.append("select bh.* ");
+        queryBuilder.append("from " + historyTable + " bh  ");
+        queryBuilder.append("left join ( select ");
+        queryBuilder.append("UNIV_FISCAL_YR, FIN_COA_CD, ACCOUNT_NBR, SUB_ACCT_NBR, FIN_OBJECT_CD, FIN_SUB_OBJ_CD,  FIN_BALANCE_TYP_CD, FIN_OBJ_TYP_CD, ");
+        queryBuilder.append("ACLN_ANNL_BAL_AMT, FIN_BEG_BAL_LN_AMT, CONTR_GR_BB_AC_AMT, MO1_ACCT_LN_AMT, MO2_ACCT_LN_AMT, MO3_ACCT_LN_AMT, MO4_ACCT_LN_AMT, MO5_ACCT_LN_AMT, MO6_ACCT_LN_AMT, MO7_ACCT_LN_AMT, MO8_ACCT_LN_AMT, MO9_ACCT_LN_AMT, MO10_ACCT_LN_AMT, MO11_ACCT_LN_AMT, MO12_ACCT_LN_AMT, MO13_ACCT_LN_AMT ");
+        queryBuilder.append("from " + balanceTable + " ) e on ");
+        queryBuilder.append("bh.UNIV_FISCAL_YR = e.UNIV_FISCAL_YR and bh.FIN_COA_CD = e.FIN_COA_CD and bh.FIN_OBJECT_CD = e.FIN_OBJECT_CD and bh.FIN_BALANCE_TYP_CD = e.FIN_BALANCE_TYP_CD and bh.SUB_ACCT_NBR = e.SUB_ACCT_NBR and bh.ACCOUNT_NBR = e.ACCOUNT_NBR and bh.FIN_SUB_OBJ_CD = e.FIN_SUB_OBJ_CD and bh.FIN_OBJ_TYP_CD = e.FIN_OBJ_TYP_CD ");
+        queryBuilder.append(" where e.UNIV_FISCAL_YR >= " + fiscalYear + " ");
+        queryBuilder.append("and (bh.ACLN_ANNL_BAL_AMT <> e.ACLN_ANNL_BAL_AMT or bh.FIN_BEG_BAL_LN_AMT <> e.FIN_BEG_BAL_LN_AMT or bh.CONTR_GR_BB_AC_AMT <> e.CONTR_GR_BB_AC_AMT or  ");
+        queryBuilder.append("bh.MO1_ACCT_LN_AMT <> e.MO1_ACCT_LN_AMT or bh.MO2_ACCT_LN_AMT <> e.MO2_ACCT_LN_AMT or bh.MO3_ACCT_LN_AMT <> e.MO3_ACCT_LN_AMT or bh.MO4_ACCT_LN_AMT <> e.MO4_ACCT_LN_AMT or bh.MO5_ACCT_LN_AMT <> e.MO5_ACCT_LN_AMT or bh.MO6_ACCT_LN_AMT <> e.MO6_ACCT_LN_AMT or  ");
+        queryBuilder.append("bh.MO7_ACCT_LN_AMT <> e.MO7_ACCT_LN_AMT or bh.MO8_ACCT_LN_AMT <> e.MO8_ACCT_LN_AMT or bh.MO9_ACCT_LN_AMT <> e.MO9_ACCT_LN_AMT or bh.MO10_ACCT_LN_AMT <> e.MO10_ACCT_LN_AMT or bh.MO11_ACCT_LN_AMT <> e.MO11_ACCT_LN_AMT or bh.MO12_ACCT_LN_AMT <> e.MO12_ACCT_LN_AMT or  ");
+        queryBuilder.append("bh.MO13_ACCT_LN_AMT <> e.MO13_ACCT_LN_AMT) ");
+
+        data = getSimpleJdbcTemplate().queryForList(queryBuilder.toString());
+
+        return data;
+
+    }
+
+    public List accountBalanceCompareHistory(String accountBalanceTable, String historyTable, int fiscalYear) {
+        List<Map<String, Object>> data = null;
+        StringBuilder queryBuilder = new StringBuilder();
+
+        queryBuilder.append("select abh.* ");
+        queryBuilder.append("from " + historyTable + " abh ");
+        queryBuilder.append("left join  ");
+        queryBuilder.append("(select UNIV_FISCAL_YR, FIN_COA_CD, ACCOUNT_NBR, SUB_ACCT_NBR, FIN_OBJECT_CD, FIN_SUB_OBJ_CD, CURR_BDLN_BAL_AMT, ACLN_ACTLS_BAL_AMT, ACLN_ENCUM_BAL_AMT ");
+        queryBuilder.append("from " + accountBalanceTable + " ) ab on ");
+        queryBuilder.append("abh.UNIV_FISCAL_YR = ab.UNIV_FISCAL_YR and abh.FIN_COA_CD = ab.FIN_COA_CD and abh.ACCOUNT_NBR = ab.ACCOUNT_NBR and abh.SUB_ACCT_NBR = ab.SUB_ACCT_NBR and abh.FIN_OBJECT_CD = ab.FIN_OBJECT_CD and abh.FIN_SUB_OBJ_CD = ab.FIN_SUB_OBJ_CD ");
+        queryBuilder.append("where ab.UNIV_FISCAL_YR >= " + fiscalYear + " ");
+        queryBuilder.append("and (abh.CURR_BDLN_BAL_AMT <> ab.CURR_BDLN_BAL_AMT or abh.ACLN_ACTLS_BAL_AMT <> ab.ACLN_ACTLS_BAL_AMT or abh.ACLN_ENCUM_BAL_AMT <> ab.ACLN_ENCUM_BAL_AMT) ");
+
+        data = getSimpleJdbcTemplate().queryForList(queryBuilder.toString());
+
+        return data;
+
+    }
+
+    public List encumbranceCompareHistory(String encumbranceTable, String historyTable, int fiscalYear) {
+        List<Map<String, Object>> data = null;
+        StringBuilder queryBuilder = new StringBuilder();
+
+        queryBuilder.append("select eh.*  ");
+        queryBuilder.append("from " + historyTable + " eh ");
+        queryBuilder.append("left join  ( ");
+        queryBuilder.append("select UNIV_FISCAL_YR, FIN_COA_CD, ACCOUNT_NBR, SUB_ACCT_NBR, FIN_OBJECT_CD, FIN_SUB_OBJ_CD, FIN_BALANCE_TYP_CD, FDOC_TYP_CD, FS_ORIGIN_CD, FDOC_NBR, ACLN_ENCUM_AMT, ACLN_ENCUM_CLS_AMT from " + encumbranceTable + " ) e on ");
+        queryBuilder.append("eh.UNIV_FISCAL_YR = e.UNIV_FISCAL_YR and eh.FIN_COA_CD = e.FIN_COA_CD and eh.ACCOUNT_NBR = e.ACCOUNT_NBR and eh.SUB_ACCT_NBR = e.SUB_ACCT_NBR and eh.FIN_OBJECT_CD = e.FIN_OBJECT_CD and eh.FIN_SUB_OBJ_CD = e.FIN_SUB_OBJ_CD and eh.FIN_BALANCE_TYP_CD = e.FIN_BALANCE_TYP_CD and eh.FDOC_TYP_CD = e.FDOC_TYP_CD and eh.FS_ORIGIN_CD = e.FS_ORIGIN_CD and eh.FDOC_NBR = e.FDOC_NBR ");
+        queryBuilder.append("where e.UNIV_FISCAL_YR >= " + fiscalYear + " and (eh.ACLN_ENCUM_AMT <> e.ACLN_ENCUM_AMT or eh.ACLN_ENCUM_CLS_AMT <> e.ACLN_ENCUM_CLS_AMT) ");
+
+        data = getSimpleJdbcTemplate().queryForList(queryBuilder.toString());
+
+        return data;
+
+    }
+
+}
