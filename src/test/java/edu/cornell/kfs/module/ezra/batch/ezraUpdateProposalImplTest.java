b520fe8cf24f5eb1ea0691dbbedc5890ef594058
Chad Bower <chad.bower379@gmail.com>
2014-07-17 16:51:18 -0400

    KFSUPGRADE 1000 Added test for update proposals

---
+++ src/test/java/edu/cornell/kfs/module/ezra/batch/ezraUpdateProposalImplTest.java
@@ -0,0 +1,53 @@
+package edu.cornell.kfs.module.ezra.batch;
+
+import static org.kuali.kfs.sys.fixture.UserNameFixture.ccs1;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import edu.cornell.kfs.module.ezra.businessobject.EzraProposalAward;
+import edu.cornell.kfs.module.ezra.service.EzraService;
+import org.kuali.rice.krad.service.BusinessObjectService;
+
+import org.kuali.kfs.module.cg.businessobject.Proposal;
+import org.kuali.kfs.sys.ConfigureContext;
+import org.kuali.kfs.sys.context.KualiTestBase;
+import org.kuali.kfs.sys.context.SpringContext;
+import edu.cornell.kfs.module.ezra.dataaccess.EzraAwardProposalDao;
+import edu.cornell.kfs.module.receiptProcessing.service.ReceiptProcessingService;
+
+@ConfigureContext(session = ccs1)
+public class ezraUpdateProposalImplTest extends KualiTestBase {
+
+    private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(EzraService.class);
+	private EzraService ezraService;	
+	private BusinessObjectService businessObjectService;
+	
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        ezraService = SpringContext.getBean(EzraService.class);
+        businessObjectService = SpringContext.getBean(BusinessObjectService.class);
+    }
+    
+    public void testUpdateProposal () {
+        Map fields = new HashMap();
+        fields.clear();
+        fields.put("proposalNumber", "13800");        
+        String grantIdTest = "ezraUpdateTest";
+        String grantIdAfter = "";
+        Proposal proposal = (Proposal)businessObjectService.findByPrimaryKey(Proposal.class, fields);
+        String startGrant = proposal.getGrantNumber();
+        proposal.setGrantNumber(grantIdTest);
+        businessObjectService.save(proposal);
+        assertTrue(ezraService.updateProposals());
+        Proposal proposalAfterUpdate = (Proposal)businessObjectService.findByPrimaryKey(Proposal.class, fields);
+        grantIdAfter = proposalAfterUpdate.getGrantNumber();
+        LOG.info("Grant Number before update "+grantIdTest+" Grant Number after update "+grantIdAfter+" Started with "+startGrant);
+        if (grantIdTest.equalsIgnoreCase(grantIdAfter)) {
+            fail("Ezra failed to update proposal");
+        }
+    }
+    
+}
