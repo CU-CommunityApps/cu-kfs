e1dc0d60259d0cae4b06654fe7543a97a39273ab
sbower <shawn.bower@gmail.com>
2014-07-25 16:08:04 -0400

    KFSUPGRADE-1004 better test, need to fix some hard coding

--- src/test/java/edu/cornell/kfs/module/ld/batch/service/impl/CuFileEnterpriseFeederServiceImplTest.java
+++ src/test/java/edu/cornell/kfs/module/ld/batch/service/impl/CuFileEnterpriseFeederServiceImplTest.java
@@ -1,13 +1,17 @@
 package edu.cornell.kfs.module.ld.batch.service.impl;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
+import org.kuali.kfs.gl.batch.service.impl.ExceptionCaughtStatus;
+import org.kuali.kfs.gl.batch.service.impl.FileReconBadLoadAbortedStatus;
 import org.kuali.kfs.module.ld.batch.service.EnterpriseFeederService;
 import org.kuali.kfs.sys.ConfigureContext;
 import org.kuali.kfs.sys.batch.service.WrappingBatchService;
 import org.kuali.kfs.sys.context.KualiTestBase;
 import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.kfs.sys.context.TestUtils;
 import org.kuali.kfs.sys.service.ReportWriterService;
 import org.kuali.kfs.sys.service.impl.ReportWriterTextServiceImpl;
 
@@ -16,6 +20,7 @@ public class CuFileEnterpriseFeederServiceImplTest extends KualiTestBase  {
     private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(CuFileEnterpriseFeederServiceImplTest.class);
     private static final String DATA_FILE_PATH = "src/test/java/edu/cornell/kfs/module/ld/fixture/SMGROS.data";
     private static final String RECON_FILE_PATH = "src/test/java/edu/cornell/kfs/module/ld/fixture/SMGROS.recon";
+    private static final String REPORTS_FOLDER = "/opt/work/reports/ld";
     
     private EnterpriseFeederService enterpriseFeederService;
       
@@ -29,9 +34,11 @@ public class CuFileEnterpriseFeederServiceImplTest extends KualiTestBase  {
         enterpriseFeederService = SpringContext.getBean(EnterpriseFeederService.class);
         
         ReportWriterTextServiceImpl laborEnterpriseFeedReportWriterService  = SpringContext.getBean(ReportWriterTextServiceImpl.class, "laborEnterpriseFeedReportWriterService");
+        laborEnterpriseFeedReportWriterService.setAggregationModeOn(true);
         laborEnterpriseFeedReportWriterService.initialize();
         
         ReportWriterTextServiceImpl laborEnterpriseFeedErrorStatisticsWriterService  = SpringContext.getBean(ReportWriterTextServiceImpl.class, "laborEnterpriseFeedErrorStatisticsWriterService");
+        laborEnterpriseFeedErrorStatisticsWriterService.setAggregationModeOn(true);
         laborEnterpriseFeedErrorStatisticsWriterService.initialize();
         
         //make sure we have a batch directory
@@ -64,8 +71,31 @@ public class CuFileEnterpriseFeederServiceImplTest extends KualiTestBase  {
         
     }
     
-    public void testFeed() {
+   public void testFeed() throws IOException {
         enterpriseFeederService.feed("SomeName", false);
+        
+        String content = TestUtils.readFileUTF8(REPORTS_FOLDER + "/labor_enterprise_feed_.txt");
+        assertTrue(content.contains("19,480.65"));
+        assertTrue(content.contains("All files were successfully loaded"));
+    }
+    
+    public void testFeedBadData() throws IOException {
+        dataFileDest.delete();
+        enterpriseFeederService.feed("SomeName", false);
+        
+        String content = TestUtils.readFileUTF8(REPORTS_FOLDER + "/labor_enterprise_feed_.txt");
+        assertTrue(content.contains("SMGROS.recon"));
+        assertTrue(content.contains("No files were successfully loaded"));
+    }
+    
+   public void testFeedBadRecon() throws IOException {
+        reconFileDest.delete();
+        enterpriseFeederService.feed("SomeName", false);
+        
+        String content = TestUtils.readFileUTF8(REPORTS_FOLDER + "/labor_enterprise_feed_.txt");
+
+        assertTrue(content.contains("SMGROS.data"));
+        assertTrue(content.contains("No files were successfully loaded"));
     }
 
 }
