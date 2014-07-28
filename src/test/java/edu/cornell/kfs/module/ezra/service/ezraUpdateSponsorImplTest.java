d1a9bcb0a17f28c4487b67e8d12ee056757e338a
Shawn Bower <shawn.bower@gmail.com>
2014-07-28 07:58:13 -0400

    Revert "unit test fix"
    This reverts commit 2dbe5eb8aa306c1267cef7d19a440c357b07c9db.

--- src/test/java/edu/cornell/kfs/module/ezra/service/ezraUpdateSponsorImplTest.java
+++ src/test/java/edu/cornell/kfs/module/ezra/service/ezraUpdateSponsorImplTest.java
@@ -89,7 +89,7 @@ public class ezraUpdateSponsorImplTest extends KualiTestBase {
                 c.add(Calendar.DATE, count);
                 sqlDate = new java.sql.Date(c.getTimeInMillis());
                 sponsors = sponsorDao.getSponsorsUpdatedSince(sqlDate);                    
-                } while (sponsors.isEmpty() || !agencyInKfs(sponsors));
+                } while (!agencyInKfs(sponsors));
         }
         count = 0;
         LOG.info("The Sql Date is "+sqlDate);
