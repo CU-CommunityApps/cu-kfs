c7f7da930558c443079933432d36ed3d337c9e33
lshyu <lshyu@vivantech.com>
2014-10-02 09:40:09 -0400

    KFSPTS-3578 : merge to develop - fixed trade in error icon is always in the last item.

---
+++ src/main/java/edu/cornell/kfs/module/purap/document/validation/impl/CuRequisitionAssignToTradeInValidation.java
@@ -0,0 +1,94 @@
+package edu.cornell.kfs.module.purap.document.validation.impl;
+
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.kuali.kfs.module.purap.PurapConstants;
+import org.kuali.kfs.module.purap.PurapKeyConstants;
+import org.kuali.kfs.module.purap.PurapPropertyConstants;
+import org.kuali.kfs.module.purap.businessobject.PurApItem;
+import org.kuali.kfs.module.purap.document.PurchasingAccountsPayableDocumentBase;
+import org.kuali.kfs.module.purap.document.validation.impl.RequisitionAssignToTradeInValidation;
+import org.kuali.kfs.sys.document.validation.event.AttributedDocumentEvent;
+import org.kuali.rice.kns.service.DataDictionaryService;
+import org.kuali.rice.krad.util.GlobalVariables;
+import org.kuali.rice.krad.util.ObjectUtils;
+
+public class CuRequisitionAssignToTradeInValidation extends RequisitionAssignToTradeInValidation {
+
+    private DataDictionaryService dataDictionaryService;
+
+    public boolean validate(AttributedDocumentEvent event) {
+        
+        // Initialize the valid and true, and get the requisition document.
+        boolean foundTradeIn = false;
+        boolean valid        = true;
+        
+        PurchasingAccountsPayableDocumentBase purapDoc = 
+            (PurchasingAccountsPayableDocumentBase) event.getDocument();
+        
+        // First, get all the items from the requisition document.  For each of
+        // the items, look for the ones that are assigned to a trade-in value.
+        // For these trade-in items, validate that the trade-in line has a valid
+        // description and amount.
+        List<PurApItem> items = (List<PurApItem>)purapDoc.getItems();
+        for (PurApItem item : items) {
+            item.refreshReferenceObject(PurapPropertyConstants.ITEM_TYPE);
+            if (item.getItemAssignedToTradeInIndicator()) {
+                foundTradeIn = true;
+                break;
+            }
+        }
+        
+        // Was a trade-in found for any of the above items?
+        if (foundTradeIn) {
+
+            // Get the trade-in item.
+            PurApItem tradeInItem = purapDoc.getTradeInItem();
+            if (tradeInItem != null) {
+                if (StringUtils.isEmpty(tradeInItem.getItemDescription())) {
+                    
+                    String attributeLabel = dataDictionaryService.getDataDictionary().getBusinessObjectEntry(tradeInItem.getClass().getName()).getAttributeDefinition(PurapPropertyConstants.ITEM_DESCRIPTION).getLabel();
+                    tradeInItem.getItemLineNumber();
+                    GlobalVariables.getMessageMap().putError(
+                            "document.item[" + getTradeInItemLineIndex(items) + "]." + PurapPropertyConstants.ITEM_DESCRIPTION, 
+                            PurapKeyConstants.ERROR_ITEM_BELOW_THE_LINE, 
+                            "The item description of " + tradeInItem.getItemType().getItemTypeDescription(), 
+                            "empty");
+                    
+                    valid = false;
+                }
+                else if (ObjectUtils.isNull(tradeInItem.getItemUnitPrice())) {
+                    
+                    String attributeLabel = dataDictionaryService.getDataDictionary().getBusinessObjectEntry(tradeInItem.getClass().getName()).getAttributeDefinition(PurapPropertyConstants.ITEM_UNIT_PRICE).getLabel();
+                    GlobalVariables.getMessageMap().putError(
+                            "document.item[" + getTradeInItemLineIndex(items) + "]." + PurapPropertyConstants.ITEM_UNIT_PRICE,
+                            PurapKeyConstants.ERROR_ITEM_BELOW_THE_LINE,
+                            tradeInItem.getItemType().getItemTypeDescription(),
+                            "zero");
+                    
+                    valid = false;
+                }
+            }
+        }
+        
+        return valid;
+    }
+
+    private int getTradeInItemLineIndex(List<PurApItem> items) {
+        int lineIndex = 0;
+        for (PurApItem item : items) {
+            if (item.getItemTypeCode().equals(PurapConstants.ItemTypeCodes.ITEM_TYPE_TRADE_IN_CODE)) {
+                break;
+            } else {
+                lineIndex++;
+            }
+        }
+        return lineIndex;
+    }
+
+    public void setDataDictionaryService(DataDictionaryService dataDictionaryService) {
+        this.dataDictionaryService = dataDictionaryService;
+    }
+
+}
