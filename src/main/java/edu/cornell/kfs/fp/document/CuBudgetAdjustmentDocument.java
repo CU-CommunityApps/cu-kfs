7f6b2821a174e52dd1e3075263ceec8681eaf449
danielabalmus <db483@cornell.edu>
2014-10-08 15:43:03 +0100

    KFSPTS-3576 Set line amount as current amount before getting source or

---
+++ src/main/java/edu/cornell/kfs/fp/document/CuBudgetAdjustmentDocument.java
@@ -0,0 +1,30 @@
+package edu.cornell.kfs.fp.document;
+
+import java.util.Iterator;
+
+import org.kuali.kfs.fp.businessobject.BudgetAdjustmentAccountingLine;
+import org.kuali.kfs.fp.document.BudgetAdjustmentDocument;
+import org.kuali.rice.core.api.util.type.KualiDecimal;
+
+public class CuBudgetAdjustmentDocument extends BudgetAdjustmentDocument {
+
+    @Override
+    public KualiDecimal getSourceTotal() {
+        for (Iterator iter = this.getSourceAccountingLines().iterator(); iter.hasNext();) {
+            BudgetAdjustmentAccountingLine line = (BudgetAdjustmentAccountingLine) iter.next();
+            line.setAmount(line.getCurrentBudgetAdjustmentAmount());
+        }
+
+        return super.getSourceTotal();
+    }
+
+    @Override
+    public KualiDecimal getTargetTotal() {
+        for (Iterator iter = this.getTargetAccountingLines().iterator(); iter.hasNext();) {
+            BudgetAdjustmentAccountingLine line = (BudgetAdjustmentAccountingLine) iter.next();
+            line.setAmount(line.getCurrentBudgetAdjustmentAmount());
+        }
+        return super.getTargetTotal();
+    }
+
+}
