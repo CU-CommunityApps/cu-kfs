d6adf9d21943dac5658543ae8ae25204f60a1bc6
sdm86 <sdm86@cornell.edu>
2014-10-10 13:25:48 -0400

    KFSPTS-3487 fix for typo that broke build

--- src/main/java/org/kuali/kfs/vnd/businessobject/VendorDetail.java
+++ src/main/java/org/kuali/kfs/vnd/businessobject/VendorDetail.java
@@ -557,7 +557,7 @@ public class VendorDetail extends PersistableBusinessObjectBase implements Vendo
             } else {
                 first = false;
             }
-            sb.append(vcc.getCommodityCode().getCommodityCodeDescription());
+            sb.append(vcc.getCommodityCode().getCommodityDescription());
         }
         sb.append(']');
         return sb.toString();
