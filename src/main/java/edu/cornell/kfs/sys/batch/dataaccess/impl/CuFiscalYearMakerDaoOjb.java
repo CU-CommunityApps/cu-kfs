a4e344cd9f1f98e3b41ff301b05d195638aa5fd5
danielabalmus <db483@cornell.edu>
2014-10-16 14:25:43 +0100

    KFSPTS-3064 Change FYM so that it would delete extended attribute table first where applicable when job is run in override mode

---
+++ src/main/java/edu/cornell/kfs/sys/batch/dataaccess/impl/CuFiscalYearMakerDaoOjb.java
@@ -0,0 +1,69 @@
+package edu.cornell.kfs.sys.batch.dataaccess.impl;
+
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.apache.ojb.broker.query.QueryByCriteria;
+import org.kuali.kfs.sys.KFSPropertyConstants;
+import org.kuali.kfs.sys.batch.dataaccess.FiscalYearMaker;
+import org.kuali.kfs.sys.batch.dataaccess.impl.FiscalYearMakersDaoOjb;
+import org.kuali.rice.krad.bo.DataObjectRelationship;
+import org.kuali.rice.krad.service.PersistenceStructureService;
+
+public class CuFiscalYearMakerDaoOjb extends FiscalYearMakersDaoOjb {
+    private static final Logger LOG = org.apache.log4j.Logger.getLogger(CuFiscalYearMakerDaoOjb.class);
+
+    protected PersistenceStructureService persistenceStructureService;
+
+    /**
+     * Deletes the new year records for the given fiscal year maker. If the object has an extension it first deletes the data
+     * in the extension table.
+     * 
+     * @see org.kuali.kfs.sys.batch.dataaccess.impl.FiscalYearMakersDaoOjb#deleteNewYearRows(java.lang.Integer,
+     *      org.kuali.kfs.sys.batch.dataaccess.FiscalYearMaker)
+     */
+    @Override
+    public void deleteNewYearRows(Integer baseYear, FiscalYearMaker objectFiscalYearMaker) {
+        // Check if an extension exists and delete extension records first
+        if (persistenceStructureService.hasReference(objectFiscalYearMaker.getBusinessObjectClass(), KFSPropertyConstants.EXTENSION)) {
+            Map<String, DataObjectRelationship> relationships = persistenceStructureService.getRelationshipMetadata(objectFiscalYearMaker.getBusinessObjectClass(), KFSPropertyConstants.EXTENSION);
+            Class extensionClass = relationships.get(KFSPropertyConstants.EXTENSION).getRelatedClass();
+
+            LOG.info(String.format("\ndeleting %s for %d", extensionClass.getName(), baseYear + 1));
+
+            QueryByCriteria queryExtension = new QueryByCriteria(extensionClass, objectFiscalYearMaker.createDeleteCriteria(baseYear));
+
+            getPersistenceBrokerTemplate().deleteByQuery(queryExtension);
+            getPersistenceBrokerTemplate().clearCache();
+        }
+
+        // Delete the main objects now
+        if (LOG.isInfoEnabled()) {
+            LOG.info(String.format("\ndeleting %s for target year(s)", objectFiscalYearMaker.getBusinessObjectClass().getName()));
+        }
+
+        QueryByCriteria queryID = new QueryByCriteria(objectFiscalYearMaker.getBusinessObjectClass(), objectFiscalYearMaker.createDeleteCriteria(baseYear));
+        getPersistenceBrokerTemplate().deleteByQuery(queryID);
+
+        getPersistenceBrokerTemplate().clearCache();
+    }
+
+    /**
+     * Gets the persistenceStructureService.
+     * 
+     * @return persistenceStructureService
+     */
+    public PersistenceStructureService getPersistenceStructureService() {
+        return persistenceStructureService;
+    }
+
+    /**
+     * Sets the persistenceStructureService.
+     * 
+     * @param persistenceStructureService
+     */
+    public void setPersistenceStructureService(PersistenceStructureService persistenceStructureService) {
+        this.persistenceStructureService = persistenceStructureService;
+    }
+
+}
