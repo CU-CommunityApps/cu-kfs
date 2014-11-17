343369d5ce7753b01c4a876a4b874aa40f4fc717
danielabalmus <db483@cornell.edu>
2014-11-17 16:54:13 +0000

    KFSPTS-3659 Change code that matches GL entries with accounting lines so that it also checks that GL Debit matches acct line type Target and Credit matches Source and so that it creates missing Capital Asset info

---
+++ src/main/java/org/kuali/kfs/module/cab/document/web/struts/CapitalAssetInformationAction.java
@@ -0,0 +1,185 @@
+/*
+ * Copyright 2008 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kfs.module.cab.document.web.struts;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.kuali.kfs.fp.businessobject.CapitalAssetInformation;
+import org.kuali.kfs.module.cab.CabConstants;
+import org.kuali.kfs.module.cab.CabPropertyConstants;
+import org.kuali.kfs.module.cab.businessobject.GeneralLedgerEntry;
+import org.kuali.kfs.module.cab.document.service.GlAndPurApHelperService;
+import org.kuali.kfs.module.cab.document.service.GlLineService;
+import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.rice.core.api.util.RiceConstants;
+import org.kuali.rice.krad.service.BusinessObjectService;
+import org.kuali.rice.krad.util.KRADConstants;
+import org.kuali.rice.krad.util.ObjectUtils;
+
+/**
+ * Struts action class that handles Capital Asset Information Screen actions
+ */
+public class CapitalAssetInformationAction extends CabActionBase {
+
+    /**
+     * Action "process" from CAB GL Lookup screen is processed by this method
+     *
+     * @param mapping {@link ActionMapping}
+     * @param form {@link ActionForm}
+     * @param request {@link HttpServletRequest}
+     * @param response {@link HttpServletResponse}
+     * @return {@link ActionForward}
+     * @throws Exception
+     */
+    public ActionForward process(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        CapitalAssetInformationForm capitalAssetForm = (CapitalAssetInformationForm) form;
+        String glAcctId = request.getParameter(CabPropertyConstants.GeneralLedgerEntry.GENERAL_LEDGER_ACCOUNT_IDENTIFIER);
+        Long cabGlEntryId = Long.valueOf(glAcctId);
+        capitalAssetForm.setGeneralLedgerAccountIdentifier(cabGlEntryId);
+
+        GeneralLedgerEntry entry = findGeneralLedgerEntry(request);
+        if (ObjectUtils.isNotNull(entry)) {
+            prepareRecordsForDisplay(capitalAssetForm, entry);
+        }
+      //  if (!entry.isActive()) {
+      //      KNSGlobalVariables.getMessageList().add(CabKeyConstants.WARNING_GL_PROCESSED);
+      //  }
+        return mapping.findForward(RiceConstants.MAPPING_BASIC);
+    }
+
+    private void prepareRecordsForDisplay(CapitalAssetInformationForm capitalAssetForm, GeneralLedgerEntry entry) {
+        GlLineService glLineService = SpringContext.getBean(GlLineService.class);
+
+        entry.setSelected(true);
+        capitalAssetForm.setGeneralLedgerEntry(entry);
+        capitalAssetForm.setPrimaryGlAccountId(entry.getGeneralLedgerAccountIdentifier());
+        
+        List<CapitalAssetInformation> capitalAssetInformations = glLineService.findAllCapitalAssetInformation(entry.getDocumentNumber());
+        Collection<GeneralLedgerEntry> glEntries = glLineService.findAllGeneralLedgerEntry(entry.getDocumentNumber()); 
+        
+        if(!capitalAssetInformations.isEmpty() && capitalAssetInformations.size() != glEntries.size()){
+        	// we need to generate missing capital asset info
+        	glLineService.setupMissingCapitalAssetInformation(entry.getDocumentNumber());
+        }
+        
+        List<CapitalAssetInformation> capitalAssetInformation = glLineService.findCapitalAssetInformationForGLLine(entry);
+        
+
+
+        // KFSMI-9881
+        // For GL Entries without capital asset information (ex: loaded by enterprise feed or Vendor Credit Memo),
+        // we need to create that information (at least a shell record) for the Capital Asset Information screen
+        // to render and subsequent processing to occur successfully.
+        if (capitalAssetInformation.isEmpty()) {
+            glLineService.setupCapitalAssetInformation(entry);
+            capitalAssetInformation = SpringContext.getBean(GlLineService.class).findCapitalAssetInformationForGLLine(entry);
+        }
+
+        capitalAssetForm.setCapitalAssetInformation(capitalAssetInformation);
+    }
+
+    /**
+     * Finds GL entry using the key from request
+     *
+     * @param request HttpServletRequest
+     * @return GeneralLedgerEntry
+     */
+    protected GeneralLedgerEntry findGeneralLedgerEntry(HttpServletRequest request) {
+        String glAcctId = request.getParameter(CabPropertyConstants.GeneralLedgerEntry.GENERAL_LEDGER_ACCOUNT_IDENTIFIER);
+        Long cabGlEntryId = Long.valueOf(glAcctId);
+        return findGeneralLedgerEntry(cabGlEntryId, false);
+    }
+
+    /**
+     * Retrieves the CAB General Ledger Entry from DB
+     *
+     * @param generalLedgerEntryId Entry Id
+     * @return GeneralLedgerEntry
+     */
+    protected GeneralLedgerEntry findGeneralLedgerEntry(Long generalLedgerEntryId, boolean requireNew) {
+        BusinessObjectService boService = SpringContext.getBean(BusinessObjectService.class);
+        Map<String, Object> pkeys = new HashMap<String, Object>();
+        pkeys.put(CabPropertyConstants.GeneralLedgerEntry.GENERAL_LEDGER_ACCOUNT_IDENTIFIER, generalLedgerEntryId);
+        if (requireNew) {
+            pkeys.put(CabPropertyConstants.GeneralLedgerEntry.ACTIVITY_STATUS_CODE, CabConstants.ActivityStatusCode.NEW);
+        }
+        GeneralLedgerEntry entry = boService.findByPrimaryKey(GeneralLedgerEntry.class, pkeys);
+        return entry;
+    }
+
+    /**
+     * Cancels the action and returns to portal main page
+     *
+     * @param mapping {@link ActionMapping}
+     * @param form {@link ActionForm}
+     * @param request {@link HttpServletRequest}
+     * @param response {@link HttpServletResponse}
+     * @return {@link ActionForward}
+     * @throws Exception
+     */
+    public ActionForward cancel(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return mapping.findForward(KRADConstants.MAPPING_PORTAL);
+    }
+
+    /**
+     * @see org.kuali.rice.kns.web.struts.action.KualiAction#showAllTabs(org.apache.struts.action.ActionMapping,
+     *      org.apache.struts.action.ActionForm, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     */
+    @Override
+    public ActionForward showAllTabs(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        CapitalAssetInformationForm capitalAssetForm = (CapitalAssetInformationForm) form;
+        GeneralLedgerEntry generalLedgerEntry = capitalAssetForm.getGeneralLedgerEntry();
+        generalLedgerEntry.setSelected(true);
+
+        return super.showAllTabs(mapping, form, request, response);
+    }
+
+    /**
+     * reloads the capital asset information screen
+     *
+     * @param mapping
+     * @param form
+     * @param request
+     * @param response
+     * @return
+     * @throws Exception
+     */
+    public ActionForward reload(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        CapitalAssetInformationForm capitalAssetForm = (CapitalAssetInformationForm) form;
+
+        GeneralLedgerEntry entry = capitalAssetForm.getGeneralLedgerEntry();
+
+     //   GeneralLedgerEntry entry = findGeneralLedgerEntry(request);
+     //   if (entry != null) {
+            prepareRecordsForDisplay(capitalAssetForm, entry);
+      //  }
+
+        return mapping.findForward(RiceConstants.MAPPING_BASIC);
+    }
+
+    protected GlAndPurApHelperService getGlAndPurApHelperService() {
+        return SpringContext.getBean(GlAndPurApHelperService.class);
+    }
+}
