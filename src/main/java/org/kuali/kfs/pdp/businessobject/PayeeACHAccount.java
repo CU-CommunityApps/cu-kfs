7f9eaa9e943c8506d3434e3c27df6a91cd5ca2fc
Kyle Oliveira <kyle.oliveira@cornell.edu>
2015-02-09 14:10:52 -0500

    [KFSUPGRADE-1082] 5.3.1. PDP changes

--- src/main/java/org/kuali/kfs/pdp/businessobject/PayeeACHAccount.java
+++ src/main/java/org/kuali/kfs/pdp/businessobject/PayeeACHAccount.java
@@ -1,17 +1,20 @@
 /*
- * Copyright 2007 The Kuali Foundation
- *
- * Licensed under the Educational Community License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.opensource.org/licenses/ecl2.php
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * The Kuali Financial System, a comprehensive financial management system for higher education.
+ * 
+ * Copyright 2005-2014 The Kuali Foundation
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.kuali.kfs.pdp.businessobject;
 
@@ -23,6 +26,7 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.builder.ReflectionToStringBuilder;
 import org.kuali.kfs.pdp.PdpConstants.PayeeIdTypeCodes;
 import org.kuali.kfs.pdp.PdpPropertyConstants;
+import org.kuali.kfs.sys.businessobject.TimestampedBusinessObjectBase;
 import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.kfs.sys.service.FinancialSystemUserService;
 import org.kuali.kfs.vnd.businessobject.VendorDetail;
@@ -35,12 +39,11 @@ import org.kuali.rice.kim.api.identity.entity.EntityDefault;
 import org.kuali.rice.kim.api.identity.principal.Principal;
 import org.kuali.rice.kim.api.services.KimApiServiceLocator;
 import org.kuali.rice.krad.bo.BusinessObject;
-import org.kuali.rice.krad.bo.PersistableBusinessObjectBase;
 import org.kuali.rice.krad.datadictionary.AttributeSecurity;
 import org.kuali.rice.krad.service.DataDictionaryService;
 import org.kuali.rice.krad.util.ObjectUtils;
 
-public class PayeeACHAccount extends PersistableBusinessObjectBase implements MutableInactivatable {
+public class PayeeACHAccount extends TimestampedBusinessObjectBase implements MutableInactivatable {
 
     private KualiInteger achAccountGeneratedIdentifier;
     private String bankRoutingNumber;
@@ -52,6 +55,8 @@ public class PayeeACHAccount extends PersistableBusinessObjectBase implements Mu
     private String achTransactionType;
     private String bankAccountTypeCode;
     private boolean active;
+    private boolean autoInactivationIndicator;
+
 
     private ACHBank bankRouting;
     private ACHTransactionType transactionType;
@@ -284,6 +289,14 @@ public class PayeeACHAccount extends PersistableBusinessObjectBase implements Mu
     @Override
     public void setActive(boolean active) {
         this.active = active;
+    }
+
+    public boolean isAutoInactivationIndicator() {
+        return autoInactivationIndicator;
+    }
+
+    public void setAutoInactivationIndicator(boolean autoInactivationIndicator) {
+        this.autoInactivationIndicator = autoInactivationIndicator;
     }
 
     /**
