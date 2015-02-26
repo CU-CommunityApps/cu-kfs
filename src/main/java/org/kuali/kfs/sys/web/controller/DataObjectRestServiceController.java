99f6f8b6e9c924d6fa922cea72c517af044e47a3
Shawn Bower <shawn.bower@gmail.com>
2015-02-26 10:11:37 -0500

    Update DataObjectRestServiceController.java
    Fix to dataobjects service

--- src/main/java/org/kuali/kfs/sys/web/controller/DataObjectRestServiceController.java
+++ src/main/java/org/kuali/kfs/sys/web/controller/DataObjectRestServiceController.java
@@ -247,12 +247,6 @@ public class DataObjectRestServiceController {
     }
 
     protected void validateRequest(FinancialSystemBusinessObjectEntry boe, String namespace, String dataobject, HttpServletRequest request) throws Exception {
-        // check for https (will be ignored in dev mode), authorization
-        if ((!ConfigContext.getCurrentContextConfig().getDevMode() && !request.isSecure())) {
-            LOG.debug("HTTPS check failed.");
-            throw new AccessDeniedException("Not authorized.");
-        }
-
         if (boe == null) {
             LOG.debug("BusinessObjectEntry is null.");
             throw new NoSuchBeanDefinitionException("Data object not found.");
