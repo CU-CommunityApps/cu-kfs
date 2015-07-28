5bbec65fd49d7ad720f8828407cebd8beddd3234
cah292 <cah292@cornell.edu>
2015-07-28 16:34:07 -0400

    KFSPTS-4388: Updated Non-Check Disbursement docs to allow for adding accounting lines with negative amounts, so long as the final total is still positive.

---
+++ src/main/java/edu/cornell/kfs/fp/document/validation/impl/CuNonCheckDisbursementDocumentAmountValidation.java
@@ -0,0 +1,49 @@
+package edu.cornell.kfs.fp.document.validation.impl;
+
+import org.kuali.kfs.fp.document.NonCheckDisbursementDocument;
+import org.kuali.kfs.sys.KFSConstants;
+import org.kuali.kfs.sys.KFSPropertyConstants;
+import org.kuali.kfs.sys.document.AccountingDocument;
+import org.kuali.kfs.sys.document.validation.GenericValidation;
+import org.kuali.kfs.sys.document.validation.event.AttributedDocumentEvent;
+import org.kuali.rice.core.api.util.type.KualiDecimal;
+import org.kuali.rice.krad.util.GlobalVariables;
+
+import edu.cornell.kfs.sys.CUKFSKeyConstants;
+
+/**
+ * Custom variant of DisbursementVoucherDocumentAmountValidation that is intended
+ * for NonCheckDisbursementDocument positive-total-amount validation instead.
+ */
+public class CuNonCheckDisbursementDocumentAmountValidation extends GenericValidation {
+
+    private AccountingDocument accountingDocumentForValidation;
+
+    @Override
+    public boolean validate(AttributedDocumentEvent event) {
+        boolean isValid = true;
+        NonCheckDisbursementDocument nonCheckDisbursementDocument = (NonCheckDisbursementDocument) accountingDocumentForValidation;
+        
+        GlobalVariables.getMessageMap().addToErrorPath(KFSPropertyConstants.DOCUMENT);
+        
+        // Accounting line total cannot be zero or negative.
+        if (KualiDecimal.ZERO.compareTo(nonCheckDisbursementDocument.getSourceTotal()) >= 0) {
+            GlobalVariables.getMessageMap().putErrorWithoutFullErrorPath(
+                    KFSConstants.ACCOUNTING_LINE_ERRORS, CUKFSKeyConstants.ERROR_ZERO_OR_NEGATIVE_ACCOUNTING_TOTAL);
+            isValid = false;
+        }
+        
+        GlobalVariables.getMessageMap().removeFromErrorPath(KFSPropertyConstants.DOCUMENT);
+        
+        return isValid;
+    }
+
+    public AccountingDocument getAccountingDocumentForValidation() {
+        return accountingDocumentForValidation;
+    }
+
+    public void setAccountingDocumentForValidation(AccountingDocument accountingDocumentForValidation) {
+        this.accountingDocumentForValidation = accountingDocumentForValidation;
+    }
+
+}
