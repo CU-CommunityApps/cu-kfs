d002ebc84cc6969829cf6d3686c44db5c00993f5
Kyle Oliveira <kyle.oliveira@cornell.edu>
2015-07-31 12:18:43 -0700

    [KFSPTS-2715] Move reopen trip logic from form to postprocessor

--- src/main/java/edu/cornell/kfs/fp/document/CuDistributionOfIncomeAndExpenseDocument.java
+++ src/main/java/edu/cornell/kfs/fp/document/CuDistributionOfIncomeAndExpenseDocument.java
@@ -1,21 +1,70 @@
 package edu.cornell.kfs.fp.document;
 
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
 import org.kuali.kfs.fp.document.DistributionOfIncomeAndExpenseDocument;
 import org.kuali.kfs.sys.KFSConstants;
+import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.rice.coreservice.framework.parameter.ParameterConstants.COMPONENT;
 import org.kuali.rice.coreservice.framework.parameter.ParameterConstants.NAMESPACE;
+import org.kuali.rice.kew.api.action.ActionTaken;
 import org.kuali.rice.kew.api.exception.WorkflowException;
+import org.kuali.rice.kew.framework.postprocessor.DocumentRouteStatusChange;
 
 import edu.cornell.kfs.fp.document.interfaces.CULegacyTravelIntegrationInterface;
+import edu.cornell.kfs.fp.document.service.CULegacyTravelService;
 
 
 @NAMESPACE(namespace = KFSConstants.CoreModuleNamespaces.FINANCIAL)
 @COMPONENT(component = "DistributionOfIncomeAndExpense")
 public class CuDistributionOfIncomeAndExpenseDocument extends DistributionOfIncomeAndExpenseDocument implements CULegacyTravelIntegrationInterface{
 
-    //TRIP INFORMATION FILEDS
+    private static final long serialVersionUID = 1L;
+
+    //TRIP INFORMATION FIELDS
     protected String tripAssociationStatusCode;
     protected String tripId;
+
+    /**
+     * Overridden to interact with the Legacy Travel service
+     * @see https://jira.cornell.edu/browse/KFSPTS-2715
+     */
+    @Override
+    public void doRouteStatusChange(DocumentRouteStatusChange statusChangeEvent) {
+      // If the DI is Cancelled or Disapproved, we need to reopen the trip in the Legacy Travel service.
+      if (getDocumentHeader().getWorkflowDocument().isCanceled() ||
+          getDocumentHeader().getWorkflowDocument().isDisapproved()) {        
+        boolean tripReOpened = false;
+        boolean isTravelDoc = false;
+        List<ActionTaken> actionsTaken = this.getDocumentHeader().getWorkflowDocument().getActionsTaken();
+        String disapprovalReason = "";
+
+        if(actionsTaken.size() > 0) {
+          String annotation = actionsTaken.get(actionsTaken.size() - 1).getAnnotation();
+          if(StringUtils.isNotEmpty(annotation)) {
+            disapprovalReason = annotation.substring("Disapproval reason - ".length());
+          }
+        }
+
+        try {
+          CULegacyTravelService cuLegacyTravelService = SpringContext.getBean(CULegacyTravelService.class);
+          isTravelDoc = cuLegacyTravelService.isCULegacyTravelIntegrationInterfaceAssociatedWithTrip(this);
+          if(isTravelDoc) {
+            // This means the DI is a Travel DI
+            tripReOpened = cuLegacyTravelService.reopenLegacyTrip(this.getDocumentNumber(), disapprovalReason);
+            LOG.info("Trip successfully reopened : " + tripReOpened);
+          } else {
+            LOG.info("DI is not a travel DI");
+          }
+        } catch (Exception ex) {
+          LOG.error("Exception occurred while trying to cancel a trip.", ex);
+        }
+        
+      }
+
+      super.doRouteStatusChange(statusChangeEvent);
+    }
 
     @Override
     public void toCopy() throws WorkflowException {
@@ -23,7 +72,7 @@ public class CuDistributionOfIncomeAndExpenseDocument extends DistributionOfInco
         setTripAssociationStatusCode(null);
         setTripId(null);
     }
-    
+
     public String getTripAssociationStatusCode() {
         return this.tripAssociationStatusCode;
     }
