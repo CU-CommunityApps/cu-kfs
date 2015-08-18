ee24f4f24f3f047cf3f589f7f00d1d4b61e23949
db483 <db483@cornell.edu>
2015-08-18 17:05:46 +0100

    KFSPTS-3718 Add I Want Doc batch feed batch job

---
+++ src/main/java/edu/cornell/kfs/module/purap/businessobject/BatchIWantNote.java
@@ -0,0 +1,17 @@
+package edu.cornell.kfs.module.purap.businessobject;
+
+public class BatchIWantNote {
+
+    private String noteText;
+
+    public BatchIWantNote() {}
+
+    public String getNoteText() {
+        return noteText;
+    }
+
+    public void setNoteText(String noteText) {
+        this.noteText = noteText;
+    }
+
+}
---
+++ src/main/java/edu/cornell/kfs/module/purap/businessobject/IWantDocumentBatchFeedStatus.java
@@ -0,0 +1,7 @@
+package edu.cornell.kfs.module.purap.businessobject;
+
+import com.rsmart.kuali.kfs.sys.businessobject.BatchFeedStatusBase;
+
+public class IWantDocumentBatchFeedStatus extends BatchFeedStatusBase {
+
+}
