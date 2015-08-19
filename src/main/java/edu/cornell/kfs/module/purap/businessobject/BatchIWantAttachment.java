5540e7dc78729a8c0c711b3cf351614944d4dc5a
db483 <db483@cornell.edu>
2015-08-19 12:06:05 +0100

    KFSPTS-3718 Add I Want Doc batch feed batch job

---
+++ src/main/java/edu/cornell/kfs/module/purap/businessobject/BatchIWantAttachment.java
@@ -0,0 +1,20 @@
+package edu.cornell.kfs.module.purap.businessobject;
+
+import org.kuali.rice.krad.bo.Attachment;
+
+public class BatchIWantAttachment extends Attachment{
+
+    private String attachmentType;
+
+    public BatchIWantAttachment() {}
+
+    public String getAttachmentType() {
+        return attachmentType;
+    }
+
+    public void setAttachmentType(String attachmentType) {
+        this.attachmentType = attachmentType;
+    }
+
+
+}
