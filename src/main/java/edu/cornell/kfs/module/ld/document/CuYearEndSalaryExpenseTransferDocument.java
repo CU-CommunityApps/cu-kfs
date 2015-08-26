98f5efc1301cd47a393914ec1f008fa021f038e6
Kyle Oliveira <kyle.oliveira@cornell.edu>
2015-08-26 13:27:37 -0700

    [KFSPTS-4397] YEST is not returning correct fringe object code on generated fringe charges
    Conflicts:
    	src/main/resources/edu/cornell/kfs/module/ld/document/datadictionary/CuYearEndSalaryExpenseTransferDocument.xml
    
    Simply had to designate which section went where because this file was added by KFSPTS-4358 at the same time.
    
    Conflicts:
    	src/main/resources/edu/cornell/kfs/module/ld/document/datadictionary/CuYearEndSalaryExpenseTransferDocument.xml

---
+++ src/main/java/edu/cornell/kfs/module/ld/document/CuYearEndSalaryExpenseTransferDocument.java
@@ -0,0 +1,48 @@
+/*
+ * The Kuali Financial System, a comprehensive financial management system for higher education.
+ *
+ * Copyright 2005-2015 The Kuali Foundation
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package edu.cornell.kfs.module.ld.document;
+
+import org.kuali.kfs.sys.KFSConstants;
+import org.kuali.kfs.fp.document.YearEndDocument;
+import org.kuali.kfs.sys.document.AccountingDocument;
+import org.kuali.rice.coreservice.framework.parameter.ParameterConstants.COMPONENT;
+import org.kuali.rice.coreservice.framework.parameter.ParameterConstants.NAMESPACE;
+
+/**
+ * Labor Document Class for the Year End Salary Expense Transfer Document for Cornell University.
+ */
+@SuppressWarnings("unchecked")
+@NAMESPACE(namespace = KFSConstants.OptionalModuleNamespaces.LABOR_DISTRIBUTION)
+@COMPONENT(component = "YearEndSalaryExpenseTransfer")
+public class CuYearEndSalaryExpenseTransferDocument extends CuSalaryExpenseTransferDocument implements YearEndDocument {
+
+  private static final long serialVersionUID = 1L;
+
+    /**
+     * Class constructor that invokes <code>SalaryExpenseTransferDocument</code> constructor.
+     */
+    public CuYearEndSalaryExpenseTransferDocument() {
+        super();
+    }
+
+    @Override
+    public Class<? extends AccountingDocument> getDocumentClassForAccountingLineValueAllowedValidation() {
+        return CuSalaryExpenseTransferDocument.class;
+    }
+}
