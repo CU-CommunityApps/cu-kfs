947dd7b2c4b73771097af37ff0d4a5baee4163e7
cah292 <cah292@cornell.edu>
2015-08-26 12:30:24 -0400

    KFSPTS-4485: Updated IWNT and DV conf attachment feature to also perform filename checks as needed, to reduce the likelihood of adding an unflagged confidential attachment.

--- src/main/java/edu/cornell/kfs/fp/document/web/struts/CuDisbursementVoucherAction.java
+++ src/main/java/edu/cornell/kfs/fp/document/web/struts/CuDisbursementVoucherAction.java
@@ -47,8 +47,7 @@ import edu.cornell.kfs.fp.document.service.CULegacyTravelService;
 import edu.cornell.kfs.module.purap.CUPurapKeyConstants;
 import edu.cornell.kfs.module.purap.document.IWantDocument;
 import edu.cornell.kfs.module.purap.document.service.IWantDocumentService;
-import edu.cornell.kfs.sys.CUKFSConstants.ConfidentialAttachmentTypeCodes;
-import edu.cornell.kfs.sys.CUKFSKeyConstants;
+import edu.cornell.kfs.sys.util.ConfidentialAttachmentUtil;
 
 public class CuDisbursementVoucherAction extends DisbursementVoucherAction {
     
@@ -298,13 +297,10 @@ public class CuDisbursementVoucherAction extends DisbursementVoucherAction {
         CuDisbursementVoucherForm dvForm = (CuDisbursementVoucherForm) form;
         Note newNote = dvForm.getNewNote();
         
-        // If not authorized to add "Confidential" attachments, then treat it as a validation error instead of letting the superclass throw an exception.
-        if (newNote.getAttachment() != null
-                && ConfidentialAttachmentTypeCodes.CONFIDENTIAL_ATTACHMENT_TYPE.equals(newNote.getAttachment().getAttachmentTypeCode())
-                && !getDocumentHelperService().getDocumentAuthorizer(dvForm.getDocument()).canAddNoteAttachment(
-                        dvForm.getDocument(), newNote.getAttachment().getAttachmentTypeCode(), GlobalVariables.getUserSession().getPerson())) {
-            GlobalVariables.getMessageMap().putError("newNote.attachment.attachmentTypeCode", CUKFSKeyConstants.ERROR_DOCUMENT_ADD_TYPED_ATTACHMENT,
-                    newNote.getAttachment().getAttachmentTypeCode());
+        // If trying to add a conf attachment without authorization or not properly flagging a potentially-conf attachment, then treat as a validation failure.
+        if (!ConfidentialAttachmentUtil.attachmentIsNonConfidentialOrCanAddConfAttachment(newNote, dvForm.getDocument(), dvForm.getAttachmentFile(),
+                getDocumentHelperService().getDocumentAuthorizer(dvForm.getDocument()))) {
+            // Just return without adding the note/attachment. The ConfidentialAttachmentUtil method will handle updating the message map accordingly.
             return mapping.findForward(RiceConstants.MAPPING_BASIC);
         }
         
