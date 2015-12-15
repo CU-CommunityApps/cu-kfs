afbbaa96e6151053746a2b1d1155a77b872f245f
cah292 <cah292@cornell.edu>
2015-12-15 13:33:06 -0500

    KFSPTS-4229: Added new role type service that allows for limiting Award routing based on doc type and the time between doc creation and the earliest account pay period.

---
+++ src/main/java/edu/cornell/kfs/module/ld/CuLaborParameterConstants.java
@@ -0,0 +1,11 @@
+package edu.cornell.kfs.module.ld;
+
+public class CuLaborParameterConstants {
+
+    public static final String DOC_TYPES_WITH_AWARD_EXCEPTION_ROUTING = "DOC_TYPES_WITH_AWARD_EXCEPTION_ROUTING";
+    public static final String DEFAULT_NUMBER_OF_FISCAL_PERIODS_FOR_AWARD_EXCEPTION_ROUTING = "DEFAULT_NUMBER_OF_FISCAL_PERIODS_FOR_AWARD_EXCEPTION_ROUTING";
+
+    protected CuLaborParameterConstants() {
+        throw new UnsupportedOperationException("do not call");
+    }
+}
