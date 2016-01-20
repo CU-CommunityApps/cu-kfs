7fca9cf765d886a7a4af5920881898f32691024b
cah292 <cah292@cornell.edu>
2016-01-20 17:00:36 -0500

    KFSPTS-4771: Added ability to send warning emails when XML-loaded PDP payments reference inactive vendors.

---
+++ src/main/java/edu/cornell/kfs/pdp/CUPdpParameterConstants.java
@@ -0,0 +1,10 @@
+package edu.cornell.kfs.pdp;
+
+public final class CUPdpParameterConstants {
+
+    public static final String WARNING_INACTIVE_VENDOR_TO_EMAIL_ADDRESSES = "WARNING_INACTIVE_VENDOR_TO_EMAIL_ADDRESSES";
+
+    private CUPdpParameterConstants() {
+        throw new UnsupportedOperationException("Do not call");
+    }
+}
