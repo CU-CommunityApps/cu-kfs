136c8b4653c03c7a466528a5c8893c71dd2eb902
cah292 <cah292@cornell.edu>
2016-02-18 12:55:21 -0500

    KFSPTS-5169: Added principal name to Payee ACH Account document, lookup and inquiry, as well as ACH Payee lookup.

---
+++ src/main/java/edu/cornell/kfs/pdp/businessobject/CuACHPayee.java
@@ -0,0 +1,46 @@
+package edu.cornell.kfs.pdp.businessobject;
+
+import org.kuali.kfs.pdp.businessobject.ACHPayee;
+import org.kuali.rice.kim.api.identity.Person;
+
+/**
+ * Custom subclass of ACHPayee that has an extra netID/principalName property.
+ */
+public class CuACHPayee extends ACHPayee {
+    private static final long serialVersionUID = -6586239257492551426L;
+
+    private String principalName;
+
+    public CuACHPayee() {
+        super();
+    }
+
+    public String getPrincipalName() {
+        return principalName;
+    }
+
+    public void setPrincipalName(String principalName) {
+        this.principalName = principalName;
+    }
+
+    /**
+     * Getter for ACH person that always returns null; it is only intended to aid with
+     * generating a lookup icon for the "principalName" property.
+     * 
+     * @return null.
+     */
+    public Person getAchPerson() {
+        return null;
+    }
+
+    /**
+     * No-op setter for ACH person; it is only intended to aid with
+     * generating a lookup icon for the "principalName" property.
+     * 
+     * @param achPerson The ACH person to set; not actually used.
+     */
+    public void setAchPerson(Person achPerson) {
+        // Do nothing.
+    }
+
+}
