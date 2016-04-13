c2bc95fbbe1bbe9505d4e3afbbde5a6b3bda3e29
Daniela Balmus <db483@cornell.edu>
2016-04-13 17:14:02 +0100

    KFSPTS-4278 Change attachments folder; remove rsmartTypes.xsd reference, always remove .done file

--- src/main/java/com/rsmart/kuali/kfs/fp/batch/service/impl/DisbursementVoucherDocumentBatchServiceImpl.java
+++ src/main/java/com/rsmart/kuali/kfs/fp/batch/service/impl/DisbursementVoucherDocumentBatchServiceImpl.java
@@ -115,6 +115,9 @@ public class DisbursementVoucherDocumentBatchServiceImpl implements Disbursement
                 LOG.error("Caught exception trying to load disbursement voucher file: " + incomingFileName, e);
                 throw new RuntimeException("Caught exception trying to load disbursement voucher file: " + incomingFileName, e);
             }
+            finally{
+            	 batchFeedHelperService.removeDoneFile(incomingFileName);
+            }
         }
     }
 
@@ -195,7 +198,6 @@ public class DisbursementVoucherDocumentBatchServiceImpl implements Disbursement
             MessageMap.putError(KFSConstants.GLOBAL_ERRORS, FPKeyConstants.ERROR_BATCH_DISBURSEMENT_VOUCHER_ERRORS_NOTIFICATION);
         }
 
-        batchFeedHelperService.removeDoneFile(incomingFileName);
     }
 
     /**
