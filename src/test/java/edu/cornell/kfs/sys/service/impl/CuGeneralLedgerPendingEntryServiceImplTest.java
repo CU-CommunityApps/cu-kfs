9e5213a7d7ed474d34148efa415619b331792132
Daniela Balmus <db483@cornell.edu>
2016-05-13 16:14:42 +0100

    Revert "KFSPTS-5545 refactor code to inject services, updated unit test"
    This reverts commit bc826ab54d4dd51998b618d28b194119739634c2.

--- src/test/java/edu/cornell/kfs/sys/service/impl/CuGeneralLedgerPendingEntryServiceImplTest.java
+++ src/test/java/edu/cornell/kfs/sys/service/impl/CuGeneralLedgerPendingEntryServiceImplTest.java
@@ -2,39 +2,26 @@ package edu.cornell.kfs.sys.service.impl;
 
 import static org.kuali.kfs.sys.fixture.UserNameFixture.ccs1;
 
-import java.sql.Date;
-import java.util.Collection;
-
-import junit.framework.TestCase;
-
 import org.apache.commons.lang.StringUtils;
-import org.kuali.kfs.coa.businessobject.AccountingPeriod;
-import org.kuali.kfs.coa.service.AccountingPeriodService;
 import org.kuali.kfs.sys.ConfigureContext;
 import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.kfs.sys.businessobject.GeneralLedgerPendingEntry;
 import org.kuali.kfs.sys.businessobject.UniversityDate;
+import org.kuali.kfs.sys.context.KualiTestBase;
+import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.kfs.sys.service.GeneralLedgerPendingEntryService;
 import org.kuali.kfs.sys.service.UniversityDateService;
 
 @ConfigureContext(session = ccs1)
-public class CuGeneralLedgerPendingEntryServiceImplTest extends TestCase {
-	private AccountingPeriodService accountingPeriodService;
-    private CuGeneralLedgerPendingEntryServiceImpl cuGeneralLedgerPendingEntryService;
-    private UniversityDateService universityDateService;  
-    private UniversityDate ud;
-    
+public class CuGeneralLedgerPendingEntryServiceImplTest extends KualiTestBase {
+    private GeneralLedgerPendingEntryService cuGeneralLedgerPendingEntryService;
+    UniversityDate ud;
     @Override
     protected void setUp() throws Exception {
+        // TODO Auto-generated method stub
         super.setUp();
-        accountingPeriodService = new MockAccountingPeriodService();
-        cuGeneralLedgerPendingEntryService = new CuGeneralLedgerPendingEntryServiceImpl();
-        cuGeneralLedgerPendingEntryService.setAccountingPeriodService(accountingPeriodService);
-        universityDateService = new MockUniversityDateService();   
-        cuGeneralLedgerPendingEntryService.setUniversityDateService(universityDateService);
-        
-        ud = new UniversityDate();
-        ud.setUniversityFiscalYear(2016);
-        ud.setUniversityFiscalAccountingPeriod("5");
+        cuGeneralLedgerPendingEntryService = SpringContext.getBean(GeneralLedgerPendingEntryService.class);
+        ud = SpringContext.getBean(UniversityDateService.class).getCurrentUniversityDate();
     }
 
     /*
@@ -70,77 +57,5 @@ public class CuGeneralLedgerPendingEntryServiceImplTest extends TestCase {
         assertTrue("should not fill in fiscal period code", StringUtils.equals("01",glpe.getUniversityFiscalPeriodCode()));            
     }
 
-    private class MockAccountingPeriodService implements AccountingPeriodService {
-
-        @Override
-        public Collection<AccountingPeriod> getAllAccountingPeriods() {
-            return null;
-        }
-
-        @Override
-        public Collection<AccountingPeriod> getOpenAccountingPeriods() {
-            return null;
-        }
-
-        @Override
-        public AccountingPeriod getByPeriod(String s, Integer integer) {
-        	AccountingPeriod accountingPeriod = new AccountingPeriod();
-        	accountingPeriod.setUniversityFiscalPeriodCode(s);
-        	accountingPeriod.setUniversityFiscalYear(integer);
-            return accountingPeriod;
-        }
-
-        @Override
-        public AccountingPeriod getByStringDate(String s) {
-            return null;
-        }
-
-        @Override
-        public AccountingPeriod getByDate(Date date) {
-            return new AccountingPeriod();
-        }
-
-        @Override
-        public int compareAccountingPeriodsByDate(AccountingPeriod accountingPeriod, AccountingPeriod accountingPeriod1) {
-            return 0;
-        }
-
-        @Override
-        public void clearCache() {
-
-        }
-    }
-    
-    private class MockUniversityDateService implements UniversityDateService{
-
-		@Override
-		public UniversityDate getCurrentUniversityDate() {
-			UniversityDate universityDate = new UniversityDate();
-			universityDate.setUniversityFiscalYear(2016);
-			universityDate.setUniversityFiscalAccountingPeriod("5");
-			return universityDate;
-		}
-
-		@Override
-		public Integer getFiscalYear(java.util.Date date) {
-			return null;
-		}
-
-		@Override
-		public java.util.Date getFirstDateOfFiscalYear(Integer fiscalYear) {
-			return null;
-		}
-
-		@Override
-		public java.util.Date getLastDateOfFiscalYear(Integer fiscalYear) {
-			return null;
-		}
-
-		@Override
-		public Integer getCurrentFiscalYear() {
-			return null;
-		}
-    	
-    }
     
 }
