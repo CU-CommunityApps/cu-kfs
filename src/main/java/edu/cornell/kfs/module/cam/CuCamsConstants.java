f252507fde8f186839d21897e4ab5c68f06b5609
cah292 <cah292@cornell.edu>
2016-05-20 16:05:44 -0400

    KFSPTS-5727: Added ability to control whether an Asset Transfer GLPE entry should have its sub-account forcibly cleared out, based on its account number and a new parameter.

--- src/main/java/edu/cornell/kfs/module/cam/CuCamsConstants.java
+++ src/main/java/edu/cornell/kfs/module/cam/CuCamsConstants.java
@@ -3,5 +3,6 @@ package edu.cornell.kfs.module.cam;
 public class CuCamsConstants {
     public static class Parameters {
         public static final String RE_USE_RETIRED_ASSET_TAG_NUMBER = "RE_USE_RETIRED_ASSET_TAG_NUMBER";
+        public static final String ASSET_TRANSFER_PLANT_ACCOUNTS_FOR_SUB_ACCOUNTS = "ASSET_TRANSFER_PLANT_ACCOUNTS_FOR_SUB_ACCOUNTS";
     }
 }
