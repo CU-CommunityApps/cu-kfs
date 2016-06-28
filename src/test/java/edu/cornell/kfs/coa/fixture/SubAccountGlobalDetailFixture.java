c255732a6714cdb6f725569e948dc722450d2b97
Daniela Balmus <db483@cornell.edu>
2016-06-28 09:13:03 +0100

    KFSPTS-3599 Add new fields to Account Global document.

---
+++ src/test/java/edu/cornell/kfs/coa/fixture/SubAccountGlobalDetailFixture.java
@@ -0,0 +1,55 @@
+package edu.cornell.kfs.coa.fixture;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.kuali.kfs.coa.businessobject.A21IndirectCostRecoveryAccount;
+import org.kuali.kfs.coa.businessobject.A21SubAccount;
+import org.kuali.kfs.coa.businessobject.SubAccount;
+
+import edu.cornell.kfs.coa.businessobject.SubAccountGlobalDetail;
+
+public enum SubAccountGlobalDetailFixture {
+
+	SUB_ACCOUNT_GLOBAL_DETAIL_1111111_2222222_98_2(
+			A21IndirectCostRecoveryAccountFixture.A21_ICR_1111111_98_PERCENT_ACTIVE,
+			A21IndirectCostRecoveryAccountFixture.A21_ICR_2222222_2_PERCENT_ACTIVE),
+
+	SUB_ACCOUNT_GLOBAL_DETAIL_1111111_98_INACTIVE_98_INACTIVE_3333333_100_PERCENT_ACTIVE(
+			A21IndirectCostRecoveryAccountFixture.A21_ICR_1111111_98_PERCENT_INACTIVE,
+			A21IndirectCostRecoveryAccountFixture.A21_ICR_1111111_98_PERCENT_INACTIVE,
+			A21IndirectCostRecoveryAccountFixture.A21_ICR_3333333_100_PERCENT_ACTIVE),
+
+	SUB_ACCOUNT_GLOBAL_DETAIL_3333333_100(
+			A21IndirectCostRecoveryAccountFixture.A21_ICR_3333333_100_PERCENT_ACTIVE),
+
+	SUB_ACCOUNT_GLOBAL_DETAIL_3333333_100_INACTIVATE(
+			A21IndirectCostRecoveryAccountFixture.A21_ICR_3333333_100_PERCENT_ACTIVE,
+			A21IndirectCostRecoveryAccountFixture.A21_ICR_1111111_98_PERCENT_INACTIVE,
+			A21IndirectCostRecoveryAccountFixture.A21_ICR_2222222_2_PERCENT_INACTIVE);
+
+	public final List<A21IndirectCostRecoveryAccountFixture> icrAccounts;
+
+	private SubAccountGlobalDetailFixture(A21IndirectCostRecoveryAccountFixture... icrAccounts) {
+		this.icrAccounts = Collections.unmodifiableList(Arrays.asList(icrAccounts));
+	}
+
+	public SubAccountGlobalDetail getSubAccountGlobalDetail() {
+		A21SubAccount a21SubAccount = new A21SubAccount();
+		SubAccount subAccount = new SubAccount();
+		SubAccountGlobalDetail subAccountGlobalDetail = new SubAccountGlobalDetail();
+		List<A21IndirectCostRecoveryAccount> icrAccountsList = new ArrayList<A21IndirectCostRecoveryAccount>();
+		
+		for(A21IndirectCostRecoveryAccountFixture icrAccount : icrAccounts){
+			icrAccountsList.add(icrAccount.getA21IndirectCostRecoveryAccountChange());
+		}
+
+		a21SubAccount.setA21IndirectCostRecoveryAccounts(icrAccountsList);
+		subAccount.setA21SubAccount(a21SubAccount);
+		subAccountGlobalDetail.setSubAccount(subAccount);
+		return subAccountGlobalDetail;
+	}
+
+}
---
+++ src/test/java/edu/cornell/kfs/coa/fixture/SubAccountGlobalFixture.java
@@ -0,0 +1,48 @@
+package edu.cornell.kfs.coa.fixture;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import edu.cornell.kfs.coa.businessobject.GlobalObjectWithIndirectCostRecoveryAccounts;
+import edu.cornell.kfs.coa.businessobject.IndirectCostRecoveryAccountChange;
+import edu.cornell.kfs.coa.businessobject.SubAccountGlobal;
+
+public enum SubAccountGlobalFixture {
+
+	SUB_ACCT_GLOBAL_1111111_2222222_98_2(
+			IndirectCostRecoveryAccountChangeFixture.ICR_CHANGE_1111111_98_PERCENT_ACTIVE,
+			IndirectCostRecoveryAccountChangeFixture.ICR_CHANGE_2222222_2_PERCENT_ACTIVE),
+
+	SUB_ACCT_GLOBAL_1111111_2222222_3333333_98_2_ACTIVE_100_INACTIVE(
+			IndirectCostRecoveryAccountChangeFixture.ICR_CHANGE_1111111_98_PERCENT_ACTIVE,
+			IndirectCostRecoveryAccountChangeFixture.ICR_CHANGE_2222222_2_PERCENT_ACTIVE,
+			IndirectCostRecoveryAccountChangeFixture.ICR_CHANGE_3333333_100_PERCENT_INACTIVE),
+
+	SUB_ACCT_GLOBAL_3333333_100_ACTIVE_1111111_2222222_98_2_INACTIVE(
+			IndirectCostRecoveryAccountChangeFixture.ICR_CHANGE_1111111_98_PERCENT_INACTIVE,
+			IndirectCostRecoveryAccountChangeFixture.ICR_CHANGE_2222222_2_PERCENT_INACTIVE,
+			IndirectCostRecoveryAccountChangeFixture.ICR_CHANGE_3333333_100_PERCENT_ACTIVE),
+
+	SUB_ACCT_GLOBAL_3333333_100(
+			IndirectCostRecoveryAccountChangeFixture.ICR_CHANGE_3333333_100_PERCENT_ACTIVE);
+
+	public final List<IndirectCostRecoveryAccountChangeFixture> icrChanges;
+
+	private SubAccountGlobalFixture(IndirectCostRecoveryAccountChangeFixture... icrChanges) {
+		this.icrChanges = Collections.unmodifiableList(Arrays.asList(icrChanges));
+	}
+
+	public GlobalObjectWithIndirectCostRecoveryAccounts getSubAccountGlobal() {
+		GlobalObjectWithIndirectCostRecoveryAccounts subAccountGlobal = new SubAccountGlobal();
+		List<IndirectCostRecoveryAccountChange> icrAccountsList = new ArrayList<IndirectCostRecoveryAccountChange>();
+
+		for(IndirectCostRecoveryAccountChangeFixture icrChange : icrChanges){
+			icrAccountsList.add(icrChange.getIndirectCostRecoveryAccountChange());
+		}
+		
+		subAccountGlobal.setIndirectCostRecoveryAccounts(icrAccountsList);
+		return subAccountGlobal;
+	}
+}
