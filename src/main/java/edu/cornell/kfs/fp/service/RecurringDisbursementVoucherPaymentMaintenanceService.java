b366199f3fe9c44d4d7f9a331068879362aa3c40
Daniela Balmus <db483@cornell.edu>
2016-08-19 12:56:27 +0100

    KFSPTS-6324 Implement cancel of spawned DVs from recurring DV

---
+++ src/main/java/edu/cornell/kfs/fp/service/RecurringDisbursementVoucherPaymentMaintenanceService.java
@@ -0,0 +1,26 @@
+package edu.cornell.kfs.fp.service;
+
+import org.kuali.kfs.pdp.service.PaymentMaintenanceService;
+import org.kuali.rice.kim.api.identity.Person;
+
+public interface RecurringDisbursementVoucherPaymentMaintenanceService {
+    
+    /**
+     * Determines if the passed in user has the authority to cancel payments from a recurring DV.
+     * @param user
+     * @return
+     */
+    boolean hasCancelPermission(Person user);
+    
+    /**
+     * This method cancels the pending payment of the given payment id if the following rules apply. -
+     * Payment status must be: "open", "held", or "pending/ACH".
+     * @param paymentGroupId Primary key of the PaymentGroup that the Payment Detail to be canceled belongs to.
+     * @param paymentDetailId Primary key of the PaymentDetail that was actually canceled.
+     * @param note Change note text entered by user.
+     * @param user The user that cancels the payment
+     * @return true if cancel payment succesful, false otherwise
+     */
+    public boolean cancelPendingPayment(Integer paymentGroupId, Integer paymentDetailId, String note, Person user);
+
+}
