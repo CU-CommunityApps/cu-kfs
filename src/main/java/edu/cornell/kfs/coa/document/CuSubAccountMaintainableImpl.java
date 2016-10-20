a6212a15fadd5460ec500015f3b35aef5726f155
Bryan Hutchinson <bh79@cornell.edu>
2016-10-20 09:32:05 -0400

    KFSPTS-6622: cleanup differences between develop and master branches

--- src/main/java/edu/cornell/kfs/coa/document/CuSubAccountMaintainableImpl.java
+++ src/main/java/edu/cornell/kfs/coa/document/CuSubAccountMaintainableImpl.java
@@ -375,21 +375,21 @@ public class CuSubAccountMaintainableImpl extends SubAccountMaintainableImpl {
         subAccount.getA21SubAccount().setA21IndirectCostRecoveryAccounts(copyIndirectCostRecoveryAccounts);
     }
     
-        public void refresh(String refreshCaller, Map fieldValues, org.kuali.kfs.kns.document.MaintenanceDocument document) {
+    public void refresh(String refreshCaller, Map fieldValues, org.kuali.kfs.kns.document.MaintenanceDocument document) {
     	super.refresh(refreshCaller, fieldValues, document);
-    	 String maintAction = super.getMaintenanceAction();   
-    	 
-    	 if (maintAction.equalsIgnoreCase(KRADConstants.MAINTENANCE_NEW_ACTION)){
-    		 SubAccount subAccount = (SubAccount) super.getBusinessObject();
-    		 A21SubAccount newSubAccount = subAccount.getA21SubAccount();
-    		 if(ObjectUtils.isNotNull(newSubAccount) && ObjectUtils.isNotNull(newSubAccount.getA21IndirectCostRecoveryAccounts()) && newSubAccount.getA21IndirectCostRecoveryAccounts().size() >0){
-    			 for(A21IndirectCostRecoveryAccount recoveryAccount : newSubAccount.getA21IndirectCostRecoveryAccounts()){
-    				 // set the newCollectionRecord indicator to true so that the entries can be deleted
-    				 recoveryAccount.setNewCollectionRecord(true);
-    			 }
-    		 }
+    	String maintAction = super.getMaintenanceAction();
+
+    	if (maintAction.equalsIgnoreCase(KRADConstants.MAINTENANCE_NEW_ACTION)){
+    	    SubAccount subAccount = (SubAccount) super.getBusinessObject();
+    		A21SubAccount newSubAccount = subAccount.getA21SubAccount();
+    		if(ObjectUtils.isNotNull(newSubAccount) && ObjectUtils.isNotNull(newSubAccount.getA21IndirectCostRecoveryAccounts()) && newSubAccount.getA21IndirectCostRecoveryAccounts().size() >0){
+    		    for(A21IndirectCostRecoveryAccount recoveryAccount : newSubAccount.getA21IndirectCostRecoveryAccounts()){
+    			    // set the newCollectionRecord indicator to true so that the entries can be deleted
+    				recoveryAccount.setNewCollectionRecord(true);
+    			}
+    		}
     		 
-    	 }
+    	}
     }
     
     /**
