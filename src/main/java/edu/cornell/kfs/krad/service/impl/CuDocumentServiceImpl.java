79c2d9dbe38466b9432867f72dfe97a85116e2c2
Bryan Hutchinson <bh79@cornell.edu>
2016-12-05 16:33:58 -0500

    KFSPTS-6707: remediate KFS maintenance XML conversion feature

---
+++ src/main/java/edu/cornell/kfs/krad/service/impl/CuDocumentServiceImpl.java
@@ -0,0 +1,74 @@
+package edu.cornell.kfs.krad.service.impl;
+
+import com.thoughtworks.xstream.core.BaseException;
+import org.kuali.rice.core.framework.persistence.jta.TransactionalNoValidationExceptionRollback;
+import org.kuali.rice.kew.api.WorkflowDocument;
+import org.kuali.kfs.krad.document.Document;
+import org.kuali.kfs.krad.maintenance.MaintenanceDocument;
+import org.kuali.kfs.krad.service.MaintainableXMLConversionService;
+import org.kuali.kfs.krad.service.impl.DocumentServiceImpl;
+
+/**
+ * Custom DocumentServiceImpl subclass that performs its own handling
+ * of just-in-time legacy maintenance XML conversion.
+ *
+ * Maintenance documents ordinarily convert the old/new maintenance XML
+ * sections separately if they need it, and are not capable of converting
+ * legacy BO notes XML. To support better conversion of such content,
+ * the default MaintainableXMLConversionService has been switched over
+ * to a no-op implementation, and instead this class will perform
+ * the XML conversion on the entire maintenance XML payload if necessary.
+ * This allows for adding the features without overlaying Rice code.
+ *
+ * This KFS version is based on its Cynergy counterpart:
+ * edu.cornell.cynergy.krad.service.impl.CynergyDocumentServiceImpl
+ */
+@TransactionalNoValidationExceptionRollback
+public class CuDocumentServiceImpl extends DocumentServiceImpl {
+
+    protected MaintainableXMLConversionService maintainableXMLConversionService;
+
+    /**
+     * Overridden to perform just-in-time legacy maintenance XML conversion if necessary.
+     *
+     * @param documentHeaderId
+     * @param workflowDocument
+     * @param document
+     * @return
+     */
+    protected Document postProcessDocument(String documentHeaderId, WorkflowDocument workflowDocument, Document document) {
+        try {
+            return super.postProcessDocument(documentHeaderId, workflowDocument, document);
+        } catch (RuntimeException e) {
+            if (shouldConvertMaintenanceXML(e, document)) {
+                return postProcessDocumentWithLegacyMaintenanceXML(documentHeaderId, workflowDocument, (MaintenanceDocument) document);
+            } else {
+                throw e;
+            }
+        }
+    }
+
+    protected Document postProcessDocumentWithLegacyMaintenanceXML(String documentHeaderId, WorkflowDocument workflowDocument, MaintenanceDocument document) {
+        String oldXml = document.getXmlDocumentContents();
+        String newXml = maintainableXMLConversionService.transformMaintainableXML(oldXml);
+
+        try {
+            document.setXmlDocumentContents(newXml);
+            return super.postProcessDocument(documentHeaderId, workflowDocument, document);
+        } catch (RuntimeException e) {
+            document.setXmlDocumentContents(oldXml);
+            throw e;
+        }
+    }
+
+    protected boolean shouldConvertMaintenanceXML(RuntimeException e, Document document) {
+        return document != null
+                && document instanceof MaintenanceDocument
+                && BaseException.class.isAssignableFrom(e.getClass());
+    }
+
+    public void setMaintainableXMLConversionService(MaintainableXMLConversionService maintainableXMLConversionService) {
+        this.maintainableXMLConversionService = maintainableXMLConversionService;
+    }
+
+}
---
+++ src/main/java/edu/cornell/kfs/krad/service/impl/NoOpMaintainableXMLConversionServiceImpl.java
@@ -0,0 +1,20 @@
+package edu.cornell.kfs.krad.service.impl;
+
+import org.kuali.kfs.krad.service.MaintainableXMLConversionService;
+
+public class NoOpMaintainableXMLConversionServiceImpl implements MaintainableXMLConversionService {
+
+    /**
+     * This implementation just returns the same XML that was passed in.
+     *
+     * @see org.kuali.kfs.krad.service.MaintainableXMLConversionService#transformMaintainableXML(java.lang.String)
+     *
+     * @param xml
+     * @return
+     */
+    @Override
+    public String transformMaintainableXML(String xml) {
+        return xml;
+    }
+
+}
