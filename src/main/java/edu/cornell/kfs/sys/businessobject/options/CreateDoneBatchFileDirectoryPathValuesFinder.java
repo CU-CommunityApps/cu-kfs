74913cc6574032c0d8305268f5135d2d7345c46e
Bryan Hutchinson <bh79@cornell.edu>
2017-01-12 15:46:52 -0500

    KFSPTS-7156: apply caching to create done batch file lookup

--- src/main/java/edu/cornell/kfs/sys/businessobject/options/CreateDoneBatchFileDirectoryPathValuesFinder.java
+++ src/main/java/edu/cornell/kfs/sys/businessobject/options/CreateDoneBatchFileDirectoryPathValuesFinder.java
@@ -1,12 +1,11 @@
 package edu.cornell.kfs.sys.businessobject.options;
 
 import edu.cornell.kfs.sys.batch.CuBatchFileUtils;
-import edu.cornell.kfs.sys.util.SubDirectoryWalker;
+import edu.cornell.kfs.sys.service.BatchFileDirectoryService;
+import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.rice.core.api.util.KeyValue;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class CreateDoneBatchFileDirectoryPathValuesFinder extends CuBatchFileDirectoryPathValuesFinder {
@@ -14,19 +13,7 @@ public class CreateDoneBatchFileDirectoryPathValuesFinder extends CuBatchFileDir
 	@Override
 	public List<KeyValue> getKeyValues() {
 		List<File> rootDirectories = CuBatchFileUtils.retrieveBatchFileStagingRootDirectories();
-        List<KeyValue> keyValues = new ArrayList<KeyValue>();
-        
-        for (File rootDirectory: rootDirectories) {
-            SubDirectoryWalker walker = new SubDirectoryWalker(keyValues);
-            try {
-                walker.addKeyValues(rootDirectory);
-            }
-            catch (IOException e) {
-                throw new RuntimeException("IOException caught.", e);
-            }
-        }
-        
-        return keyValues;
+        return SpringContext.getBean(BatchFileDirectoryService.class).buildDirectoryKeyValuesList(rootDirectories);
 	}
 
 }
