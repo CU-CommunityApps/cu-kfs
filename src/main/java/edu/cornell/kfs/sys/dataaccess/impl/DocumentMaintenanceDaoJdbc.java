f4bab7b9106f060946deda51ac9c07c073dedd53
Bryan Hutchinson <bh79@cornell.edu>
2017-02-06 14:51:20 -0500

    KFSPTS-7537: fix failing documentRequeuerJob batch job (#291)

---
+++ src/main/java/edu/cornell/kfs/sys/dataaccess/impl/DocumentMaintenanceDaoJdbc.java
@@ -0,0 +1,176 @@
+package edu.cornell.kfs.sys.dataaccess.impl;
+
+import edu.cornell.kfs.sys.CUKFSParameterKeyConstants;
+import edu.cornell.kfs.sys.batch.DocumentRequeueStep;
+import edu.cornell.kfs.sys.dataaccess.DocumentMaintenanceDao;
+import org.kuali.kfs.coreservice.framework.parameter.ParameterService;
+import org.kuali.kfs.sys.KFSConstants;
+import org.kuali.rice.core.framework.persistence.jdbc.dao.PlatformAwareDaoBaseJdbc;
+import org.kuali.rice.kew.actionrequest.ActionRequestValue;
+import org.kuali.rice.kew.api.KEWPropertyConstants;
+import org.kuali.rice.kew.api.KewApiConstants;
+import org.kuali.rice.kew.routeheader.DocumentRouteHeaderValue;
+import org.springframework.jdbc.core.ConnectionCallback;
+import org.springframework.util.StringUtils;
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+import java.lang.annotation.Annotation;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+public class DocumentMaintenanceDaoJdbc extends PlatformAwareDaoBaseJdbc implements DocumentMaintenanceDao {
+	private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(DocumentMaintenanceDaoJdbc.class);
+
+	public static final String WORKFLOW_DOCUMENT_HEADER_ID_SEARCH_RESULT_KEY = "documentId";
+
+    private ParameterService parameterService;
+
+	/**
+	 * @see DocumentMaintenanceDao#getDocumentRequeueValues()#getDocumentRequeueFileValues()
+	 */
+	@Override
+	public Collection<String> getDocumentRequeueValues() {
+		return getJdbcTemplate().execute((ConnectionCallback<Collection<String>>) con -> {
+            PreparedStatement selectStatement = null;
+            ResultSet queryResultSet = null;
+            List<String> documentIdsToRequeue = new ArrayList<>();
+
+            try {
+                final Collection<String> docTypeIds = parameterService.getParameterValuesAsString(DocumentRequeueStep.class, CUKFSParameterKeyConstants.NON_REQUEUABLE_DOCUMENT_TYPES);
+                final Collection<String> roleIds = parameterService.getParameterValuesAsString(DocumentRequeueStep.class, CUKFSParameterKeyConstants.REQUEUABLE_ROLES);
+
+                String selectStatementSql = buildRequeueSqlQuery(docTypeIds.size(), roleIds.size());
+                selectStatement = con.prepareStatement(selectStatementSql);
+
+                int index = 1;
+                for (String docTypeId: docTypeIds) {
+                    selectStatement.setString(index++, docTypeId);
+                }
+
+                for (String roleId: roleIds) {
+                    selectStatement.setString(index++, roleId);
+                }
+
+                queryResultSet = selectStatement.executeQuery();
+
+                while (queryResultSet.next()) {
+                    String documentId = queryResultSet.getString(1);
+                    documentIdsToRequeue.add(documentId);
+                }
+
+                queryResultSet.close();
+            } finally {
+                if (queryResultSet != null) {
+                    try {
+                        queryResultSet.close();
+                    } catch (SQLException e) {
+                        LOG.error("getDocumentRequeueValues: Could not close ResultSet");
+                    }
+                }
+                if (selectStatement != null) {
+                    try {
+                        selectStatement.close();
+                    } catch (SQLException e) {
+                        LOG.error("getDocumentRequeueValues: Could not close selection PreparedStatement");
+                    }
+                }
+            }
+            return documentIdsToRequeue;
+        });
+	}
+
+	private String buildRequeueSqlQuery(int docTypeIdCount, int roleIdCount) {
+		StringBuilder sql = new StringBuilder();
+
+		sql.append("select DOC_HDR_ID from CYNERGY.KREW_DOC_HDR_T where ");
+		sql.append(retrieveColumnNameFromAnnotations(DocumentRouteHeaderValue.class, "docRouteStatus"));
+		sql.append("='");
+		sql.append(KewApiConstants.ROUTE_HEADER_ENROUTE_CD);
+		sql.append("' AND ");
+		sql.append(retrieveColumnNameFromAnnotations(DocumentRouteHeaderValue.class, KEWPropertyConstants.DOCUMENT_TYPE_ID));
+		sql.append(" NOT IN (?");
+
+		for (int i=1; i<docTypeIdCount; i++) {
+			sql.append(",?");
+		}
+
+		sql.append(") AND ");
+
+		final String workflowDocumentHeaderColumnName = retrieveColumnNameFromAnnotations(DocumentRouteHeaderValue.class, WORKFLOW_DOCUMENT_HEADER_ID_SEARCH_RESULT_KEY);
+		sql.append(workflowDocumentHeaderColumnName);
+		sql.append(" IN (select distinct(");
+		sql.append(retrieveColumnNameFromAnnotations(ActionRequestValue.class, WORKFLOW_DOCUMENT_HEADER_ID_SEARCH_RESULT_KEY));
+		sql.append(") from CYNERGY.");
+		sql.append(retrieveTableNameFromAnnotations(ActionRequestValue.class));
+		sql.append(" where ");
+		sql.append("RSP_ID");
+		sql.append(" in (select ");
+		sql.append("RSP_ID");
+		sql.append(" from ");
+		sql.append("CYNERGY.KRIM_ROLE_RSP_T");
+		sql.append(" where ");
+		sql.append("ROLE_ID");
+		sql.append(" in (?");
+
+		for (int i=1; i<roleIdCount; i++) {
+			sql.append(",?");
+		}
+
+		sql.append("))) order by ");
+		sql.append(workflowDocumentHeaderColumnName);
+		sql.append(" ASC");
+
+		return sql.toString();
+	}
+	
+	private String retrieveColumnNameFromAnnotations(Class className, String fieldName) {
+		String columnName = KFSConstants.EMPTY_STRING;
+
+		try {
+			Annotation[] annotations = className.getDeclaredField(fieldName).getAnnotations();
+			for(Annotation annotation: annotations) {
+				if(annotation instanceof Column) {
+					columnName = ((Column) annotation).name();
+					break;
+				}
+			}
+		} catch (NoSuchFieldException e) {
+			LOG.warn("retrieveColumnNameFromAnnotations - caught Exception when trying to determine columnName", e);
+		}
+		
+		return columnName;
+	}
+	
+	private String retrieveTableNameFromAnnotations(Class className) {
+		String tableName = KFSConstants.EMPTY_STRING;
+
+		Annotation[] annotations = className.getDeclaredAnnotations();
+		for (Annotation annotation: annotations) {
+			if (annotation instanceof Table) {
+				tableName = ((Table) annotation).name();
+				break;
+			}
+		}
+
+		return tableName;
+	}
+	
+	public ParameterService getParameterService() {
+		return parameterService;
+	}
+
+	public void setParameterService(ParameterService parameterService) {
+		this.parameterService = parameterService;
+	}
+	
+}
--- src/main/java/edu/cornell/kfs/sys/service/impl/DocumentMaintenanceServiceImpl.java
+++ src/main/java/edu/cornell/kfs/sys/service/impl/DocumentMaintenanceServiceImpl.java
@@ -1,88 +1,42 @@
-/**
- * 
- */
 package edu.cornell.kfs.sys.service.impl;
-
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-
-import org.kuali.kfs.sys.context.SpringContext;
-import org.kuali.rice.core.api.config.CoreConfigHelper;
-import org.kuali.rice.kew.api.KewApiServiceLocator;
-import org.kuali.rice.kew.api.document.DocumentRefreshQueue;
-import org.kuali.rice.kew.api.document.attribute.DocumentAttributeIndexingQueue;
-import org.springframework.transaction.annotation.Transactional;
 
 import edu.cornell.kfs.sys.dataaccess.DocumentMaintenanceDao;
 import edu.cornell.kfs.sys.service.DocumentMaintenanceService;
+import org.kuali.rice.core.api.config.CoreConfigHelper;
+import org.kuali.rice.kew.api.KewApiServiceLocator;
+import org.kuali.rice.kew.api.document.DocumentRefreshQueue;
+import org.springframework.transaction.annotation.Transactional;
 
-/**
- * @author Admin-dwf5
- *
- */
+import java.util.Collection;
+import java.util.Iterator;
+
 public class DocumentMaintenanceServiceImpl implements DocumentMaintenanceService {
-		private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(DocumentMaintenanceServiceImpl.class);
+	private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(DocumentMaintenanceServiceImpl.class);
 
-		private DocumentMaintenanceDao documentMaintenanceDao;
+	public static final int WAIT_TIME_NO_WAIT = 0;
 
-		/**
-		 * 
-		 */
-		@Transactional
-		public boolean requeueDocuments() {
-		    boolean result = true;
-			Collection<String> docIds = documentMaintenanceDao.getDocumentRequeueValues();
-		    LOG.info("Total number of documents flagged for requeuing: "+docIds.size());
-		    
-			for (Iterator<String> it = docIds.iterator(); it.hasNext(); ) {
-				Long id = new Long(it.next());
-                DocumentRefreshQueue documentRequeuer = KewApiServiceLocator.getDocumentRequeuerService(CoreConfigHelper.getApplicationId(), id.toString(), 0 /*no wait*/);
-                documentRequeuer.refreshDocument(id.toString());
-			}
-			
-			return result;
+	private DocumentMaintenanceDao documentMaintenanceDao;
+
+	@Transactional
+	public boolean requeueDocuments() {
+		Collection<String> docIds = documentMaintenanceDao.getDocumentRequeueValues();
+		LOG.info("Total number of documents flagged for requeuing: " + docIds.size());
+
+		for (String docId: docIds) {
+			DocumentRefreshQueue documentRequeuer = KewApiServiceLocator.getDocumentRequeuerService(CoreConfigHelper.getApplicationId(), docId, WAIT_TIME_NO_WAIT);
+			documentRequeuer.refreshDocument(docId);
 		}
 
-		/**
-		 * 
-		 */
-		@Transactional
-		public boolean reindexDocuments() {
-			boolean result = true;
-			final DocumentAttributeIndexingQueue queue = KewApiServiceLocator.getDocumentAttributeIndexingQueue();
+		return true;
+	}
 
-		    List<String> docIds = new ArrayList<String>();
+	public DocumentMaintenanceDao getDocumentMaintenanceDao() {
+			return documentMaintenanceDao;
+	}
 
-		    docIds = documentMaintenanceDao.getDocumentReindexValues();
-		    LOG.info("Total number of documents flagged for reindexing: "+docIds.size());
-		    
-			for (Iterator<String> it = docIds.iterator(); it.hasNext(); ) {
-				Long id = new Long(it.next());
-				queue.indexDocument(id.toString());
-			}
-			
-			return result;
-		}
-		
-		/**
-		 * @return the documentMaintenanceDao
-		 */
-		public DocumentMaintenanceDao getDocumentMaintenanceDao() {
-				return documentMaintenanceDao;
-		}
-
-		/**
-		 * @param documentMaintenanceDao the documentMaintenanceDao to set
-		 */
-		public void setDocumentMaintenanceDao(DocumentMaintenanceDao documentMaintenanceDao) {
-				this.documentMaintenanceDao = documentMaintenanceDao;
-		}
+	public void setDocumentMaintenanceDao(DocumentMaintenanceDao documentMaintenanceDao) {
+			this.documentMaintenanceDao = documentMaintenanceDao;
+	}
 
 }
 	
