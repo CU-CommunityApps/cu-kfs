ddf5463aab016388f00b865bda2e38bf7bf98772
Jay Hulslander <jdh34@cornell.edu>
2017-02-15 10:02:53 -0500

    KFSPTS-7617 fixing eIncoice case sensitifity with the domain

--- src/main/java/edu/cornell/kfs/module/purap/batch/CuElectronicInvoiceInputFileType.java
+++ src/main/java/edu/cornell/kfs/module/purap/batch/CuElectronicInvoiceInputFileType.java
@@ -5,10 +5,19 @@ import org.kuali.kfs.module.purap.batch.ElectronicInvoiceInputFileType;
 import edu.cornell.kfs.sys.batch.CuBatchInputFileType;
 
 public class CuElectronicInvoiceInputFileType extends ElectronicInvoiceInputFileType implements CuBatchInputFileType {
+	
+	private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(CuElectronicInvoiceInputFileType.class);
 
     @Override
     public boolean isDoneFileRequired() {
         return false;
+    }
+    
+    @Override
+    public String getSchemaLocation() {
+    	LOG.info("The schema is actually '" + schemaLocation + "' but actually returning http://localhost:8080/kfs/static/xsd/purap/electronicInvoice.xsd");
+        //return schemaLocation;
+    	return "http://localhost:8080/kfs/static/xsd/purap/electronicInvoice.xsd";
     }
 
 }
