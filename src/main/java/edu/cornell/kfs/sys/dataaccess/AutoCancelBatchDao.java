3698d5fceb48db9046ecdbae9b4616509bf03acc
Bryan Hutchinson <bh79@cornell.edu>
2017-02-28 14:41:48 -0500

    KFSPTS-7810 fix autoCancelBatchJob

--- src/main/java/edu/cornell/kfs/sys/dataaccess/AutoCancelBatchDao.java
+++ src/main/java/edu/cornell/kfs/sys/dataaccess/AutoCancelBatchDao.java
@@ -1,16 +1,21 @@
-/**
- * 
- */
 package edu.cornell.kfs.sys.dataaccess;
 
-/**
- * @author Admin-dwf5
- *
- */
 public interface AutoCancelBatchDao {
 
-	public boolean cancelFYIsAndAcknowledgements() throws Exception;
-	
-	public void cancelDocuments() throws Exception;
+	/**
+	 * Cancel FYI and Acknowledgement Documents that are eligible to be canceled.
+	 *
+	 * @return true if successful, false otherwise
+	 * @throws Exception
+	 */
+	boolean cancelFYIsAndAcknowledgements() throws Exception;
+
+	/**
+	 * Super User Cancel Documents
+	 * Use parameters to retrieve aging period and doc types to be canceled
+	 *
+	 * @throws Exception
+	 */
+	void cancelDocuments() throws Exception;
 	
 }
