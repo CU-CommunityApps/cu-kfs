db9f629d2ade8da142f4c58a55f76bdf3d61c61b
Bryan Hutchinson <bh79@cornell.edu>
2017-03-01 14:41:59 -0500

    KFSPTS-7810 fix autoCancelBatchJob (#318)

---
+++ src/main/java/edu/cornell/kfs/sys/dataaccess/impl/AutoCancelBatchDaoJdbc.java
@@ -0,0 +1,202 @@
+package edu.cornell.kfs.sys.dataaccess.impl;
+
+import edu.cornell.kfs.sys.CUKFSParameterKeyConstants;
+import edu.cornell.kfs.sys.batch.AutoCancelBatchStep;
+import edu.cornell.kfs.sys.dataaccess.AutoCancelBatchDao;
+import edu.cornell.kfs.sys.dataaccess.DbmsOutput;
+import org.apache.commons.lang.StringUtils;
+import org.kuali.kfs.coreservice.framework.parameter.ParameterService;
+import org.kuali.kfs.krad.document.Document;
+import org.kuali.kfs.krad.service.BusinessObjectService;
+import org.kuali.kfs.krad.service.DocumentService;
+import org.kuali.kfs.krad.service.SessionDocumentService;
+import org.kuali.kfs.krad.util.GlobalVariables;
+import org.kuali.kfs.krad.util.ObjectUtils;
+import org.kuali.kfs.krad.workflow.service.WorkflowDocumentService;
+import org.kuali.rice.core.framework.persistence.jdbc.dao.PlatformAwareDaoBaseJdbc;
+import org.kuali.rice.kew.api.KewApiServiceLocator;
+import org.kuali.rice.kew.api.doctype.DocumentType;
+import org.kuali.rice.kew.api.document.DocumentStatus;
+import org.kuali.rice.kew.api.exception.WorkflowException;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class AutoCancelBatchDaoJdbc extends PlatformAwareDaoBaseJdbc implements AutoCancelBatchDao {
+
+    private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(AutoCancelBatchDaoJdbc.class);
+
+    private BusinessObjectService businessObjectService;
+    private DocumentService documentService;
+    private ParameterService parameterService;
+    private SessionDocumentService sessionDocumentService;
+    private WorkflowDocumentService workflowDocumentService;
+
+    private Collection<String> cancelDocumentTypes = Collections.emptySet();
+    private Map<String, DocumentType> docTypes = new HashMap<>();
+
+    /**
+     * @see AutoCancelBatchDao#cancelFYIsAndAcknowledgements()
+     */
+    @Override
+    public boolean cancelFYIsAndAcknowledgements() {
+        Connection conn = null;
+        CallableStatement cs = null;
+        DbmsOutput dbmsOutput = null;
+        boolean result;
+
+        try {
+            conn = getJdbcTemplate().getDataSource().getConnection();
+            cs = conn.prepareCall("{call CYNERGY.AUTO_CANCEL_FYI_ACK}");
+            dbmsOutput = new DbmsOutput(conn);
+            dbmsOutput.enable(1000000);
+            result = cs.execute();
+            dbmsOutput.show();
+        } catch (SQLException e) {
+            throw new RuntimeException("Issue encountered with cancelFYIsAndAcknowledgements", e);
+        } finally {
+            if (dbmsOutput != null) {
+                dbmsOutput.close();
+            }
+
+            if (cs != null) {
+                try {
+                    cs.close();
+                } catch (SQLException e) {
+                    LOG.error("cancelFYIsAndAcknowledgements: Could not close CallableStatement");
+                }
+            }
+
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    LOG.error("cancelFYIsAndAcknowledgements: Could not close Connection");
+                }
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * @see AutoCancelBatchDao#cancelDocuments()
+     */
+    @Override
+    public void cancelDocuments() throws Exception {
+        final String daysToAutoCancel = parameterService.getParameterValueAsString(AutoCancelBatchStep.class, CUKFSParameterKeyConstants.DAYS_TO_AUTO_CANCEL_PARAMETER);
+
+        if (StringUtils.isNotBlank(daysToAutoCancel)) {
+            Map<String, String> cancelIds = findSavedDocumentIds(Integer.parseInt(daysToAutoCancel));
+            Set<String> cancelDocumentIds = cancelIds.keySet();
+
+            int canceledDocumentCount = 0;
+            for (String docId : cancelDocumentIds) {
+                String docTypeId = cancelIds.get(docId);
+                if (canAutoCancelDocType(docTypeId)) {
+                    LOG.info("Retrieving document : " + docId.trim());
+                    Document document = documentService.getByDocumentHeaderId(docId.trim());
+
+                    try {
+                        if (!ObjectUtils.isNull(document)) {
+                            LOG.info("Document Number to cancel : " + document.getDocumentNumber());
+                            canceledDocumentCount++;
+                            documentService.prepareWorkflowDocument(document);
+                            workflowDocumentService.superUserCancel(document.getDocumentHeader().getWorkflowDocument(), "AutoCancelBatchStep: Older Than " + daysToAutoCancel + " Days");
+                            sessionDocumentService.addDocumentToUserSession(GlobalVariables.getUserSession(), document.getDocumentHeader().getWorkflowDocument());
+                        }
+                    } catch (WorkflowException e) {
+                        LOG.error("AutoCancelBatchStep Encountered WorkflowException " + document.getDocumentNumber(), e);
+                    }
+                }
+            }
+            LOG.info("Total number of docs canceled : " + canceledDocumentCount);
+        } else {
+            LOG.info("ERROR: DAYS_TO_CANCEL parameter is empty or missing");
+        }
+
+    }
+
+    private Map<String, String> findSavedDocumentIds(int daysToAutoCancel) {
+        Map<String, String> ids = new HashMap<String, String>();
+
+        StringBuilder sql = new StringBuilder();
+        sql.append("select DOC_HDR_ID, DOC_TYP_ID from CYNERGY.KREW_DOC_HDR_T where ");
+        sql.append("DOC_HDR_STAT_CD = '" + DocumentStatus.SAVED.getCode() + "' and (trunc(CRTE_DT) + " + daysToAutoCancel + ") <= trunc(SYSDATE)");
+
+        LOG.info("SQL Statement: " + sql);
+        List<Map<String, Object>> results = getSimpleJdbcTemplate().queryForList(sql.toString());
+
+        for (Map<String, Object> result : results) {
+            String docId = (String) result.get("DOC_HDR_ID");
+            String docTypId = (String) result.get("DOC_TYP_ID");
+            LOG.info("Doc ID : " + docId + " Doc Typ ID : " + docTypId);
+            ids.put(docId, docTypId);
+        }
+
+        return ids;
+    }
+
+    private boolean canAutoCancelDocType(String docTypeId) {
+        DocumentType docType = docTypes.get(docTypeId);
+
+        if (ObjectUtils.isNull(docType)) {
+            docType = KewApiServiceLocator.getDocumentTypeService().getDocumentTypeById(docTypeId);
+            docTypes.put(docTypeId, docType);
+        }
+
+        if (cancelDocumentTypes.isEmpty()) {
+            cancelDocumentTypes = parameterService.getParameterValuesAsString(AutoCancelBatchStep.class, CUKFSParameterKeyConstants.AUTO_CANCEL_DOC_TYPES_PARAMETER);
+        }
+
+        return cancelDocumentTypes.contains(docType.getName());
+    }
+
+    public ParameterService getParameterService() {
+        return parameterService;
+    }
+
+    public void setParameterService(ParameterService parameterSerivce) {
+        this.parameterService = parameterSerivce;
+    }
+
+    public BusinessObjectService getBusinessObjectService() {
+        return businessObjectService;
+    }
+
+    public void setBusinessObjectService(BusinessObjectService businessObjectService) {
+        this.businessObjectService = businessObjectService;
+    }
+
+    public DocumentService getDocumentService() {
+        return documentService;
+    }
+
+    public void setDocumentService(DocumentService documentService) {
+        this.documentService = documentService;
+    }
+
+    public SessionDocumentService getSessionDocumentService() {
+        return sessionDocumentService;
+    }
+
+    public void setSessionDocumentService(SessionDocumentService sessionDocumentService) {
+        this.sessionDocumentService = sessionDocumentService;
+    }
+
+    public WorkflowDocumentService getWorkflowDocumentService() {
+        return workflowDocumentService;
+    }
+
+    public void setWorkflowDocumentService(WorkflowDocumentService workflowDocumentService) {
+        this.workflowDocumentService = workflowDocumentService;
+    }
+
+}
