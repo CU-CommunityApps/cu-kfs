8a17fcf58724e375071707b85f85214e7f28a32c
Bryan Hutchinson <bh79@cornell.edu>
2017-03-02 14:05:47 -0500

    KFSPTS-7809: fix failing test

--- src/test/java/edu/cornell/kfs/fp/batch/service/impl/ProcurementCardErrorEmailServiceImplTest.java
+++ src/test/java/edu/cornell/kfs/fp/batch/service/impl/ProcurementCardErrorEmailServiceImplTest.java
@@ -1,29 +1,33 @@
 package edu.cornell.kfs.fp.batch.service.impl;
 
 import edu.cornell.kfs.sys.service.mock.MockParameterServiceImpl;
-import junit.framework.TestCase;
-
-import org.kuali.kfs.sys.ConfigureContext;
+import org.junit.Before;
+import org.junit.Test;
+import org.kuali.kfs.coreservice.framework.parameter.ParameterService;
 import org.kuali.kfs.sys.service.impl.EmailServiceImpl;
 
 import java.util.ArrayList;
 
-@ConfigureContext
-public class ProcurementCardErrorEmailServiceImplTest extends TestCase {
-	private ProcurementCardErrorEmailServiceImpl procurementCardErrorEmailService;
+public class ProcurementCardErrorEmailServiceImplTest {
+    private ProcurementCardErrorEmailServiceImpl procurementCardErrorEmailService;
 
-	@Override
-    protected void setUp() throws Exception {
-        super.setUp();
+    @Before
+    public void setUp() throws Exception {
         procurementCardErrorEmailService = new ProcurementCardErrorEmailServiceImpl();
-        procurementCardErrorEmailService.setEmailService(new EmailServiceImpl());
-	procurementCardErrorEmailService.setParameterService(new MockParameterServiceImpl());
+
+        EmailServiceImpl emailService = new EmailServiceImpl();
+        ParameterService parameterService = new MockParameterServiceImpl();
+
+        emailService.setParameterService(parameterService);
+        procurementCardErrorEmailService.setEmailService(emailService);
+        procurementCardErrorEmailService.setParameterService(parameterService);
     }
-	
-	public void testProcurementCardErrorEmailService(){
-		ArrayList<String> errorMessages = new ArrayList<String>();
-		errorMessages.add("TEST ERROR FOR PROCUREMENT CARD");
-		procurementCardErrorEmailService.sendErrorEmail(errorMessages);
-	}
+
+    @Test
+    public void testProcurementCardErrorEmailService() {
+        ArrayList<String> errorMessages = new ArrayList<String>();
+        errorMessages.add("TEST ERROR FOR PROCUREMENT CARD");
+        procurementCardErrorEmailService.sendErrorEmail(errorMessages);
+    }
 
 }
