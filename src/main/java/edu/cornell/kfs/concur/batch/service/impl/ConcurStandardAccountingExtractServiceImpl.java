6f159113562ea80f72c65ae251af23ee98515aac
Jay Hulslander <jdh34@cornell.edu>
2017-03-06 16:29:49 -0500

    KFSPTS-7868 creating PDP feed file from SAE

--- src/main/java/edu/cornell/kfs/concur/batch/service/impl/ConcurStandardAccountingExtractServiceImpl.java
+++ src/main/java/edu/cornell/kfs/concur/batch/service/impl/ConcurStandardAccountingExtractServiceImpl.java
@@ -5,24 +5,47 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Date;
 import java.util.List;
+
+import javax.xml.bind.JAXBException;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
+import org.codehaus.plexus.util.StringUtils;
+import org.kuali.kfs.kns.service.DataDictionaryService;
 import org.kuali.kfs.krad.exception.ValidationException;
+import org.kuali.kfs.krad.util.KRADConstants;
+import org.kuali.kfs.pdp.PdpConstants;
+import org.kuali.kfs.pdp.businessobject.PaymentDetail;
+import org.kuali.kfs.pdp.businessobject.PaymentGroup;
+import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.kfs.sys.batch.BatchInputFileType;
 import org.kuali.kfs.sys.batch.service.BatchInputFileService;
+import org.kuali.rice.core.api.util.type.KualiDecimal;
 
 import edu.cornell.kfs.concur.batch.businessobject.ConcurStandardAccountingExtractDetailLine;
 import edu.cornell.kfs.concur.batch.businessobject.ConcurStandardAccountingExtractFile;
 import edu.cornell.kfs.concur.batch.service.ConcurStandardAccountingExtractService;
+import edu.cornell.kfs.concur.batch.xmlObjects.PdpFeedAccountingEntry;
+import edu.cornell.kfs.concur.batch.xmlObjects.PdpFeedDetailEntry;
+import edu.cornell.kfs.concur.batch.xmlObjects.PdpFeedFileBaseEntry;
+import edu.cornell.kfs.concur.batch.xmlObjects.PdpFeedGroupEntry;
+import edu.cornell.kfs.concur.batch.xmlObjects.PdpFeedHeaderEntry;
+import edu.cornell.kfs.concur.batch.xmlObjects.PdpFeedPayeeIdEntry;
+import edu.cornell.kfs.concur.batch.xmlObjects.PdpFeedTrailerEntry;
+import edu.cornell.kfs.sys.service.CUMarshalService;
 
 public class ConcurStandardAccountingExtractServiceImpl implements ConcurStandardAccountingExtractService {
     private static final Logger LOG = Logger.getLogger(ConcurStandardAccountingExtractServiceImpl.class);
 
     protected String reimbursementFeedDirectory;
     protected BatchInputFileService batchInputFileService;
+    protected CUMarshalService cuMarshalService;
+    protected DataDictionaryService dataDictionaryService;
     protected BatchInputFileType batchInputFileType;
+    
+    private Integer payeeNameFieldSize;
 
     @Override
     public ConcurStandardAccountingExtractFile parseStandardAccoutingExtractFile(String standardAccountingExtractFileName) throws ValidationException {
@@ -73,7 +96,8 @@ public class ConcurStandardAccountingExtractServiceImpl implements ConcurStandar
             if (saeFile != null) {
                 LOG.debug("logDetailedInfoForConcurStandardAccountingExtractFile, " + saeFile.getDebugInformation());
                 if (saeFile.getConcurStandardAccountingExtractDetailLines() != null) {
-                    LOG.debug("logDetailedInfoForConcurStandardAccountingExtractFile, Number of line items: " + saeFile.getConcurStandardAccountingExtractDetailLines().size());
+                    LOG.debug("logDetailedInfoForConcurStandardAccountingExtractFile, Number of line items: " + 
+                            saeFile.getConcurStandardAccountingExtractDetailLines().size());
                     for (ConcurStandardAccountingExtractDetailLine line : saeFile.getConcurStandardAccountingExtractDetailLines()) {
                         LOG.debug("logDetailedInfoForConcurStandardAccountingExtractFile, " + line.getDebugInformation());
                     }
@@ -99,7 +123,149 @@ public class ConcurStandardAccountingExtractServiceImpl implements ConcurStandar
 
     @Override
     public boolean extractPdpFeedFromStandardAccounitngExtract(ConcurStandardAccountingExtractFile concurStandardAccountingExtractFile) {
-        return true;
+        boolean success = true;
+        
+        if (!concurStandardAccountingExtractFile.getConcurStandardAccountingExtractDetailLines().isEmpty()){
+            PdpFeedFileBaseEntry pdpFeedFileBaseEntry = new PdpFeedFileBaseEntry();
+            pdpFeedFileBaseEntry.setHeader(buildPdpFeedHeaderEntry(concurStandardAccountingExtractFile.getBatchDate()));
+            
+            PdpFeedGroupEntry currentGroup = new PdpFeedGroupEntry();
+            currentGroup.setPayeeId(new PdpFeedPayeeIdEntry());
+            PdpFeedDetailEntry currentDetailEntry = new PdpFeedDetailEntry();
+            PdpFeedAccountingEntry currentAccountingEntry = new PdpFeedAccountingEntry();
+            KualiDecimal pdpTotal = KualiDecimal.ZERO;
+            for (ConcurStandardAccountingExtractDetailLine line : 
+                concurStandardAccountingExtractFile.getConcurStandardAccountingExtractDetailLines()) {
+                if (!StringUtils.equalsIgnoreCase(line.getEmployeeId(), currentGroup.getPayeeId().getContent())) {
+                    if (StringUtils.isNotBlank(currentGroup.getPayeeId().getContent())) {
+                        pdpFeedFileBaseEntry.getGroup().add(currentGroup);
+                    }
+                    currentGroup = new PdpFeedGroupEntry();
+                    currentGroup.setPayeeName(buildPayeeName(line.getEmployeeLastName(), line.getEmployeeFirstName(), 
+                            line.getEmployeeMiddleInitital()));
+                    currentGroup.setPayeeId(buildPayeeIdEntry(line));
+                    currentGroup.setPaymentDate(line.getBatchDate().toString());
+                    currentGroup.setCombineGroupInd("Y");
+                    currentGroup.setBankCode("DISB");
+                }
+                if (!StringUtils.equalsIgnoreCase(currentDetailEntry.getSourceDocNbr(), buildSourceDocumentNumber(line.getReportId()))) {
+                    if (StringUtils.isNotBlank(currentDetailEntry.getSourceDocNbr())) {
+                        currentGroup.getDetail().add(currentDetailEntry);
+                    }
+                    currentDetailEntry = new PdpFeedDetailEntry();
+                    currentDetailEntry.setSourceDocNbr(buildSourceDocumentNumber(line.getReportId()));
+                    currentDetailEntry.setFsOriginCd("Z6");
+                    currentDetailEntry.setFdocTypCd("APTR");
+                }
+                if (!isCurrentAccountingEntrySameAsLineDetail(currentAccountingEntry, line)) {
+                    if (StringUtils.isNotBlank(currentAccountingEntry.getAccountNbr())) {
+                        currentDetailEntry.getAccounting().add(currentAccountingEntry);
+                    }
+                    currentAccountingEntry =  new PdpFeedAccountingEntry();
+                    currentAccountingEntry.setCoaCd(line.getChartOfAccountsCode());
+                    currentAccountingEntry.setAccountNbr(line.getAccountNumber());
+                    currentAccountingEntry.setObjectCd(line.getJournalAccountCode());
+                    currentAccountingEntry.setSubObjectCd(line.getSubObjectCode());
+                    currentAccountingEntry.setOrgRefId(line.getOrgRefId());
+                    currentAccountingEntry.setProjectCd(line.getProjectCode());
+                    currentAccountingEntry.setAmount("0");
+                }
+                pdpTotal = pdpTotal.add(line.getJournalAmount());
+                currentAccountingEntry.setAmount(addAmounts(currentAccountingEntry.getAmount(), line.getJournalAmount()));
+            }
+            currentDetailEntry.getAccounting().add(currentAccountingEntry);
+            currentGroup.getDetail().add(currentDetailEntry);
+            pdpFeedFileBaseEntry.getGroup().add(currentGroup);
+            pdpFeedFileBaseEntry.setTrailer(buildPdpFeedTrailerEntry(pdpFeedFileBaseEntry, pdpTotal));
+            
+            String outputFilePath = getReimbursementFeedDirectory() + "foo.xml";
+            success = marshalPdpFeedFle(pdpFeedFileBaseEntry, outputFilePath);
+        }
+        
+        return success;
+    }
+
+    private PdpFeedTrailerEntry buildPdpFeedTrailerEntry(PdpFeedFileBaseEntry pdpFeedFileBaseEntry, KualiDecimal pdpTotal) {
+        PdpFeedTrailerEntry trailerEntry = new PdpFeedTrailerEntry();
+        trailerEntry.setDetailCount(pdpFeedFileBaseEntry.getGroup().size());
+        trailerEntry.setDetailTotAmt(new Double(pdpTotal.doubleValue()));
+        return trailerEntry;
+    }
+    
+    protected String addAmounts(String startingAmount, KualiDecimal addAmount) {
+        return new KualiDecimal(startingAmount).add(addAmount).toString();
+    }
+    
+    protected boolean isCurrentAccountingEntrySameAsLineDetail(PdpFeedAccountingEntry currentAccountingEntry, 
+            ConcurStandardAccountingExtractDetailLine line) {
+        boolean isSame = StringUtils.equals(currentAccountingEntry.getCoaCd(), line.getChartOfAccountsCode()) &&
+                StringUtils.equals(currentAccountingEntry.getAccountNbr(), line.getAccountNumber()) &&
+                StringUtils.equals(currentAccountingEntry.getSubAccountNbr(), line.getSubAccountNumber()) &&
+                StringUtils.equals(currentAccountingEntry.getObjectCd(), line.getJournalAccountCode()) &&
+                StringUtils.equals(currentAccountingEntry.getSubObjectCd(), line.getSubObjectCode()) &&
+                StringUtils.equals(currentAccountingEntry.getOrgRefId(), line.getOrgRefId()) &&
+                StringUtils.equals(currentAccountingEntry.getProjectCd(), line.getProjectCode());
+        return isSame;
+        
+    }
+    
+    protected String buildSourceDocumentNumber(String reportId) {
+        String sourceDocNumber = "APTR" + StringUtils.substring(reportId, 0, 19);
+        return sourceDocNumber;
+    }
+
+    private PdpFeedPayeeIdEntry buildPayeeIdEntry(ConcurStandardAccountingExtractDetailLine line) {
+        PdpFeedPayeeIdEntry payeeIdEntry = new PdpFeedPayeeIdEntry();
+        payeeIdEntry.setContent(line.getEmployeeId());
+        if (StringUtils.equalsIgnoreCase(line.getEmployeeStatus(), "EMPLOYEE")) {
+            payeeIdEntry.setIdType("E");
+        } else {
+            payeeIdEntry.setIdType("Y");
+        }
+        return payeeIdEntry;
+    }
+
+    protected PdpFeedHeaderEntry buildPdpFeedHeaderEntry(Date batchDate) {
+        PdpFeedHeaderEntry value = new PdpFeedHeaderEntry();
+        value.setChart("IT");
+        value.setCreationDate(batchDate.toString());
+        value.setSubUnit("CNCR");
+        value.setUnit("CRNL");
+        return value;
+    }
+    
+    protected String buildPayeeName(String lastName, String firstName, String middleInitial) {
+        String seperator = KFSConstants.COMMA + KFSConstants.BLANK_SPACE;
+        String fullName = lastName + seperator + firstName;
+        if (StringUtils.isNotBlank(middleInitial)) {
+            fullName = fullName + seperator + middleInitial + KFSConstants.DELIMITER;
+        }
+        if(fullName.length() > getPayeeNameFieldSize()) {
+            fullName = StringUtils.substring(fullName, 0, getPayeeNameFieldSize());
+            fullName = removeLastCharacterWhenComma(fullName);
+        }
+        return fullName;
+    }
+
+    private String removeLastCharacterWhenComma(String fullName) {
+        if (fullName.lastIndexOf(KFSConstants.COMMA) >= getPayeeNameFieldSize()-2) {
+            fullName = fullName.substring(0, fullName.lastIndexOf(KFSConstants.COMMA));
+        }
+        return fullName;
+    }
+
+    private boolean marshalPdpFeedFle(PdpFeedFileBaseEntry cdpFeedFileBaseEntry, String outputFilePath) {
+        boolean success = true;
+        try {
+            File pdpFeedFile = getCuMarshalService().marshalObjectToXML(cdpFeedFileBaseEntry, outputFilePath);
+            success = true;
+        } catch (JAXBException | IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            LOG.error("There was an error marshalling the PDP feed file.", e);
+            success = false;
+        }
+        return success;
     }
 
     @Override
@@ -123,12 +289,40 @@ public class ConcurStandardAccountingExtractServiceImpl implements ConcurStandar
         this.batchInputFileService = batchInputFileService;
     }
 
+    public CUMarshalService getCuMarshalService() {
+        return cuMarshalService;
+    }
+
+    public void setCuMarshalService(CUMarshalService cuMarshalService) {
+        this.cuMarshalService = cuMarshalService;
+    }
+
+    public DataDictionaryService getDataDictionaryService() {
+        return dataDictionaryService;
+    }
+
+    public void setDataDictionaryService(DataDictionaryService dataDictionaryService) {
+        this.dataDictionaryService = dataDictionaryService;
+    }
+
     public BatchInputFileType getBatchInputFileType() {
         return batchInputFileType;
     }
 
     public void setBatchInputFileType(BatchInputFileType batchInputFileType) {
         this.batchInputFileType = batchInputFileType;
+    }
+
+    public Integer getPayeeNameFieldSize() {
+        if (payeeNameFieldSize == null) {
+            payeeNameFieldSize = getDataDictionaryService().getAttributeMaxLength(PaymentGroup.class, PdpConstants.PaymentDetail.PAYEE_NAME);
+        }
+        LOG.info("getPayeeNameFieldSizem return " + payeeNameFieldSize.intValue());
+        return payeeNameFieldSize;
+    }
+
+    public void setPayeeNameFieldSize(Integer payeeNameFieldSize) {
+        this.payeeNameFieldSize = payeeNameFieldSize;
     }
 
 }
---
+++ src/test/java/edu/cornell/kfs/concur/batch/service/impl/ConcurStandardAccountingExtractServiceImplTest.java
@@ -0,0 +1,69 @@
+package edu.cornell.kfs.concur.batch.service.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.kuali.rice.core.api.util.type.KualiDecimal;
+
+public class ConcurStandardAccountingExtractServiceImplTest {
+    
+    ConcurStandardAccountingExtractServiceImpl concurStandardAccountingExtractServiceImpl;
+
+    @Before
+    public void setUp() throws Exception {
+        concurStandardAccountingExtractServiceImpl = new ConcurStandardAccountingExtractServiceImpl();
+        concurStandardAccountingExtractServiceImpl.setPayeeNameFieldSize(new Integer(40));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        concurStandardAccountingExtractServiceImpl = null;
+    }
+
+    @Test
+    public void buildPayeeName() {
+        String results = concurStandardAccountingExtractServiceImpl.buildPayeeName("Doe", "John", "Q");
+        String expected = "Doe, John, Q.";
+        assertEquals("The names should be the same", expected, results);
+    }
+    
+    @Test
+    public void buildPayeeNameNoMiddleInitial() {
+        String results = concurStandardAccountingExtractServiceImpl.buildPayeeName("Doe", "John", "   ");
+        String expected = "Doe, John";
+        assertEquals("The names should be the same", expected, results);
+    }
+    
+    @Test
+    public void buildPayeeNameOverFortyChars() {
+        String results = concurStandardAccountingExtractServiceImpl.buildPayeeName("zxcvbnmasdfghjklqwer", "qwertyuiopasdfghjklz", "l");
+        String expected = "zxcvbnmasdfghjklqwer, qwertyuiopasdfghjk";
+        assertEquals("The names should be the same", expected, results);
+    }
+    
+    @Test
+    public void buildPayeeNameOverFortyCharsEndWithComma() {
+        String results = concurStandardAccountingExtractServiceImpl.buildPayeeName("zxcvbnmasdfghjklqwer", "qwertyuiopasdfgh", "l");
+        String expected = "zxcvbnmasdfghjklqwer, qwertyuiopasdfgh";
+        assertEquals("The names should be the same", expected, results);
+    }
+    
+    @Test
+    public void buildPayeeNameOverFortyCharsEndWithComma2() {
+        String results = concurStandardAccountingExtractServiceImpl.buildPayeeName("zxcvbnmasdfghjklqwer", "qwertyuiopasdfghi", "l");
+        String expected = "zxcvbnmasdfghjklqwer, qwertyuiopasdfghi";
+        assertEquals("The names should be the same", expected, results);
+    }
+    
+    @Test
+    public void addAmounts() {
+        String starting = "100";
+        KualiDecimal addAmount = new KualiDecimal(500.45); 
+        String results = concurStandardAccountingExtractServiceImpl.addAmounts(starting, addAmount);
+        String expected = "600.45";
+        assertEquals("The amounts should be the same", expected, results);
+    }
+
+}
