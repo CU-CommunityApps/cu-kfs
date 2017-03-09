affc11f23167b30435d18a1fc54c45befcf318cd
cah292 <cah292@users.noreply.github.com>
2017-03-09 12:09:06 -0500

    KFSPTS-7909: Add template SAE-to-collector processing. (#328)

--- src/main/java/edu/cornell/kfs/concur/batch/service/ConcurStandardAccountingExtractService.java
+++ src/main/java/edu/cornell/kfs/concur/batch/service/ConcurStandardAccountingExtractService.java
@@ -14,4 +14,6 @@ public interface ConcurStandardAccountingExtractService {
 
     boolean extractPdpFeedFromStandardAccounitngExtract(ConcurStandardAccountingExtractFile concurStandardAccountingExtractFile);
 
+    boolean extractCollectorFeedFromStandardAccountingExtract(ConcurStandardAccountingExtractFile concurStandardAccountingExtractFile);
+
 }
