8d22097570fb2bd3246545c8468b79b28f8ea466
Jay Hulslander <jdh34@cornell.edu>
2017-03-21 18:21:56 -0400

    put comment back in

--- src/main/java/org/kuali/kfs/pdp/businessobject/PaymentGroup.java
+++ src/main/java/org/kuali/kfs/pdp/businessobject/PaymentGroup.java
@@ -4,6 +4,7 @@
  * Copyright 2005-2017 Kuali, Inc.
  * 
  * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  * 
