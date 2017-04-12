7132773d8418a14f590a4d565754967dcf5cf201
Bryan Hutchinson <bh79@cornell.edu>
2017-04-12 11:28:21 -0400

    KFSPTS-5868: cleanup

--- src/test/java/edu/cornell/kfs/sys/service/impl/CuUserFavoriteAccountServiceImplTest.java
+++ src/test/java/edu/cornell/kfs/sys/service/impl/CuUserFavoriteAccountServiceImplTest.java
@@ -11,18 +11,15 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.kuali.kfs.krad.service.impl.BusinessObjectServiceImpl;
-import org.kuali.kfs.module.purap.businessobject.PurApAccountingLine;
+import org.kuali.kfs.module.purap.businessobject.PurApAccountingLineBase;
 import org.kuali.kfs.module.purap.businessobject.PurchaseOrderAccount;
-import org.kuali.kfs.sys.businessobject.AccountingLineBase;
 import org.kuali.kfs.sys.businessobject.GeneralLedgerPendingEntrySourceDetail;
 import org.kuali.rice.core.api.util.type.KualiDecimal;
 import org.kuali.rice.krad.bo.BusinessObject;
 
-import java.lang.reflect.Field;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -64,38 +61,47 @@ public class CuUserFavoriteAccountServiceImplTest {
     }
 
     @Test
-    public void testPopulatedNewAccount() {
-        Map<String, String> fieldMap = new HashMap<>();
-        fieldMap.put("primaryInd", "Y");
-        FavoriteAccount favoriteAccount = FavoriteAccountFixture.FAVORITE_ACCOUNT_1.createFavoriteAccount();
+    public void testPopulatedNewRequisitionAccount() {
+        final String accountType = "REQS";
+        final Class<TestableRequisitionAccount> accountClass = TestableRequisitionAccount.class;
+        validateAccount(accountType, accountClass);
+    }
 
-        GeneralLedgerPendingEntrySourceDetail iwntAccount;
-        
-        PurApAccountingLine acct = userFavoriteAccountService.getPopulatedNewAccount(favoriteAccount, TestableRequisitionAccount.class);
+    @Test
+    public void testPopulatedNewPurchaseOrderAccount() {
+        final String accountType = "PO";
+        final Class<PurchaseOrderAccount> accountClass = PurchaseOrderAccount.class;
+        validateAccount(accountType, accountClass);
+    }
+
+    @Test
+    public void testPopulatedNewIWantAccount() {
+        final String accountType = "IWNT";
+        final Class<IWantAccount> accountClass = IWantAccount.class;
+        validateAccount(accountType, accountClass);
+    }
+
+    private void validateAccount(String accountType, Class accountClass) {
+        final FavoriteAccount favoriteAccount = FavoriteAccountFixture.FAVORITE_ACCOUNT_1.createFavoriteAccount();
+        final GeneralLedgerPendingEntrySourceDetail acct = userFavoriteAccountService.getPopulatedNewAccount(favoriteAccount, accountClass);
         Assert.assertNotNull("Account should be populated", acct);
-        Assert.assertTrue("Account should be REQS Account", acct instanceof TestableRequisitionAccount);
+        Assert.assertTrue("Account should be " + accountType + " Account", accountClass.isInstance(acct));
         Assert.assertEquals("Account Number should be populated", favoriteAccount.getAccountNumber(), acct.getAccountNumber());
         Assert.assertEquals("Object Code should be populated", favoriteAccount.getFinancialObjectCode(), acct.getFinancialObjectCode());
-        Assert.assertEquals("Incorrect percentage (comparison against 100% should have been zero)", 0, acct.getAccountLinePercent().compareTo(new BigDecimal(100)));
 
-        acct = userFavoriteAccountService.getPopulatedNewAccount(favoriteAccount, PurchaseOrderAccount.class);
-        Assert.assertNotNull("Account should be populated", acct);
-        Assert.assertTrue("Account should be PO Account", acct instanceof PurchaseOrderAccount);
-        Assert.assertEquals("Account Number should be populated", favoriteAccount.getAccountNumber(), acct.getAccountNumber());
-        Assert.assertEquals("Object Code should be populated", favoriteAccount.getFinancialObjectCode(), acct.getFinancialObjectCode());
-        Assert.assertEquals("Incorrect percentage (comparison against 100% should have been zero)", 0, acct.getAccountLinePercent().compareTo(new BigDecimal(100)));
+        if (acct instanceof IWantAccount) {
+            final KualiDecimal accountLinePercent = ((IWantAccount) acct).getAmountOrPercent();
+            Assert.assertEquals("Incorrect percentage (comparison against 100% should have been zero)", 0, accountLinePercent.compareTo(new KualiDecimal(100)));
+            Assert.assertEquals("Amount-or-Percent indicator should be Percent", CUPurapConstants.PERCENT, ((IWantAccount) acct).getUseAmountOrPercent());
+        } else {
+            final BigDecimal accountLinePercent = ((PurApAccountingLineBase) acct).getAccountLinePercent();
+            Assert.assertEquals("Incorrect percentage (comparison against 100% should have been zero)", 0, accountLinePercent.compareTo(new BigDecimal(100)));
+        }
+    }
 
-        iwntAccount = userFavoriteAccountService.getPopulatedNewAccount(favoriteAccount, IWantAccount.class);
-        Assert.assertNotNull("Account should be populated", iwntAccount);
-        Assert.assertTrue("Account should be IWNT Account", iwntAccount instanceof IWantAccount);
-        Assert.assertEquals("Account Number should be populated", favoriteAccount.getAccountNumber(), iwntAccount.getAccountNumber());
-        Assert.assertEquals("Object Code should be populated", favoriteAccount.getFinancialObjectCode(), iwntAccount.getFinancialObjectCode());
-        Assert.assertEquals("Amount-or-Percent indicator should be Percent",
-                CUPurapConstants.PERCENT, ((IWantAccount) iwntAccount).getUseAmountOrPercent());
-        Assert.assertEquals("Incorrect percentage (comparison against 100% should have been zero)",
-                0, ((IWantAccount) iwntAccount).getAmountOrPercent().compareTo(new KualiDecimal(100)));
-
-        acct = userFavoriteAccountService.getPopulatedNewAccount(null, PurchaseOrderAccount.class);
+    @Test
+    public void testPopulatedNewAccountNull() {
+        GeneralLedgerPendingEntrySourceDetail acct = userFavoriteAccountService.getPopulatedNewAccount(null, PurchaseOrderAccount.class);
         Assert.assertNull("Account should not be populated", acct);
     }
 
@@ -105,35 +111,6 @@ public class CuUserFavoriteAccountServiceImplTest {
         FavoriteAccount favoriteAccount2 = userFavoriteAccountService.getSelectedFavoriteAccount(favoriteAccount1.getAccountLineIdentifier());
         Assert.assertTrue("Favorite Account should exist", favoriteAccount2 != null);
         Assert.assertTrue("should have the same PK", favoriteAccount1.getAccountLineIdentifier().equals(favoriteAccount2.getAccountLineIdentifier()));
-    }
-
-    private class TestUserFavoriteAccountServiceImpl extends UserFavoriteAccountServiceImpl {
-//        @Override
-//        public FavoriteAccount getSelectedFavoriteAccount(Integer accountLineIdentifier) {
-//            if (TEST_FAVORITE_ACCOUNT_LINE_ID.equals(accountLineIdentifier)) {
-//                return testFavoriteAccount;
-//            } else if (TEST_ALT_FAVORITE_ACCOUNT_LINE_ID.equals(accountLineIdentifier)) {
-//                return testAltFavoriteAccount;
-//            }
-//            return null;
-//        }
-
-        @Override
-        protected void populateAccountNumberOnPurApAccountingLine(FavoriteAccount account, PurApAccountingLine acctLine) {
-            // To avoid Spring calls, we need to set the account number field manually instead of using the setter.
-            try {
-                Field accountNumberField = AccountingLineBase.class.getDeclaredField("accountNumber");
-                accountNumberField.setAccessible(true);
-                accountNumberField.set(acctLine, account.getAccountNumber());
-            } catch (NoSuchFieldException | IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        @Override
-        protected void refreshReferenceObjectsForPopulatedAccountingLine(GeneralLedgerPendingEntrySourceDetail acctLine) {
-            // Do nothing.
-        }
     }
 
     private class MockBusinessObjectServiceImpl extends BusinessObjectServiceImpl {
