1a443d1ae9a2a43a088b2dc9679ad6b5164649c5
Bryan Hutchinson <bh79@cornell.edu>
2017-04-12 16:38:04 -0400

    KFSPTS-5689: fix another test

--- src/test/java/edu/cornell/kfs/module/purap/document/service/impl/CuCreditMemoServiceImplTest.java
+++ src/test/java/edu/cornell/kfs/module/purap/document/service/impl/CuCreditMemoServiceImplTest.java
@@ -1,11 +1,8 @@
 package edu.cornell.kfs.module.purap.document.service.impl;
 
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.sql.Date;
-import java.sql.Timestamp;
-import java.util.ArrayList;
-
+import edu.cornell.kfs.module.purap.document.CuVendorCreditMemoDocument;
+import edu.cornell.kfs.vnd.fixture.VendorDetailExtensionFixture;
+import edu.cornell.kfs.vnd.fixture.VendorHeaderFixture;
 import org.easymock.EasyMock;
 import org.easymock.IMockBuilder;
 import org.easymock.Mock;
@@ -13,21 +10,31 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.kuali.kfs.kns.service.DataDictionaryService;
+import org.kuali.kfs.kns.service.impl.DataDictionaryServiceImpl;
 import org.kuali.kfs.krad.UserSession;
 import org.kuali.kfs.krad.bo.Note;
+import org.kuali.kfs.krad.bo.PersistableBusinessObjectExtension;
 import org.kuali.kfs.krad.document.Document;
+import org.kuali.kfs.krad.service.DocumentService;
 import org.kuali.kfs.krad.service.NoteService;
 import org.kuali.kfs.krad.service.impl.DocumentServiceImpl;
 import org.kuali.kfs.krad.util.GlobalVariables;
 import org.kuali.kfs.module.purap.PurapConstants;
+import org.kuali.kfs.module.purap.document.AccountsPayableDocument;
 import org.kuali.kfs.module.purap.document.VendorCreditMemoDocument;
+import org.kuali.kfs.module.purap.document.service.AccountsPayableService;
 import org.kuali.kfs.module.purap.document.service.PurapService;
+import org.kuali.kfs.module.purap.document.service.impl.AccountsPayableServiceImpl;
+import org.kuali.kfs.module.purap.util.ExpiredOrClosedAccountEntry;
 import org.kuali.kfs.sys.businessobject.FinancialSystemDocumentHeader;
 import org.kuali.kfs.sys.fixture.UserNameFixture;
+import org.kuali.kfs.vnd.businessobject.VendorAddress;
+import org.kuali.kfs.vnd.businessobject.VendorDetail;
+import org.kuali.kfs.vnd.document.service.VendorService;
+import org.kuali.kfs.vnd.document.service.impl.VendorServiceImpl;
+import org.kuali.kfs.vnd.fixture.VendorAddressFixture;
 import org.kuali.rice.core.api.datetime.DateTimeService;
-import org.kuali.kfs.krad.service.DocumentService;
-
-import edu.cornell.kfs.module.purap.document.CuVendorCreditMemoDocument;
 import org.kuali.rice.core.api.util.type.KualiDecimal;
 import org.kuali.rice.core.impl.datetime.DateTimeServiceImpl;
 import org.kuali.rice.kew.api.exception.WorkflowException;
@@ -37,33 +44,50 @@ import org.powermock.api.easymock.PowerMock;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(CuCreditMemoServiceImplTest.TestCuCreditMemoServiceImpl.class)
 public class CuCreditMemoServiceImplTest {
 
 	private CuCreditMemoServiceImpl creditMemoServiceImpl;
 	private DateTimeService dateTimeService;
-	private VendorCreditMemoDocument creditMemoDocument;
+	private CuVendorCreditMemoDocument creditMemoDocument;
 	private DocumentService documentService;
 	private NoteService noteService;
     private PurapService purapService;
+    private AccountsPayableService accountsPayableService;
+    private VendorService vendorService;
+    private DataDictionaryService dataDictionaryService;
     @Mock
     private static Person mo14Person;
     @Mock
     private static UserSession mo14Session;
 
-	@Before
+    @Before
 	public void setUp() throws Exception {
 	    documentService = new MockDocumentServiceImpl();
         noteService = EasyMock.createMock(NoteService.class);
         purapService = EasyMock.createMock(PurapService.class);
         dateTimeService = new DateTimeServiceImpl();
+        accountsPayableService = new MockAccountsPayableServiceImpl();
+        vendorService = new MockVendorServiceImpl();
+        dataDictionaryService = new MockDataDictionaryServiceImpl();
 
         creditMemoServiceImpl = PowerMock.createPartialMock(CuCreditMemoServiceImplTest.TestCuCreditMemoServiceImpl.class, "reIndexDocument", "getCreditMemoDocumentById");
 
         creditMemoServiceImpl.setDocumentService(documentService);
         creditMemoServiceImpl.setNoteService(noteService);
         creditMemoServiceImpl.setPurapService(purapService);
+        creditMemoServiceImpl.setAccountsPayableService(accountsPayableService);
+        creditMemoServiceImpl.setVendorService(vendorService);
+        creditMemoServiceImpl.setDataDictionaryService(dataDictionaryService);
+
 		creditMemoDocument = setupVendorCreditMemoDocument();
 
 		mo14Person = createMockPerson(UserNameFixture.mo14);
@@ -71,14 +95,14 @@ public class CuCreditMemoServiceImplTest {
         GlobalVariables.setUserSession(mo14Session);
 	}
 
-    private VendorCreditMemoDocument setupVendorCreditMemoDocument() {
+    private CuVendorCreditMemoDocument setupVendorCreditMemoDocument() {
         ArrayList<String> methodNames = new ArrayList<>();
         for (Method method : VendorCreditMemoDocument.class.getMethods()) {
             if (!Modifier.isFinal(method.getModifiers()) && !method.getName().startsWith("set") && !method.getName().startsWith("get") && !method.getName().startsWith("is")) {
                 methodNames.add(method.getName());
             }
         }
-        IMockBuilder<VendorCreditMemoDocument> builder = EasyMock.createMockBuilder(VendorCreditMemoDocument.class).addMockedMethods(methodNames.toArray(new String[0]));
+        IMockBuilder<CuVendorCreditMemoDocument> builder = EasyMock.createMockBuilder(CuVendorCreditMemoDocument.class).addMockedMethods(methodNames.toArray(new String[0]));
         creditMemoDocument = builder.createNiceMock();
 
 	    // mock out DocumentService and assert number of times save is called?
@@ -109,6 +133,7 @@ public class CuCreditMemoServiceImplTest {
         Person person = EasyMock.createMock(PersonImpl.class);
         EasyMock.expect(person.getPrincipalName()).andStubReturn(userNameFixture.toString());
         EasyMock.expect(person.getPrincipalId()).andStubReturn(userNameFixture.toString());
+        EasyMock.expect(person.getCampusCode()).andStubReturn(userNameFixture.toString());
         EasyMock.replay(person);
         return person;
     }
@@ -170,8 +195,8 @@ public class CuCreditMemoServiceImplTest {
 	public void testPopulateDocumentAfterInit() throws WorkflowException {
 		creditMemoServiceImpl.populateDocumentAfterInit(creditMemoDocument);
 
-		Assert.assertNotNull(((CuVendorCreditMemoDocument)creditMemoDocument).getPaymentMethodCode());
-		Assert.assertEquals("P", ((CuVendorCreditMemoDocument)creditMemoDocument).getPaymentMethodCode());
+		Assert.assertNotNull(creditMemoDocument.getPaymentMethodCode());
+		Assert.assertEquals("P", creditMemoDocument.getPaymentMethodCode());
 	}
 
 	private class MockDocumentServiceImpl extends DocumentServiceImpl {
@@ -201,6 +226,46 @@ public class CuCreditMemoServiceImplTest {
         @Override
         public VendorCreditMemoDocument getCreditMemoDocumentById(Integer purchasingDocumentIdentifier) {
             return setupVendorCreditMemoDocument();
+        }
+    }
+
+    private class MockAccountsPayableServiceImpl extends AccountsPayableServiceImpl {
+        @Override
+        public HashMap<String, ExpiredOrClosedAccountEntry> getExpiredOrClosedAccountList(AccountsPayableDocument document) {
+            return new HashMap<>();
+        }
+    }
+
+    private class MockVendorDetail extends VendorDetail {
+        @Override
+        public PersistableBusinessObjectExtension getExtension() {
+            return VendorDetailExtensionFixture.EXTENSION.createVendorDetailExtension();
+        }
+    }
+
+    private class MockVendorServiceImpl extends VendorServiceImpl {
+        @Override
+        public VendorDetail getVendorDetail(Integer headerId, Integer detailId) {
+            VendorDetail vendorDetail = new MockVendorDetail();
+            vendorDetail.setVendorName("Anak Inc");
+            vendorDetail.setVendorHeaderGeneratedIdentifier(4506);
+            vendorDetail.setVendorDetailAssignedIdentifier(0);
+            vendorDetail.setVendorParentIndicator(true);
+            vendorDetail.setVendorHeader(VendorHeaderFixture.ONE.createVendorHeader());
+
+            return vendorDetail;
+        }
+
+        @Override
+        public VendorAddress getVendorDefaultAddress(Integer vendorHeaderId, Integer vendorDetailId, String addressType, String campus, boolean activeCheck) {
+            return VendorAddressFixture.address1.createAddress();
+        }
+    }
+
+    private class MockDataDictionaryServiceImpl extends DataDictionaryServiceImpl {
+        @Override
+        public Integer getAttributeMaxLength(Class dataObjectClass, String attributeName) {
+            return 200;
         }
     }
 }
