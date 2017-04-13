78ffd3783cc5ed02c8344638ff8d91dc98205ec8
Jay Hulslander <jdh34@cornell.edu>
2017-04-13 13:58:01 -0400

    KFSPTS-8219 error message handling (#367)

--- src/main/java/edu/cornell/kfs/concur/businessobjects/ValidationResult.java
+++ src/main/java/edu/cornell/kfs/concur/businessobjects/ValidationResult.java
@@ -3,6 +3,8 @@ package edu.cornell.kfs.concur.businessobjects;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
 import org.kuali.kfs.sys.KFSConstants;
 
 public class ValidationResult {
@@ -43,15 +45,25 @@ public class ValidationResult {
         if (messages == null) {
             messages = new ArrayList<String>();
         }
-        messages.add(message);
+        if (StringUtils.isNotBlank(message) && isNotDuplicateMessage(message)) {
+            messages.add(message);
+        }
+    }
+    
+    private boolean isNotDuplicateMessage(String message) {
+        for (String currentMessage : getMessages()) {
+            if (StringUtils.equalsIgnoreCase(currentMessage, message)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public void addMessages(List<String> messagesToAdd) {
-        if (messages == null) {
-            messages = new ArrayList<String>();
-        }
-        if (messagesToAdd != null) {
-            messages.addAll(messagesToAdd);
+        if (CollectionUtils.isNotEmpty(messagesToAdd)) {
+            for (String messageToAdd : messagesToAdd) {
+                addMessage(messageToAdd);
+            }
         }
     }
 
--- src/test/java/edu/cornell/kfs/concur/service/impl/ConcurEventNotificationServiceImplTest.java
+++ src/test/java/edu/cornell/kfs/concur/service/impl/ConcurEventNotificationServiceImplTest.java
@@ -2,9 +2,12 @@ package edu.cornell.kfs.concur.service.impl;
 
 import static org.junit.Assert.*;
 
+import org.apache.commons.lang.StringUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import edu.cornell.kfs.concur.ConcurConstants;
 
 public class ConcurEventNotificationServiceImplTest {
     
@@ -26,6 +29,40 @@ public class ConcurEventNotificationServiceImplTest {
         String expectedNotifcationId = "gWu2zdPNXG6oQHp0VDaEmNn77NGJ5aQ9rww";
         String actualNotificationId = concurEventNotificationServiceImpl.findNotificationId(notificationURI);
         assertEquals("Expected NotificationId and ActualNotificationId should be the same", expectedNotifcationId, actualNotificationId);
+    }
+    
+    @Test
+    public void validateShortRespone() {
+        String shortMessage = "some short message";
+        String actualResults = concurEventNotificationServiceImpl.truncateValidationResultMessageToMaximumDatabaseFieldSize(shortMessage);
+        assertEquals("short message should be the same", shortMessage, actualResults);
+    }
+    
+    @Test
+    public void validateNullRespone() {
+        String nullMessage = null;
+        String actualResults = concurEventNotificationServiceImpl.truncateValidationResultMessageToMaximumDatabaseFieldSize(nullMessage);
+        assertEquals("null message should be the same", nullMessage, actualResults);
+    }
+    
+    @Test
+    public void validateLongRespone() {
+        String longMessage = buildLongMessage();
+        assertEquals("long message should 2000 characters long", ConcurConstants.VALIDATION_RESULT_MESSAGE_MAX_LENGTH, longMessage.length());
+        String actualResults = concurEventNotificationServiceImpl.truncateValidationResultMessageToMaximumDatabaseFieldSize(longMessage);
+        assertEquals("long message should be the same", longMessage, actualResults);
+    }
+    
+    @Test
+    public void validateOverageRespone() {
+        String longMessage = buildLongMessage();
+        String overageMessage = longMessage + "some extra stuff to exceed max length";
+        String actualResults = concurEventNotificationServiceImpl.truncateValidationResultMessageToMaximumDatabaseFieldSize(overageMessage);
+        assertEquals("message should be the long message without the extra stuff", longMessage, actualResults);
+    }
+    
+    private String buildLongMessage() {
+        return StringUtils.repeat("A", ConcurConstants.VALIDATION_RESULT_MESSAGE_MAX_LENGTH);
     }
 
 }
