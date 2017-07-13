d915a83499f59e3e55d9d040b887b6d25b1a4ef3
cah292 <cah292@cornell.edu>
2017-07-13 17:41:50 -0400

    KFSPTS-9248: Add perm template for download-only batch file access.

---
+++ src/main/java/edu/cornell/kfs/sys/batch/service/impl/CuBatchFileAdminAuthorizationServiceImpl.java
@@ -0,0 +1,42 @@
+package edu.cornell.kfs.sys.batch.service.impl;
+
+import org.kuali.kfs.pdp.PdpConstants;
+import org.kuali.kfs.sys.KFSConstants;
+import org.kuali.kfs.sys.batch.BatchFile;
+import org.kuali.kfs.sys.batch.service.impl.BatchFileAdminAuthorizationServiceImpl;
+import org.kuali.rice.kim.api.identity.Person;
+
+import edu.cornell.kfs.sys.CUKFSConstants;
+
+/**
+ * Custom BatchFileAdminAuthorizationServiceImpl subclass that adds an alternative permission template
+ * for controlling the "Download" link on the Batch File lookup. This allows for granting users
+ * permission to download files without necessarily granting permission to delete them.
+ */
+public class CuBatchFileAdminAuthorizationServiceImpl extends BatchFileAdminAuthorizationServiceImpl {
+
+    /**
+     * Overridden to also check the "View Batch File" template, as an alternative means
+     * of authorizing the "Download" link. Uses logic similar to that of the superclass method,
+     * in addition to calling the superclass method to check the "Administer Batch File" template.
+     * 
+     * @see org.kuali.kfs.sys.batch.service.impl.BatchFileAdminAuthorizationServiceImpl#canDownload(
+     * org.kuali.kfs.sys.batch.BatchFile, org.kuali.rice.kim.api.identity.Person)
+     */
+    @SuppressWarnings("deprecation")
+    @Override
+    public boolean canDownload(BatchFile batchFile, Person user) {
+        boolean isAuthorized = false;
+        if (batchFile.getFileName().indexOf(PdpConstants.RESEARCH_PARTICIPANT_FILE_PREFIX) >= 0) {
+            isAuthorized = getIdentityManagementService().hasPermissionByTemplateName(user.getPrincipalId(),
+                    KFSConstants.CoreModuleNamespaces.KFS, CUKFSConstants.SysKimApiConstants.VIEW_BATCH_FILE_PERMISSION_TEMPLATE_NAME,
+                    generateDownloadCheckPermissionDetails(batchFile, user));
+        } else {
+            isAuthorized = getIdentityManagementService().isAuthorizedByTemplateName(user.getPrincipalId(),
+                    KFSConstants.CoreModuleNamespaces.KFS, CUKFSConstants.SysKimApiConstants.VIEW_BATCH_FILE_PERMISSION_TEMPLATE_NAME,
+                    generateDownloadCheckPermissionDetails(batchFile, user), generateDownloadCheckRoleQualifiers(batchFile, user));
+        }
+        return isAuthorized || super.canDownload(batchFile, user);
+    }
+
+}
