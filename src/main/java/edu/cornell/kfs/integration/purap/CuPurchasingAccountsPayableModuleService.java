385a2ee601fc17f1cb49051f977e0d9099f00f22
Jay Hulslander <jdh34@cornell.edu>
2017-08-03 11:19:22 -0400

    KFSPTS-9313 making sure each note saved has a valid extended attribu… (#431)

--- src/main/java/edu/cornell/kfs/integration/purap/CuPurchasingAccountsPayableModuleService.java
+++ src/main/java/edu/cornell/kfs/integration/purap/CuPurchasingAccountsPayableModuleService.java
@@ -1,9 +1,12 @@
 package edu.cornell.kfs.integration.purap;
 
 import org.kuali.kfs.integration.purap.PurchasingAccountsPayableModuleService;
+import org.kuali.kfs.krad.document.Document;
 
 public interface CuPurchasingAccountsPayableModuleService extends PurchasingAccountsPayableModuleService {
     
     public void handlePurchasingBatchCancels(String documentNumber, String financialSystemDocumentTypeCode, boolean primaryCancel, boolean disbursedPayment, boolean crCancel);
+    
+    public void createAndSaveNote(Document purchasingDocument, String noteText);
 
 }
