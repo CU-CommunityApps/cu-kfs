ea7b1208f2b97ab2e442d7ececc1689502dd3050
Daniela Balmus <db483@cornell.edu>
2017-08-22 14:28:50 +0100

    Revert "KFSPTS-9313 making sure each note saved has a valid extended attribu… (#431)"
    This reverts commit 385a2ee601fc17f1cb49051f977e0d9099f00f22.

--- src/main/java/edu/cornell/kfs/module/purap/document/service/impl/CuPurchasingAccountsPayableModuleServiceImpl.java
+++ src/main/java/edu/cornell/kfs/module/purap/document/service/impl/CuPurchasingAccountsPayableModuleServiceImpl.java
@@ -1,6 +1,5 @@
 package edu.cornell.kfs.module.purap.document.service.impl;
 
-import org.kuali.kfs.krad.document.Document;
 import org.kuali.kfs.module.purap.PurapConstants;
 import org.kuali.kfs.module.purap.PurapParameterConstants;
 import org.kuali.kfs.module.purap.document.PaymentRequestDocument;
@@ -8,24 +7,14 @@ import org.kuali.kfs.module.purap.document.VendorCreditMemoDocument;
 import org.kuali.kfs.module.purap.document.service.CreditMemoService;
 import org.kuali.kfs.module.purap.document.service.PaymentRequestService;
 import org.kuali.kfs.module.purap.document.service.impl.PurchasingAccountsPayableModuleServiceImpl;
-import org.kuali.kfs.module.purap.document.web.struts.PurchasingFormBase;
 import org.kuali.kfs.sys.context.SpringContext;
-import org.apache.commons.lang.StringUtils;
 import org.kuali.kfs.coreservice.framework.parameter.ParameterService;
-import org.kuali.kfs.krad.bo.Note;
-import org.kuali.kfs.krad.service.NoteService;
-import org.kuali.kfs.krad.service.SequenceAccessorService;
 
 import edu.cornell.kfs.integration.purap.CuPurchasingAccountsPayableModuleService;
-import edu.cornell.kfs.sys.CUKFSConstants;
-import edu.cornell.kfs.sys.businessobject.NoteExtendedAttribute;
 
 public class CuPurchasingAccountsPayableModuleServiceImpl extends PurchasingAccountsPayableModuleServiceImpl implements CuPurchasingAccountsPayableModuleService {
 
     private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(CuPurchasingAccountsPayableModuleServiceImpl.class);
-    
-    protected NoteService noteService;
-    protected SequenceAccessorService sequenceAccessorService;
     
    /**
     * @see org.kuali.kfs.integration.pdp.service.PurchasingAccountsPayableModuleService#handlePurchasingBatchCancels(java.lang.String)
@@ -73,32 +62,4 @@ public class CuPurchasingAccountsPayableModuleServiceImpl extends PurchasingAcco
            }
        }
    }
-   
-    @Override
-    public void createAndSaveNote(Document purchasingDocument, String noteText) {
-        LOG.debug("createAndSaveNote, entering");
-        Note noteObj = documentService.createNoteFromDocument(purchasingDocument, noteText);
-        Long newNoteId = sequenceAccessorService.getNextAvailableSequenceNumber(CUKFSConstants.NOTE_SEQUENCE_NAME);
-        noteObj.setNoteIdentifier(newNoteId);
-
-        NoteExtendedAttribute noteExtension = new NoteExtendedAttribute();
-        noteExtension.setNoteIdentifier(noteObj.getNoteIdentifier());
-        noteObj.setExtension(noteExtension);
-
-        purchasingDocument.addNote(noteObj);
-        noteService.saveNoteList(purchasingDocument.getNotes());
-        
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("createAndSaveNote, Created a new note with an ID of " + newNoteId + " with a note text: " + noteText);
-        }
-    }
-
-    public void setNoteService(NoteService noteService) {
-        this.noteService = noteService;
-    }
-
-    public void setSequenceAccessorService(SequenceAccessorService sequenceAccessorService) {
-        this.sequenceAccessorService = sequenceAccessorService;
-    }
-   
 }
