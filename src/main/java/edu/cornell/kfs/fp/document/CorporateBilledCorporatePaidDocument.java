72ecd5d7861deaa49912a18a734e48808fcfe075
Jay Hulslander <jdh34@cornell.edu>
2017-10-27 09:25:47 -0400

    getting DD to work for CBCP

--- src/main/java/edu/cornell/kfs/fp/document/CorporateBilledCorporatePaidDocument.java
+++ src/main/java/edu/cornell/kfs/fp/document/CorporateBilledCorporatePaidDocument.java
@@ -1,9 +1,15 @@
 package edu.cornell.kfs.fp.document;
 
+import org.kuali.kfs.coreservice.framework.parameter.ParameterConstants.COMPONENT;
+import org.kuali.kfs.coreservice.framework.parameter.ParameterConstants.NAMESPACE;
 import org.kuali.kfs.fp.document.ProcurementCardDocument;
+import org.kuali.kfs.sys.KFSConstants;
 
 import edu.cornell.kfs.fp.CuFPConstants;
 
+
+@NAMESPACE(namespace = KFSConstants.CoreModuleNamespaces.FINANCIAL)
+@COMPONENT(component = "CorporateBilledCorporatePaid")
 public class CorporateBilledCorporatePaidDocument extends ProcurementCardDocument {
     private static final long serialVersionUID = 8032811224624474218L;
     protected static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(CorporateBilledCorporatePaidDocument.class);
