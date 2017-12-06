aabc42a50cde6968ebb8bfdeb7f53bd0b836fc39
Jay Hulslander <jdh34@cornell.edu>
2017-12-06 11:30:53 -0500

    cleaning up formating issues of cu-ojb-fp.xml

--- src/main/java/edu/cornell/kfs/fp/batch/service/CuProcurementCardLoadTransactionsService.java
+++
@@ -1,7 +0,0 @@
-package edu.cornell.kfs.fp.batch.service;
-
-import org.kuali.kfs.fp.batch.service.ProcurementCardLoadTransactionsService;
-
-public interface CuProcurementCardLoadTransactionsService extends ProcurementCardLoadTransactionsService {
-    boolean loadProcurementCardFile(String fileName);
-}
