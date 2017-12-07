779082c7dd43d1cc1bfd584abb08a2d93e56eccf
cah292 <cah292@cornell.edu>
2017-12-07 10:33:54 -0500

    KFSPTS-10391: Fix country handling on Payment Detail inquiry

--- src/main/java/edu/cornell/kfs/pdp/CUPdpPropertyConstants.java
+++ src/main/java/edu/cornell/kfs/pdp/CUPdpPropertyConstants.java
@@ -4,6 +4,14 @@ public final class CUPdpPropertyConstants {
 
     public static final String PAYEE_PRINCIPAL_NAME = "payeePrincipalName";
 
+    public static final class PaymentDetail {
+        public static final String PAYMENT_COUNTRY = "paymentGroup.country";
+        
+        private PaymentDetail() {
+            throw new UnsupportedOperationException("do not call constructor");
+        }
+    }
+
     private CUPdpPropertyConstants() {
         throw new UnsupportedOperationException("do not call");
     }
