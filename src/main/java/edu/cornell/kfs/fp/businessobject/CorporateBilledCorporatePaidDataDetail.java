f29240772dce13abc0716d665c7ce97f49df3fd1
Jay Hulslander <jdh34@cornell.edu>
2017-12-07 17:16:48 -0500

    KFSPTS-9131 Creating Corporate Billed Corporate Paid document type and batch job (#483)
    Conflicts:
    	src/main/webapp/WEB-INF/institutional-struts-config.xml

---
+++ src/main/java/edu/cornell/kfs/fp/businessobject/CorporateBilledCorporatePaidDataDetail.java
@@ -0,0 +1,6 @@
+package edu.cornell.kfs.fp.businessobject;
+
+public class CorporateBilledCorporatePaidDataDetail extends PurchasingDataDetail {
+    private static final long serialVersionUID = -4391818051392640185L;
+
+}
---
+++ src/main/java/edu/cornell/kfs/fp/businessobject/CorporateBilledCorporatePaidTargetAccountingLine.java
@@ -0,0 +1,8 @@
+package edu.cornell.kfs.fp.businessobject;
+
+import org.kuali.kfs.fp.businessobject.ProcurementCardTargetAccountingLine;
+
+public class CorporateBilledCorporatePaidTargetAccountingLine extends ProcurementCardTargetAccountingLine {
+    private static final long serialVersionUID = -3090715460211979834L;
+
+}
