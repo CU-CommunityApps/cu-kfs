56193517bf51c37ca373fc3349969164f9eb0fbe
cah292 <cah292@cornell.edu>
2018-01-11 17:23:22 -0500

    KFSPTS-10631: Fix sub-object validation issue in Scrubber

--- src/main/java/edu/cornell/kfs/gl/service/impl/CuScrubberValidatorImpl.java
+++ src/main/java/edu/cornell/kfs/gl/service/impl/CuScrubberValidatorImpl.java
@@ -1,6 +1,7 @@
 package edu.cornell.kfs.gl.service.impl;
 
 import org.kuali.kfs.coa.businessobject.AccountingPeriod;
+import org.kuali.kfs.coa.businessobject.SubObjectCode;
 import org.kuali.kfs.gl.batch.service.AccountingCycleCachingService;
 import org.kuali.kfs.gl.businessobject.OriginEntryInformation;
 import org.kuali.kfs.gl.service.impl.ScrubberValidatorImpl;
@@ -56,6 +57,42 @@ public class CuScrubberValidatorImpl extends ScrubberValidatorImpl {
                     + universityRunDate.getUniversityFiscalYear() + ", period " 
                     + universityRunDate.getUniversityFiscalAccountingPeriod(), Message.TYPE_FATAL);
         }
+        return null;
+    }
+
+    /**
+     * Overridden to use a method version similar to the one from KualiCo's 01/11/2018 patch,
+     * to fix an issue where invalid sub-object codes were not being scrubbed properly.
+     * 
+     * @see org.kuali.kfs.gl.service.impl.ScrubberValidatorImpl#validateSubObjectCode(
+     * org.kuali.kfs.gl.businessobject.OriginEntryInformation, org.kuali.kfs.gl.businessobject.OriginEntryInformation,
+     * org.kuali.kfs.gl.batch.service.AccountingCycleCachingService)
+     */
+    @Override
+    protected Message validateSubObjectCode(
+            OriginEntryInformation originEntry, OriginEntryInformation workingEntry, AccountingCycleCachingService accountingCycleCachingService) {
+        LOG.debug("validateFinancialSubObjectCode() started");
+
+        if (!StringUtils.hasText(originEntry.getFinancialSubObjectCode())) {
+            workingEntry.setFinancialSubObjectCode(KFSConstants.getDashFinancialSubObjectCode());
+            return null;
+        }
+
+        if (!KFSConstants.getDashFinancialSubObjectCode().equals(originEntry.getFinancialSubObjectCode())) {
+            SubObjectCode originEntrySubObject = accountingCycleCachingService.getSubObjectCode(
+                    originEntry.getUniversityFiscalYear(), originEntry.getChartOfAccountsCode(), originEntry.getAccountNumber(),
+                    originEntry.getFinancialObjectCode(), originEntry.getFinancialSubObjectCode());
+            if (originEntrySubObject != null) {
+                if (!originEntrySubObject.isActive()) {
+                    workingEntry.setFinancialSubObjectCode(KFSConstants.getDashFinancialSubObjectCode());
+                    return null;
+                }
+            } else {
+                workingEntry.setFinancialSubObjectCode(KFSConstants.getDashFinancialSubObjectCode());
+                return null;
+            }
+        }
+        workingEntry.setFinancialSubObjectCode(originEntry.getFinancialSubObjectCode());
         return null;
     }
 
