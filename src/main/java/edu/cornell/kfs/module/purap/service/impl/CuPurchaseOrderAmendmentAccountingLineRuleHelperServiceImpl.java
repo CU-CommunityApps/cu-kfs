6e0f5475559a27f1f0b7f0280005fdbade4c24b9
cah292 <cah292@cornell.edu>
2018-01-23 15:43:29 -0500

    KFSPTS-10604: More changes to allow expired POA accounts

--- src/main/java/edu/cornell/kfs/module/purap/service/impl/CuPurchaseOrderAmendmentAccountingLineRuleHelperServiceImpl.java
+++ src/main/java/edu/cornell/kfs/module/purap/service/impl/CuPurchaseOrderAmendmentAccountingLineRuleHelperServiceImpl.java
@@ -6,15 +6,15 @@ import org.kuali.kfs.sys.businessobject.AccountingLine;
 public class CuPurchaseOrderAmendmentAccountingLineRuleHelperServiceImpl extends PurchasingAccountingLineRuleHelperServiceImpl {
 
     /**
-     * Overridden to behave like the PurapAccountingLineRuleHelperServiceImpl "grandparent" class and just return true,
-     * to skip the expired account validation from the PurchasingAccountingLineRuleHelperServiceImpl parent class.
+     * Overridden to allow expired accounts if the users have configured the appropriate override fields on the document,
+     * and to skip the no-expired-accounts validation from the PurchasingAccountingLineRuleHelperServiceImpl parent class.
      * 
      * @see org.kuali.kfs.module.purap.service.impl.PurchasingAccountingLineRuleHelperServiceImpl#hasRequiredOverrides(
      * org.kuali.kfs.sys.businessobject.AccountingLine, java.lang.String)
      */
     @Override
     public boolean hasRequiredOverrides(AccountingLine line, String overrideCode) {
-        return true;
+        return hasAccountRequiredOverrides(line, overrideCode);
     }
 
 }
