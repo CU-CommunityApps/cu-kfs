487a0aed3fd6da1496611df1a9a629b1a05385ac
Jay Hulslander <jdh34@cornell.edu>
2018-01-29 15:46:28 -0500

    Revert "implement DisposableClientServiceImplBase"
    This reverts commit 46cf8d46926b736e32d9ad169ecc9e687ee22034.

--- src/main/java/edu/cornell/kfs/concur/service/impl/ConcurReportsServiceImpl.java
+++ src/main/java/edu/cornell/kfs/concur/service/impl/ConcurReportsServiceImpl.java
@@ -6,12 +6,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.HttpMethod;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.apache.commons.lang.StringUtils;
+import org.glassfish.jersey.client.ClientConfig;
 import org.kuali.kfs.coreservice.framework.parameter.ParameterService;
 import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.rice.krad.util.KRADConstants;
@@ -33,18 +36,23 @@ import edu.cornell.kfs.concur.service.ConcurAccessTokenService;
 import edu.cornell.kfs.concur.service.ConcurReportsService;
 import edu.cornell.kfs.sys.CUKFSConstants;
 import edu.cornell.kfs.sys.CUKFSParameterKeyConstants;
-import edu.cornell.kfs.sys.service.impl.DisposableClientServiceImplBase;
 import edu.cornell.kfs.sys.util.CURestClientUtils;
 
-public class ConcurReportsServiceImpl extends DisposableClientServiceImplBase implements ConcurReportsService, DisposableBean {
+public class ConcurReportsServiceImpl implements ConcurReportsService, DisposableBean {
     
     private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(ConcurReportsServiceImpl.class);
     protected ConcurAccessTokenService concurAccessTokenService;
     protected ParameterService parameterService;
+    private volatile Client client;
     private String concurExpenseWorkflowUpdateNamespace;
     private String concurRequestWorkflowUpdateNamespace;
     private String concurFailedRequestQueueEndpoint;
     private String concurFailedRequestDeleteNotificationEndpoint;
+
+    @Override
+    public void destroy() throws Exception {
+        closeClientQuietly();
+    }
 
     @Override
     public ConcurReport extractConcurReport(String reportURI) {
@@ -287,6 +295,36 @@ public class ConcurReportsServiceImpl extends DisposableClientServiceImplBase im
         } finally {
             CURestClientUtils.closeQuietly(response);
         }
+    }
+
+    protected Client getClient() {
+        // Use double-checked locking to lazy-load the Client object, similar to related locking in Rice.
+        // See effective java 2nd ed. pg. 71
+        Client jerseyClient = client;
+        if (jerseyClient == null) {
+            synchronized (this) {
+                jerseyClient = client;
+                if (jerseyClient == null) {
+                    ClientConfig clientConfig = new ClientConfig();
+                    jerseyClient = ClientBuilder.newClient(clientConfig);
+                    client = jerseyClient;
+                }
+            }
+        }
+        return jerseyClient;
+    }
+
+    protected void closeClientQuietly() {
+        // Use double-checked locking to retrieve the Client object, similar to related locking in Rice.
+        // See effective java 2nd ed. pg. 71
+        Client jerseyClient = client;
+        if (jerseyClient == null) {
+            synchronized (this) {
+                jerseyClient = client;
+            }
+        }
+        
+        CURestClientUtils.closeQuietly(jerseyClient);
     }
 
     private String addConcurMessageHeaderAndTruncate(String message, int maxLength) {
