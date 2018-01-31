a5fb64fa16c1eeb7c0e875a7b77165f5b6be8069
Andrew Dickson <ajd299@cornell.edu>
2018-01-31 11:15:47 -0500

    Cleaning up Java code based on best practices

--- src/main/java/edu/cornell/kfs/fp/document/validation/impl/TotalAmountNonZeroValidation.java
+++ src/main/java/edu/cornell/kfs/fp/document/validation/impl/TotalAmountNonZeroValidation.java
@@ -1,14 +1,17 @@
 package edu.cornell.kfs.fp.document.validation.impl;
 
-import edu.cornell.kfs.sys.CUKFSKeyConstants;
 import org.kuali.kfs.krad.util.GlobalVariables;
+import org.kuali.kfs.krad.util.ObjectUtils;
 import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.kfs.sys.document.AccountingDocument;
 import org.kuali.kfs.sys.document.validation.GenericValidation;
 import org.kuali.kfs.sys.document.validation.event.AttributedDocumentEvent;
 import org.kuali.rice.core.api.util.type.KualiDecimal;
 
+import edu.cornell.kfs.sys.CUKFSKeyConstants;
+
 public class TotalAmountNonZeroValidation extends GenericValidation {
+
     private AccountingDocument accountingDocumentForValidation;
 
     public boolean validate(AttributedDocumentEvent event) {
@@ -17,16 +20,20 @@ public class TotalAmountNonZeroValidation extends GenericValidation {
 
         boolean ret = true;
 
-        if(sourceTotalAmount == null || !sourceTotalAmount.isNonZero()){
-            GlobalVariables.getMessageMap().putError(KFSConstants.AMOUNT_PROPERTY_NAME, CUKFSKeyConstants.ERROR_ZERO_AMOUNT_TOTAL, "From", sourceTotalAmount.toString());
+        if (ObjectUtils.isNull(sourceTotalAmount) || sourceTotalAmount.isZero()) {
+            addGlobalThresholdValidationError("From", sourceTotalAmount);
             ret = false;
         }
-        if(targetTotalAmount == null || !targetTotalAmount.isNonZero()){
-            GlobalVariables.getMessageMap().putError(KFSConstants.AMOUNT_PROPERTY_NAME, CUKFSKeyConstants.ERROR_ZERO_AMOUNT_TOTAL, "To", targetTotalAmount.toString());
+        if(ObjectUtils.isNull(targetTotalAmount) || targetTotalAmount.isZero()){
+            addGlobalThresholdValidationError("To", targetTotalAmount);
             ret = false;
         }
 
         return ret;
+    }
+
+    private void addGlobalThresholdValidationError(String FromOrTo, KualiDecimal totalAmount){
+        GlobalVariables.getMessageMap().putError(KFSConstants.AMOUNT_PROPERTY_NAME, CUKFSKeyConstants.ERROR_ZERO_AMOUNT_TOTAL, FromOrTo, totalAmount.toString());
     }
 
     public AccountingDocument getAccountingDocumentForValidation() {
