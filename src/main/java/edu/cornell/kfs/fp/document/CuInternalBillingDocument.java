0e3e86aea8d7cb2dfdee163e3a1ee12a92855e9e
Andrew Dickson <ajd299@cornell.edu>
2018-02-02 16:56:57 -0500

    Prepend disapproved into description

--- src/main/java/edu/cornell/kfs/fp/document/CuInternalBillingDocument.java
+++ src/main/java/edu/cornell/kfs/fp/document/CuInternalBillingDocument.java
@@ -2,6 +2,7 @@ package edu.cornell.kfs.fp.document;
 
 import org.kuali.kfs.krad.bo.DocumentHeader;
 import org.kuali.kfs.sys.KFSConstants;
+import org.kuali.rice.kew.api.document.DocumentStatus;
 import org.kuali.rice.kew.framework.postprocessor.DocumentRouteStatusChange;
 import org.kuali.kfs.fp.document.InternalBillingDocument;
 
@@ -16,8 +17,11 @@ public class CuInternalBillingDocument extends InternalBillingDocument {
         super.doRouteStatusChange(statusChangeEvent);
 
         DocumentHeader documentHeader = getDocumentHeader();
-        if(documentHeader.getWorkflowDocument().isDisapproved()){
-            String newDescription = ("Disapproved: " + documentHeader.getDocumentDescription()).substring(0, KFSConstants.getMaxLengthOfDocumentDescription());
+        if (documentHeader.getWorkflowDocument().isDisapproved() && statusChangeEvent.getNewRouteStatus().equals(DocumentStatus.DISAPPROVED.getCode())) {
+            String newDescription = "Disapproved: " + documentHeader.getDocumentDescription();
+            if(newDescription.length() > KFSConstants.getMaxLengthOfDocumentDescription()){
+                newDescription = newDescription.substring(0, KFSConstants.getMaxLengthOfDocumentDescription());
+            }
             documentHeader.setDocumentDescription(newDescription);
         }
     }
