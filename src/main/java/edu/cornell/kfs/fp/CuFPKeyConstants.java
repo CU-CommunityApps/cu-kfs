0c97fe4eb838e71010e5631d039158db41786dfc
Jay Hulslander <jdh34@cornell.edu>
2018-02-09 10:09:15 -0500

    KFSPTS-10464 prevent DBCP route errors from stoping next cbcp docs from routing, and adding report (#515)

--- src/main/java/edu/cornell/kfs/fp/CuFPKeyConstants.java
+++ src/main/java/edu/cornell/kfs/fp/CuFPKeyConstants.java
@@ -5,4 +5,9 @@ public class CuFPKeyConstants {
     public static final String ERROR_CBCP_BATCH_UPLOAD_EMAIL_SUBJECT = "error.cbcp.batch.upload.email.subject";
     public static final String ERROR_CBCP_BATCH_UPLOAD_EMAIL_BODY_STARTER = "error.cbcp.batch.upload.email.body.starter";
     public static final String ERROR_CREATE_ACCOUNTING_DOCUMENT_ATTACHMENT_DOWNLOAD = "error.create.accounting.document.attachment.download";
+    public static final String MESSAGE_CBCP_ROUTE_STEP_REPORT_SUPMMARY_SUBHEADER = "message.cbcp.route.step.report.summary.subheader";
+    public static final String MESSAGE_CBCP_ROUTE_STEP_REPORT_SUPMMARY_DETAIL_LINE = "message.cbcp.route.step.report.summary.detail.line";
+    public static final String MESSAGE_CBCP_ROUTE_STEP_REPORT_ERROR_SUBHEADER = "message.cbcp.route.step.report.error.subheader";
+    public static final String MESSAGE_CBCP_ROUTE_STEP_REPORT_ERROR_DETAIL_LINE = "message.cbcp.route.step.report.error.detail.line";
+    public static final String MESSAGE_CBCP_ROUTE_STEP_REPORT_NO_ERRORS = "message.cbcp.route.step.report.no.errors";
 }
