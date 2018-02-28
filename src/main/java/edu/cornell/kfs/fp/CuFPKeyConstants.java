4c14d59f26f6a72b82d1bedffc31272a971024d6
db483 <db483@cornell.edu>
2018-02-28 19:04:44 +0000

    KFSPTS-11109 add user friendly message for xml error (#529)

--- src/main/java/edu/cornell/kfs/fp/CuFPKeyConstants.java
+++ src/main/java/edu/cornell/kfs/fp/CuFPKeyConstants.java
@@ -29,4 +29,5 @@ public class CuFPKeyConstants {
     public static final String REPORT_CREATE_ACCOUNTING_DOCUMENT_SHARED_DOCUMENT_TYPE = "report.create.accounting.document.shared.document.type";
     public static final String REPORT_CREATE_ACCOUNTING_DOCUMENT_SHARED_DOCUMENT_DESCRIPTION = "report.create.accounting.document.shared.document.description";
     public static final String REPORT_CREATE_ACCOUNTING_DOCUMENT_SHARED_DOCUMENT_EXPLANATION = "report.create.accounting.document.shared.document.explanation";
+    public static final String REPORT_CREATE_ACCOUNTING_DOCUMENT_XML_PROCESSING_ERROR = "report.create.accounting.document.xml.processing.error";
 }
