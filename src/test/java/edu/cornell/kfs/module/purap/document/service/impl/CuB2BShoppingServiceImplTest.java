f1af2deadb5594a3a8e1798cb172a2dd6f19aa4c
Andrew Dickson <ajd299@cornell.edu>
2018-03-08 10:26:05 -0500

    Added test for multiple accounts on multiple items

--- src/test/java/edu/cornell/kfs/module/purap/document/service/impl/CuB2BShoppingServiceImplTest.java
+++ src/test/java/edu/cornell/kfs/module/purap/document/service/impl/CuB2BShoppingServiceImplTest.java
@@ -3,6 +3,7 @@ package edu.cornell.kfs.module.purap.document.service.impl;
 import java.util.List;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.kuali.kfs.module.purap.businessobject.PurApAccountingLine;
 import org.kuali.kfs.module.purap.businessobject.RequisitionItem;
 import org.kuali.kfs.module.purap.document.RequisitionDocument;
 import org.kuali.kfs.module.purap.util.cxml.B2BShoppingCart;
@@ -46,13 +47,29 @@ public class CuB2BShoppingServiceImplTest extends KualiTestBase {
 		assertFalse(CollectionUtils.isEmpty(cuRequisitionDocuments));
 	}
 
-	public void testCheckRequisitionAccountsAreUnique() throws Exception {
+    public void testCheckRequisitionAccountsAreUnique() throws Exception {
         B2BShoppingCart cart = CuB2BShoppingCartFixture.B2B_CART_WITH_DUPLICATE_ITEM.createB2BShoppingCart();
         cart.addShoppingCartItem(B2BShoppingCartItemFixture.B2B_CART_ITEM_DUPLICATE.createB2BShoppingCartItem());
 
         List<CuRequisitionDocument> cuRequisitionDocuments = b2bShoppingService.createRequisitionsFromCxml(cart, UserNameFixture.ccs1.getPerson());
         List requisitionAccounts = ((RequisitionItem)cuRequisitionDocuments.get(0).getItems().get(0)).getSourceAccountingLines();
         requisitionAccounts.add(requisitionAccounts.get(0));
+
+        assertFalse(b2bShoppingService.checkRequisitionAccountsAreUnique(cuRequisitionDocuments.get(0)));
+    }
+
+    public void testCheckRequisitionAccountsAreUniqueMultipleItems() throws Exception {
+        B2BShoppingCart cart = CuB2BShoppingCartFixture.B2B_CART_WITH_DUPLICATE_ITEM.createB2BShoppingCart();
+        cart.addShoppingCartItem(B2BShoppingCartItemFixture.B2B_CART_ITEM_DUPLICATE.createB2BShoppingCartItem());
+
+        List<CuRequisitionDocument> cuRequisitionDocuments = b2bShoppingService.createRequisitionsFromCxml(cart, UserNameFixture.ccs1.getPerson());
+        List<RequisitionItem> requisitionItems = cuRequisitionDocuments.get(0).getItems();
+        assertTrue("Expected more than one RequisitionItem", requisitionItems.size() > 1);
+
+        for (RequisitionItem requisitionItem : requisitionItems){
+            List<PurApAccountingLine> requisitionAccounts = requisitionItem.getSourceAccountingLines();
+            requisitionAccounts.add(requisitionAccounts.get(0));
+        }
 
         assertFalse(b2bShoppingService.checkRequisitionAccountsAreUnique(cuRequisitionDocuments.get(0)));
     }
