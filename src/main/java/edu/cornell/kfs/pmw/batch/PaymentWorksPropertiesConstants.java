04355262dfc752fa32868057df4afb321ba131f7
nkimble <nkk4@cornell.edu>
2018-03-15 10:45:32 -0400

    KFSPTS-9545: Control by KFS system parameter PMW_INTEGRATION_IS_ACTIVE_IND the tracking of Approve/Disapprove/Cancel actions of PVEN for new domestic DV vendors whose data orignated from PMW or AVF or hand keyed. (#541)

--- src/main/java/edu/cornell/kfs/pmw/batch/PaymentWorksPropertiesConstants.java
+++ src/main/java/edu/cornell/kfs/pmw/batch/PaymentWorksPropertiesConstants.java
@@ -4,10 +4,15 @@ public class PaymentWorksPropertiesConstants {
     
     public static final class PaymentWorksVendor {
         public static final String PMW_VENDOR_REQUEST_ID = "pmwVendorRequestId";
+        public static final String KFS_VENDOR_DOCUMENT_NUMBER = "kfsVendorDocumentNumber";
     }
 
     public static final class PaymentWorksFieldMapping {
         public static final String PMW_FIELD_ID = "paymentWorksFieldId";
     }
     
+    public static final class PaymentWorksIsoCountryToFipsCountryAssociation {
+        public static final String FIPS_CNTRY_CD = "fipsCountryCode";
+    }
+
 }
