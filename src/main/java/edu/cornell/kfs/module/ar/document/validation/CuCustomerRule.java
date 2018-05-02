4812dd4757079d36070d79094abd1fd75b9392ca
Daniela Balmus <db483@cornell.edu>
2018-05-02 13:58:51 +0100

    KFSPTS-11615 customize CustomerRule so that it will allow spaces in the first 3 characters of the customer name

---
+++ src/main/java/edu/cornell/kfs/module/ar/document/validation/CuCustomerRule.java
@@ -0,0 +1,22 @@
+package edu.cornell.kfs.module.ar.document.validation;
+
+import org.kuali.kfs.krad.util.GlobalVariables;
+import org.kuali.kfs.module.ar.document.validation.impl.CustomerRule;
+import org.kuali.kfs.module.ar.ArKeyConstants;
+import org.kuali.kfs.module.ar.ArPropertyConstants;
+import org.kuali.kfs.sys.KFSConstants;
+
+public class CuCustomerRule extends CustomerRule {
+    
+    @Override
+    public boolean checkNameIsValidLength(String customerName) {
+        boolean success = true;
+        if (customerName.length() < 3) {
+            success = false;
+            GlobalVariables.getMessageMap().putError(KFSConstants.MAINTENANCE_NEW_MAINTAINABLE + ArPropertyConstants.CustomerFields.CUSTOMER_NAME, ArKeyConstants.CustomerConstants.ERROR_CUSTOMER_NAME_LESS_THAN_THREE_CHARACTERS);
+        }
+
+        return success;
+    }
+
+}
