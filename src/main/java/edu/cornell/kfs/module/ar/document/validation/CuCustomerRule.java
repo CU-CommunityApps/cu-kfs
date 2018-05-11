381f48157f6b0c161129cfe23030e6e140ba2d1e
Daniela Balmus <db483@cornell.edu>
2018-05-11 10:42:59 +0100

    KFSPTS-11615 add more changes

--- src/main/java/edu/cornell/kfs/module/ar/document/validation/CuCustomerRule.java
+++ src/main/java/edu/cornell/kfs/module/ar/document/validation/CuCustomerRule.java
@@ -5,18 +5,47 @@ import org.kuali.kfs.module.ar.document.validation.impl.CustomerRule;
 import org.kuali.kfs.module.ar.ArKeyConstants;
 import org.kuali.kfs.module.ar.ArPropertyConstants;
 import org.kuali.kfs.sys.KFSConstants;
+import org.kuali.kfs.kns.document.MaintenanceDocument;
+import org.kuali.kfs.krad.util.MessageMap;
 
 public class CuCustomerRule extends CustomerRule {
     
+    /**
+     * @see org.kuali.rice.kns.maintenance.rules.MaintenanceDocumentRuleBase#processCustomRouteDocumentBusinessRules(org.kuali.rice.kns.document.MaintenanceDocument)
+     */
     @Override
-    public boolean checkNameIsValidLength(String customerName) {
-        boolean success = true;
-        if (customerName.length() < 3) {
-            success = false;
-            GlobalVariables.getMessageMap().putError(KFSConstants.MAINTENANCE_NEW_MAINTAINABLE + ArPropertyConstants.CustomerFields.CUSTOMER_NAME, ArKeyConstants.CustomerConstants.ERROR_CUSTOMER_NAME_LESS_THAN_THREE_CHARACTERS);
+    protected boolean processCustomRouteDocumentBusinessRules(MaintenanceDocument document) {
+
+        boolean isValid = true;
+        isValid &= super.processCustomRouteDocumentBusinessRules(document);
+        MessageMap errorMap = GlobalVariables.getMessageMap();
+        //negate the return value from hasErrors() becase when there are no errors
+        //the method returns false so we need to negate the resuls otherwise
+        //out validations will fail.
+        isValid &= !errorMap.hasErrors();
+        if (isValid) {
+            initializeAttributes(document);
+            isValid &= checkCustomerHasAddress(newCustomer);
+
+            if (isValid) {
+                isValid &= validateAddresses(newCustomer);
+            }
+
+            if (isValid) {
+                isValid &= checkAddresses(newCustomer);
+            }
+
+            if (isValid) {
+                isValid &= checkTaxNumber(newCustomer);
+            }
+
+            if (isValid) {
+                isValid &= checkStopWorkReason();
+            }
         }
 
-        return success;
+        return isValid;
     }
+
 
 }
