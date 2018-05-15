5d11b8014d8e2273758052f5c7cf8ccc381cdebf
db483 <db483@cornell.edu>
2018-05-15 13:54:01 +0100

    KFSPTS-11615 remove BRs for customer name, change customer number to be numeric

---
+++ src/main/java/edu/cornell/kfs/module/ar/batch/service/impl/CuCustomerLoadServiceImpl.java
@@ -0,0 +1,27 @@
+package edu.cornell.kfs.module.ar.batch.service.impl;
+
+import edu.cornell.kfs.module.ar.document.validation.CuCustomerRule;
+
+import org.kuali.kfs.kns.document.MaintenanceDocument;
+import org.kuali.kfs.module.ar.batch.report.CustomerLoadBatchErrors;
+import org.kuali.kfs.module.ar.batch.service.impl.CustomerLoadServiceImpl;
+import org.kuali.kfs.module.ar.document.validation.impl.CustomerRule;
+
+public class CuCustomerLoadServiceImpl extends CustomerLoadServiceImpl {
+    
+    @Override
+    protected boolean validateSingle(MaintenanceDocument maintDoc, CustomerLoadBatchErrors batchErrors, String customerName) {
+        boolean result = true;
+
+        //  get an instance of the business rule
+        CustomerRule rule = new CuCustomerRule();
+
+        //  run the business rules
+        result &= rule.processRouteDocument(maintDoc);
+
+        extractGlobalVariableErrors(batchErrors, customerName);
+
+        return result;
+    }
+
+}
