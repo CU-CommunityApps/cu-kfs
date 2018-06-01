a48cac9dfb54ddd61701f2d97c80f21f4e647a8c
Jay Hulslander <jdh34@cornell.edu>
2018-06-01 15:50:08 -0400

    adding in object balance search

---
+++ src/main/java/edu/cornell/kfs/coa/businessobject/CuObjectCodeGlobalSearch.java
@@ -0,0 +1,39 @@
+package edu.cornell.kfs.coa.businessobject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kuali.kfs.coa.businessobject.ObjectCode;
+import org.kuali.kfs.gl.businessobject.Balance;
+import org.kuali.rice.core.api.util.type.KualiDecimal;
+
+public class CuObjectCodeGlobalSearch extends ObjectCode {
+    private static final long serialVersionUID = 7461668856007571783L;
+    
+    protected List<Balance> balances;
+    //private transient KualiDecimal calculatedBalance;
+    
+    public CuObjectCodeGlobalSearch() {
+        super();
+        balances = new ArrayList<Balance>();
+        //calculatedBalance = KualiDecimal.ZERO;
+    }
+
+    public List<Balance> getBalances() {
+        return balances;
+    }
+
+    public void setBalances(List<Balance> balances) {
+        this.balances = balances;
+    }
+
+    public KualiDecimal getCalculatedBalance() {
+        KualiDecimal calculatedBalance = KualiDecimal.ZERO;
+        for (Balance balance : balances) {
+            calculatedBalance = calculatedBalance.add(balance.getAccountLineAnnualBalanceAmount());
+        }
+        return calculatedBalance;
+    }
+
+
+}
