983f15c7bd3294d817544a0b0e68281ad91764b4
Andrew Dickson <33495813+ajd299@users.noreply.github.com>
2018-06-12 16:18:16 -0400

    KFSPTS-12220: Fixes to Error handling for ContractsGrantsInvoiceCreateDocument Job (#588)

---
+++ src/main/java/edu/cornell/kfs/module/ar/batch/service/impl/CuContractsGrantsInvoiceCreateDocumentServiceImpl.java
@@ -0,0 +1,73 @@
+package edu.cornell.kfs.module.ar.batch.service.impl;
+
+import org.kuali.kfs.integration.cg.ContractsAndGrantsBillingAward;
+import org.kuali.kfs.integration.cg.ContractsAndGrantsBillingAwardAccount;
+import org.kuali.kfs.krad.util.ObjectUtils;
+import org.kuali.kfs.module.ar.businessobject.ContractsGrantsInvoiceDocumentErrorLog;
+import org.kuali.kfs.module.ar.businessobject.ContractsGrantsInvoiceDocumentErrorMessage;
+import org.kuali.kfs.module.ar.service.impl.ContractsGrantsInvoiceCreateDocumentServiceImpl;
+import org.kuali.kfs.sys.businessobject.SystemOptions;
+import org.kuali.rice.core.api.util.type.KualiDecimal;
+import org.springframework.util.CollectionUtils;
+
+import java.sql.Date;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+public class CuContractsGrantsInvoiceCreateDocumentServiceImpl extends ContractsGrantsInvoiceCreateDocumentServiceImpl {
+
+    /**
+     * Fixes NullPointerException that can occur when getting the award total amount.
+     * award.getAwardTotalAmount().bigDecimalValue() causes the NullPointerException if the total amount returned null.
+     */
+    @Override
+    protected void storeValidationErrors(Map<ContractsAndGrantsBillingAward, List<String>> invalidGroup, Collection<ContractsGrantsInvoiceDocumentErrorLog> contractsGrantsInvoiceDocumentErrorLogs, String creationProcessTypeCode) {
+        for (ContractsAndGrantsBillingAward award : invalidGroup.keySet()) {
+            KualiDecimal cumulativeExpenses = KualiDecimal.ZERO;
+            ContractsGrantsInvoiceDocumentErrorLog contractsGrantsInvoiceDocumentErrorLog = new ContractsGrantsInvoiceDocumentErrorLog();
+
+            if (ObjectUtils.isNotNull(award)) {
+                Date beginningDate = award.getAwardBeginningDate();
+                Date endingDate = award.getAwardEndingDate();
+                final SystemOptions systemOptions = optionsService.getCurrentYearOptions();
+
+                contractsGrantsInvoiceDocumentErrorLog.setProposalNumber(award.getProposalNumber());
+                contractsGrantsInvoiceDocumentErrorLog.setAwardBeginningDate(beginningDate);
+                contractsGrantsInvoiceDocumentErrorLog.setAwardEndingDate(endingDate);
+                KualiDecimal awardTotalAmount = award.getAwardTotalAmount() == null ? KualiDecimal.ZERO : award.getAwardTotalAmount();
+                contractsGrantsInvoiceDocumentErrorLog.setAwardTotalAmount(awardTotalAmount.bigDecimalValue());
+                if (ObjectUtils.isNotNull(award.getAwardPrimaryFundManager())) {
+                    contractsGrantsInvoiceDocumentErrorLog.setPrimaryFundManagerPrincipalId(award.getAwardPrimaryFundManager().getPrincipalId());
+                }
+                if (!CollectionUtils.isEmpty(award.getActiveAwardAccounts())) {
+                    boolean firstLineFlag = true;
+
+                    for (ContractsAndGrantsBillingAwardAccount awardAccount : award.getActiveAwardAccounts()) {
+
+                        cumulativeExpenses = cumulativeExpenses.add(contractsGrantsInvoiceDocumentService.getBudgetAndActualsForAwardAccount(awardAccount, systemOptions.getActualFinancialBalanceTypeCd(), beginningDate));
+                        if (firstLineFlag) {
+                            firstLineFlag = false;
+                            contractsGrantsInvoiceDocumentErrorLog.setAccounts(awardAccount.getAccountNumber());
+                        } else {
+                            contractsGrantsInvoiceDocumentErrorLog.setAccounts(contractsGrantsInvoiceDocumentErrorLog.getAccounts() + ";" + awardAccount.getAccountNumber());
+                        }
+                    }
+                }
+                contractsGrantsInvoiceDocumentErrorLog.setCumulativeExpensesAmount(cumulativeExpenses.bigDecimalValue());
+            }
+
+            for (String vCat : invalidGroup.get(award)) {
+                ContractsGrantsInvoiceDocumentErrorMessage contractsGrantsInvoiceDocumentErrorCategory = new ContractsGrantsInvoiceDocumentErrorMessage();
+                contractsGrantsInvoiceDocumentErrorCategory.setErrorMessageText(vCat);
+                contractsGrantsInvoiceDocumentErrorLog.getErrorMessages().add(contractsGrantsInvoiceDocumentErrorCategory);
+            }
+
+            contractsGrantsInvoiceDocumentErrorLog.setErrorDate(dateTimeService.getCurrentTimestamp());
+            contractsGrantsInvoiceDocumentErrorLog.setCreationProcessTypeCode(creationProcessTypeCode);
+            businessObjectService.save(contractsGrantsInvoiceDocumentErrorLog);
+            contractsGrantsInvoiceDocumentErrorLogs.add(contractsGrantsInvoiceDocumentErrorLog);
+        }
+    }
+
+}
