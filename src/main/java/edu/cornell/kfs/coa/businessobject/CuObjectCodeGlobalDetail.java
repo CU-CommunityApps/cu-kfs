0a461d935d9564fb463fee4413d7a5b717f2aed7
Jay Hulslander <jdh34@cornell.edu>
2018-06-20 15:13:27 -0400

    KFSPTS-9100 new document for multiple object code activate/inactivate (#585)

---
+++ src/main/java/edu/cornell/kfs/coa/businessobject/CuObjectCodeGlobalDetail.java
@@ -0,0 +1,17 @@
+package edu.cornell.kfs.coa.businessobject;
+
+import org.kuali.kfs.coa.businessobject.ObjectCodeGlobalDetail;
+
+public class CuObjectCodeGlobalDetail extends ObjectCodeGlobalDetail {
+    private static final long serialVersionUID = 7971543974891105660L;
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("CuObjectCodeGlobalDetail: ");
+        sb.append("chart of accounts :").append(getChartOfAccountsCode());
+        sb.append(" fiscal year: ").append(getUniversityFiscalYear());
+        sb.append(" object code: ").append(getFinancialObjectCode());
+        return sb.toString();
+    }
+
+}
