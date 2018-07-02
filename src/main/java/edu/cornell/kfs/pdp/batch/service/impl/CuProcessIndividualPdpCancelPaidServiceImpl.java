53930f67e2c20e42351ba5516d843514486a3a92
cah292 <cah292@users.noreply.github.com>
2018-07-02 05:24:56 -0400

    KFSPTS-12428: Upgrade to 2018-06-14 version of financials (#595)

---
+++ src/main/java/edu/cornell/kfs/pdp/batch/service/impl/CuProcessIndividualPdpCancelPaidServiceImpl.java
@@ -0,0 +1,67 @@
+package edu.cornell.kfs.pdp.batch.service.impl;
+
+import java.sql.Date;
+
+import org.kuali.kfs.krad.util.ObjectUtils;
+import org.kuali.kfs.pdp.PdpConstants;
+import org.kuali.kfs.pdp.batch.service.impl.ProcessIndividualPdpCancelPaidServiceImpl;
+import org.kuali.kfs.pdp.businessobject.PaymentDetail;
+import org.kuali.kfs.sys.batch.service.PaymentSourceToExtractService;
+import org.kuali.kfs.sys.document.PaymentSource;
+import org.kuali.rice.kew.api.exception.WorkflowException;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.cornell.kfs.integration.purap.CuPurchasingAccountsPayableModuleService;
+import edu.cornell.kfs.pdp.businessobject.PaymentDetailExtendedAttribute;
+
+public class CuProcessIndividualPdpCancelPaidServiceImpl extends ProcessIndividualPdpCancelPaidServiceImpl {
+
+    private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(CuProcessIndividualPdpCancelPaidServiceImpl.class);
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void processPdpCancel(Date processDate, PaymentDetail paymentDetail) {
+        String documentTypeCode = paymentDetail.getFinancialDocumentTypeCode();
+        String documentNumber = paymentDetail.getCustPaymentDocNbr();
+
+        boolean primaryCancel = paymentDetail.getPrimaryCancelledPayment();
+        boolean disbursedPayment = PdpConstants.PaymentStatusCodes.CANCEL_PAYMENT.equals(paymentDetail.getPaymentGroup().getPaymentStatusCode());
+
+        //KFSPTS-2719
+        boolean crCancel = false;
+        PaymentDetailExtendedAttribute paymentDetailExtendedAttribute = (PaymentDetailExtendedAttribute) paymentDetail.getExtension();
+        if (ObjectUtils.isNotNull(paymentDetailExtendedAttribute)) {
+            crCancel = paymentDetailExtendedAttribute.getCrCancelledPayment();
+        }
+
+        if (purchasingAccountsPayableModuleService.isPurchasingBatchDocument(documentTypeCode)) {
+            ((CuPurchasingAccountsPayableModuleService) purchasingAccountsPayableModuleService).handlePurchasingBatchCancels(
+                    documentNumber, documentTypeCode, primaryCancel, disbursedPayment, crCancel);
+        } else {
+            PaymentSourceToExtractService<PaymentSource> extractService = getPaymentSourceToExtractService(paymentDetail);
+            if (extractService != null) {
+                try {
+                    PaymentSource dv = (PaymentSource) documentService.getByDocumentHeaderId(documentNumber);
+                    if (dv != null) {
+                        if (disbursedPayment || primaryCancel || crCancel) {
+                            if (!crCancel) {
+                                extractService.cancelPayment(dv, processDate);
+                            }
+                        } else {
+                            extractService.resetFromExtraction(dv);
+                        }
+                    }
+                } catch (WorkflowException we) {
+                    throw new RuntimeException("Could not retrieve document #" + documentNumber, we);
+                }
+            } else {
+                LOG.warn("processPdpCancel() Unknown document type (" + documentTypeCode + ") for document ID: " + documentNumber);
+                return;
+            }
+        }
+
+        paymentGroupService.processCancelledGroup(paymentDetail.getPaymentGroup(), processDate);
+    }
+
+}
