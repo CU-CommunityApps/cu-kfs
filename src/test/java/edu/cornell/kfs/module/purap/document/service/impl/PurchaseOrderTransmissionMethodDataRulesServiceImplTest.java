8c537afbedad71326e13d6fa560e69ed8e715023
Andrew Dickson <ajd299@cornell.edu>
2018-07-03 15:13:22 -0400

    Remove import

--- src/test/java/edu/cornell/kfs/module/purap/document/service/impl/PurchaseOrderTransmissionMethodDataRulesServiceImplTest.java
+++ src/test/java/edu/cornell/kfs/module/purap/document/service/impl/PurchaseOrderTransmissionMethodDataRulesServiceImplTest.java
@@ -1,6 +1,7 @@
 package edu.cornell.kfs.module.purap.document.service.impl;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
