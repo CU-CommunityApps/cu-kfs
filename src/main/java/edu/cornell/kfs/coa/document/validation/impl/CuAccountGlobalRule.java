888177052f199615f6a9097c2fada3829d87ade6
Jay Hulslander <jdh34@cornell.edu>
2018-08-21 14:04:54 -0400

    improvements

--- src/main/java/edu/cornell/kfs/coa/document/validation/impl/CuAccountGlobalRule.java
+++ src/main/java/edu/cornell/kfs/coa/document/validation/impl/CuAccountGlobalRule.java
@@ -8,34 +8,44 @@ import org.kuali.kfs.coa.document.validation.impl.AccountGlobalRule;
 import org.kuali.kfs.kns.document.MaintenanceDocument;
 
 import edu.cornell.kfs.sys.CUKFSKeyConstants;
+import edu.cornell.kfs.sys.CUKFSParameterKeyConstants;
+import edu.cornell.kfs.sys.CUKFSPropertyConstants;
 
+@SuppressWarnings("deprecation")
 public class CuAccountGlobalRule extends AccountGlobalRule {
     
     @Override
     protected boolean processCustomRouteDocumentBusinessRules(MaintenanceDocument document) {
         boolean valid = super.processCustomRouteDocumentBusinessRules(document);
-        if (StringUtils.isNotBlank(newAccountGlobal.getAccountRestrictedStatusCode()) && StringUtils.isBlank(newAccountGlobal.getSubFundGroupCode())) {
+        if (doesAccountGlobalHaveAccountRestrictionCodeAndNoSubFUndGroupCode()) {
             valid = isTheNewRestrictionCodeValidForEachAccount() && valid;
         }
         return valid;
+    }
+
+    protected boolean doesAccountGlobalHaveAccountRestrictionCodeAndNoSubFUndGroupCode() {
+        return StringUtils.isNotBlank(newAccountGlobal.getAccountRestrictedStatusCode()) && StringUtils.isBlank(newAccountGlobal.getSubFundGroupCode());
     }
     
     protected boolean isTheNewRestrictionCodeValidForEachAccount() {
         boolean valid = true;
         int accountDetailIndex = 0;
         for (AccountGlobalDetail detail : newAccountGlobal.getAccountGlobalDetails()) {
-            String accountSubFundGroupRestrictionCode = detail.getAccount().getSubFundGroup().getAccountRestrictedStatusCode();
+            String subFundDefaultRestriictionCode = detail.getAccount().getSubFundGroup().getAccountRestrictedStatusCode();
             String accountGlobalRestrictionCode = newAccountGlobal.getAccountRestrictedStatusCode();
-            if (StringUtils.isNotBlank(accountSubFundGroupRestrictionCode) && 
-                    !StringUtils.equalsAnyIgnoreCase(accountSubFundGroupRestrictionCode, accountGlobalRestrictionCode)) {
+            if (ifSubFundDefaultRestrictionCodeExistsDoesItDifferFromAccountGlobalRestrictionCode(subFundDefaultRestriictionCode, accountGlobalRestrictionCode)) {
                 valid = false;
-                //String fieldNameFormat = "accountGlobalDetails[{0}].accountNumber";
-                String fieldNameFormat = "accountGlobalDetails[{0}]";
-                putFieldError(MessageFormat.format(fieldNameFormat, String.valueOf(accountDetailIndex)), 
-                        CUKFSKeyConstants.ERROR_DOCUMENT_SUB_ACCOUNT_GLOBAL_DETAILS_INVALID_RESTRICTION_CODE_CHANGE, detail.getAccountNumber());
+                String accountSectionFieldName = MessageFormat.format(CUKFSPropertyConstants.ACCOUNT_GLOBAL_ACCOUNT_SECTION_FIELD_NAME_FORMAT, String.valueOf(accountDetailIndex));
+                putFieldError(accountSectionFieldName, CUKFSKeyConstants.ERROR_DOCUMENT_SUB_ACCOUNT_GLOBAL_DETAILS_INVALID_RESTRICTION_CODE_CHANGE, detail.getAccountNumber());
             }
             accountDetailIndex++;
         }
         return valid;
+    }
+
+    protected boolean ifSubFundDefaultRestrictionCodeExistsDoesItDifferFromAccountGlobalRestrictionCode(
+            String subFundDefaultRestriictionCode, String accountGlobalRestrictionCode) {
+        return StringUtils.isNotBlank(subFundDefaultRestriictionCode) && 
+                !StringUtils.equalsIgnoreCase(subFundDefaultRestriictionCode, accountGlobalRestrictionCode);
     }
 }
