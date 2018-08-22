07392a07237ad699f33e555ae03fc2c0c4f4af33
Jay Hulslander <jdh34@cornell.edu>
2018-08-22 11:01:05 -0400

    KFSPTS-12866 change CINV default document description

--- src/main/java/edu/cornell/kfs/module/ar/batch/service/impl/CuContractsGrantsInvoiceCreateDocumentServiceImpl.java
+++ src/main/java/edu/cornell/kfs/module/ar/batch/service/impl/CuContractsGrantsInvoiceCreateDocumentServiceImpl.java
@@ -1,21 +1,32 @@
 package edu.cornell.kfs.module.ar.batch.service.impl;
 
+import java.sql.Date;
+import java.text.MessageFormat;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.kuali.kfs.integration.cg.ContractsAndGrantsBillingAward;
 import org.kuali.kfs.integration.cg.ContractsAndGrantsBillingAwardAccount;
+import org.kuali.kfs.krad.util.ErrorMessage;
 import org.kuali.kfs.krad.util.ObjectUtils;
 import org.kuali.kfs.module.ar.businessobject.ContractsGrantsInvoiceDocumentErrorLog;
 import org.kuali.kfs.module.ar.businessobject.ContractsGrantsInvoiceDocumentErrorMessage;
+import org.kuali.kfs.module.ar.businessobject.ContractsGrantsLetterOfCreditReviewDetail;
+import org.kuali.kfs.module.ar.businessobject.InvoiceAccountDetail;
+import org.kuali.kfs.module.ar.document.ContractsGrantsInvoiceDocument;
 import org.kuali.kfs.module.ar.service.impl.ContractsGrantsInvoiceCreateDocumentServiceImpl;
 import org.kuali.kfs.sys.businessobject.SystemOptions;
 import org.kuali.rice.core.api.util.type.KualiDecimal;
 import org.springframework.util.CollectionUtils;
 
-import java.sql.Date;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import edu.cornell.kfs.module.arl.CuArParameterConstants;
 
 public class CuContractsGrantsInvoiceCreateDocumentServiceImpl extends ContractsGrantsInvoiceCreateDocumentServiceImpl {
+    private static final Logger LOG = LogManager.getLogger(CuContractsGrantsInvoiceCreateDocumentServiceImpl.class);
 
     /**
      * Fixes NullPointerException that can occur when getting the award total amount.
@@ -68,6 +79,48 @@ public class CuContractsGrantsInvoiceCreateDocumentServiceImpl extends Contracts
             businessObjectService.save(contractsGrantsInvoiceDocumentErrorLog);
             contractsGrantsInvoiceDocumentErrorLogs.add(contractsGrantsInvoiceDocumentErrorLog);
         }
+    }
+    
+    @Override
+    public ContractsGrantsInvoiceDocument createCGInvoiceDocumentByAwardInfo(ContractsAndGrantsBillingAward awd,
+            List<ContractsAndGrantsBillingAwardAccount> accounts, String chartOfAccountsCode, String organizationCode,
+            List<ErrorMessage> errorMessages, List<ContractsGrantsLetterOfCreditReviewDetail> accountDetails, String locCreationType) {
+        LOG.info("createCGInvoiceDocumentByAwardInfo, entering");
+        ContractsGrantsInvoiceDocument cgInvoiceDocument = 
+                super.createCGInvoiceDocumentByAwardInfo(awd, accounts, chartOfAccountsCode, organizationCode, errorMessages, accountDetails, locCreationType);
+        if (ObjectUtils.isNotNull(cgInvoiceDocument)) {
+            populateDocumentDescription(cgInvoiceDocument);
+        }
+        
+        return cgInvoiceDocument;
+    }
+
+    protected void populateDocumentDescription(ContractsGrantsInvoiceDocument cgInvoiceDocument) {
+        String proposalNumber = cgInvoiceDocument.getInvoiceGeneralDetail().getProposalNumber();
+        if (StringUtils.isNotBlank(proposalNumber)) {
+            String contractControlAccount = findContractControlAccountNumber(cgInvoiceDocument.getAccountDetails());
+            String newTitle =  MessageFormat.format(findTitleFormatString(), proposalNumber, contractControlAccount);
+            if (LOG.isInfoEnabled()) {
+                LOG.info("populateDocumentDescription, on CINV document " + cgInvoiceDocument.getDocumentNumber() + " resetting description to " + newTitle);
+            }
+            cgInvoiceDocument.getDocumentHeader().setDocumentDescription(newTitle);
+        } else {
+            LOG.error("populateDocumentDescription, unable to do get a proposal number for CINV document " + cgInvoiceDocument.getDocumentNumber());
+        }
+    }
+    
+    protected String findContractControlAccountNumber(List<InvoiceAccountDetail> details) {
+        for (InvoiceAccountDetail detail : details) {
+            if (StringUtils.isNotBlank(detail.getContractControlAccountNumber())) {
+                return detail.getContractControlAccountNumber();
+            }
+        }
+        LOG.error("findContractControlAccountNumber, could not find contract control account number");
+        return StringUtils.EMPTY;
+    }
+    
+    protected String findTitleFormatString() {
+        return getConfigurationService().getPropertyValueAsString(CuArParameterConstants.CONTRACTS_GRANTS_INVOICE_DOCUMENT_TITLE_FORMAT);
     }
 
 }
