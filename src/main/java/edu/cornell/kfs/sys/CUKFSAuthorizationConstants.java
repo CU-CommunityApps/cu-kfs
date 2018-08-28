6d09ad0abd8e6e0a27c7fd95ab335c2fefc50932
db483 <db483@cornell.edu>
2018-08-28 22:16:22 +0100

    KFSPTS-12996 hide invoice fields on DV and RCDV docs (#634)

--- src/main/java/edu/cornell/kfs/sys/CUKFSAuthorizationConstants.java
+++ src/main/java/edu/cornell/kfs/sys/CUKFSAuthorizationConstants.java
@@ -6,6 +6,7 @@ public class CUKFSAuthorizationConstants  {
 
     public static class DisbursementVoucherEditMode extends TransactionalEditMode {
         public static final String TRAVEL_SYSTEM_GENERATED_ENTRY = "travelSystemGeneratedEntry";
+        public static final String DISPLAY_INVOICE_FIELDS = "displayInvoiceFields";
 
     }
     
