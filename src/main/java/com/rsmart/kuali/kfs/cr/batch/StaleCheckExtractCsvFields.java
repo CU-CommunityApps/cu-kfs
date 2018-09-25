41d671b24aa55370b2ed94e05930b336a5b13016
Andrew Dickson <33495813+ajd299@users.noreply.github.com>
2018-09-25 14:14:36 -0400

    Revert "KFSPTS-12390 Stale Check Extract CSV Upload (#642)" (#647)
    This reverts commit 727b3dde639631d528e0f2191d8fc05ab99b7cf8.

--- src/main/java/com/rsmart/kuali/kfs/cr/batch/StaleCheckExtractCsvFields.java
+++
@@ -1,9 +0,0 @@
-package com.rsmart.kuali.kfs.cr.batch;
-
-public enum StaleCheckExtractCsvFields {
-    CHECK_ISSUED_DATE,
-    BANK_CODE,
-    CHECK_STATUS,
-    CHECK_NUMBER,
-    CHECK_TOTAL_AMOUNT
-}
