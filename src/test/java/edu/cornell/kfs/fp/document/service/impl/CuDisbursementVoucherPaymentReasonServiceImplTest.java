231e667bcb7f650a664b92d5003ea91a6d12f37f
Jay Hulslander <jdh34@cornell.edu>
2018-10-17 13:02:18 -0400

    Revert "start to refactor CuDisbursementVoucherPaymentReasonServiceImplTest.java"
    This reverts commit 39165bd830eeb4316bb66ca6bb0fcddb8e2a21f5.

--- src/test/java/edu/cornell/kfs/fp/document/service/impl/CuDisbursementVoucherPaymentReasonServiceImplTest.java
+++ src/test/java/edu/cornell/kfs/fp/document/service/impl/CuDisbursementVoucherPaymentReasonServiceImplTest.java
@@ -7,39 +7,27 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.kuali.kfs.fp.document.service.DisbursementVoucherPayeeService;
 import org.kuali.kfs.fp.document.service.impl.DisbursementVoucherPaymentReasonServiceImpl;
 import org.kuali.kfs.sys.ConfigureContext;
 import org.kuali.kfs.sys.context.KualiTestBase;
-import org.mockito.Mockito;
 
-import static org.junit.Assert.*;
-
-
-public class CuDisbursementVoucherPaymentReasonServiceImplTest{
+@ConfigureContext(session = ccs1)
+public class CuDisbursementVoucherPaymentReasonServiceImplTest extends KualiTestBase {
 	private CuDisbursementVoucherPaymentReasonServiceImpl cuDisbursementVoucherPaymentReasonService;
 	private DisbursementVoucherPaymentReasonServiceImpl disbursementVoucherPaymentReasonService;
 	private Class cuDisbursementVoucherPaymentReasonServiceImplClass;
     private Class DisbursementVoucherPaymentReasonServiceImplClass;
 	
-	@Before
-    public void setUp() throws Exception {
+	@Override
+    protected void setUp() throws Exception {
+        super.setUp();
         disbursementVoucherPaymentReasonService = DisbursementVoucherPaymentReasonServiceImpl.class.newInstance();
         cuDisbursementVoucherPaymentReasonService = CuDisbursementVoucherPaymentReasonServiceImpl.class.newInstance();
-        cuDisbursementVoucherPaymentReasonService.setDisbursementVoucherPayeeService(buildMockDisbursementVoucherPayeeService());
         cuDisbursementVoucherPaymentReasonServiceImplClass = CuDisbursementVoucherPaymentReasonServiceImpl.class;  
         DisbursementVoucherPaymentReasonServiceImplClass = DisbursementVoucherPaymentReasonServiceImpl.class;
-	} 
+	}          
 	
-	private DisbursementVoucherPayeeService buildMockDisbursementVoucherPayeeService() {
-	    DisbursementVoucherPayeeService payeeService = Mockito.mock(DisbursementVoucherPayeeService.class);
-	    Mockito.when(payeeService.getPayeeTypeDescription(Mockito.anyString())).thenReturn("Test");
-	    return payeeService;
-	}
-	
-	@Test
+                  
 	public void test(){
 		
        Collection<String> payeeTypeCds = new ArrayList<String>();
