6f9fa08fe4c16abb45e7fa8a2649f19ee1cc2abf
Jay Hulslander <jdh34@cornell.edu>
2018-10-18 10:06:50 -0400

    KFSPTS-5546 remove spring context calls from CuDisbursementVoucherPaymentReasonServiceImpl (#663)

--- src/test/java/edu/cornell/kfs/fp/document/service/impl/CuDisbursementVoucherPaymentReasonServiceImplTest.java
+++ src/test/java/edu/cornell/kfs/fp/document/service/impl/CuDisbursementVoucherPaymentReasonServiceImplTest.java
@@ -1,85 +1,104 @@
 package edu.cornell.kfs.fp.document.service.impl;
 
-import static org.kuali.kfs.sys.fixture.UserNameFixture.ccs1;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.kuali.kfs.fp.document.service.DisbursementVoucherPayeeService;
 import org.kuali.kfs.fp.document.service.impl.DisbursementVoucherPaymentReasonServiceImpl;
-import org.kuali.kfs.sys.ConfigureContext;
-import org.kuali.kfs.sys.context.KualiTestBase;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
 
-@ConfigureContext(session = ccs1)
-public class CuDisbursementVoucherPaymentReasonServiceImplTest extends KualiTestBase {
-	private CuDisbursementVoucherPaymentReasonServiceImpl cuDisbursementVoucherPaymentReasonService;
-	private DisbursementVoucherPaymentReasonServiceImpl disbursementVoucherPaymentReasonService;
-	private Class cuDisbursementVoucherPaymentReasonServiceImplClass;
-    private Class DisbursementVoucherPaymentReasonServiceImplClass;
-	
-	@Override
-    protected void setUp() throws Exception {
-        super.setUp();
-        disbursementVoucherPaymentReasonService = DisbursementVoucherPaymentReasonServiceImpl.class.newInstance();
-        cuDisbursementVoucherPaymentReasonService = CuDisbursementVoucherPaymentReasonServiceImpl.class.newInstance();
-        cuDisbursementVoucherPaymentReasonServiceImplClass = CuDisbursementVoucherPaymentReasonServiceImpl.class;  
-        DisbursementVoucherPaymentReasonServiceImplClass = DisbursementVoucherPaymentReasonServiceImpl.class;
-	}          
-	
-                  
-	public void test(){
-		
-       Collection<String> payeeTypeCds = new ArrayList<String>();
-               
-       payeeTypeCds.add("E");
-       payeeTypeCds.add("F");
-       payeeTypeCds.add("T");
-       payeeTypeCds.add("V");
-       payeeTypeCds.add("E");
-       payeeTypeCds.add("F");
-       payeeTypeCds.add("X");
-       payeeTypeCds.add("S");       
-        
-        Method eduMethod = null;
-        Method orgMethod = null;
-		try {
-				eduMethod = cuDisbursementVoucherPaymentReasonServiceImplClass.getDeclaredMethod("getDescriptivePayeeTypesAsString", Collection.class);
-				orgMethod = DisbursementVoucherPaymentReasonServiceImpl.class.getDeclaredMethod("getDescriptivePayeeTypesAsString", Collection.class);
-				eduMethod.setAccessible(true);
-				orgMethod.setAccessible(true);
-				
-		} catch (NoSuchMethodException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SecurityException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+import static org.junit.Assert.*;
 
-		Object eduResult = null;
-		Object orgResult = null;
-        
-		try {
-			eduResult = eduMethod.invoke(cuDisbursementVoucherPaymentReasonService, payeeTypeCds);
-			orgResult = orgMethod.invoke(disbursementVoucherPaymentReasonService, payeeTypeCds);
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-       
-		if (eduResult.toString().length() >= orgResult.toString().length()) {
-			fail("The edu method should return a smaller string");
-		}
-		
+public class CuDisbursementVoucherPaymentReasonServiceImplTest {
+    private CuDisbursementVoucherPaymentReasonServiceImpl cuDisbursementVoucherPaymentReasonService;
+    private TestableDisbursementVoucherPaymentReasonServiceImpl disbursementVoucherPaymentReasonService;
+    private Map<String, String> payeeTypeCodeMap;
 
-	}
+    @Before
+    public void setUp() throws Exception {
+        populatePayeetypeCodeMap();
+        disbursementVoucherPaymentReasonService = new TestableDisbursementVoucherPaymentReasonServiceImpl();
+        disbursementVoucherPaymentReasonService.setDisbursementVoucherPayeeService(buildMockDisbursementVoucherPayeeService());
+        cuDisbursementVoucherPaymentReasonService = new CuDisbursementVoucherPaymentReasonServiceImpl();
+        cuDisbursementVoucherPaymentReasonService.setDisbursementVoucherPayeeService(buildMockDisbursementVoucherPayeeService());
+    }
     
+    @After
+    public void tearDown() {
+        disbursementVoucherPaymentReasonService = null;
+        disbursementVoucherPaymentReasonService = null;
+        payeeTypeCodeMap = null;
+    }
+
+    private void populatePayeetypeCodeMap() {
+        payeeTypeCodeMap = new HashMap<String, String>();
+        payeeTypeCodeMap.put("E", "Employees Students Alumni");
+        payeeTypeCodeMap.put("S", "Employees Students Alumni");
+        payeeTypeCodeMap.put("A", "Employees Students Alumni");
+        payeeTypeCodeMap.put("V", "Vendor");
+    }
+
+    private DisbursementVoucherPayeeService buildMockDisbursementVoucherPayeeService() {
+        DisbursementVoucherPayeeService payeeService = Mockito.mock(DisbursementVoucherPayeeService.class);
+        Mockito.when(payeeService.getPayeeTypeDescription(Mockito.any())).then(this::findPayeeTypeDescription);
+        return payeeService;
+    }
+    
+    private String findPayeeTypeDescription(InvocationOnMock invocation) {
+        String payeeTypeCode = invocation.getArgument(0);
+        if (payeeTypeCodeMap.containsKey(payeeTypeCode)) {
+            return payeeTypeCodeMap.get(payeeTypeCode);
+        }
+        return StringUtils.EMPTY;
+    }
+    
+    @Test
+    public void testGetDescriptivePayeeTypesAsString() {
+        Collection<String> payeeTypeCds = buildPayeeTypeCodeCollection();
+
+        String eduResult = cuDisbursementVoucherPaymentReasonService.getDescriptivePayeeTypesAsString(payeeTypeCds);
+        String orgResult = disbursementVoucherPaymentReasonService.getDescriptivePayeeTypesAsString(payeeTypeCds);
+
+        if (eduResult.toString().length() >= orgResult.toString().length()) {
+            fail("The edu method should return a smaller string");
+        }
+
+    }
+
+    protected Collection<String> buildPayeeTypeCodeCollection() {
+        Collection<String> payeeTypeCds = new ArrayList<String>();
+        payeeTypeCds.add("E");
+        payeeTypeCds.add("F");
+        payeeTypeCds.add("T");
+        payeeTypeCds.add("V");
+        payeeTypeCds.add("E");
+        payeeTypeCds.add("F");
+        payeeTypeCds.add("X");
+        payeeTypeCds.add("S");
+        return payeeTypeCds;
+    }
+
+    public class TestableDisbursementVoucherPaymentReasonServiceImpl extends DisbursementVoucherPaymentReasonServiceImpl {
+
+        @Override
+        public String getDescriptivePayeeTypesAsString(Collection<String> payeeTypeCodes) {
+            List<String> payeeTypeDescriptions = new ArrayList<>();
+
+            for (String payeeTypeCode : payeeTypeCodes) {
+                String description = disbursementVoucherPayeeService.getPayeeTypeDescription(payeeTypeCode);
+                payeeTypeDescriptions.add(description);
+            }
+
+            return this.convertListToString(payeeTypeDescriptions);
+        }
+    }
 
 }
