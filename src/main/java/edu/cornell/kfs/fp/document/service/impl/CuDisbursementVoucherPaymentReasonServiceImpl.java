ac2079481b90e16a785803e0e3a015d68ab6e196
Jay Hulslander <jdh34@cornell.edu>
2018-10-18 10:06:50 -0400

    KFSPTS-5546 remove spring context calls from CuDisbursementVoucherPaymentReasonServiceImpl (#663)

--- src/main/java/edu/cornell/kfs/fp/document/service/impl/CuDisbursementVoucherPaymentReasonServiceImpl.java
+++ src/main/java/edu/cornell/kfs/fp/document/service/impl/CuDisbursementVoucherPaymentReasonServiceImpl.java
@@ -4,24 +4,21 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import org.kuali.kfs.fp.document.service.DisbursementVoucherPayeeService;
 import org.kuali.kfs.fp.document.service.impl.DisbursementVoucherPaymentReasonServiceImpl;
-import org.kuali.kfs.sys.context.SpringContext;
 
 public class CuDisbursementVoucherPaymentReasonServiceImpl extends DisbursementVoucherPaymentReasonServiceImpl {
-
+    
+    @Override
     protected String getDescriptivePayeeTypesAsString(Collection<String> payeeTypeCodes) {
         List<String> payeeTypeDescriptions = new ArrayList<>();
 
         for (String payeeTypeCode : payeeTypeCodes) {
-            String description = SpringContext.getBean(DisbursementVoucherPayeeService.class).getPayeeTypeDescription(payeeTypeCode);
+            String description = disbursementVoucherPayeeService.getPayeeTypeDescription(payeeTypeCode);
             if (payeeTypeDescriptions.indexOf(description) == -1) {
                 payeeTypeDescriptions.add(description);
             }
         }
 
-        return this.convertListToString(payeeTypeDescriptions);
+        return convertListToString(payeeTypeDescriptions);
     }
-    
-
 }
