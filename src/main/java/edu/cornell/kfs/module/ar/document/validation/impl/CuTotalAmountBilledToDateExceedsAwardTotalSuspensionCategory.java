9a473f92dd9447123d7885aa5b89b91bb254c11a
Jay Hulslander <jdh34@cornell.edu>
2018-10-29 14:50:31 -0400

    cleanup and completing unit tests

--- src/main/java/edu/cornell/kfs/module/ar/document/validation/impl/CuTotalAmountBilledToDateExceedsAwardTotalSuspensionCategory.java
+++ src/main/java/edu/cornell/kfs/module/ar/document/validation/impl/CuTotalAmountBilledToDateExceedsAwardTotalSuspensionCategory.java
@@ -19,12 +19,12 @@ public class CuTotalAmountBilledToDateExceedsAwardTotalSuspensionCategory extend
     @Override
     public boolean shouldSuspend(ContractsGrantsInvoiceDocument contractsGrantsInvoiceDocument) {
         if (shouldValidateOnBudgetTotal()) {
-            LOG.info("shouldSuspend, validating based on award budget total");
+            LOG.debug("shouldSuspend, validating based on award budget total");
             CuAward cuAward = (CuAward) contractsGrantsInvoiceDocument.getInvoiceGeneralDetail().getAward();
             AwardExtendedAttribute awardExtension = (AwardExtendedAttribute) cuAward.getExtension();
             return contractsGrantsInvoiceDocument.getInvoiceGeneralDetail().getTotalAmountBilledToDate().isGreaterThan(awardExtension.getBudgetTotalAmount());
         } else {
-            LOG.info("shouldSuspend, validating based on ward total");
+            LOG.debug("shouldSuspend, validating based on ward total");
             return super.shouldSuspend(contractsGrantsInvoiceDocument);
         }
     }
@@ -32,11 +32,11 @@ public class CuTotalAmountBilledToDateExceedsAwardTotalSuspensionCategory extend
     protected boolean shouldValidateOnBudgetTotal() {
         Boolean validateOnBudgetTotal = parameterService.getParameterValueAsBoolean(ArConstants.AR_NAMESPACE_CODE, ArConstants.CONTRACTS_GRANTS_INVOICE_COMPONENT, CuArConstants.CG_INVOICE_AMT_BILLED_SUSPENION_BY_BUDGET_TOTAL);
         
-        if (LOG.isInfoEnabled()) {
+        if (LOG.isDebugEnabled()) {
             if (validateOnBudgetTotal == null) {
-                LOG.info("shouldValidateOnBudgetTotal, no parameter named " + CuArConstants.CG_INVOICE_AMT_BILLED_SUSPENION_BY_BUDGET_TOTAL + " in KFS-AR name space, so defaulting to true.");
+                LOG.debug("shouldValidateOnBudgetTotal, no parameter named " + CuArConstants.CG_INVOICE_AMT_BILLED_SUSPENION_BY_BUDGET_TOTAL + " in KFS-AR name space, so defaulting to true.");
             } else {
-                LOG.info("shouldValidateOnBudgetTotal, the value of " + CuArConstants.CG_INVOICE_AMT_BILLED_SUSPENION_BY_BUDGET_TOTAL + " is " + validateOnBudgetTotal.booleanValue());
+                LOG.debug("shouldValidateOnBudgetTotal, the value of " + CuArConstants.CG_INVOICE_AMT_BILLED_SUSPENION_BY_BUDGET_TOTAL + " is " + validateOnBudgetTotal.booleanValue());
             }
         }
         
