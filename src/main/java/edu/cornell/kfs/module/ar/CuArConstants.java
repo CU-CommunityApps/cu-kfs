3417c969f9c1ce9dcfa619146a42ee409df04c98
Jay Hulslander <jdh34@cornell.edu>
2018-10-30 17:46:06 -0400

    KFSPTS-13257 award invoice suspension by award budget total (#670)

---
+++ src/main/java/edu/cornell/kfs/module/ar/CuArConstants.java
@@ -0,0 +1,7 @@
+package edu.cornell.kfs.module.ar;
+
+public class CuArConstants {
+    
+    public static final String CG_INVOICE_AMT_BILLED_SUSPENSION_BY_BUDGET_TOTAL = "CG_INVOICE_AMT_BILLED_SUSPENSION_BY_BUDGET_TOTAL";
+
+}
