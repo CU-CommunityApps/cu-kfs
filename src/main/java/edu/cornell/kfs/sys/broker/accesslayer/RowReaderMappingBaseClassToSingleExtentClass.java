1547ee83fc5ec458cc2a41abbaae26b21027dfce
cah292 <cah292@cornell.edu>
2018-11-01 14:12:09 -0400

    KFSPTS-13687: Force OJB Award references to map to CuAward

---
+++ src/main/java/edu/cornell/kfs/sys/broker/accesslayer/RowReaderMappingBaseClassToSingleExtentClass.java
@@ -0,0 +1,55 @@
+package edu.cornell.kfs.sys.broker.accesslayer;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.ojb.broker.PersistenceBrokerException;
+import org.apache.ojb.broker.accesslayer.RowReaderDefaultImpl;
+import org.apache.ojb.broker.metadata.ClassDescriptor;
+
+/**
+ * Helper RowReader implementation that forces OJB queries against the base class
+ * to instead go against one specific extent class. Useful for situations where
+ * a custom BO subclass has been introduced and it is stored in the same table,
+ * but overriding the various BO service and DAO calls to reference
+ * the new subclass is impractical. It also eliminates the need for storing
+ * a column containing the object implementation classname, which would normally
+ * be required when storing a class and all its subclasses in the same table.
+ * 
+ * The base class descriptor that uses this implementation must have
+ * exactly one extent class defined in its OJB metadata.
+ */
+public class RowReaderMappingBaseClassToSingleExtentClass extends RowReaderDefaultImpl {
+
+    private static final long serialVersionUID = -6193233283850750044L;
+
+    public RowReaderMappingBaseClassToSingleExtentClass(ClassDescriptor classDescriptor) {
+        super(classDescriptor);
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Override
+    protected ClassDescriptor selectClassDescriptor(Map row) throws PersistenceBrokerException {
+        ClassDescriptor baseClassDescriptor = getClassDescriptor();
+        List<?> extentClassNames = baseClassDescriptor.getExtentClassNames();
+        if (extentClassNames.size() != 1) {
+            throw new PersistenceBrokerException("The class descriptor for " + baseClassDescriptor.getClassNameOfObject()
+                    + " should have had exactly 1 extent class, but instead had " + extentClassNames.size() + " extents: "
+                    + extentClassNames);
+        }
+        
+        String extentClassName = (String) extentClassNames.get(0);
+        ClassDescriptor extentClassDescriptor = baseClassDescriptor.getRepository().getDescriptorFor(extentClassName);
+        if (extentClassDescriptor == null) {
+            throw new PersistenceBrokerException("Could not find " + extentClassName
+                    + " extent class descriptor for base class " + baseClassDescriptor.getClassNameOfObject());
+        } else if (StringUtils.equals(baseClassDescriptor.getClassNameOfObject(), extentClassDescriptor.getClassNameOfObject())) {
+            throw new PersistenceBrokerException("The base class descriptor for " + baseClassDescriptor.getClassNameOfObject()
+                    + " should not have been selected as the implicit descriptor for extent class " + extentClassName);
+        }
+        
+        return extentClassDescriptor;
+    }
+
+}
