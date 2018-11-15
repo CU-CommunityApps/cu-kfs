21fe19f07324c00f01ac44dc6993151cfe041e72
Nancy Kimble <nkk4@cornell.edu>
2018-11-15 16:04:32 -0500

    KFSPTS-13246: Implementation of new button allowing user to delete a line from Applied Funds section of CGB PaymentApplication

---
+++ src/main/java/org/kuali/kfs/module/ar/dataaccess/InvoicePaidAppliedDao.java
@@ -0,0 +1,9 @@
+package org.kuali.kfs.module.ar.dataaccess;
+
+import org.kuali.kfs.module.ar.businessobject.InvoicePaidApplied;
+
+public interface InvoicePaidAppliedDao {
+    
+    void deleteAllInvoicePaidApplieds(String documentNumber);
+    
+}
