f1da9d96297ee68c7d5a08d5a86ccc247c7d9fdb
Jay Hulslander <jdh34@cornell.edu>
2018-11-21 08:40:19 -0500

    KFSPTS-13005 creating CU Customization of TimeBasedBillingPeriod

---
+++ src/main/java/org/kuali/kfs/module/ar/businessobject/TimeBasedBillingPeriod.java
@@ -0,0 +1,132 @@
+/**
+ * The Kuali Financial System, a comprehensive financial management system for higher education.
+ *
+ * Copyright 2005-2018 Kuali, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.kuali.kfs.module.ar.businessobject;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.joda.time.DateTimeZone;
+import org.joda.time.LocalDate;
+import org.kuali.kfs.coa.businessobject.AccountingPeriod;
+import org.kuali.kfs.coa.service.AccountingPeriodService;
+import org.kuali.kfs.module.ar.ArConstants;
+
+import java.sql.Date;
+
+public class TimeBasedBillingPeriod extends BillingPeriod {
+    
+    /*
+     * CU Customization
+     */
+    private static final Logger LOG = LogManager.getLogger(TimeBasedBillingPeriod.class);
+    
+    public TimeBasedBillingPeriod(ArConstants.BillingFrequencyValues billingFrequency, Date awardStartDate, Date currentDate, Date lastBilledDate, AccountingPeriodService accountingPeriodService) {
+        super(billingFrequency, awardStartDate, currentDate, lastBilledDate, accountingPeriodService);
+    }
+
+    @Override
+    protected Date determineEndDateByFrequency() {
+        final AccountingPeriod accountingPeriod = findPreviousAccountingPeriod(currentDate);
+        return accountingPeriod.getUniversityFiscalPeriodEndDate();
+    }
+
+    protected Integer calculatePreviousPeriodByFrequency(Integer currentAccountingPeriodCode, int periodsInBillingFrequency) {
+        Integer previousAccountingPeriodCode;
+        final int subAmt = (currentAccountingPeriodCode % periodsInBillingFrequency) == 0 ? periodsInBillingFrequency : currentAccountingPeriodCode % periodsInBillingFrequency;
+
+        previousAccountingPeriodCode = currentAccountingPeriodCode - subAmt;
+        return previousAccountingPeriodCode;
+    }
+
+    @Override
+    protected boolean canThisBeBilledByBillingFrequency() {
+        if (ArConstants.BillingFrequencyValues.ANNUALLY.equals(billingFrequency) && accountingPeriodService.getByDate(lastBilledDate).getUniversityFiscalYear() >= accountingPeriodService.getByDate(currentDate).getUniversityFiscalYear()) {
+            return false;
+        } else if (StringUtils.equals(findPreviousAccountingPeriod(currentDate).getUniversityFiscalPeriodCode(), findPreviousAccountingPeriod(lastBilledDate).getUniversityFiscalPeriodCode()) &&
+            accountingPeriodService.getByDate(lastBilledDate).getUniversityFiscalYear().equals(accountingPeriodService.getByDate(currentDate).getUniversityFiscalYear())) {
+            return false;
+        }
+
+        return true;
+    }
+    
+    /*
+     * CU Customization
+     */
+    @Override
+    protected Date determineStartDateByFrequency() {
+        if (lastBilledDate == null) {
+            LOG.error("determineStartDateByFrequency, unable to calculate start date by frequency when the last billed date iis NULL");
+            throw new IllegalStateException("Can not calculate the start date when the last billed date is NULL");
+        }
+        
+        LocalDate localDateLastBilledDate = new LocalDate(lastBilledDate.getTime(), DateTimeZone.UTC);
+        LocalDate localDateNextStartDate = localDateLastBilledDate.plusDays(1);
+        Date sqlDateNextStartDate = new Date(localDateNextStartDate.toDate().getTime());
+        
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("determineStartDateByFrequency, lastBilledDate is: " + lastBilledDate + " and next start date is: " + sqlDateNextStartDate);
+        }
+        
+        
+        return sqlDateNextStartDate;
+        //AccountingPeriod lastBilledDateAccountingPeriod = findPreviousAccountingPeriod(lastBilledDate);
+        //return calculateNextDay(lastBilledDateAccountingPeriod.getUniversityFiscalPeriodEndDate());
+    }
+
+    protected AccountingPeriod findPreviousAccountingPeriod(final Date date) {
+        final AccountingPeriod currentAccountingPeriod = findAccountingPeriodBy(date);
+        final Integer currentAccountingPeriodCode = Integer.parseInt(currentAccountingPeriod.getUniversityFiscalPeriodCode());
+        Integer previousAccountingPeriodCode;
+        previousAccountingPeriodCode = findPreviousAccountingPeriodCode(currentAccountingPeriodCode);
+
+        Integer currentFiscalYear = currentAccountingPeriod.getUniversityFiscalYear();
+        if (previousAccountingPeriodCode == 0) {
+            previousAccountingPeriodCode = 12;
+            currentFiscalYear -= 1;
+        }
+
+        String periodCode;
+        if (previousAccountingPeriodCode < 10) {
+            periodCode = "0" + previousAccountingPeriodCode;
+        } else {
+            periodCode = "" + previousAccountingPeriodCode;
+        }
+
+        final AccountingPeriod previousAccountingPeriod = accountingPeriodService.getByPeriod(periodCode, currentFiscalYear);
+
+        return previousAccountingPeriod;
+    }
+
+    protected Integer findPreviousAccountingPeriodCode(Integer currentAccountingPeriodCode) {
+        Integer previousAccountingPeriodCode;
+        if (ArConstants.BillingFrequencyValues.MONTHLY.equals(billingFrequency) ||
+            ArConstants.BillingFrequencyValues.MILESTONE.equals(billingFrequency) ||
+            ArConstants.BillingFrequencyValues.PREDETERMINED_BILLING.equals(billingFrequency)) {
+            previousAccountingPeriodCode = calculatePreviousPeriodByFrequency(currentAccountingPeriodCode, 1);
+        } else if (ArConstants.BillingFrequencyValues.QUARTERLY.equals(billingFrequency)) {
+            previousAccountingPeriodCode = calculatePreviousPeriodByFrequency(currentAccountingPeriodCode, 3);
+        } else if (ArConstants.BillingFrequencyValues.SEMI_ANNUALLY.equals(billingFrequency)) {
+            previousAccountingPeriodCode = calculatePreviousPeriodByFrequency(currentAccountingPeriodCode, 6);
+        } else {
+            previousAccountingPeriodCode = calculatePreviousPeriodByFrequency(currentAccountingPeriodCode, 12);
+        }
+        return previousAccountingPeriodCode;
+    }
+}
