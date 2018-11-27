ab793e942e19f2039322518551d7b2999277f557
Jay Hulslander <jdh34@cornell.edu>
2018-11-27 10:55:11 -0500

    Revert "KFSPTS-13257 award invoice suspension by award budget total (#678)"
    This reverts commit fdc866a82d8ddfcd78ba2e070f7195e40e72b4c2.

--- src/main/java/edu/cornell/kfs/module/ar/CuArConstants.java
+++
@@ -1,7 +0,0 @@
-package edu.cornell.kfs.module.ar;
-
-public class CuArConstants {
-    
-    public static final String CG_INVOICE_AMT_BILLED_SUSPENSION_BY_BUDGET_TOTAL = "CG_INVOICE_AMT_BILLED_SUSPENSION_BY_BUDGET_TOTAL";
-
-}
