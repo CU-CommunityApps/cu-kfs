6cd70c51b835e5e57b195e8168ba655e7a95a436
db483 <db483@cornell.edu>
2018-12-11 15:44:45 +0000

    KFSPTS-13973 add changes to overlays

--- src/main/java/edu/cornell/kfs/module/cam/document/service/impl/CuAssetServiceImpl.java
+++ src/main/java/edu/cornell/kfs/module/cam/document/service/impl/CuAssetServiceImpl.java
@@ -6,13 +6,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.kuali.kfs.coreservice.framework.parameter.ParameterService;
+import org.kuali.kfs.krad.service.BusinessObjectService;
 import org.kuali.kfs.module.cam.CamsConstants;
 import org.kuali.kfs.module.cam.CamsPropertyConstants;
 import org.kuali.kfs.module.cam.businessobject.Asset;
 import org.kuali.kfs.module.cam.document.service.impl.AssetServiceImpl;
-import org.kuali.kfs.sys.context.SpringContext;
-import org.kuali.kfs.coreservice.framework.parameter.ParameterService;
-import org.kuali.kfs.krad.service.BusinessObjectService;
 
 import edu.cornell.kfs.module.cam.CuCamsConstants;
 
@@ -21,9 +20,9 @@ public class CuAssetServiceImpl extends AssetServiceImpl {
     private ParameterService parameterService;
 
     public List<Asset> findActiveAssetsMatchingTagNumber(String campusTagNumber) {
-        List<Asset> activeMatches = new ArrayList<Asset>();
+        List<Asset> activeMatches = new ArrayList<>();
         // find all assets matching this tag number
-        Map<String, String> params = new HashMap<String, String>();
+        Map<String, String> params = new HashMap<>();
         params.put(CamsPropertyConstants.Asset.CAMPUS_TAG_NUMBER, campusTagNumber);
         Collection<Asset> tagMatches = businessObjectService.findMatching(Asset.class, params);
         if (tagMatches != null && !tagMatches.isEmpty()) {
