a8eaaab75a5939fd7d731dc73247e411b78335a5
db483 <db483@cornell.edu>
2018-12-13 10:50:46 +0000

    KFSPTS-13973 cleanup

--- src/main/java/edu/cornell/kfs/module/receiptProcessing/batch/ReceiptProcessingCSVInputFileType.java
+++ src/main/java/edu/cornell/kfs/module/receiptProcessing/batch/ReceiptProcessingCSVInputFileType.java
@@ -23,6 +23,7 @@ import java.util.Map;
 import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.kuali.kfs.sys.batch.CsvBatchInputFileTypeBase;
 import org.kuali.kfs.sys.exception.ParseException;
 
 import edu.cornell.kfs.module.receiptProcessing.businessobject.ReceiptProcessing;
