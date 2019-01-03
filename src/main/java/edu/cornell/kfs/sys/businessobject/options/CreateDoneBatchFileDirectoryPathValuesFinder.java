3d986455ed7c3a067f82af53f3626fdbead3d48b
db483 <db483@cornell.edu>
2019-01-03 10:05:11 +0000

    KFSPTS-14093 cleanup

--- src/main/java/edu/cornell/kfs/sys/businessobject/options/CreateDoneBatchFileDirectoryPathValuesFinder.java
+++ src/main/java/edu/cornell/kfs/sys/businessobject/options/CreateDoneBatchFileDirectoryPathValuesFinder.java
@@ -1,16 +1,8 @@
 package edu.cornell.kfs.sys.businessobject.options;
 
-import edu.cornell.kfs.sys.service.CuBatchFileDirectoryService;
-import org.kuali.kfs.sys.context.SpringContext;
-import org.kuali.rice.core.api.util.KeyValue;
+import org.kuali.kfs.sys.businessobject.options.BatchFileDirectoryPathValuesFinder;
 
-import java.util.List;
-
-public class CreateDoneBatchFileDirectoryPathValuesFinder extends CuBatchFileDirectoryPathValuesFinder {
+public class CreateDoneBatchFileDirectoryPathValuesFinder extends BatchFileDirectoryPathValuesFinder {
 	
-	@Override
-	public List<KeyValue> getKeyValues() {
-		return SpringContext.getBean(CuBatchFileDirectoryService.class).buildBatchFileStagingDirectories();
-	}
 
 }
