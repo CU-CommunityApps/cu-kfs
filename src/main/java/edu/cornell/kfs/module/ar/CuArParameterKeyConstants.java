9ce38d05ba0c09a72d4fb42efd730223670d9e69
nkimble <nkk4@cornell.edu>
2019-01-08 14:16:44 -0500

    KFSPTS-13256: Mod to exclude expenditures associated with specific sub-funds from CG invoice calculation based on new KFS system parameter. (#707)

---
+++ src/main/java/edu/cornell/kfs/module/ar/CuArParameterKeyConstants.java
@@ -0,0 +1,5 @@
+package edu.cornell.kfs.module.ar;
+
+public class CuArParameterKeyConstants {
+    public static final String CG_INVOICING_EXCLUDE_EXPENSES_SUB_FUNDS = "CG_INVOICING_EXCLUDE_EXPENSES_SUB_FUNDS";
+}
