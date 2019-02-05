c0fe512131c66c026de7c2a958a96c8cbc398824
Jay Hulslander <jdh34@cornell.edu>
2019-02-05 14:37:44 -0500

    KFSPTS-14460 remove  CULaborObjectService, CULaborObjectDao, and related unit test (#731)

--- src/main/java/edu/cornell/kfs/module/ld/dataaccess/CULaborObjectDao.java
+++
@@ -1,9 +0,0 @@
-package edu.cornell.kfs.module.ld.dataaccess;
-
-import org.kuali.kfs.module.ld.businessobject.LaborObject;
-import org.kuali.kfs.module.ld.dataaccess.LaborObjectDao;
-
-public interface CULaborObjectDao  extends LaborObjectDao{
-
-	 public LaborObject getByPrimaryId( int fiscalYear, String chartCode,String objectCode );
-}
