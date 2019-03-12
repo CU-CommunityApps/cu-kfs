83a0e8bddb398ce046e859d4b99c0d05819b97c1
Jay Hulslander <jdh34@cornell.edu>
2019-03-12 14:37:52 -0400

    fixing

--- src/test/java/edu/cornell/kfs/gl/service/impl/fixture/EmailAdressTestValue.java
+++ src/test/java/edu/cornell/kfs/gl/service/impl/fixture/EmailAdressTestValue.java
@@ -1,20 +1,16 @@
 package edu.cornell.kfs.gl.service.impl.fixture;
 
 public enum EmailAdressTestValue {
-    THREE_PART_ADDRESS_GOOD("ww5@a.b.c.org", true),
-    ALPHANUMERIC_DOMAIN_GOOD("something.else@a2.com", true),
+    THREE_PART_DOMAIN_GOOD("ww5@a.b.c.org", true),
+    ALPHANUMERIC_DOMAIN_DOT_ADDRESS_GOOD("something.else@a2.com", true),
     ALPHANUMERIC_DOMAIN_UNDERSCORE_ADDRESS_GOOD("something_else@a2.com", true),
     ALPHANUMERIC_DOMAIN_HYPHEN_ADDRESS_GOOD("something-else@a2.com", true),
     UNDERSCORE_ADDRESS_GOOD("something_else@something.else.com", true),
     HYPHEN_DOCMAIN_GOOD("something-else@et-tu.com", true),
     NO_ADDRESS_BAD("@a.b.c.org", false),
     NO_DOMAIN_BAD("a", false),
-    //NO_ZONE_BAD("1@org", false),
-    //NO_ZONE2_BAD("1@a", false),
     SPACE_BAD(" @a", false),
-    //DOT_ADDRESS_BAD(".@a.org", false),
     HYPHEN_ADDRESS_BAD("\"-@a.org\"", false),
-    //HYPHEN_ZONE_BAD("something@a.o-rg", true),
     UNDERSCOREW_DOMAIN_BAD("someone@foo_bar.com", false);
     
     public final String emailAdress;
