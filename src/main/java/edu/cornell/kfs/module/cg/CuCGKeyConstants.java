7959027189a8c9d953483710c821a1f9cf78756a
nkimble <nkk4@cornell.edu>
2019-03-25 12:14:04 -0400

    KFSPTS-14893: Have error message when character 1 is a space, do not have an error message when character 2 or 3 is a space. (#757)

---
+++ src/main/java/edu/cornell/kfs/module/cg/CuCGKeyConstants.java
@@ -0,0 +1,8 @@
+package edu.cornell.kfs.module.cg;
+
+public class CuCGKeyConstants {
+    
+    public static class AgencyConstants {
+        public static final String ERROR_AGENCY_NAME_NOT_BLANK_OR_NO_SPACE_IN_FIRST_CHARACTER = "error.document.agency.agencyNameNotBlankOrNoSpaceInFirstCharacter";
+    }
+}
