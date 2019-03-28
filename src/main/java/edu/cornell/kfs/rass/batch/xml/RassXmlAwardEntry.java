97e078e1e593a9b0294063e00d08fe41f83747c8
Jay Hulslander <jdh34@cornell.edu>
2019-03-28 14:31:55 -0400

    removed some extra required flags;

--- src/main/java/edu/cornell/kfs/rass/batch/xml/RassXmlAwardEntry.java
+++ src/main/java/edu/cornell/kfs/rass/batch/xml/RassXmlAwardEntry.java
@@ -25,76 +25,76 @@ import edu.cornell.kfs.sys.xmladapters.RassStringToJavaShortDateTimeAdapter;
 @XmlRootElement(name = "Award", namespace = StringUtils.EMPTY)
 public class RassXmlAwardEntry {
     
-    @XmlElement(name = "Proposal_Number", namespace = StringUtils.EMPTY, required = false)
+    @XmlElement(name = "Proposal_Number", namespace = StringUtils.EMPTY)
     @XmlJavaTypeAdapter(NormalizedStringAdapter.class)
     private String proposalNumber;
     
-    @XmlElement(name = "Status", namespace = StringUtils.EMPTY, required = false)
+    @XmlElement(name = "Status", namespace = StringUtils.EMPTY)
     @XmlJavaTypeAdapter(NormalizedStringAdapter.class)
     private String status;
     
-    @XmlElement(name = "Agency_Number", namespace = StringUtils.EMPTY, required = false)
+    @XmlElement(name = "Agency_Number", namespace = StringUtils.EMPTY)
     @XmlJavaTypeAdapter(NormalizedStringAdapter.class)
     private String agencyNumber;
     
-    @XmlElement(name = "Project_Title", namespace = StringUtils.EMPTY, required = false)
+    @XmlElement(name = "Project_Title", namespace = StringUtils.EMPTY)
     @XmlJavaTypeAdapter(NormalizedStringAdapter.class)
     private String projectTitle;
     
-    @XmlElement(name = "Start_Date", namespace = StringUtils.EMPTY, required = true)
+    @XmlElement(name = "Start_Date", namespace = StringUtils.EMPTY)
     @XmlJavaTypeAdapter(RassStringToJavaShortDateTimeAdapter.class)
     private Date startDate;
     
-    @XmlElement(name = "Stop_Date", namespace = StringUtils.EMPTY, required = true)
+    @XmlElement(name = "Stop_Date", namespace = StringUtils.EMPTY)
     @XmlJavaTypeAdapter(RassStringToJavaShortDateTimeAdapter.class)
     private Date stopDate;
     
-    @XmlElement(name = "Direct_Cost_Amount", namespace = StringUtils.EMPTY, required = false)
+    @XmlElement(name = "Direct_Cost_Amount", namespace = StringUtils.EMPTY)
     @XmlJavaTypeAdapter(KualiDecimalNullPossibleXmlAdapter.class)
     private KualiDecimal directCostAmount;
     
-    @XmlElement(name = "Indirect_Cost_Amount", namespace = StringUtils.EMPTY, required = false)
+    @XmlElement(name = "Indirect_Cost_Amount", namespace = StringUtils.EMPTY)
     @XmlJavaTypeAdapter(KualiDecimalNullPossibleXmlAdapter.class)
     private KualiDecimal indirectCostAmount;
     
-    @XmlElement(name = "Total_Amount", namespace = StringUtils.EMPTY, required = false)
+    @XmlElement(name = "Total_Amount", namespace = StringUtils.EMPTY)
     @XmlJavaTypeAdapter(KualiDecimalNullPossibleXmlAdapter.class)
     private KualiDecimal totalAmount;
     
-    @XmlElement(name = "Grant_Number", namespace = StringUtils.EMPTY, required = false)
+    @XmlElement(name = "Grant_Number", namespace = StringUtils.EMPTY)
     @XmlJavaTypeAdapter(NormalizedStringAdapter.class)
     private String grantNumber;
     
-    @XmlElement(name = "Grant_Description", namespace = StringUtils.EMPTY, required = false)
+    @XmlElement(name = "Grant_Description", namespace = StringUtils.EMPTY)
     @XmlJavaTypeAdapter(NormalizedStringAdapter.class)
     private String grantDescription;
     
-    @XmlElement(name = "Federal_Pass_Through", namespace = StringUtils.EMPTY, required = false)
+    @XmlElement(name = "Federal_Pass_Through", namespace = StringUtils.EMPTY)
     @XmlJavaTypeAdapter(BooleanNullPossibleXmlAdapter.class)
     private Boolean federalPassThrough;
     
-    @XmlElement(name = "Federal_Pass_Through_Agency_Number", namespace = StringUtils.EMPTY, required = false)
+    @XmlElement(name = "Federal_Pass_Through_Agency_Number", namespace = StringUtils.EMPTY)
     @XmlJavaTypeAdapter(NormalizedStringAdapter.class)
     private String federalPassThroughAgencyNumber;
     
-    @XmlElement(name = "CFDA_Number", namespace = StringUtils.EMPTY, required = false)
+    @XmlElement(name = "CFDA_Number", namespace = StringUtils.EMPTY)
     @XmlJavaTypeAdapter(NormalizedStringAdapter.class)
     private String cfdaNumber;
     
-    @XmlElement(name = "Organization_Code", namespace = StringUtils.EMPTY, required = false)
+    @XmlElement(name = "Organization_Code", namespace = StringUtils.EMPTY)
     @XmlJavaTypeAdapter(NormalizedStringAdapter.class)
     private String organizationCode;
     
-    @XmlElement(name = "Cost_Share_Required", namespace = StringUtils.EMPTY, required = false)
+    @XmlElement(name = "Cost_Share_Required", namespace = StringUtils.EMPTY)
     @XmlJavaTypeAdapter(BooleanNullPossibleXmlAdapter.class)
     private Boolean costShareRequired;
     
-    @XmlElement(name = "Final_Fiscal_Report_Due_Date", namespace = StringUtils.EMPTY, required = true)
+    @XmlElement(name = "Final_Fiscal_Report_Due_Date", namespace = StringUtils.EMPTY)
     @XmlJavaTypeAdapter(RassStringToJavaShortDateTimeAdapter.class)
     private Date finalReportDueDate;
     
-    @XmlElementWrapper(name = "PI_and_CoPIs", namespace = StringUtils.EMPTY, required = true)
-    @XmlElement(name = "PI_or_CoPI", namespace = StringUtils.EMPTY, required = true)
+    @XmlElementWrapper(name = "PI_and_CoPIs", namespace = StringUtils.EMPTY)
+    @XmlElement(name = "PI_or_CoPI", namespace = StringUtils.EMPTY)
     private List<RassXMLAwardPiCoPiEntry> principalAndCoPrincipalInvestigators;
     
     public RassXmlAwardEntry() {
