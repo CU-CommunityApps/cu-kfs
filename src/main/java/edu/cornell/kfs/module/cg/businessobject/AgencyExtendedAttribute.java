78ee0e3443291f9f1bf8345c3768055ca15521db
cah292 <cah292@users.noreply.github.com>
2019-04-04 08:19:31 -0400

    KFSPTS-14894 Add Agency Extension and Agency Origin (#761)

---
+++ src/main/java/edu/cornell/kfs/module/cg/businessobject/AgencyExtendedAttribute.java
@@ -0,0 +1,47 @@
+package edu.cornell.kfs.module.cg.businessobject;
+
+import org.kuali.kfs.krad.bo.PersistableBusinessObjectExtensionBase;
+
+public class AgencyExtendedAttribute extends PersistableBusinessObjectExtensionBase {
+
+    private static final long serialVersionUID = 2063791496740952591L;
+
+    private String agencyNumber;
+    private String agencyCommonName;
+    private String agencyOriginCode;
+
+    private AgencyOrigin agencyOrigin;
+
+    public String getAgencyNumber() {
+        return agencyNumber;
+    }
+
+    public void setAgencyNumber(String agencyNumber) {
+        this.agencyNumber = agencyNumber;
+    }
+
+    public String getAgencyCommonName() {
+        return agencyCommonName;
+    }
+
+    public void setAgencyCommonName(String agencyCommonName) {
+        this.agencyCommonName = agencyCommonName;
+    }
+
+    public String getAgencyOriginCode() {
+        return agencyOriginCode;
+    }
+
+    public void setAgencyOriginCode(String agencyOriginCode) {
+        this.agencyOriginCode = agencyOriginCode;
+    }
+
+    public AgencyOrigin getAgencyOrigin() {
+        return agencyOrigin;
+    }
+
+    public void setAgencyOrigin(AgencyOrigin agencyOrigin) {
+        this.agencyOrigin = agencyOrigin;
+    }
+
+}
