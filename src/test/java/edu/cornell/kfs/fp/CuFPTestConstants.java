eedba351ab5d561c16081795cf5e10aba296b05c
Jay Hulslander <jdh34@cornell.edu>
2019-04-19 10:28:38 -0400

    KFSPTS-15091 enable YEBA in create accounting documents job (#774)

--- src/test/java/edu/cornell/kfs/fp/CuFPTestConstants.java
+++ src/test/java/edu/cornell/kfs/fp/CuFPTestConstants.java
@@ -8,6 +8,7 @@ public class CuFPTestConstants {
     public static final String TEST_CREDENTIAL_GROUP_CODE = "TESTGRP";
     public static final String AWS_CREDENTIAL_GROUP_CODE = "AWS-Bill";
     public static final String BUDGET_ADJUSTMENT_DOC_TYPE = "BA";
+    public static final String YEAR_END_BUDGET_ADJUSTMENT_DOC_TYPE = "YEBA";
     public static final String DISBURSEMENT_VOUCHER_DOC_TYPE = "DV";
     public static final int FY_2016 = 2016;
     public static final int FY_2018 = 2018;
--- src/test/java/edu/cornell/kfs/fp/batch/xml/fixture/AccountingDocumentMapping.java
+++ src/test/java/edu/cornell/kfs/fp/batch/xml/fixture/AccountingDocumentMapping.java
@@ -10,6 +10,7 @@ import org.kuali.kfs.fp.document.InternalBillingDocument;
 import org.kuali.kfs.fp.document.ServiceBillingDocument;
 import org.kuali.kfs.fp.document.TransferOfFundsDocument;
 import org.kuali.kfs.fp.document.YearEndDistributionOfIncomeAndExpenseDocument;
+import org.kuali.kfs.fp.document.YearEndBudgetAdjustmentDocument;
 import org.kuali.kfs.krad.bo.AdHocRoutePerson;
 import org.kuali.kfs.krad.bo.Note;
 import org.kuali.kfs.krad.document.Document;
@@ -25,6 +26,7 @@ import edu.cornell.kfs.fp.batch.service.impl.CuDistributionOfIncomeAndExpenseDoc
 import edu.cornell.kfs.fp.batch.service.impl.InternalBillingDocumentGenerator;
 import edu.cornell.kfs.fp.batch.service.impl.ServiceBillingDocumentGenerator;
 import edu.cornell.kfs.fp.batch.service.impl.TransferOfFundsDocumentGenerator;
+import edu.cornell.kfs.fp.batch.service.impl.CuYearEndBudgetAdjustmentDocumentGenerator;
 import edu.cornell.kfs.fp.businessobject.TestBudgetAdjustmentSourceAccountingLine;
 import edu.cornell.kfs.fp.businessobject.TestBudgetAdjustmentTargetAccountingLine;
 import edu.cornell.kfs.fp.document.CuBudgetAdjustmentDocument;
@@ -56,7 +58,10 @@ public enum AccountingDocumentMapping {
             TestableCuDisbursementVoucherDocumentGenerator::new),
     YEDI_DOCUMENT(KFSConstants.FinancialDocumentTypeCodes.YEAR_END_DISTRIBUTION_OF_INCOME_AND_EXPENSE,
             YearEndDistributionOfIncomeAndExpenseDocument.class, TestSourceAccountingLine.class, TestTargetAccountingLine.class,
-            CuYearEndDistributionOfIncomeAndExpenseDocumentGenerator::new);
+            CuYearEndDistributionOfIncomeAndExpenseDocumentGenerator::new),
+    YEBA_DOCUMENT(CuFPTestConstants.YEAR_END_BUDGET_ADJUSTMENT_DOC_TYPE,
+            YearEndBudgetAdjustmentDocument.class, TestBudgetAdjustmentSourceAccountingLine.class, TestBudgetAdjustmentTargetAccountingLine.class,
+            CuYearEndBudgetAdjustmentDocumentGenerator::new);
 
     public static final String MAPPING_ENUM_CONST_SUFFIX = "_DOCUMENT";
 
