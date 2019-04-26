492a2c9fa891273f8867d87a6a849b93741a9b20
cah292 <cah292@cornell.edu>
2019-04-26 12:48:16 -0400

    KFSPTS-15342 Initial changes for CINV due date improvements

--- src/main/java/edu/cornell/kfs/module/ar/CuArParameterKeyConstants.java
+++ src/main/java/edu/cornell/kfs/module/ar/CuArParameterKeyConstants.java
@@ -2,4 +2,5 @@ package edu.cornell.kfs.module.ar;
 
 public class CuArParameterKeyConstants {
     public static final String CG_INVOICING_EXCLUDE_EXPENSES_SUB_FUNDS = "CG_INVOICING_EXCLUDE_EXPENSES_SUB_FUNDS";
+    public static final String CG_INVOICE_TERMS_DUE_DATE = "CG_INVOICE_TERMS_DUE_DATE";
 }
