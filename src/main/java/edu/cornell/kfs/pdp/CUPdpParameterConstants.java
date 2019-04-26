75d2de2e9152427297043bd9d385ef147132c7e2
Jay Hulslander <jdh34@cornell.edu>
2019-04-26 09:57:43 -0400

    KFSPTS-15359 moving check address values to parameters (#782)

--- src/main/java/edu/cornell/kfs/pdp/CUPdpParameterConstants.java
+++ src/main/java/edu/cornell/kfs/pdp/CUPdpParameterConstants.java
@@ -13,6 +13,16 @@ public final class CUPdpParameterConstants {
     public static final String UPDATED_PAYEE_ACH_ACCOUNT_EMAIL_SUBJECT = "UPDATED_PAYEE_ACH_ACCOUNT_EMAIL_SUBJECT";
     public static final String UPDATED_PAYEE_ACH_ACCOUNT_EMAIL_BODY = "UPDATED_PAYEE_ACH_ACCOUNT_EMAIL_BODY";
     public static final String PDP_ACH_INVALID_EMAIL_ERROR_REPORT_TO_ADDRESSES = "PDP_ACH_INVALID_EMAIL_ERROR_REPORT_TO_ADDRESSES";
+    
+    public static final class CuPayeeAddressService {
+        public static final String CU_PAYEE_ADDRESS_SERVICE_COMPONENT = "CuPayeeAddressService";
+        public static final String PAYER_NAME_PARAMETER = "PAYER_NAME";
+        public static final String PAYER_ADDRESS_LINE1_PARAMETER = "PAYER_ADDRESS_LINE1";
+        public static final String PAYER_ADDRESS_LINE2_PARAMETER = "PAYER_ADDRESS_LINE2";
+        public static final String PAYER_CITY_PARAMETER = "PAYER_CITY";
+        public static final String PAYER_STATE_PARAMETER = "PAYER_STATE";
+        public static final String PAYER_ZIP_CODE_PARAMETER = "PAYER_ZIP_CODE";
+    }
 
     private CUPdpParameterConstants() {
         throw new UnsupportedOperationException("Do not call");
