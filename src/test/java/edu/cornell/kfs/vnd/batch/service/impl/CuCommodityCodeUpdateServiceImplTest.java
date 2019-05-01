3b2f68a4126686a2a2899982c5403c253a3f55b4
Jay Hulslander <jdh34@cornell.edu>
2019-05-01 11:21:03 -0400

    fixing format

--- src/test/java/edu/cornell/kfs/vnd/batch/service/impl/CuCommodityCodeUpdateServiceImplTest.java
+++ src/test/java/edu/cornell/kfs/vnd/batch/service/impl/CuCommodityCodeUpdateServiceImplTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,41 +16,40 @@ import edu.cornell.kfs.sys.batch.service.impl.CuBatchInputFileServiceImpl;
 import edu.cornell.kfs.vnd.batch.CommodityCodeInputFileType;
 
 public class CuCommodityCodeUpdateServiceImplTest {
-	
-	private CommodityCodeUpdateServiceImpl commodityCodeUpdateService;
 
-	private static final String DATA_FILE_PATH = "src/test/resources/edu/cornell/kfs/vnd/fixture/commodityCodeFlatFile.txt";
-	private static final int NUMBER_OF_RECORDS_IN_DATA_FILE = 53321;
-    
-	@Before
-	public void setUp() throws Exception {
-	    commodityCodeUpdateService = Mockito.spy(new CommodityCodeUpdateServiceImpl());
-	    Mockito.doReturn(true).when(commodityCodeUpdateService).updateCommodityCodes(Mockito.any());
-		
-		CuBatchInputFileServiceImpl batchInputFileService = new CuBatchInputFileServiceImpl();
-		commodityCodeUpdateService.setBatchInputFileService(batchInputFileService);
-		
-		CommodityCodeInputFileType commodityCodeInputFileType = new CommodityCodeInputFileType();
-		commodityCodeUpdateService.setCommodityCodeInputFileType(commodityCodeInputFileType);
-	}
-	
-	@After
-	public void tearDown() {
-	    commodityCodeUpdateService = null;
-	}
-	
-	@Test
-	public void testParseCommodityCodeList() {
-		byte[] content = commodityCodeUpdateService.getFileContent(DATA_FILE_PATH);
-		ArrayList<CommodityCode> someCommodityCodes = (ArrayList<CommodityCode>) commodityCodeUpdateService.parseCommodityCodeList(content);
-		
-		assertNotEquals(content.length, 0);
-		assertEquals(NUMBER_OF_RECORDS_IN_DATA_FILE, someCommodityCodes.size());
-	}
-	
-	@Test
-	public void testLoadCommodityCodeFile() {
-	    assertTrue(commodityCodeUpdateService.loadCommodityCodeFile(DATA_FILE_PATH));
-	}
+    private CommodityCodeUpdateServiceImpl commodityCodeUpdateService;
+
+    private static final String DATA_FILE_PATH = "src/test/resources/edu/cornell/kfs/vnd/fixture/commodityCodeFlatFile.txt";
+    private static final int NUMBER_OF_RECORDS_IN_DATA_FILE = 53321;
+
+    @Before
+    public void setUp() throws Exception {
+        commodityCodeUpdateService = Mockito.spy(new CommodityCodeUpdateServiceImpl());
+        Mockito.doReturn(true).when(commodityCodeUpdateService).updateCommodityCodes(Mockito.any());
+
+        CuBatchInputFileServiceImpl batchInputFileService = new CuBatchInputFileServiceImpl();
+        commodityCodeUpdateService.setBatchInputFileService(batchInputFileService);
+
+        CommodityCodeInputFileType commodityCodeInputFileType = new CommodityCodeInputFileType();
+        commodityCodeUpdateService.setCommodityCodeInputFileType(commodityCodeInputFileType);
+    }
+
+    @After
+    public void tearDown() {
+        commodityCodeUpdateService = null;
+    }
+
+    @Test
+    public void testParseCommodityCodeList() {
+        byte[] content = commodityCodeUpdateService.getFileContent(DATA_FILE_PATH);
+        ArrayList<CommodityCode> someCommodityCodes = (ArrayList<CommodityCode>) commodityCodeUpdateService.parseCommodityCodeList(content);
+        assertNotEquals(content.length, 0);
+        assertEquals(NUMBER_OF_RECORDS_IN_DATA_FILE, someCommodityCodes.size());
+    }
+
+    @Test
+    public void testLoadCommodityCodeFile() {
+        assertTrue(commodityCodeUpdateService.loadCommodityCodeFile(DATA_FILE_PATH));
+    }
 
 }
