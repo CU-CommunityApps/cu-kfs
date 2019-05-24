b28df7489424057237a59bc4ef7b02f96868e77c
nkimble <nkk4@cornell.edu>
2019-05-24 13:37:53 -0400

    KFSPTS-15655: Add server logging to CGB lastBilledDate calculation logic. (#799)

--- src/main/java/edu/cornell/kfs/module/ar/service/impl/CuContractsGrantsInvoiceCreateDocumentServiceImpl.java
+++ src/main/java/edu/cornell/kfs/module/ar/service/impl/CuContractsGrantsInvoiceCreateDocumentServiceImpl.java
@@ -10,12 +10,15 @@ import java.util.Set;
 
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.kuali.kfs.coa.businessobject.Account;
 import org.kuali.kfs.integration.cg.ContractsAndGrantsBillingAward;
 import org.kuali.kfs.integration.cg.ContractsAndGrantsBillingAwardAccount;
 import org.kuali.kfs.krad.util.ErrorMessage;
 import org.kuali.kfs.krad.util.ObjectUtils;
 import org.kuali.kfs.module.ar.ArConstants;
+import org.kuali.kfs.module.ar.ArKeyConstants;
 import org.kuali.kfs.module.ar.businessobject.ContractsGrantsInvoiceDocumentErrorLog;
 import org.kuali.kfs.module.ar.businessobject.ContractsGrantsInvoiceDocumentErrorMessage;
 import org.kuali.kfs.module.ar.businessobject.ContractsGrantsLetterOfCreditReviewDetail;
@@ -23,15 +26,21 @@ import org.kuali.kfs.module.ar.businessobject.InvoiceAccountDetail;
 import org.kuali.kfs.module.ar.document.ContractsGrantsInvoiceDocument;
 import org.kuali.kfs.module.ar.service.impl.ContractsGrantsInvoiceCreateDocumentServiceImpl;
 import org.kuali.kfs.sys.KFSConstants;
+import org.kuali.kfs.sys.businessobject.ChartOrgHolder;
 import org.kuali.kfs.sys.businessobject.SystemOptions;
+import org.kuali.kfs.sys.document.validation.event.DocumentSystemSaveEvent;
 import org.kuali.rice.core.api.util.type.KualiDecimal;
+import org.kuali.rice.kew.api.exception.WorkflowException;
 
 import edu.cornell.kfs.module.ar.CuArParameterConstants;
 import edu.cornell.kfs.module.ar.CuArParameterKeyConstants;
+import edu.cornell.kfs.module.ar.batch.service.CuVerifyBillingFrequencyService;
 import edu.cornell.kfs.module.ar.document.service.CuContractsGrantsInvoiceDocumentService;
 import edu.cornell.kfs.sys.CUKFSParameterKeyConstants;
 
 public class CuContractsGrantsInvoiceCreateDocumentServiceImpl extends ContractsGrantsInvoiceCreateDocumentServiceImpl {
+    
+    private static final Logger LOG = LogManager.getLogger(CuContractsGrantsInvoiceCreateDocumentServiceImpl.class);
     
     protected CuContractsGrantsInvoiceDocumentService cuContractsGrantsInvoiceDocumentService;
     
@@ -178,6 +187,113 @@ public class CuContractsGrantsInvoiceCreateDocumentServiceImpl extends Contracts
         super.populateInvoiceFromAward(award, awardAccounts, document, accountDetails, locCreationType);
         cuContractsGrantsInvoiceDocumentService.setInvoiceDueDateBasedOnNetTermsAndCurrentDate(document);
     }
+    
+    /**
+     * Generates and then saves a Contracts & Grants Invoice Document
+     *
+     * @param awd                the award for the document
+     * @param validAwardAccounts the award accounts which should appear on the document
+     * @param errorMessages      a List of error messages, to be appended to if there are errors in document generation
+     * @param accountDetails     letter of credit details if we're creating via loc
+     * @param locCreationType    letter of credit creation type if we're creating via loc
+     */
+    @Override
+    protected void generateAndSaveContractsAndGrantsInvoiceDocument(ContractsAndGrantsBillingAward awd,
+            List<ContractsAndGrantsBillingAwardAccount> validAwardAccounts, List<ErrorMessage> errorMessages,
+            List<ContractsGrantsLetterOfCreditReviewDetail> accountDetails, String locCreationType) {
+        ChartOrgHolder chartOrgHolder = financialSystemUserService.getPrimaryOrganization(
+                awd.getAwardPrimaryFundManager().getFundManager().getPrincipalId(),
+                KFSConstants.OptionalModuleNamespaces.ACCOUNTS_RECEIVABLE);
+        ContractsGrantsInvoiceDocument cgInvoiceDocument = createCGInvoiceDocumentByAwardInfo(awd, validAwardAccounts,
+                chartOrgHolder.getChartOfAccountsCode(), chartOrgHolder.getOrganizationCode(), errorMessages,
+                accountDetails, locCreationType);
+        if (ObjectUtils.isNotNull(cgInvoiceDocument)) {
+            
+            LOG.info("generateAndSaveContractsAndGrantsInvoiceDocument: Award/Proposal# = " + awd.getProposalNumber() 
+                    + " Save will be attempted for Positive Total Invoice Amount OR Milestone Billing OR Predetermined Billing"
+                    + " cgInvoiceDocument.getTotalInvoiceAmount() = " + cgInvoiceDocument.getTotalInvoiceAmount() 
+                    + " cgInvoiceDocument.getTotalInvoiceAmount().isPositive() = " +  cgInvoiceDocument.getTotalInvoiceAmount().isPositive() 
+                    + " isMilstone = " + ArConstants.BillingFrequencyValues.isMilestone(awd) 
+                    + " isPredeterminedBilling = " + ArConstants.BillingFrequencyValues.isPredeterminedBilling(awd));
+
+            if (cgInvoiceDocument.getTotalInvoiceAmount().isPositive() ||
+                ArConstants.BillingFrequencyValues.isMilestone(awd) ||
+                ArConstants.BillingFrequencyValues.isPredeterminedBilling(awd)) {
+                // Saving the document
+                try {
+                    documentService.saveDocument(cgInvoiceDocument, DocumentSystemSaveEvent.class);
+                } catch (WorkflowException ex) {
+                    LOG.error("Error creating cgin documents: " + ex.getMessage(), ex);
+                    throw new RuntimeException("Error creating cgin documents: " + ex.getMessage(), ex);
+                }
+            } else {
+                ErrorMessage errorMessage;
+                List<InvoiceAccountDetail> invoiceAccounts = cgInvoiceDocument.getAccountDetails();
+                if (!invoiceAccounts.isEmpty()) {
+                    errorMessage = new ErrorMessage(
+                      ArKeyConstants.ContractsGrantsInvoiceCreateDocumentConstants.NON_BILLABLE, 
+                      invoiceAccounts.get(0).getAccountNumber(), awd.getProposalNumber());
+                } else {
+                    errorMessage = new ErrorMessage(
+                            ArKeyConstants.ContractsGrantsInvoiceCreateDocumentConstants.NON_BILLABLE, null,
+                            awd.getProposalNumber());
+                }
+                errorMessages.add(errorMessage);
+            }
+        } else {
+            LOG.info("generateAndSaveContractsAndGrantsInvoiceDocument: cgInvoiceDocument is NULL. Not attempting Save.");
+        }
+    }
+    
+    @Override
+    public Collection<ContractsAndGrantsBillingAward> validateAwards(Collection<ContractsAndGrantsBillingAward> awards,
+            Collection<ContractsGrantsInvoiceDocumentErrorLog> contractsGrantsInvoiceDocumentErrorLogs,
+            String errOutputFile, String creationProcessTypeCode) {
+        LOG.info("validateAwards: creationProcessTypeCode = " + ArConstants.ContractsAndGrantsInvoiceDocumentCreationProcessType.getName(creationProcessTypeCode));
+        return super.validateAwards(awards, contractsGrantsInvoiceDocumentErrorLogs, errOutputFile, creationProcessTypeCode);
+    }
+    
+    @Override
+    protected void performAwardValidation(Collection<ContractsAndGrantsBillingAward> awards,
+            Map<ContractsAndGrantsBillingAward, List<String>> invalidGroup,
+            List<ContractsAndGrantsBillingAward> qualifiedAwards) {
+        Set<ContractsAndGrantsBillingAward> awardsWithDuplicateAccounts = findAwardsWithDuplicateAccounts(awards);
+
+        for (ContractsAndGrantsBillingAward award : awards) {
+            List<String> errorList = new ArrayList<>();
+
+            if (award.isExcludedFromInvoicing()) {
+                errorList.add(configurationService.getPropertyValueAsString(ArKeyConstants.CGINVOICE_CREATION_AWARD_EXCLUDED_FROM_INVOICING));
+            } else {
+                if (awardsWithDuplicateAccounts.contains(award)) {
+                    errorList.add(configurationService.getPropertyValueAsString(ArKeyConstants.CGINVOICE_CREATION_ACCOUNT_ON_MULTIPLE_AWARDS));
+                }
+                if (ArConstants.BillingFrequencyValues.isLetterOfCredit(award)) {
+                    errorList.add(configurationService.getPropertyValueAsString(ArKeyConstants.CGINVOICE_CREATION_AWARD_LOCB_BILLING_FREQUENCY));
+                } else {
+                    if (award.getAwardBeginningDate() != null) {
+                        if (award.getBillingFrequencyCode() != null && getContractsGrantsBillingAwardVerificationService().isValueOfBillingFrequencyValid(award)) {
+                            if (verifyBillingFrequencyService.validateBillingFrequency(award)) {
+                                validateAward(errorList, award);
+                            } else {
+                                errorList.add(configurationService.getPropertyValueAsString(ArKeyConstants.CGINVOICE_CREATION_AWARD_INVALID_BILLING_PERIOD));
+                            }
+                        } else {
+                            errorList.add(configurationService.getPropertyValueAsString(ArKeyConstants.CGINVOICE_CREATION_BILLING_FREQUENCY_MISSING_ERROR));
+                        }
+                    } else {
+                        errorList.add(configurationService.getPropertyValueAsString(ArKeyConstants.CGINVOICE_CREATION_AWARD_START_DATE_MISSING_ERROR));
+                    }
+                }
+            }
+            if (errorList.size() > 0) {
+                invalidGroup.put(award, errorList);
+            } else {
+                qualifiedAwards.add(award);
+            }
+
+        }
+    }
 
     public CuContractsGrantsInvoiceDocumentService getCuContractsGrantsInvoiceDocumentService() {
         return cuContractsGrantsInvoiceDocumentService;
@@ -187,5 +303,5 @@ public class CuContractsGrantsInvoiceCreateDocumentServiceImpl extends Contracts
             CuContractsGrantsInvoiceDocumentService cuContractsGrantsInvoiceDocumentService) {
         this.cuContractsGrantsInvoiceDocumentService = cuContractsGrantsInvoiceDocumentService;
     }
-    
+
 }
