390f59a9d74cb3dfbc3db05457d30d58af36ba63
Jay Hulslander <jdh34@cornell.edu>
2019-07-19 10:26:20 -0400

    fixing some OJB stuff

--- src/main/java/edu/cornell/kfs/module/cg/document/CuAwardMaintainableImpl.java
+++ src/main/java/edu/cornell/kfs/module/cg/document/CuAwardMaintainableImpl.java
@@ -1,9 +1,15 @@
 package edu.cornell.kfs.module.cg.document;
 
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.kuali.kfs.krad.util.ObjectUtils;
 import org.kuali.kfs.module.cg.businessobject.Award;
+import org.kuali.kfs.module.cg.businessobject.AwardAccount;
 import org.kuali.kfs.module.cg.businessobject.Proposal;
 import org.kuali.kfs.module.cg.document.AwardMaintainableImpl;
 import org.kuali.kfs.sys.KFSPropertyConstants;
+
+import edu.cornell.kfs.module.cg.businessobject.AwardAccountExtendedAttribute;
 
 public class CuAwardMaintainableImpl extends AwardMaintainableImpl {
 
@@ -27,6 +33,25 @@ public class CuAwardMaintainableImpl extends AwardMaintainableImpl {
         refreshNonUpdateableReferences(award.getAwardAccounts());
         refreshNonUpdateableReferences(award.getAwardSubcontractors());
         refreshAwardProjectDirectors(refreshFromLookup);
+    }
+    
+    @Override
+    public void prepareForSave() {
+        super.prepareForSave();
+        if (CollectionUtils.isNotEmpty(getAward().getAwardAccounts())) {
+            for (AwardAccount awardAccount : getAward().getAwardAccounts()) {
+                AwardAccountExtendedAttribute extension;
+                if (ObjectUtils.isNotNull(awardAccount.getExtension())) {
+                    extension = (AwardAccountExtendedAttribute) awardAccount.getExtension();
+                } else {
+                    extension = new AwardAccountExtendedAttribute();
+                    awardAccount.setExtension(extension);
+                }
+                extension.setAccountNumber(awardAccount.getAccountNumber());
+                extension.setChartOfAccountsCode(awardAccount.getChartOfAccountsCode());
+                extension.setProposalNumber(getAward().getProposalNumber());
+            }
+        }
     }
 
 }
