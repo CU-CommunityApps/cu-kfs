0dc4c138723223afc41755660bf7fd1fd9b672fc
Jay Hulslander <jdh34@cornell.edu>
2019-07-19 13:25:18 -0400

    cleanup

--- src/main/java/edu/cornell/kfs/module/ar/document/validation/impl/AutoApproveDisabledSuspensionCategory.java
+++ src/main/java/edu/cornell/kfs/module/ar/document/validation/impl/AutoApproveDisabledSuspensionCategory.java
@@ -9,18 +9,17 @@ import org.kuali.kfs.module.cg.businessobject.Award;
 import edu.cornell.kfs.module.cg.businessobject.AwardExtendedAttribute;
 
 public class AutoApproveDisabledSuspensionCategory extends SuspensionCategoryBase {
-    
     private static final Logger LOG = LogManager.getLogger(AutoApproveDisabledSuspensionCategory.class);
 
     @Override
     public boolean shouldSuspend(ContractsGrantsInvoiceDocument contractsGrantsInvoiceDocument) {
         Award award = (Award) contractsGrantsInvoiceDocument.getInvoiceGeneralDetail().getAward();
         if (award.getAutoApproveIndicator()) {
-            LOG.info("shouldSuspend, auto apporve is selected so no need to suspend");
+            LOG.debug("shouldSuspend, auto apporve is selected so no need to suspend");
             return false;
         } else {
             AwardExtendedAttribute awardExtension = (AwardExtendedAttribute) award.getExtension();
-            LOG.info("shouldSuspend, auto apporve is NOT selected so suspend!");
+            LOG.debug("shouldSuspend, auto apporve is NOT selected so suspend!");
             return true;
         }
     }
