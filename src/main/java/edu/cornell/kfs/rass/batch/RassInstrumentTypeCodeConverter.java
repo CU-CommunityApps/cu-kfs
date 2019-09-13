b21cbc3031bf5537442e1d56d38dd2e82f902ba0
db483 <db483@cornell.edu>
2019-09-13 13:49:56 +0100

    KFSPTS-16446 update to use latest update from Jay

--- src/main/java/edu/cornell/kfs/rass/batch/RassInstrumentTypeCodeConverter.java
+++ src/main/java/edu/cornell/kfs/rass/batch/RassInstrumentTypeCodeConverter.java
@@ -1,31 +1,20 @@
 package edu.cornell.kfs.rass.batch;
 
-import org.kuali.kfs.coreservice.framework.parameter.ParameterService;
-import org.kuali.kfs.krad.bo.PersistableBusinessObject;
-import org.kuali.kfs.sys.service.impl.KfsParameterConstants;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import edu.cornell.kfs.rass.RassConstants;
 import edu.cornell.kfs.rass.RassParameterConstants;
 
-public class RassInstrumentTypeCodeConverter extends RassValueConverterBase {
-
-    private ParameterService parameterService;
+public class RassInstrumentTypeCodeConverter extends RassParameterMappingValueConverterBase {
+    private static final Logger LOG = LogManager.getLogger(RassInstrumentTypeCodeConverter.class);
+    
+    @Override
+    public String getParameterName() {
+        return RassParameterConstants.INSTRUMENT_TYPE_CODE_MAPPINGS;
+    }
 
     @Override
-    public Object convert(Class<? extends PersistableBusinessObject> businessObjectClass, RassPropertyDefinition propertyMapping, Object propertyValue) {
-        return getMappedInstrumentTypeCodeValue((String) propertyValue);
-    }
-
-    protected String getMappedInstrumentTypeCodeValue(String value) {
-        return parameterService.getSubParameterValueAsString(RassConstants.RASS_MODULE, KfsParameterConstants.BATCH_COMPONENT,
-                RassParameterConstants.INSTRUMENT_TYPE_CODE_MAPPINGS, value);
-    }
-
-    public ParameterService getParameterService() {
-        return parameterService;
-    }
-
-    public void setParameterService(ParameterService parameterService) {
-        this.parameterService = parameterService;
+    public Logger getLog() {
+        return LOG;
     }
 }
