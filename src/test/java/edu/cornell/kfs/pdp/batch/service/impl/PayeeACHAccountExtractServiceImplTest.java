41cda1047c560eac90ab48cd98f34a057cda9be9
Jay Hulslander <jdh34@cornell.edu>
2019-09-26 11:37:41 -0400

    KFSPTS-16564 clean dashes out of ACH bank accounts before processing (#860)

--- src/test/java/edu/cornell/kfs/pdp/batch/service/impl/PayeeACHAccountExtractServiceImplTest.java
+++ src/test/java/edu/cornell/kfs/pdp/batch/service/impl/PayeeACHAccountExtractServiceImplTest.java
@@ -724,5 +724,69 @@ public class PayeeACHAccountExtractServiceImplTest {
             this.validRow = false;
         }
     }
+    
+    @Test
+    public void testCleanPayeeACHAccountExtractDetailNoClean() {
+        String bankAccountNumber = "12345";
+        validateCleanPayeeACHAccountExtractDetail(bankAccountNumber, bankAccountNumber);
+    }
+    
+    @Test
+    public void testCleanPayeeACHAccountExtractDetailCleanDashes() {
+        String bankAccountNumber = "1-2---3-4-5----6-";
+        String expectedBankAccount = "123456";
+        validateCleanPayeeACHAccountExtractDetail(bankAccountNumber, expectedBankAccount);
+    }
+    
+    @Test
+    public void testCleanPayeeACHAccountExtractDetailCleanDashesWithLetters() {
+        String bankAccountNumber = "A1-2---3-4-5---6-";
+        String expectedBankAccount = "A123456";
+        validateCleanPayeeACHAccountExtractDetail(bankAccountNumber, expectedBankAccount);
+    }
+    
+    @Test
+    public void testCleanPayeeACHAccountExtractDetailWithLetters() {
+        String bankAccountNumber = "A12345";
+        validateCleanPayeeACHAccountExtractDetail(bankAccountNumber, bankAccountNumber);
+    }
+    
+    @Test
+    public void testCleanPayeeACHAccountExtractDetailWithNull() {
+        validateCleanPayeeACHAccountExtractDetail(null, null);
+    }
+    
+    @Test
+    public void testCleanPayeeACHAccountExtractDetailWithEmptyString() {
+        validateCleanPayeeACHAccountExtractDetail(StringUtils.EMPTY, StringUtils.EMPTY);
+    }
+    
+    @Test
+    public void testCleanPayeeACHAccountExtractDetailWithSpaces() {
+        validateCleanPayeeACHAccountExtractDetail(" 1 23  4 5 6 ", "123456");
+    }
+    
+    @Test
+    public void testCleanPayeeACHAccountExtractDetailWithSpacesAndDashes() {
+        validateCleanPayeeACHAccountExtractDetail(" 1 2----3  4 - 5 6-7 ", "1234567");
+    }
+    
+    @Test
+    public void testCleanPayeeACHAccountExtractDetailWithSpacesAndDashesAndLetters() {
+        validateCleanPayeeACHAccountExtractDetail("A 1 2----3  4 - 5 6-7 B ", "A1234567B");
+    }
+    
+    @Test
+    public void testCleanPayeeACHAccountExtractDetailWithJustSpaces() {
+        validateCleanPayeeACHAccountExtractDetail("   ", StringUtils.EMPTY);
+    }
+    
+    private void validateCleanPayeeACHAccountExtractDetail(String bankAccountNumber, String expectedCleanedBankAccountNumber) {
+        PayeeACHAccountExtractDetail detail = new PayeeACHAccountExtractDetail();
+        detail.setBankAccountNumber(bankAccountNumber);
+        payeeACHAccountExtractService.cleanPayeeACHAccountExtractDetail(detail);
+        
+        assertEquals(expectedCleanedBankAccountNumber, detail.getBankAccountNumber());
+    }
 
 }
