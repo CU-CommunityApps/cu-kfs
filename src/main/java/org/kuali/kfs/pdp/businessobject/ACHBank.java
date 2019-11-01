8e369534eb47973ab44f94378a0b04186a73d065
Jay Hulslander <jdh34@cornell.edu>
2019-11-01 09:55:00 -0400

    Revert "KFSPTS-16975 adding bank state and postal code setters to ACHBank, also added logging to help with future debugging (#879)"
    This reverts commit b969c1bd9e03e8174a9004bca685e0d9224cae31.

--- src/main/java/org/kuali/kfs/pdp/businessobject/ACHBank.java
+++ src/main/java/org/kuali/kfs/pdp/businessobject/ACHBank.java
@@ -18,10 +18,6 @@
  */
 package org.kuali.kfs.pdp.businessobject;
 
-import org.apache.commons.lang3.ObjectUtils;
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.kuali.kfs.krad.bo.PersistableBusinessObjectBase;
 import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.kfs.sys.businessobject.PostalCode;
@@ -29,7 +25,6 @@ import org.kuali.kfs.sys.businessobject.State;
 import org.kuali.rice.core.api.mo.common.active.MutableInactivatable;
 
 public class ACHBank extends PersistableBusinessObjectBase implements MutableInactivatable {
-    private static final Logger LOG = LogManager.getLogger(ACHBank.class);
 
     protected String bankRoutingNumber;
     protected String bankOfficeCode;
@@ -256,27 +251,5 @@ public class ACHBank extends PersistableBusinessObjectBase implements MutableIna
 
     public void setBankCountryCode(String bankCountryCode) {
         this.bankCountryCode = bankCountryCode;
-    }
-
-    public void setBankState(State bankState) {
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("setBankState, setting bank state to: " + buildToStringBuilderString(bankState));
-        }
-        this.bankState = bankState;
-    }
-
-    public void setPostalCode(PostalCode postalCode) {
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("setPostalCode, setting postal code to: " + buildToStringBuilderString(postalCode));
-        }
-        this.postalCode = postalCode;
-    }
-    
-    private String buildToStringBuilderString(PersistableBusinessObjectBase object) {
-        if (ObjectUtils.anyNotNull(object)) {
-            return ToStringBuilder.reflectionToString(object);
-        } else {
-            return "<null>";
-        }
     }
 }
