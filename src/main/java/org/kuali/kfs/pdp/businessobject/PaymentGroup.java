16131a86446abafcd0dbe49bcf616e0f38f7d6ee
db483 <db483@cornell.edu>
2019-11-27 15:18:56 +0000

    KFSPTS-17347 add ObjectUtils null checks to prevent errors (#896)

--- src/main/java/org/kuali/kfs/pdp/businessobject/PaymentGroup.java
+++ src/main/java/org/kuali/kfs/pdp/businessobject/PaymentGroup.java
@@ -892,9 +892,9 @@ public class PaymentGroup extends PersistableBusinessObjectBase {
         setLine1Address(addr.getVendorLine1Address());
         setLine2Address(addr.getVendorLine2Address());
         setCity(addr.getVendorCityName());
-        setState(addr.getVendorState()!=null?addr.getVendorState().getCode():"");
+        setState(ObjectUtils.isNotNull(addr.getVendorState()) ? addr.getVendorState().getCode() : "");
         setZipCd(addr.getVendorZipCode());
-        setCountry(addr.getVendorCountry()!=null?addr.getVendorCountry().getName():"");
+        setCountry(ObjectUtils.isNotNull(addr.getVendorCountry()) ? addr.getVendorCountry().getName() : "");
     }
     
     /**
