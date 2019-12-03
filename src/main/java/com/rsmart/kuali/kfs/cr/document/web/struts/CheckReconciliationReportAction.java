73e072fa8153dfcd942c6970a42c4f5edcbe9c2d
Jay Hulslander <jdh34@cornell.edu>
2019-12-03 14:51:06 -0500

    clean

--- src/main/java/com/rsmart/kuali/kfs/cr/document/web/struts/CheckReconciliationReportAction.java
+++ src/main/java/com/rsmart/kuali/kfs/cr/document/web/struts/CheckReconciliationReportAction.java
@@ -21,10 +21,8 @@ import com.rsmart.kuali.kfs.cr.document.service.CheckReconciliationReportService
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import edu.cornell.kfs.fp.batch.service.impl.ProcurementCardCreateDocumentServiceImpl;
 import net.sf.jasperreports.engine.JRParameter;
 
-//import org.apache.log4j.Level;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
