67c5eef0b452c8a6992e795bf43cb7965c258e07
Jay Hulslander <jdh34@cornell.edu>
2019-12-10 13:12:42 -0500

    KFSPTS-17197 removing sprintcontext from values finder

--- src/main/java/edu/cornell/kfs/module/cg/businessobject/options/AgencyOriginValuesFinder.java
+++ src/main/java/edu/cornell/kfs/module/cg/businessobject/options/AgencyOriginValuesFinder.java
@@ -8,19 +8,18 @@ import org.kuali.kfs.krad.keyvalues.KeyValuesBase;
 import org.kuali.kfs.krad.service.KeyValuesService;
 import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.kfs.sys.KFSPropertyConstants;
-import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.rice.core.api.util.ConcreteKeyValue;
 import org.kuali.rice.core.api.util.KeyValue;
 
 import edu.cornell.kfs.module.cg.businessobject.AgencyOrigin;
 
 public class AgencyOriginValuesFinder extends KeyValuesBase {
-
     private static final long serialVersionUID = 1L;
+    private KeyValuesService keyValuesService;
 
     @Override
     public List<KeyValue> getKeyValues() {
-        Collection<AgencyOrigin> agencyOrigins = getKeyValuesService().findAllOrderBy(
+        Collection<AgencyOrigin> agencyOrigins = keyValuesService.findAllOrderBy(
                 AgencyOrigin.class, KFSPropertyConstants.CODE, false);
         List<KeyValue> keyValues = new ArrayList<>();
         keyValues.add(new ConcreteKeyValue(KFSConstants.EMPTY_STRING, KFSConstants.EMPTY_STRING));
@@ -30,8 +29,8 @@ public class AgencyOriginValuesFinder extends KeyValuesBase {
         return keyValues;
     }
 
-    protected KeyValuesService getKeyValuesService() {
-        return SpringContext.getBean(KeyValuesService.class);
+    public void setKeyValuesService(KeyValuesService keyValuesService) {
+        this.keyValuesService = keyValuesService;
     }
-
+    
 }
