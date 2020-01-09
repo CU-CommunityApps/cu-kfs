e3a508bd1ff7cca3ed656f5bc3cad081026ff1c5
Nancy Kimble <nkk4@cornell.edu>
2020-01-09 08:18:55 -0500

    Revert "KFSPTS-16511: New integration to create csv file of closed accounts for WorkDay. Code has ability to create initial seed file as well as incremental file based on KFS system parameter. (#907)"
    This reverts commit 7ea219df7397ed37d58643afa971904349ed9eac.

--- src/main/java/edu/cornell/kfs/coa/batch/service/CreateClosedLaborAccountsCsvService.java
+++
@@ -1,9 +0,0 @@
-package edu.cornell.kfs.coa.batch.service;
-
-import java.io.IOException;
-
-public interface CreateClosedLaborAccountsCsvService {
-    
-    void createClosedLaborAccountCsvByParameterPastDays() throws IOException;
-    
-}
