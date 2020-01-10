0ae09015ce19159fe4b84afa66c1811342c047c3
Nancy Kimble <nkk4@cornell.edu>
2020-01-10 08:56:36 -0500

    KFSPTS-16511: New integration to create csv file of closed accounts for Workday. Code has ability to create initial seed file as well as incremental file based on KFS system parameters.

---
+++ src/main/java/edu/cornell/kfs/coa/batch/CuCoaBatchParameterConstants.java
@@ -0,0 +1,7 @@
+package edu.cornell.kfs.coa.batch;
+
+public class CuCoaBatchParameterConstants {
+    public static final String CREATE_CLOSED_ACCOUNTS_CSV_STEP = "createClosedAccountsCsvStep";
+    public static final String ACCOUNTS_CLOSED_OVER_PAST_DAYS = "ACCOUNTS_CLOSED_OVER_PAST_DAYS";
+    public static final String CLOSED_ACCOUNTS_SEED_FILE_DEFAULT_FROM_DATE = "CLOSED_ACCOUNTS_SEED_FILE_DEFAULT_FROM_DATE";
+}
