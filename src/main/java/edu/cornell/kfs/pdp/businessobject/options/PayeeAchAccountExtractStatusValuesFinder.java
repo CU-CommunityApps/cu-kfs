4a99aae3b2fb8373d20c8987d7fa1d66ce8d34a7
db483 <db483@cornell.edu>
2020-02-13 17:04:57 +0000

    KFSPTS-17866 Make PayeeACHAccountExtractDetail persistable and allow … (#920)

---
+++ src/main/java/edu/cornell/kfs/pdp/businessobject/options/PayeeAchAccountExtractStatusValuesFinder.java
@@ -0,0 +1,22 @@
+package edu.cornell.kfs.pdp.businessobject.options;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kuali.kfs.krad.keyvalues.KeyValuesBase;
+import org.kuali.rice.core.api.util.ConcreteKeyValue;
+import org.kuali.rice.core.api.util.KeyValue;
+
+import edu.cornell.kfs.pdp.CUPdpConstants;
+
+public class PayeeAchAccountExtractStatusValuesFinder extends KeyValuesBase {
+
+    public List<KeyValue> getKeyValues() {
+        List<KeyValue> keyValues = new ArrayList<>();
+        keyValues.add(new ConcreteKeyValue(CUPdpConstants.PayeeAchAccountExtractStatuses.OPEN, CUPdpConstants.PayeeAchAccountExtractStatuses.OPEN));
+        keyValues.add(new ConcreteKeyValue(CUPdpConstants.PayeeAchAccountExtractStatuses.CANCELED, CUPdpConstants.PayeeAchAccountExtractStatuses.CANCELED));
+        keyValues.add(new ConcreteKeyValue(CUPdpConstants.PayeeAchAccountExtractStatuses.PROCESSED, CUPdpConstants.PayeeAchAccountExtractStatuses.PROCESSED));
+        return keyValues;
+    }
+
+}
