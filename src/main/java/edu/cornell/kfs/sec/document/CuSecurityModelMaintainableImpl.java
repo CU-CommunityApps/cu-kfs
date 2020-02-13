ead93ef5869b23f9b400b91e1b70c5d63adb2bdf
Jay Hulslander <jdh34@cornell.edu>
2020-02-13 09:01:02 -0500

    KFSPTS-11929 make security model generated roleID by just numeric

---
+++ src/main/java/edu/cornell/kfs/sec/document/CuSecurityModelMaintainableImpl.java
@@ -0,0 +1,18 @@
+package edu.cornell.kfs.sec.document;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.kuali.kfs.sec.businessobject.SecurityModel;
+import org.kuali.kfs.sec.document.SecurityModelMaintainableImpl;
+
+public class CuSecurityModelMaintainableImpl extends SecurityModelMaintainableImpl {
+    private static final Logger LOG = LogManager.getLogger(CuSecurityModelMaintainableImpl.class);
+    
+    @Override
+    protected String buildModelRoleId(SecurityModel securityModel) {
+        String roleId = String.valueOf(securityModel.getId());
+        LOG.info("buildModelRoleId, returning roleId: " + roleId);
+        return roleId;
+    }
+
+}
