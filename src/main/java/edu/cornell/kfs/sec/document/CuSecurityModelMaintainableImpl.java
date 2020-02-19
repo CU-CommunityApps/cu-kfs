165e1cd36cfcb9bf95c32b8539b7881b20f5b306
db483 <db483@cornell.edu>
2020-02-19 16:52:37 +0000

    KFSPTS-11929 leave the role id null so that the role id sequence is used to set it when saving to the database (#924)

---
+++ src/main/java/edu/cornell/kfs/sec/document/CuSecurityModelMaintainableImpl.java
@@ -0,0 +1,13 @@
+package edu.cornell.kfs.sec.document;
+
+import org.kuali.kfs.sec.businessobject.SecurityModel;
+import org.kuali.kfs.sec.document.SecurityModelMaintainableImpl;
+
+public class CuSecurityModelMaintainableImpl extends SecurityModelMaintainableImpl {
+
+    @Override
+    protected String buildModelRoleId(SecurityModel securityModel) {
+        return null;
+    }
+    
+}
