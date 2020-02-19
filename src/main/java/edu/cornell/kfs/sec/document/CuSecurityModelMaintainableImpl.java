064768340deb839f5578e1bf1ecf2b60978ca7f9
db483 <db483@cornell.edu>
2020-02-19 08:11:04 +0000

    KFSPTS-11929 make security model generated roleID be numeric

--- src/main/java/edu/cornell/kfs/sec/document/CuSecurityModelMaintainableImpl.java
+++ src/main/java/edu/cornell/kfs/sec/document/CuSecurityModelMaintainableImpl.java
@@ -1,7 +1,15 @@
 package edu.cornell.kfs.sec.document;
 
+import org.kuali.kfs.krad.service.SequenceAccessorService;
+import org.kuali.kfs.sec.businessobject.SecurityModel;
 import org.kuali.kfs.sec.document.SecurityModelMaintainableImpl;
+import org.kuali.kfs.sys.context.SpringContext;
 
 public class CuSecurityModelMaintainableImpl extends SecurityModelMaintainableImpl {
+    private static final String KRIM_ROLE_ID_SEQ = "KRIM_ROLE_ID_S"; 
 
+    protected String buildModelRoleId(SecurityModel securityModel) {
+        return String.valueOf(SpringContext.getBean(SequenceAccessorService.class).getNextAvailableSequenceNumber(KRIM_ROLE_ID_SEQ));
+    }
+    
 }
