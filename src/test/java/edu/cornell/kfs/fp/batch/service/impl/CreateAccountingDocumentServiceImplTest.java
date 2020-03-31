707c7fe75ed8a06725ea520ec3d62b596040dc52
Jay Hulslander <jdh34@cornell.edu>
2020-03-31 11:57:36 -0400

    KFSPTS-17966 add invoice number and date to DV in create accounting document job (#944)

--- src/test/java/edu/cornell/kfs/fp/batch/service/impl/CreateAccountingDocumentServiceImplTest.java
+++ src/test/java/edu/cornell/kfs/fp/batch/service/impl/CreateAccountingDocumentServiceImplTest.java
@@ -116,6 +116,7 @@ import edu.cornell.kfs.fp.batch.xml.fixture.AccountingXmlDocumentListWrapperFixt
 import edu.cornell.kfs.fp.document.CuDisbursementVoucherDocument;
 import edu.cornell.kfs.fp.document.CuDistributionOfIncomeAndExpenseDocument;
 import edu.cornell.kfs.fp.document.service.CuDisbursementVoucherDefaultDueDateService;
+import edu.cornell.kfs.sys.CUKFSConstants;
 import edu.cornell.kfs.sys.batch.JAXBXmlBatchInputFileTypeBase;
 import edu.cornell.kfs.sys.businessobject.WebServiceCredential;
 import edu.cornell.kfs.sys.businessobject.fixture.WebServiceCredentialFixture;
@@ -142,6 +143,7 @@ public class CreateAccountingDocumentServiceImplTest {
     private TestCreateAccountingDocumentServiceImpl createAccountingDocumentService;
     private List<AccountingDocument> routedAccountingDocuments;
     private List<String> creationOrderedBaseFileNames;
+    private SimpleDateFormat dateFormat;
 
     @Before
     public void setUp() throws Exception {
@@ -170,11 +172,13 @@ public class CreateAccountingDocumentServiceImplTest {
         routedAccountingDocuments = new ArrayList<>();
         creationOrderedBaseFileNames = new ArrayList<>();
         createTargetTestDirectory();
+        dateFormat = new SimpleDateFormat(CUKFSConstants.DATE_FORMAT_yyyyMMdd);
     }
 
     @After
     public void tearDown() throws Exception {
         deleteTargetTestDirectory();
+        dateFormat = null;
     }
 
     @Test
@@ -457,6 +461,13 @@ public class CreateAccountingDocumentServiceImplTest {
         attachService.setForceUseOfRealClientToTestAttachmentUrls(true);
         assertDocumentsAreGeneratedCorrectlyByBatchProcess(AccountingXmlDocumentListWrapperFixture.SINGLE_DI_DOCUMENT_WITH_BAD_ATTACHMENT_TEST);
     }
+    
+    @Test
+    public void testDvDocumentTest() throws Exception {
+        copyTestFilesAndCreateDoneFiles("dv-document-test");
+        assertDocumentsAreGeneratedCorrectlyByBatchProcess(
+                AccountingXmlDocumentListWrapperFixture.DV_DOCUMENT_TEST);
+    }
 
     private void assertDocumentsAreGeneratedCorrectlyByBatchProcess(AccountingXmlDocumentListWrapperFixture... fixtures) {
         boolean actualResults = createAccountingDocumentService.createAccountingDocumentsFromXml();
@@ -677,6 +688,10 @@ public class CreateAccountingDocumentServiceImplTest {
                 actualDvDocument.getDvNonEmployeeTravel().getDvNonEmployeeExpenses(), this::assertTravelExpenseCorrect);
         assertObjectListIsCorrect("non employee prepaid travel expenses aren't correct", expectedDvDocument.getDvNonEmployeeTravel().getDvPrePaidEmployeeExpenses(), 
                 actualDvDocument.getDvNonEmployeeTravel().getDvPrePaidEmployeeExpenses(), this::assertTravelExpenseCorrect);
+        assertEquals("Due Dates should match", dateFormat.format(expectedDvDocument.getDisbursementVoucherDueDate()), 
+                dateFormat.format(actualDvDocument.getDisbursementVoucherDueDate()) );
+        assertEquals("Invoice Dates should match", expectedDvDocument.getInvoiceDate(), actualDvDocument.getInvoiceDate());
+        assertEquals("Invoice numbers should match", expectedDvDocument.getInvoiceNumber(), actualDvDocument.getInvoiceNumber());
     }
     
     private void assertTravelExpenseCorrect(DisbursementVoucherNonEmployeeExpense expectedExpense, DisbursementVoucherNonEmployeeExpense actualExpense) {
