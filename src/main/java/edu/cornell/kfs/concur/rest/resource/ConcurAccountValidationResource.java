110172259cf04a1271e04dc51f81d8c8b8a39850
cah292 <cah292@users.noreply.github.com>
2020-04-17 04:51:13 -0400

    KFSPTS-18438 Add XML logging for Concur account validation (#954)

--- src/main/java/edu/cornell/kfs/concur/rest/resource/ConcurAccountValidationResource.java
+++ src/main/java/edu/cornell/kfs/concur/rest/resource/ConcurAccountValidationResource.java
@@ -8,6 +8,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.bind.JAXBException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -17,24 +18,35 @@ import edu.cornell.kfs.concur.businessobjects.ConcurEventNotification;
 import edu.cornell.kfs.concur.eventnotification.rest.xmlObjects.ConcurEventNotificationDTO;
 import edu.cornell.kfs.concur.service.ConcurEventNotificationConversionService;
 import edu.cornell.kfs.concur.service.ConcurEventNotificationService;
+import edu.cornell.kfs.sys.service.CUMarshalService;
 
 @Path("/validate")
 public class ConcurAccountValidationResource {
 	private static final Logger LOG = LogManager.getLogger(ConcurAccountValidationResource.class);
     protected static volatile ConcurEventNotificationConversionService concurEventNotificationConversionService;
     protected static volatile ConcurEventNotificationService concurEventNotificationService;
+    protected static volatile CUMarshalService cuMarshalService;
     
     @POST
     @Consumes(MediaType.APPLICATION_XML)
-    public Response validate(ConcurEventNotificationDTO concurEventNotificationDTO) {
+    public Response validate(String concurEventNotificationXml) {
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("validate(): Validating the following event notification:\n" + concurEventNotificationXml);
+        }
         ConcurEventNotification concurEventNotification = null;
         try {
+            ConcurEventNotificationDTO concurEventNotificationDTO = getCuMarshalService().unmarshalString(
+                    concurEventNotificationXml, ConcurEventNotificationDTO.class);
             concurEventNotification = getConcurEventNotificationConversionService().convertConcurEventNotification(concurEventNotificationDTO);
-        } catch (ParseException e) {
+        } catch (ParseException | JAXBException e) {
             LOG.error("validate():" + e.getMessage(), e);
             throw new BadRequestException();
         }
         getConcurEventNotificationService().saveConcurEventNotification(concurEventNotification);
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("validate(): Completed validation of event notification for object: "
+                    + concurEventNotification.getObjectURI());
+        }
         return Response.ok().build();
     }    
 
@@ -50,6 +62,13 @@ public class ConcurAccountValidationResource {
             concurEventNotificationConversionService = SpringContext.getBean(ConcurEventNotificationConversionService.class);
         }
         return concurEventNotificationConversionService;
+    }
+
+    public static CUMarshalService getCuMarshalService() {
+        if (cuMarshalService == null) {
+            cuMarshalService = SpringContext.getBean(CUMarshalService.class);
+        }
+        return cuMarshalService;
     }
 
 }
