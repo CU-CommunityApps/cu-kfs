945d014754a6adf23cdd76193d2848c13fc440d5
Jay Hulslander <jdh34@cornell.edu>
2020-06-25 16:02:50 -0400

    removing PaymentWorksTaxRuleService.java, does not appear to be needed

--- src/main/java/edu/cornell/kfs/pmw/batch/service/PaymentWorksTaxRuleService.java
+++
@@ -1,9 +0,0 @@
-package edu.cornell.kfs.pmw.batch.service;
-
-import edu.cornell.kfs.pmw.batch.businessobject.PaymentWorksVendor;
-
-public interface PaymentWorksTaxRuleService {
-    
-    int determineTaxRuleToUseForDataPopulation(PaymentWorksVendor pmwVendor, String pmwVendorFipsTaxCountryCode);
-
-}
