adce4c37e81b3e4356040254402610169a381f2a
Andrew Dickson <33495813+ajd299@users.noreply.github.com>
2020-07-01 05:05:10 -0400

    KFSPTS-18896 CapAsset Inventory Website API Lookups (#988)
    (cherry picked from commit 3604083f089148d4bb70432ab9ccf6d5db0bd952)

---
+++ src/main/java/edu/cornell/kfs/module/cam/dataaccess/impl/CuCapAssetInventoryDaoOjb.java
@@ -0,0 +1,61 @@
+package edu.cornell.kfs.module.cam.dataaccess.impl;
+
+import edu.cornell.kfs.module.cam.CuCamsConstants;
+import edu.cornell.kfs.module.cam.dataaccess.CuCapAssetInventoryDao;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.ojb.broker.query.Criteria;
+import org.apache.ojb.broker.query.Query;
+import org.apache.ojb.broker.query.QueryByCriteria;
+import org.apache.ojb.broker.query.QueryFactory;
+import org.kuali.kfs.module.cam.businessobject.Asset;
+import org.kuali.kfs.module.cam.businessobject.AssetCondition;
+import org.kuali.kfs.sys.KFSPropertyConstants;
+import org.kuali.kfs.sys.businessobject.Building;
+import org.kuali.kfs.sys.businessobject.Room;
+import org.kuali.rice.core.framework.persistence.ojb.dao.PlatformAwareDaoBaseOjb;
+
+import java.util.List;
+
+public class CuCapAssetInventoryDaoOjb extends PlatformAwareDaoBaseOjb implements CuCapAssetInventoryDao {
+
+    private static final Logger LOG = LogManager.getLogger(CuCapAssetInventoryDaoOjb.class);
+
+    public List<Building> getBuildings(String campusCode, String queryCode, String queryName) {
+        Criteria criteria = new Criteria();
+        criteria.addEqualTo(CuCamsConstants.CapAssetApi.CAMPUS_CODE_PARAMETER, campusCode);
+        if (StringUtils.isNotEmpty(queryName)) {
+            criteria.addLike(getDbPlatform().getUpperCaseFunction() + "(" + KFSPropertyConstants.BUILDING_NAME + ")", "%" + queryName.toUpperCase() + "%");
+        } else if (StringUtils.isNotEmpty(queryCode)) {
+            criteria.addLike(getDbPlatform().getUpperCaseFunction() + "(" + KFSPropertyConstants.BUILDING_CODE + ")", "%" + queryCode.toUpperCase() + "%");
+        }
+
+        Query query = QueryFactory.newQuery(Building.class, criteria);
+        return (List<Building>) getPersistenceBrokerTemplate().getCollectionByQuery(query);
+    }
+
+    public List<Room> getBuildingRooms(String campusCode, String buildingCode) {
+        Criteria criteria = new Criteria();
+        criteria.addEqualTo(CuCamsConstants.CapAssetApi.CAMPUS_CODE_PARAMETER, campusCode);
+        criteria.addEqualTo(KFSPropertyConstants.BUILDING_CODE, buildingCode);
+        Query query = QueryFactory.newQuery(Room.class, criteria);
+        return (List<Room>) getPersistenceBrokerTemplate().getCollectionByQuery(query);
+    }
+
+    public List<AssetCondition> getAssetConditions() {
+        Criteria criteria = new Criteria();
+        criteria.addEqualTo(CuCamsConstants.CapAssetApi.ACTIVE, CuCamsConstants.CapAssetApi.YES);
+        Query query = QueryFactory.newQuery(AssetCondition.class, criteria);
+        return (List<AssetCondition>) getPersistenceBrokerTemplate().getCollectionByQuery(query);
+    }
+
+    public Asset getAssetByTagNumber(String assetTagNumber) {
+        Criteria criteria = new Criteria();
+        criteria.addEqualTo(CuCamsConstants.CapAssetApi.CAMPUS_TAG_NUMBER, assetTagNumber);
+        Query query = QueryFactory.newQuery(Asset.class, criteria);
+        ((QueryByCriteria) query).setCriteria(criteria);
+        return (Asset) getPersistenceBrokerTemplate().getObjectByQuery(query);
+    }
+
+}
---
+++ src/main/java/edu/cornell/kfs/module/cam/web/filter/CuCapAssetInventoryServerAuthFilter.java
@@ -0,0 +1,74 @@
+package edu.cornell.kfs.module.cam.web.filter;
+
+import com.google.gson.Gson;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import edu.cornell.kfs.module.cam.CuCamsConstants;
+import edu.cornell.kfs.sys.service.WebServiceCredentialService;
+import org.kuali.kfs.sys.context.SpringContext;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CuCapAssetInventoryServerAuthFilter implements Filter {
+    private static final Logger LOG = LogManager.getLogger(CuCapAssetInventoryServerAuthFilter.class);
+
+    private WebServiceCredentialService webServiceCredentialService;
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "DELETE, PUT, POST, GET, OPTIONS");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Authorization, Access-Control-Request-Method, Access-Control-Request-Headers, capital_asset_scanning_api_key");
+        if (httpServletRequest.getMethod().equalsIgnoreCase("options")) {
+            chain.doFilter(request, response);
+        } else {
+            this.checkAuthorization(httpServletRequest, httpServletResponse, chain);
+        }
+    }
+
+    private void checkAuthorization(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (isAuthorized(request)) {
+            LOG.info("CapAssetInventoryApi authorized " + request.getMethod() + " " + request.getPathInfo());
+            chain.doFilter(request, response);
+        } else {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().println(new Gson().toJson(CuCamsConstants.CapAssetApi.UNAUTHORIZED));
+        }
+    }
+
+    private boolean isAuthorized(HttpServletRequest request) {
+        //todo switch to verify Cognito User Pool Key in request Header with Cognito public key
+        String correctApiKey = getWebServiceCredentialService().getWebServiceCredentialValue(CuCamsConstants.CapAssetApi.CAPITAL_ASSET_CREDENTIAL_GROUP_CODE,
+                CuCamsConstants.CapAssetApi.CAPITAL_ASSET_API_KEY_CREDENTIAL_NAME);
+        String submittedApiKey = request.getHeader(CuCamsConstants.CapAssetApi.CAPITAL_ASSET_API_KEY_CREDENTIAL_NAME);
+        return !StringUtils.isEmpty(correctApiKey) && !StringUtils.isEmpty(submittedApiKey) && submittedApiKey.equals(correctApiKey);
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    protected WebServiceCredentialService getWebServiceCredentialService() {
+        if (this.webServiceCredentialService == null) {
+            this.webServiceCredentialService = SpringContext.getBean(WebServiceCredentialService.class);
+        }
+
+        return this.webServiceCredentialService;
+    }
+
+}
