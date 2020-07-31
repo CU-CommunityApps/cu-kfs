f4e242ae05c071fbbc49c71378ebe2a84e36aa3d
Jay Hulslander <jdh34@cornell.edu>
2020-07-31 15:38:18 -0400

    Revert "Revert "undoing paymentworks call back prevention""
    This reverts commit ffc3f02996ef0f7599ee9276db07d9c560462d33.

--- src/main/java/edu/cornell/kfs/pmw/batch/service/impl/PaymentWorksNewVendorRequestsServiceImpl.java
+++ src/main/java/edu/cornell/kfs/pmw/batch/service/impl/PaymentWorksNewVendorRequestsServiceImpl.java
@@ -107,11 +107,7 @@ public class PaymentWorksNewVendorRequestsServiceImpl implements PaymentWorksNew
             } else {
                 LOG.error("processEachPaymentWorksNewVendorRequestIntoKFS, vendor data cannot be processed for pmwNewVendorRequestId: " + pmwNewVendorRequestId);
             }
-            /**
-             * @todo reenable this before merging
-             */
-            //getPaymentWorksWebServiceCallsService().sendProcessedStatusToPaymentWorksForNewVendor(pmwNewVendorRequestId);
-            
+            getPaymentWorksWebServiceCallsService().sendProcessedStatusToPaymentWorksForNewVendor(pmwNewVendorRequestId);
         }
         getPaymentWorksNewVendorRequestsReportService().generateAndEmailProcessingReport(reportData);
     }
