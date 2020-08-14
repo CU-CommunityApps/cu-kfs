471f755f3ef4f4474cac6db7332431d73a4126c5
Jay Hulslander <jdh34@cornell.edu>
2020-08-14 07:50:37 -0400

    KFSPTS-18900 implement PMW foreign vendors step 3 (#1010)

---
+++ src/test/java/edu/cornell/kfs/pmw/batch/service/impl/PaymentWorksTaxRuleDependencyServiceImplTest.java
@@ -0,0 +1,100 @@
+package edu.cornell.kfs.pmw.batch.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Date;
+import java.util.Calendar;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.kuali.kfs.sys.KFSConstants;
+
+import edu.cornell.kfs.pmw.batch.PaymentWorksConstants;
+import edu.cornell.kfs.pmw.batch.TaxRule;
+import edu.cornell.kfs.pmw.batch.businessobject.PaymentWorksVendor;
+
+class PaymentWorksTaxRuleDependencyServiceImplTest {
+    private PaymentWorksTaxRuleDependencyServiceImpl taxRuleService;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        Configurator.setLevel(PaymentWorksTaxRuleDependencyServiceImpl.class.getName(), Level.DEBUG);
+        taxRuleService = new PaymentWorksTaxRuleDependencyServiceImpl();
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        taxRuleService = null;
+    }
+
+    @Test
+    void testDetermineTaxRuleToUseForDataPopulationOther() {
+        PaymentWorksVendor pmwVendor = new PaymentWorksVendor();
+        TaxRule actualResults = taxRuleService.determineTaxRuleToUseForDataPopulation(pmwVendor, KFSConstants.COUNTRY_CODE_UNITED_STATES);
+        assertEquals(TaxRule.OTHER, actualResults);
+    }
+    
+    @Test
+    void testDetermineTaxRuleToUseForDataPopulationIndividualUsSSN() {
+        PaymentWorksVendor pmwVendor = new PaymentWorksVendor();
+        pmwVendor.setRequestingCompanyTaxClassificationCode(PaymentWorksConstants.INDIVIDUAL_SOLE_PROPRIETOR_TAX_CLASSIFICATION_INDICATOR);
+        pmwVendor.setRequestingCompanyTinType(PaymentWorksConstants.PaymentWorksTinType.SSN.getPmwCodeAsString());
+        TaxRule actualResults = taxRuleService.determineTaxRuleToUseForDataPopulation(pmwVendor, KFSConstants.COUNTRY_CODE_UNITED_STATES);
+        assertEquals(TaxRule.INDIVIDUAL_US_SSN, actualResults);
+    }
+    
+    @Test
+    void testDetermineTaxRuleToUseForDataPopulationIndividualUsEIN() {
+        PaymentWorksVendor pmwVendor = new PaymentWorksVendor();
+        pmwVendor.setRequestingCompanyTaxClassificationCode(PaymentWorksConstants.INDIVIDUAL_SOLE_PROPRIETOR_TAX_CLASSIFICATION_INDICATOR);
+        pmwVendor.setRequestingCompanyTinType(PaymentWorksConstants.PaymentWorksTinType.FEIN.getPmwCodeAsString());
+        TaxRule actualResults = taxRuleService.determineTaxRuleToUseForDataPopulation(pmwVendor, KFSConstants.COUNTRY_CODE_UNITED_STATES);
+        assertEquals(TaxRule.INDIVIDUAL_US_EIN, actualResults);
+    }
+    
+    @Test
+    void testDetermineTaxRuleToUseForDataPopulationNotIndividualUs() {
+        PaymentWorksVendor pmwVendor = new PaymentWorksVendor();
+        pmwVendor.setRequestingCompanyTaxClassificationCode(PaymentWorksConstants.C_CORPORATION_TAX_CLASSIFICATION_INDICATOR);
+        TaxRule actualResults = taxRuleService.determineTaxRuleToUseForDataPopulation(pmwVendor, KFSConstants.COUNTRY_CODE_UNITED_STATES);
+        assertEquals(TaxRule.NOT_INDIVIDUAL_US, actualResults);
+    }
+    
+    @Test
+    void testDetermineTaxRuleToUseForDataPopulationForeignIndividual() {
+        PaymentWorksVendor pmwVendor = new PaymentWorksVendor();
+        pmwVendor.setSupplierCategory("Foreign Individual");
+        pmwVendor.setRequestingCompanyTinType(PaymentWorksConstants.PaymentWorksTinType.FOREIGN_TIN.getPmwCodeAsString());
+        TaxRule actualResults = taxRuleService.determineTaxRuleToUseForDataPopulation(pmwVendor, "CA");
+        assertEquals(TaxRule.FOREIGN_INDIVIDUAL, actualResults);
+    }
+    
+    @Test
+    void testDetermineTaxRuleToUseForDataPopulationForeignIndividualUsTaxPayerId() {
+        PaymentWorksVendor pmwVendor = new PaymentWorksVendor();
+        pmwVendor.setSupplierCategory("Foreign Individual");
+        pmwVendor.setRequestingCompanyTinType(PaymentWorksConstants.PaymentWorksTinType.SSN.getPmwCodeAsString());
+        TaxRule actualResults = taxRuleService.determineTaxRuleToUseForDataPopulation(pmwVendor, "CA");
+        assertEquals(TaxRule.FOREIGN_INDIVIDUAL_US_TAX_PAYER, actualResults);
+    }
+    
+    @Test
+    void testDetermineTaxRuleToUseForDataPopulationForeignEntity() {
+        PaymentWorksVendor pmwVendor = new PaymentWorksVendor();
+        pmwVendor.setSupplierCategory("Foreign Entity");
+        TaxRule actualResults = taxRuleService.determineTaxRuleToUseForDataPopulation(pmwVendor, "CA");
+        assertEquals(TaxRule.FOREIGN_ENTITY, actualResults);
+    }
+    
+    @Test
+    void testBuildDateFromString() {
+        Date actualDate = taxRuleService.buildDateFromString("2020-07-01");
+        Date expectedDate = PaymentWorksVendorSupplierDiversityServiceImplTest.createCalendar(2020, Calendar.JULY, 1);
+        
+        assertEquals(expectedDate, actualDate);
+    }
+
+}
