733ba42ba7109df17b490a76cc0f1b68f0fd3265
Jay Hulslander <jdh34@cornell.edu>
2020-08-20 14:54:34 -0400

    adding unit test that got missed earlier

---
+++ src/test/java/edu/cornell/kfs/pmw/batch/TaxRuleTest.java
@@ -0,0 +1,94 @@
+package edu.cornell.kfs.pmw.batch;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Test;
+
+public class TaxRuleTest {
+
+    @Test
+    void testIndividualSSN() {
+        TaxRule rule = TaxRule.INDIVIDUAL_US_SSN;
+        assertEquals(PaymentWorksConstants.PaymentWorksTinType.SSN.getKfsTaxTypeCodeAsString(), rule.taxTypeCode);
+        assertEquals(PaymentWorksConstants.PaymentWorksTaxClassification.INDIVIDUAL_SOLE_PROPRIETOR.getTranslationToKfsOwnershipTypeCode(), 
+                rule.ownershipTypeCode);
+        assertTrue(rule.populateW9Attributes);
+        assertTrue(rule.populateFirstLastLegalName);
+        assertFalse(rule.populateBusinessLegalName);
+        assertDefaultForeignValuesForUSVendor(rule);
+    }
+    
+    @Test
+    void testIndividualEIN() {
+        TaxRule rule = TaxRule.INDIVIDUAL_US_EIN;
+        assertEquals(PaymentWorksConstants.PaymentWorksTinType.FEIN.getKfsTaxTypeCodeAsString(), rule.taxTypeCode);
+        assertEquals(PaymentWorksConstants.PaymentWorksTaxClassification.INDIVIDUAL_SOLE_PROPRIETOR.getTranslationToKfsOwnershipTypeCode(), 
+                rule.ownershipTypeCode);
+        assertTrue(rule.populateW9Attributes);
+        assertFalse(rule.populateFirstLastLegalName);
+        assertTrue(rule.populateBusinessLegalName);
+        assertDefaultForeignValuesForUSVendor(rule);
+    }
+    
+    @Test
+    void testNotIndividualUs() {
+        TaxRule rule = TaxRule.NOT_INDIVIDUAL_US;
+        assertEquals(PaymentWorksConstants.PaymentWorksTinType.FEIN.getKfsTaxTypeCodeAsString(), rule.taxTypeCode);
+        assertEquals(StringUtils.EMPTY, rule.ownershipTypeCode);
+        assertTrue(rule.populateW9Attributes);
+        assertFalse(rule.populateFirstLastLegalName);
+        assertTrue(rule.populateBusinessLegalName);
+        assertDefaultForeignValuesForUSVendor(rule);
+    }
+    
+    void assertDefaultForeignValuesForUSVendor(TaxRule rule) {
+        assertFalse(rule.isForeign);
+        assertFalse(rule.populateDateOfBirth);
+        assertEquals(StringUtils.EMPTY, rule.w8TypeCode);
+        assertFalse(rule.populateForeignSSN);
+    }
+    
+    @Test
+    void testForeignIndividualUSTaxPayer() {
+        TaxRule rule = TaxRule.FOREIGN_INDIVIDUAL_US_TAX_PAYER;
+        assertEquals(PaymentWorksConstants.PaymentWorksTinType.SSN.getKfsTaxTypeCodeAsString(), rule.taxTypeCode);
+        assertTrue(rule.populateForeignSSN);
+        assertStandardForeignIndividualValues(rule);
+    }
+    
+    @Test
+    void testForeignIndividual() {
+        TaxRule rule = TaxRule.FOREIGN_INDIVIDUAL;
+        assertEquals(PaymentWorksConstants.PaymentWorksTinType.FOREIGN_TIN.getKfsTaxTypeCodeAsString(), rule.taxTypeCode);
+        assertFalse(rule.populateForeignSSN);
+        assertStandardForeignIndividualValues(rule);
+    }
+
+    protected void assertStandardForeignIndividualValues(TaxRule rule) {
+        assertEquals(PaymentWorksConstants.PaymentWorksTaxClassification.INDIVIDUAL_SOLE_PROPRIETOR.getTranslationToKfsOwnershipTypeCode(), 
+                rule.ownershipTypeCode);
+        assertFalse(rule.populateW9Attributes);
+        assertTrue(rule.populateFirstLastLegalName);
+        assertFalse(rule.populateBusinessLegalName);
+        assertTrue(rule.isForeign);
+        assertTrue(rule.populateDateOfBirth);
+        assertEquals("BN", rule.w8TypeCode);
+    }
+    
+    @Test
+    void testForeignEntityl() {
+        TaxRule rule = TaxRule.FOREIGN_ENTITY;
+        assertEquals(PaymentWorksConstants.PaymentWorksTinType.FOREIGN_TIN.getKfsTaxTypeCodeAsString(), rule.taxTypeCode);
+        assertEquals(PaymentWorksConstants.PaymentWorksTaxClassification.C_CORPORATION.getTranslationToKfsOwnershipTypeCode(), 
+                rule.ownershipTypeCode);
+        assertFalse(rule.populateW9Attributes);
+        assertFalse(rule.populateFirstLastLegalName);
+        assertTrue(rule.populateBusinessLegalName);
+        assertTrue(rule.isForeign);
+        assertFalse(rule.populateDateOfBirth);
+        assertEquals("BE", rule.w8TypeCode);
+        assertFalse(rule.populateForeignSSN);
+    }
+
+}
