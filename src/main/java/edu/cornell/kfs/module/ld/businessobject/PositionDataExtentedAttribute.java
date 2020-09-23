53b5dbbdeb18c821659047a2df8e6ef8c87d0a08
db483 <db483@cornell.edu>
2020-09-23 10:41:33 +0100

    KFSPTS-19100 add inactive date to position data

--- src/main/java/edu/cornell/kfs/module/ld/businessobject/PositionDataExtentedAttribute.java
+++ src/main/java/edu/cornell/kfs/module/ld/businessobject/PositionDataExtentedAttribute.java
@@ -1,49 +1,51 @@
 package edu.cornell.kfs.module.ld.businessobject;
+
 import java.sql.Date;
 
 import org.kuali.kfs.krad.bo.PersistableBusinessObjectExtensionBase;
 
-
 public class PositionDataExtentedAttribute extends PersistableBusinessObjectExtensionBase {
-	private static final long serialVersionUID = 1L;
-	
-	  private String positionNumber;
-	  private String orgCode;
-	  private Date effectiveDate;
-	  
-	  public PositionDataExtentedAttribute(){
-		 
-	  }
+    private static final long serialVersionUID = 1L;
 
-	  
+    private String positionNumber;
+    private String orgCode;
+    private Date effectiveDate;
+    private Date inactivationDate;
 
-	public String getPositionNumber() {
-		return positionNumber;
-	}
+    public PositionDataExtentedAttribute() {
 
-	public void setPositionNumber(String positionNumber) {
-		this.positionNumber = positionNumber;
-	}
+    }
 
-	public String getOrgCode() {
-		return orgCode;
-	}
+    public String getPositionNumber() {
+        return positionNumber;
+    }
 
-	public void setOrgCode(String orgCode) {
-		this.orgCode = orgCode;
-	}
+    public void setPositionNumber(String positionNumber) {
+        this.positionNumber = positionNumber;
+    }
 
-	public Date getEffectiveDate() {
-		return effectiveDate;
-	}
+    public String getOrgCode() {
+        return orgCode;
+    }
 
-	public void setEffectiveDate(Date effectiveDate) {
-		this.effectiveDate = effectiveDate;
-	}
+    public void setOrgCode(String orgCode) {
+        this.orgCode = orgCode;
+    }
 
-	  
+    public Date getEffectiveDate() {
+        return effectiveDate;
+    }
 
+    public void setEffectiveDate(Date effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
 
-	  
-	  
+    public Date getInactivationDate() {
+        return inactivationDate;
+    }
+
+    public void setInactivationDate(Date inactivationDate) {
+        this.inactivationDate = inactivationDate;
+    }
+
 }
