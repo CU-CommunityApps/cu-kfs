64fb966e5c09001fef3191ae1a41e1c39087d77a
db483 <db483@cornell.edu>
2020-10-12 11:53:18 +0100

    KFSPTS-19961 remove unrecognizable characters

--- src/main/java/org/kuali/kfs/gl/web/struts/BalanceInquiryLookupAction.java
+++ src/main/java/org/kuali/kfs/gl/web/struts/BalanceInquiryLookupAction.java
@@ -171,7 +171,7 @@ public class BalanceInquiryLookupAction extends KualiMultipleValueLookupAction {
                         KFSKeyConstants.ERROR_CUSTOM, "Fiscal Year must be a four-digit number");
             } catch (Exception e) {
                 GlobalVariables.getMessageMap().putError(KFSConstants.DOCUMENT_ERRORS, KFSKeyConstants.ERROR_CUSTOM,
-                        "Please report the server error.≥");
+                        "Please report the server error.>>");
                 LOG.error("Application Errors", e);
             }
         }
