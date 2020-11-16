81631b76505ed35bb4b4950bfdaad323add9124f
nkimble <nkk4@cornell.edu>
2020-11-16 16:16:51 -0500

    KFSPTS-19994: Remove customization allowing Fiscal Officers to adjust accounts that are not theirs on REQS. (#1051)

--- src/main/java/edu/cornell/kfs/module/purap/document/service/impl/CuRequisitionServiceImpl.java
+++ src/main/java/edu/cornell/kfs/module/purap/document/service/impl/CuRequisitionServiceImpl.java
@@ -1,7 +1,6 @@
 package edu.cornell.kfs.module.purap.document.service.impl;
 
 import java.math.BigDecimal;
-import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
@@ -27,13 +26,13 @@ public class CuRequisitionServiceImpl extends RequisitionServiceImpl {
     private static final Logger LOG = LogManager.getLogger(CuRequisitionServiceImpl.class);
     
     /**
-     * Checks the rule for Automatic Purchase Order eligibility of the requisition and return a String containing the reason why the
-     * requisition was not eligible to become an APO if it was not eligible, or return an empty String if the requisition is
-     * eligible to become an APO
-     * 
+     * Checks the rule for Automatic Purchase Order eligibility of the requisition and return a String containing the
+     * reason why the requisition was not eligible to become an APO if it was not eligible, or return an empty String
+     * if the requisition is eligible to become an APO
+     *
      * @param requisition the requisition document to be checked for APO eligibility.
-     * @return String containing the reason why the requisition was not eligible to become an APO if it was not eligible, or an
-     *         empty String if the requisition is eligible to become an APO.
+     * @return String containing the reason why the requisition was not eligible to become an APO if it was not
+     *         eligible, or an empty String if the requisition is eligible to become an APO.
      */
     @Override
     protected String checkAutomaticPurchaseOrderRules(RequisitionDocument requisition) {
@@ -43,7 +42,8 @@ public class CuRequisitionServiceImpl extends RequisitionServiceImpl {
         requisition.setOrganizationAutomaticPurchaseOrderLimit(apoLimit);
 
         if (LOG.isDebugEnabled()) {
-            LOG.debug("isAPO() reqId = " + requisition.getPurapDocumentIdentifier() + "; apoLimit = " + apoLimit + "; reqTotal = " + reqTotal);
+            LOG.debug("isAPO() reqId = " + requisition.getPurapDocumentIdentifier() + "; apoLimit = " + apoLimit +
+                    "; reqTotal = " + reqTotal);
         }
         if (apoLimit == null) {
             return "APO limit is empty.";
@@ -58,9 +58,11 @@ public class CuRequisitionServiceImpl extends RequisitionServiceImpl {
         }
 
         if (LOG.isDebugEnabled()) {
-            LOG.debug("isAPO() vendor #" + requisition.getVendorHeaderGeneratedIdentifier() + "-" + requisition.getVendorDetailAssignedIdentifier());
+            LOG.debug("isAPO() vendor #" + requisition.getVendorHeaderGeneratedIdentifier() + "-" +
+                    requisition.getVendorDetailAssignedIdentifier());
         }
-        if (requisition.getVendorHeaderGeneratedIdentifier() == null || requisition.getVendorDetailAssignedIdentifier() == null) {
+        if (requisition.getVendorHeaderGeneratedIdentifier() == null
+                || requisition.getVendorDetailAssignedIdentifier() == null) {
             return "Vendor was not selected from the vendor database.";
         } else {
             VendorDetail vendorDetail = vendorService.getVendorDetail(requisition.getVendorHeaderGeneratedIdentifier(),
@@ -68,10 +70,11 @@ public class CuRequisitionServiceImpl extends RequisitionServiceImpl {
             if (vendorDetail == null) {
                 return "Error retrieving vendor from the database.";
             }
-            if (StringUtils.isBlank(requisition.getVendorLine1Address()) 
-                    ||  StringUtils.isBlank(requisition.getVendorCityName()) 
-                    || StringUtils.isBlank(requisition.getVendorCountryCode())) {
-                return "Requisition does not have all of the vendor address fields that are required for Purchase Order.";
+            if (StringUtils.isBlank(requisition.getVendorLine1Address()) ||
+                StringUtils.isBlank(requisition.getVendorCityName()) ||
+                StringUtils.isBlank(requisition.getVendorCountryCode())) {
+                return "Requisition does not have all of the vendor address fields that are required for Purchase " +
+                        "Order.";
             }
             requisition.setVendorRestrictedIndicator(vendorDetail.getVendorRestrictedIndicator());
             if (requisition.getVendorRestrictedIndicator() != null && requisition.getVendorRestrictedIndicator()) {
@@ -82,115 +85,144 @@ public class CuRequisitionServiceImpl extends RequisitionServiceImpl {
             }
             requisition.setVendorDetail(vendorDetail);
 
-            if ((!PurapConstants.RequisitionSources.B2B.equals(requisitionSource)) && ObjectUtils.isNull(requisition.getVendorContractGeneratedIdentifier())) {
-               Person initiator = personService.getPerson(requisition.getDocumentHeader().getWorkflowDocument().getInitiatorPrincipalId());
-                VendorContract b2bContract = vendorService.getVendorB2BContract(vendorDetail, initiator.getCampusCode());
+            if (!PurapConstants.RequisitionSources.B2B.equals(requisitionSource)
+                    && ObjectUtils.isNull(requisition.getVendorContractGeneratedIdentifier())) {
+                Person initiator = personService.getPerson(requisition.getDocumentHeader().getWorkflowDocument()
+                        .getInitiatorPrincipalId());
+                VendorContract b2bContract = vendorService.getVendorB2BContract(vendorDetail,
+                        initiator.getCampusCode());
                 if (b2bContract != null) {
-                    return "Standard requisition with no contract selected but a B2B contract exists for the selected vendor.";
+                    return "Standard requisition with no contract selected but a B2B contract exists for the " +
+                            "selected vendor.";
                 }
             }
-            
-            //vendor contract expirated date validation....KFSMI-8502
-            // if the vendor is selected through vendor contract is selected 
+
+            //vendor contract expiration date validation....KFSMI-8502
+            // if the vendor is selected through vendor contract is selected
             if (StringUtils.isNotBlank(requisition.getVendorContractName())) {
+                
+                //CU mod: KFSUPGRADE-926
                 boolean routeToCM = parameterService.getParameterValueAsBoolean(RequisitionDocument.class,
                         CUPurapParameterConstants.ROUTE_REQS_WITH_EXPIRED_CONTRACT_TO_CM, Boolean.FALSE);
 
-                if (routeToCM && vendorService.isVendorContractExpired(requisition, requisition.getVendorContractGeneratedIdentifier(), vendorDetail)) {
+                if (routeToCM && 
+                    vendorService.isVendorContractExpired(requisition,
+                        requisition.getVendorContractGeneratedIdentifier(), vendorDetail)) {
                     return "Contracted Vendor used where the contract end date is expired.";
                 }
             }
         }
         
         //if vendor address isn't complete, no APO
-        if (StringUtils.isBlank(requisition.getVendorLine1Address()) 
-                || StringUtils.isBlank(requisition.getVendorCityName()) 
-                || StringUtils.isBlank(requisition.getVendorCountryCode()) 
+        if (StringUtils.isBlank(requisition.getVendorLine1Address())
+                || StringUtils.isBlank(requisition.getVendorCityName())
+                || StringUtils.isBlank(requisition.getVendorCountryCode())
                 || !postalCodeValidationService.validateAddress(requisition.getVendorCountryCode(),
-                        requisition.getVendorStateCode(), requisition.getVendorPostalCode(), "", "")) {
-            return "Requistion does not contain a complete vendor address";
+                    requisition.getVendorStateCode(), requisition.getVendorPostalCode(), "", "")) {
+            return "Requisition does not contain a complete vendor address";
         }
 
-        // These are needed for commodity codes. They are put in here so that
-        // we don't have to loop through items too many times.
-        String purchaseOrderRequiresCommodityCode = parameterService.getParameterValueAsString(PurchaseOrderDocument.class,
-                PurapRuleConstants.ITEMS_REQUIRE_COMMODITY_CODE_IND);
+        // These are needed for commodity codes. They are put in here so that we don't have to loop through items too
+        // many times.
+        String purchaseOrderRequiresCommodityCode = parameterService.getParameterValueAsString(
+                PurchaseOrderDocument.class, PurapRuleConstants.ITEMS_REQUIRE_COMMODITY_CODE_IND);
         boolean commodityCodeRequired = "Y".equals(purchaseOrderRequiresCommodityCode);
         
-        for (Iterator iter = requisition.getItems().iterator(); iter.hasNext();) {
-            RequisitionItem item = (RequisitionItem) iter.next();
+        for (Object anItem : requisition.getItems()) {
+            RequisitionItem item = (RequisitionItem) anItem;
             if (item.isItemRestrictedIndicator()) {
                 return "Requisition contains an item that is marked as restricted.";
             }
-            
+
             //We only need to check the commodity codes if this is an above the line item.
             if (item.getItemType().isLineItemIndicator()) {
-                String commodityCodesReason = "";
-                List<VendorCommodityCode> vendorCommodityCodes = commodityCodeRequired ? requisition.getVendorDetail().getVendorCommodities() : null;
-                commodityCodesReason = checkAPORulesPerItemForCommodityCodes(item, vendorCommodityCodes, commodityCodeRequired);
+                List<VendorCommodityCode> vendorCommodityCodes = commodityCodeRequired ?
+                        requisition.getVendorDetail().getVendorCommodities() : null;
+                String commodityCodesReason = checkAPORulesPerItemForCommodityCodes(item, vendorCommodityCodes,
+                        commodityCodeRequired);
                 if (StringUtils.isNotBlank(commodityCodesReason)) {
                     return commodityCodesReason;
                 }
             }
             
-            if (PurapConstants.ItemTypeCodes.ITEM_TYPE_ORDER_DISCOUNT_CODE.equals(item.getItemType().getItemTypeCode())
-                    || PurapConstants.ItemTypeCodes.ITEM_TYPE_TRADE_IN_CODE.equals(item.getItemType().getItemTypeCode())) {
-                if ((item.getItemUnitPrice() != null) && ((BigDecimal.ZERO.compareTo(item.getItemUnitPrice())) != 0)) {
+            if (PurapConstants.ItemTypeCodes.ITEM_TYPE_ORDER_DISCOUNT_CODE.equals(
+                        item.getItemType().getItemTypeCode())
+                    || PurapConstants.ItemTypeCodes.ITEM_TYPE_TRADE_IN_CODE.equals(
+                            item.getItemType().getItemTypeCode())) {
+                if (item.getItemUnitPrice() != null && BigDecimal.ZERO.compareTo(item.getItemUnitPrice()) != 0) {
                     // discount or trade-in item has unit price that is not empty or zero
-                    return "Requisition contains a " + item.getItemType().getItemTypeDescription() + " item, so it does not qualify as an APO.";
+                    return "Requisition contains a " + item.getItemType().getItemTypeDescription() +
+                            " item, so it does not qualify as an APO.";
                 }
             }
-            
-            
+//            //Base code logic check not in CU mod
+//            if (!PurapConstants.RequisitionSources.B2B.equals(requisitionSource)) {
+//                for (PurApAccountingLine accountingLine : item.getSourceAccountingLines()) {
+//                    if (capitalAssetManagementModuleService.doesAccountingLineFailAutomaticPurchaseOrderRules(
+//                            accountingLine)) {
+//                        return "Requisition contains accounting line with capital object level";
+//                    }
+//                }
+//            }
 
         }
-
-        
+//        //Base code logic check not in CU mod
+//        if (capitalAssetManagementModuleService.doesDocumentFailAutomaticPurchaseOrderRules(requisition)) {
+//            return "Requisition contains capital asset items.";
+//        }
 
         if (StringUtils.isNotEmpty(requisition.getRecurringPaymentTypeCode())) {
             return "Payment type is marked as recurring.";
         }
 
-        if ((requisition.getPurchaseOrderTotalLimit() != null) && (KualiDecimal.ZERO.compareTo(requisition.getPurchaseOrderTotalLimit()) != 0)) {
+        if (requisition.getPurchaseOrderTotalLimit() != null
+                && KualiDecimal.ZERO.compareTo(requisition.getPurchaseOrderTotalLimit()) != 0) {
             LOG.debug("isAPO() po total limit is not null and not equal to zero; return false.");
             return "The 'PO not to exceed' amount has been entered.";
         }
 
-        if (StringUtils.isNotEmpty(requisition.getAlternate1VendorName()) || StringUtils.isNotEmpty(requisition.getAlternate2VendorName()) 
-                || StringUtils.isNotEmpty(requisition.getAlternate3VendorName()) || StringUtils.isNotEmpty(requisition.getAlternate4VendorName())
+        if (StringUtils.isNotEmpty(requisition.getAlternate1VendorName())
+                || StringUtils.isNotEmpty(requisition.getAlternate2VendorName())
+                || StringUtils.isNotEmpty(requisition.getAlternate3VendorName())
+                || StringUtils.isNotEmpty(requisition.getAlternate4VendorName())
                 || StringUtils.isNotEmpty(requisition.getAlternate5VendorName())) {
             LOG.debug("isAPO() alternate vendor name exists; return false.");
             return "Requisition contains additional suggested vendor names.";
         }
-        
+
         if (requisition.isPostingYearNext() && !purapService.isTodayWithinApoAllowedRange()) {
             return "Requisition is set to encumber next fiscal year and approval is not within APO allowed date range.";
         }
 
         return "";
     }
-    
+
+
     /**
-     * Checks the APO rules for Commodity Codes. 
+     * Checks the APO rules for Commodity Codes.
      * The rules are as follow:
-     * 1. If an institution does not require a commodity code on a requisition but 
-     *    does require a commodity code on a purchase order:
-     *    a. If the requisition qualifies for an APO and the commodity code is blank
-     *       on any line item then the system should use the default commodity code
-     *       for the vendor.
-     *    b. If there is not a default commodity code for the vendor then the
-     *       requisition is not eligible to become an APO.
-     * 2. The commodity codes where the restricted indicator is Y should disallow
-     *    the requisition from becoming an APO.
-     *    
+     * 1. If an institution does not require a commodity code on a requisition but does require a commodity code on a
+     * purchase order:
+     * a. If the requisition qualifies for an APO and the commodity code is blank on any line item then the system
+     * should use the default commodity code for the vendor.
+     * b. If there is not a default commodity code for the vendor then the requisition is not eligible to become an
+     * APO.
+     * 2. The commodity codes where the restricted indicator is Y should disallow the requisition from becoming an
+     * APO.
+     * 
+     * KFSPTS-1319 Removed this validation check
+     * 3. If the commodity code is Inactive when the requisition is finally approved do not allow the requisition to
+     * become an APO.
+     *
      * @param purItem
      * @param vendorCommodityCodes
      * @param commodityCodeRequired
      * @return
      */
-    protected String checkAPORulesPerItemForCommodityCodes(RequisitionItem purItem, List<VendorCommodityCode>vendorCommodityCodes, boolean commodityCodeRequired) {
-        // If the commodity code is blank on any line item and a commodity code is required,
-        // then the system should use the default commodity code for the vendor
+    protected String checkAPORulesPerItemForCommodityCodes(RequisitionItem purItem,
+            List<VendorCommodityCode> vendorCommodityCodes, boolean commodityCodeRequired) {
+        // If the commodity code is blank on any line item and a commodity code is required, then the system should
+        // use the default commodity code for the vendor
         if (purItem.getCommodityCode() == null && commodityCodeRequired) {
             for (VendorCommodityCode vcc : vendorCommodityCodes) {
                 if (vcc.isCommodityDefaultIndicator()) {
@@ -200,10 +232,14 @@ public class CuRequisitionServiceImpl extends RequisitionServiceImpl {
             }
         }
         if (purItem.getCommodityCode() == null) {
-            // If there is not a default commodity code for the vendor then the requisition is not eligible to become an APO.
+            // If there is not a default commodity code for the vendor then the requisition is not eligible to become
+            // an APO.
             if (commodityCodeRequired) {
                 return "There are missing commodity code(s).";
             }
+//        KFSPTS-1319: Removed inactive commodity code validation check
+//        } else if (!purItem.getCommodityCode().isActive()) {
+//            return "Requisition contains inactive commodity codes.";
         } else if (purItem.getCommodityCode().isRestrictedItemsIndicator()) {
             return "Requisition contains an item with a restricted commodity code.";
         }
--- src/main/java/edu/cornell/kfs/module/purap/service/impl/CuPurapAccountingServiceImpl.java
+++ src/main/java/edu/cornell/kfs/module/purap/service/impl/CuPurapAccountingServiceImpl.java
@@ -1,7 +1,6 @@
 package edu.cornell.kfs.module.purap.service.impl;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,108 +11,21 @@ import org.apache.commons.lang3.StringUtils;
 import org.kuali.kfs.module.purap.PurapConstants;
 import org.kuali.kfs.module.purap.PurapConstants.PaymentRequestStatuses;
 import org.kuali.kfs.module.purap.PurapPropertyConstants;
-import org.kuali.kfs.module.purap.businessobject.PaymentRequestAccount;
 import org.kuali.kfs.module.purap.businessobject.PaymentRequestItem;
 import org.kuali.kfs.module.purap.businessobject.PurApAccountingLine;
-import org.kuali.kfs.module.purap.businessobject.PurApAccountingLineBase;
 import org.kuali.kfs.module.purap.businessobject.PurApItem;
-import org.kuali.kfs.module.purap.businessobject.PurchaseOrderAccount;
-import org.kuali.kfs.module.purap.businessobject.RequisitionAccount;
 import org.kuali.kfs.module.purap.document.PaymentRequestDocument;
 import org.kuali.kfs.module.purap.document.PurchasingAccountsPayableDocument;
 import org.kuali.kfs.module.purap.document.PurchasingAccountsPayableDocumentBase;
 import org.kuali.kfs.module.purap.document.VendorCreditMemoDocument;
 import org.kuali.kfs.module.purap.document.validation.event.PurchasingAccountsPayableItemPreCalculateEvent;
 import org.kuali.kfs.module.purap.service.impl.PurapAccountingServiceImpl;
-import org.kuali.kfs.sys.KFSConstants;
-import org.kuali.kfs.sys.businessobject.SourceAccountingLine;
-import org.kuali.kfs.sys.identity.KfsKimAttributes;
 import org.kuali.rice.core.api.util.type.KualiDecimal;
 import org.kuali.rice.kew.api.WorkflowDocument;
-import org.kuali.kfs.kim.api.KimConstants;
-import org.kuali.rice.kim.api.role.RoleService;
-import org.kuali.kfs.krad.service.KualiRuleService;
-import org.kuali.kfs.krad.util.GlobalVariables;
 import org.kuali.kfs.krad.util.ObjectUtils;
 
-import edu.cornell.kfs.module.purap.service.CuPurapAccountingService;
+public class CuPurapAccountingServiceImpl extends PurapAccountingServiceImpl {
 
-
-public class CuPurapAccountingServiceImpl extends PurapAccountingServiceImpl implements CuPurapAccountingService {
-
-    private RoleService roleService;
-    
-    public boolean isFiscalOfficersForAllAcctLines(PurchasingAccountsPayableDocument document) {
-
-        boolean isFoForAcctLines = true;
-        String personId = GlobalVariables.getUserSession().getPrincipalId();
-        for (SourceAccountingLine accountingLine : (List<SourceAccountingLine>)document.getSourceAccountingLines()) {
-            List<String> fiscalOfficers = new ArrayList<String>();
-            Map<String,String> roleQualifier = new HashMap<String,String>();
-            // KFSPTS-1273 : this method is final, so can't be overriden by REQ Auth.  This is a fix for REQ existing issue.  a broader solution need more work.
-            // the authorizer is called by validation rule and rendertag, so has to do it here.  otherwise the invalid account will be saved.
-            boolean isExistingReqAcctline = false;
-            String updatedAccountNumber = KFSConstants.EMPTY_STRING;
-            if (accountingLine instanceof RequisitionAccount && ((RequisitionAccount) accountingLine).getAccountIdentifier() != null) {
-                RequisitionAccount dbAcctLine = (RequisitionAccount) getAccountFromDb((RequisitionAccount) accountingLine, RequisitionAccount.class);
-                if (dbAcctLine != null && !StringUtils.equals(accountingLine.getAccountNumber(), dbAcctLine.getAccountNumber())) {
-                    updatedAccountNumber = accountingLine.getAccountNumber();
-                    accountingLine.setAccountNumber(dbAcctLine.getAccountNumber());
-                    isExistingReqAcctline = true;
-                }
-            }
-            if (accountingLine instanceof PurchaseOrderAccount && ((PurchaseOrderAccount) accountingLine).getAccountIdentifier() != null) {
-                PurchaseOrderAccount dbAcctLine = (PurchaseOrderAccount) getAccountFromDb((PurchaseOrderAccount) accountingLine, PurchaseOrderAccount.class);
-                if (dbAcctLine != null && !StringUtils.equals(accountingLine.getAccountNumber(), dbAcctLine.getAccountNumber())) {
-                    updatedAccountNumber = accountingLine.getAccountNumber();
-                    accountingLine.setAccountNumber(dbAcctLine.getAccountNumber());
-                    isExistingReqAcctline = true;
-                }
-            }
-            if (accountingLine instanceof PaymentRequestAccount && ((PaymentRequestAccount) accountingLine).getAccountIdentifier() != null) {
-                PaymentRequestAccount dbAcctLine = (PaymentRequestAccount) 
-                        getAccountFromDb((PaymentRequestAccount) accountingLine, PaymentRequestAccount.class);
-                if (dbAcctLine != null && !StringUtils.equals(accountingLine.getAccountNumber(), dbAcctLine.getAccountNumber())) {
-                    updatedAccountNumber = accountingLine.getAccountNumber();
-                    accountingLine.setAccountNumber(dbAcctLine.getAccountNumber());
-                    isExistingReqAcctline = true;
-                }
-            }
-
-            roleQualifier.put(KimConstants.AttributeConstants.DOCUMENT_NUMBER,document.getDocumentNumber());
-            roleQualifier.put(KimConstants.AttributeConstants.DOCUMENT_TYPE_NAME, document.getDocumentHeader().getWorkflowDocument().getDocumentTypeName());
-            roleQualifier.put(KfsKimAttributes.FINANCIAL_DOCUMENT_TOTAL_AMOUNT,
-                    document.getFinancialSystemDocumentHeader().getFinancialDocumentTotalAmount().toString());
-            roleQualifier.put(KfsKimAttributes.CHART_OF_ACCOUNTS_CODE,accountingLine.getChartOfAccountsCode());
-            roleQualifier.put(KfsKimAttributes.ACCOUNT_NUMBER,accountingLine.getAccountNumber());
-            fiscalOfficers.addAll(roleService.getRoleMemberPrincipalIds(KFSConstants.CoreModuleNamespaces.KFS,
-                    KFSConstants.SysKimApiConstants.FISCAL_OFFICER_KIM_ROLE_NAME,roleQualifier));
-            if (!fiscalOfficers.contains(personId)) {
-                fiscalOfficers.addAll(roleService.getRoleMemberPrincipalIds(
-                                        KFSConstants.CoreModuleNamespaces.KFS,KFSConstants.SysKimApiConstants.FISCAL_OFFICER_PRIMARY_DELEGATE_KIM_ROLE_NAME,
-                                        roleQualifier));
-            }
-            if (!fiscalOfficers.contains(personId)) {
-                fiscalOfficers.addAll(roleService.getRoleMemberPrincipalIds(KFSConstants.CoreModuleNamespaces.KFS,
-                                        KFSConstants.SysKimApiConstants.FISCAL_OFFICER_SECONDARY_DELEGATE_KIM_ROLE_NAME,roleQualifier));
-            }
-            if (isExistingReqAcctline) {
-                accountingLine.setAccountNumber(updatedAccountNumber);
-            }
-            if (!fiscalOfficers.contains(personId)) {
-                isFoForAcctLines = false;
-                break;
-            }
-        }
-
-        return isFoForAcctLines;
-    }
-    private PurApAccountingLineBase getAccountFromDb(PurApAccountingLineBase accountingLine, Class clazz) {
-        Map<String, Object> primaryKeys = new HashMap<String, Object>();
-        primaryKeys.put("accountIdentifier", accountingLine.getAccountIdentifier());
-        return (PurApAccountingLineBase)businessObjectService.findByPrimaryKey(clazz, primaryKeys);
-    }
-    
     /**
      * @see org.kuali.kfs.module.purap.service.PurapAccountingService#updateAccountAmounts(org.kuali.kfs.module.purap.document.PurchasingAccountsPayableDocument)
      */
@@ -121,7 +33,6 @@ public class CuPurapAccountingServiceImpl extends PurapAccountingServiceImpl imp
     public void updateAccountAmounts(PurchasingAccountsPayableDocument document) {
 
         PurchasingAccountsPayableDocumentBase purApDocument = (PurchasingAccountsPayableDocumentBase) document;
-        String accountDistributionMethod = purApDocument.getAccountDistributionMethod();
 
         WorkflowDocument workflowDocument = purApDocument.getDocumentHeader().getWorkflowDocument();
 
@@ -354,9 +265,6 @@ public class CuPurapAccountingServiceImpl extends PurapAccountingServiceImpl imp
                 account.setAmount(KualiDecimal.ZERO);
             }
         }
-    }
-    public void setRoleService(RoleService roleService) {
-        this.roleService = roleService;
     }
 
 }
