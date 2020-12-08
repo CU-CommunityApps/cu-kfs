9d616a20f4bddd2dfb3418f3c284893832c0d7d0
cah292 <cah292@cornell.edu>
2020-12-08 17:51:43 -0500

    KFSPTS-20411 Fix Any/None payment reason handling

--- src/main/java/edu/cornell/kfs/tax/CUTaxPropertyConstants.java
+++ src/main/java/edu/cornell/kfs/tax/CUTaxPropertyConstants.java
@@ -8,6 +8,7 @@ public final class CUTaxPropertyConstants {
     public static final String TAX_TYPE = "taxType";
     public static final String FORM_TYPE = "formType";
     public static final String BOX_NUMBER = "boxNumber";
+    public static final String DV_PAYMENT_REASON_CODE = "dvPaymentReasonCode";
 
     private CUTaxPropertyConstants() {
         throw new UnsupportedOperationException("do not call");
