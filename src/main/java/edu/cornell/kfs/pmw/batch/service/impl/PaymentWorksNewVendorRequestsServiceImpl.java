aa1a3186c60224b30134771111933d6918addf4b
Jay Hulslander <jdh34@cornell.edu>
2020-12-22 16:19:12 -0500

    Revert "temporarily disable call back to PMW"
    This reverts commit 886c55c67a101ca136dc585813aba81938ed17d0.

--- src/main/java/edu/cornell/kfs/pmw/batch/service/impl/PaymentWorksNewVendorRequestsServiceImpl.java
+++ src/main/java/edu/cornell/kfs/pmw/batch/service/impl/PaymentWorksNewVendorRequestsServiceImpl.java
@@ -104,11 +104,7 @@ public class PaymentWorksNewVendorRequestsServiceImpl implements PaymentWorksNew
             } else {
                 LOG.error("processEachPaymentWorksNewVendorRequestIntoKFS, vendor data cannot be processed for pmwNewVendorRequestId: " + pmwNewVendorRequestId);
             }
-            /**
-             * @todo re-enable this
-             */
-            LOG.info("processEachPaymentWorksNewVendorRequestIntoKFS, disabled call back to paymentworks");
-            //getPaymentWorksWebServiceCallsService().sendProcessedStatusToPaymentWorksForNewVendor(pmwNewVendorRequestId);
+            getPaymentWorksWebServiceCallsService().sendProcessedStatusToPaymentWorksForNewVendor(pmwNewVendorRequestId);
         }
         getPaymentWorksNewVendorRequestsReportService().generateAndEmailProcessingReport(reportData);
     }
