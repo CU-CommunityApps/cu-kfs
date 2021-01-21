12f34799bdfbae2087eccfc78fea06e41048d69a
db483 <db483@cornell.edu>
2021-01-21 14:28:42 +0000

    KFSPTS-20236 Remove dashes from ach bank account number pmw value (#1084)

--- src/test/java/edu/cornell/kfs/pmw/batch/service/impl/PaymentWorksDtoToPaymentWorksVendorConversionServiceImplTest.java
+++ src/test/java/edu/cornell/kfs/pmw/batch/service/impl/PaymentWorksDtoToPaymentWorksVendorConversionServiceImplTest.java
@@ -1,8 +1,7 @@
 package edu.cornell.kfs.pmw.batch.service.impl;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.*;
-import java.lang.reflect.Field;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,6 +17,9 @@ class PaymentWorksDtoToPaymentWorksVendorConversionServiceImplTest {
     
     private static final String FILE_VALUE = "https://sandbox.paymentworks.com/api/files/jaydhulslander-1/private/custom-fields/53cee13c-8c6b-4410-8640-bb3b8a0c8624/yankees-jack_yQVOSVw.jpg";
     private static final String FIELD_VALUE_VALUE = "testing field value";
+    private static final String PMW_ACH_BANK_ACCT_NUMBER_VALUE_WITH_DASH = "123-5-7";
+    private static final String PMW_ACH_BANK_ACCT_NUMBER_SANITIZED_VALUE = "12357";
+    private static final String PMW_ACH_BANK_ACCT_NUMBER_VALUE_WITHOUT_DASH = "1234567";
     
     private PaymentWorksCustomFieldDTO paymentWorksCustomFieldDTO;
 
@@ -62,6 +64,36 @@ class PaymentWorksDtoToPaymentWorksVendorConversionServiceImplTest {
             return;
         }
         assertTrue("We should have caught an illegal arguement exception, something went wrong", false);
+    }
+    
+    @Test
+    public void testSanitizeACHBankAccountNumberValueWithDash() {
+        String actualResults = paymentWorksDtoToPaymentWorksVendorConversionServiceImpl.sanitizeBankAcctBankAccountNumberValue(PMW_ACH_BANK_ACCT_NUMBER_VALUE_WITH_DASH);
+        assertEquals(PMW_ACH_BANK_ACCT_NUMBER_SANITIZED_VALUE, actualResults);
+    }
+    
+    @Test
+    public void testSanitizeACHBankAccountNumberValueWithoutDash() {
+        String actualResults = paymentWorksDtoToPaymentWorksVendorConversionServiceImpl.sanitizeBankAcctBankAccountNumberValue(PMW_ACH_BANK_ACCT_NUMBER_VALUE_WITHOUT_DASH);
+        assertEquals(PMW_ACH_BANK_ACCT_NUMBER_VALUE_WITHOUT_DASH, actualResults);
+    }
+    
+    @Test
+    public void testSanitizeACHBankAccountNumberValueEmpty() {
+        String actualResults = paymentWorksDtoToPaymentWorksVendorConversionServiceImpl.sanitizeBankAcctBankAccountNumberValue("");
+        assertEquals("", actualResults);
+    }
+    
+    @Test
+    public void testSanitizeACHBankAccountNumberValueNull() {
+        String actualResults = paymentWorksDtoToPaymentWorksVendorConversionServiceImpl.sanitizeBankAcctBankAccountNumberValue(null);
+        assertEquals(null, actualResults);
+    }
+    
+    @Test
+    public void testSanitizeACHBankAccountNumberValueEmptySpaces() {
+        String actualResults = paymentWorksDtoToPaymentWorksVendorConversionServiceImpl.sanitizeBankAcctBankAccountNumberValue("  ");
+        assertEquals("  ", actualResults);
     }
 
 }
