05350cd96fdffe970908c2bf12899df079e0d2b3
Jay Hulslander <jdh34@cornell.edu>
2021-02-19 14:22:37 -0500

    removing ezra integration

--- src/main/java/edu/cornell/kfs/module/ezra/EzraPropertyConstants.java
+++
@@ -1,8 +0,0 @@
-package edu.cornell.kfs.module.ezra;
-
-public class EzraPropertyConstants {
-
-    public static final String PROJECT_ID = "projectId";
-    public static final String AWARD_PROPOSAL_ID = "awardProposalId";
-
-}
