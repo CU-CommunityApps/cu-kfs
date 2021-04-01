1b9fcbd428468c509851a25cdecbbdccb8162e0b
Jay Hulslander <jdh34@cornell.edu>
2021-04-01 16:08:37 -0400

    code review clean up

--- src/main/java/edu/cornell/kfs/vnd/businessobject/options/EinvoiceIndicatorValuesFinder.java
+++ src/main/java/edu/cornell/kfs/vnd/businessobject/options/EinvoiceIndicatorValuesFinder.java
@@ -4,20 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.kuali.kfs.krad.keyvalues.KeyValuesBase;
 import org.kuali.rice.core.api.util.ConcreteKeyValue;
 import org.kuali.rice.core.api.util.KeyValue;
 
 public class EinvoiceIndicatorValuesFinder extends KeyValuesBase {
     private static final long serialVersionUID = 6328041347235067740L;
-    private static final Logger LOG = LogManager.getLogger();
 
     @Override
     public List<KeyValue> getKeyValues() {
-        LOG.debug("getKeyValues, entering");
-        List<KeyValue> keyValues = new ArrayList<KeyValue>();
+        List<KeyValue> keyValues = new ArrayList<>();
         keyValues.add(new ConcreteKeyValue(EinvoiceIndicator.NONE.code, EinvoiceIndicator.NONE.description));
         keyValues.add(new ConcreteKeyValue(EinvoiceIndicator.SFTP.code, EinvoiceIndicator.SFTP.description));
         keyValues.add(new ConcreteKeyValue(EinvoiceIndicator.WEB.code, EinvoiceIndicator.WEB.description));
