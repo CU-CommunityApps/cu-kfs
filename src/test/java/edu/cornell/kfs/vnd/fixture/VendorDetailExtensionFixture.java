7d1bf8b6dc67fde1e2f19c7d54c9f9a92c06c0bf
Jay Hulslander <jdh34@cornell.edu>
2021-04-02 14:02:59 -0400

    KFSPTS-21309 update einvoiceindicator to support None, SFTP, or Web (#1102)

--- src/test/java/edu/cornell/kfs/vnd/fixture/VendorDetailExtensionFixture.java
+++ src/test/java/edu/cornell/kfs/vnd/fixture/VendorDetailExtensionFixture.java
@@ -1,22 +1,23 @@
 package edu.cornell.kfs.vnd.fixture;
 
 import edu.cornell.kfs.vnd.businessobject.VendorDetailExtension;
+import edu.cornell.kfs.vnd.businessobject.options.EinvoiceIndicatorValuesFinder;
 
 public enum VendorDetailExtensionFixture {
 	
-	EXTENSION(true, "P");
+	EXTENSION(EinvoiceIndicatorValuesFinder.EinvoiceIndicator.SFTP.code, "P");
 	
-	public final boolean isEinvoice;
+	public final String einvoice;
 	public final String defaultB2BPaymentMethodCode;
 
-	private VendorDetailExtensionFixture(boolean isEinvoice, String defaultB2BPaymentMethodCode) {
-		this.isEinvoice = isEinvoice;
+	private VendorDetailExtensionFixture(String einvoice, String defaultB2BPaymentMethodCode) {
+		this.einvoice = einvoice;
 		this.defaultB2BPaymentMethodCode = defaultB2BPaymentMethodCode;
 	}
 	
 	public VendorDetailExtension createVendorDetailExtension() {
 		VendorDetailExtension vendorDetailExtension = new VendorDetailExtension();
-		vendorDetailExtension.setEinvoiceVendorIndicator(isEinvoice);
+		vendorDetailExtension.setEinvoiceVendorIndicator(einvoice);
 		vendorDetailExtension.setDefaultB2BPaymentMethodCode(defaultB2BPaymentMethodCode);
 		return vendorDetailExtension;
 	}
