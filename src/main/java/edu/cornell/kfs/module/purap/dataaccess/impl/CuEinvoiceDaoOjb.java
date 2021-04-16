48a8c42ea0e145360ce615ffdb3cd1d0dcbe6856
Jay Hulslander <jdh34@cornell.edu>
2021-04-16 12:55:14 -0400

    KFSPTS-20851 adding webservice to KFS to support einvoice pagination

--- src/main/java/edu/cornell/kfs/module/purap/dataaccess/impl/CuEinvoiceDaoOjb.java
+++ src/main/java/edu/cornell/kfs/module/purap/dataaccess/impl/CuEinvoiceDaoOjb.java
@@ -2,30 +2,90 @@ package edu.cornell.kfs.module.purap.dataaccess.impl;
 
 import edu.cornell.kfs.module.purap.CUPurapConstants;
 import edu.cornell.kfs.module.purap.dataaccess.CuEinvoiceDao;
+import edu.cornell.kfs.vnd.businessobject.options.EinvoiceIndicatorValuesFinder;
+
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.ojb.broker.accesslayer.LookupException;
 import org.apache.ojb.broker.query.Criteria;
 import org.apache.ojb.broker.query.Query;
 import org.apache.ojb.broker.query.QueryFactory;
+import org.kuali.kfs.krad.service.BusinessObjectService;
 import org.kuali.kfs.krad.util.ObjectUtils;
 import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.kfs.vnd.businessobject.VendorDetail;
 import org.kuali.rice.core.framework.persistence.ojb.dao.PlatformAwareDaoBaseOjb;
+import org.springframework.dao.DataAccessResourceFailureException;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class CuEinvoiceDaoOjb extends PlatformAwareDaoBaseOjb implements CuEinvoiceDao {
-
     private static final Logger LOG = LogManager.getLogger(CuEinvoiceDaoOjb.class);
-
+    
+    protected BusinessObjectService businessObjectService;
+    
+    @Override
     public List<VendorDetail> getVendors(List<String> vendorNumbers) {
         HashMap<Integer, List<Integer>> vendorDetailToHeaderMap = buildVendorDetailMap(vendorNumbers);
         Criteria criteria = buildQueryCriteria(vendorDetailToHeaderMap);
         Query query = QueryFactory.newQuery(VendorDetail.class, criteria);
         return (List<VendorDetail>) getPersistenceBrokerTemplate().getCollectionByQuery(query);
+    }
+    
+    @Override
+    public List<String> getFilteredVendorNumbers(String searchCritiera) {
+        List<String> vendorNumbers = new ArrayList<>();
+        ResultSet results = queryVendors(searchCritiera);
+        
+        try {
+            while(results.next()) {
+                String vendorNumber = results.getString(1) + KFSConstants.DASH + results.getString(2);
+                if (!vendorNumbers.contains(vendorNumber)) {
+                    vendorNumbers.add(vendorNumber);
+                }
+            }
+        } catch (SQLException e) {
+            LOG.error("getFilteredVendorNumbers, had an error processing the query results.", e);
+            throw new RuntimeException(e);
+        }
+        
+        return vendorNumbers;
+    }
+
+    protected ResultSet queryVendors(String searchCritiera) {
+        ResultSet queryResults;
+        try {
+            Connection connection = getPersistenceBroker(true).serviceConnectionManager().getConnection();
+            PreparedStatement query = connection.prepareCall(buildSQl());
+            String formattedSearchCriteria = KFSConstants.PERCENTAGE_SIGN +  StringUtils.upperCase(searchCritiera).trim() + KFSConstants.PERCENTAGE_SIGN;
+            query.setString(1, formattedSearchCriteria);
+            query.setString(2, formattedSearchCriteria);
+            queryResults = query.executeQuery();
+        } catch (DataAccessResourceFailureException | LookupException | IllegalStateException | SQLException e) {
+            LOG.error("queryVendors, there was an error querying the database: ", e);
+            throw new RuntimeException(e);
+        }
+        return queryResults;
+    }
+    
+    protected String buildSQl() {
+        StringBuilder sb = new StringBuilder("SELECT DETAIL.VNDR_HDR_GNRTD_ID, DETAIL.VNDR_DTL_ASND_ID ");
+        sb.append("FROM KFS.PUR_VNDR_DTL_T DETAIL, KFS.PUR_VNDR_DTL_TX EXTENSION ");
+        sb.append("WHERE DETAIL.VNDR_HDR_GNRTD_ID = EXTENSION.VNDR_HDR_GNRTD_ID ");
+        sb.append("AND DETAIL.VNDR_DTL_ASND_ID = EXTENSION.VNDR_DTL_ASND_ID ");
+        sb.append("AND EXTENSION.EINV_IND = '").append(EinvoiceIndicatorValuesFinder.EinvoiceIndicator.WEB.code).append("' ");
+        sb.append("AND (UPPER(DETAIL.VNDR_NM) LIKE ? ");
+        sb.append("OR UPPER(DETAIL.VNDR_DUNS_NBR) LIKE ?)");
+        String sql = sb.toString();
+        LOG.debug("buildSQl, generated SQL: " + sql);
+        return sql;
     }
 
     private HashMap<Integer, List<Integer>> buildVendorDetailMap(List<String> vendorNumbers) {
