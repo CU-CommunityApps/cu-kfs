1b337d0ddde3b8b7dd6d1aa4814393f1d7ac4c20
Jay Hulslander <jdh34@cornell.edu>
2021-04-21 11:34:51 -0400

    iterative improvements

--- src/main/java/edu/cornell/kfs/module/ar/CuArConstants.java
+++ src/main/java/edu/cornell/kfs/module/ar/CuArConstants.java
@@ -10,39 +10,4 @@ public class CuArConstants {
     public static final int CINV_DATE_RANGE_END_DATE_START_INDEX = 14;
     public static final String QUESTION_NEWLINE_STRING = "[br]";
 
-    /*
-     * CUMod: KFSPTS-14970
-     */
-    public enum AwardInvoicingOptionCodeToName {
-        INV_AWARD(ArIntegrationConstants.AwardInvoicingOptions.INV_AWARD, "Invoice By Award"),
-        INV_ACCOUNT(ArIntegrationConstants.AwardInvoicingOptions.INV_ACCOUNT, "Invoice By Account"),
-        INV_CONTRACT_CONTROL_ACCOUNT(ArIntegrationConstants.AwardInvoicingOptions.INV_CONTRACT_CONTROL_ACCOUNT, "Invoice By Contract Control Account"),
-        INV_SCHEDULE(ArIntegrationConstants.AwardInvoicingOptions.INV_SCHEDULE, "Invoice By Schedule");
-
-        private String code;
-        private String name;
-
-        AwardInvoicingOptionCodeToName(String code, String name) {
-            this.code = code;
-            this.name = name;
-        }
-
-        public String getCode() {
-            return this.code;
-        }
-
-        public String getName() {
-            return this.name;
-        }
-
-        public static String getName(String code) {
-            for (AwardInvoicingOptionCodeToName option : AwardInvoicingOptionCodeToName.values()) {
-                if (option.getCode().equals(code)) {
-                    return option.getName();
-                }
-            }
-            return null;
-        }
-    }
-
 }
