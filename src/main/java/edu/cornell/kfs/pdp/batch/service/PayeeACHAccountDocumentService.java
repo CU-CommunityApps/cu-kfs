69f5779226c98a61d4f82827996474daba0bf05f
Jay Hulslander <jdh34@cornell.edu>
2021-05-21 14:06:20 -0400

    KFSPTS-21519 making adding and editing a PAAT document through batch job transactional

---
+++ src/main/java/edu/cornell/kfs/pdp/batch/service/PayeeACHAccountDocumentService.java
@@ -0,0 +1,14 @@
+package edu.cornell.kfs.pdp.batch.service;
+
+import org.kuali.kfs.pdp.businessobject.PayeeACHAccount;
+import org.kuali.rice.kim.api.identity.Person;
+
+import edu.cornell.kfs.pdp.businessobject.PayeeACHAccountExtractDetail;
+
+public interface PayeeACHAccountDocumentService {
+    
+    String addACHAccount(Person payee, PayeeACHAccountExtractDetail achDetail, String payeeType);
+    String updateACHAccountIfNecessary(Person payee, PayeeACHAccountExtractDetail achDetail, PayeeACHAccount achAccount);
+    String getDirectDepositTransactionType();
+
+}
