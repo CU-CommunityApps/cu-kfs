2413892bc25c99d4e43237b36f910faa8299a99a
cah292 <cah292@users.noreply.github.com>
2021-06-16 09:31:41 -0400

    KFSPTS-21719 Update Location BO conversion rules (#1126)

--- src/test/java/edu/cornell/kfs/krad/service/impl/CuMaintainableXMLConversionServiceImplTest.java
+++ src/test/java/edu/cornell/kfs/krad/service/impl/CuMaintainableXMLConversionServiceImplTest.java
@@ -16,6 +16,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.kuali.kfs.core.api.util.CoreUtilities;
 
 /**
@@ -119,6 +121,23 @@ public class CuMaintainableXMLConversionServiceImplTest {
     @Test
     void testConversionOfKfsParameterFromBeforeKewUpgrade() throws Exception {
         assertXMLFromTestFileConvertsAsExpected("PreKewUpgradeParameterTest.xml");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "LegacyCampusTest.xml",
+        "RiceCampusTest.xml",
+        "LegacyCountryTest.xml",
+        "RiceCountryTest.xml",
+        "LegacyCountyTest.xml",
+        "RiceCountyTest.xml",
+        "LegacyPostalCodeTest.xml",
+        "RicePostalCodeTest.xml",
+        "LegacyStateTest.xml",
+        "RiceStateTest.xml"
+    })
+    void testConversionOfLocationMaintenanceDocuments(String locationTestFile) throws Exception {
+        assertXMLFromTestFileConvertsAsExpected(locationTestFile);
     }
 
     protected void assertXMLFromTestFileConvertsAsExpected(String fileLocalName) throws Exception {
