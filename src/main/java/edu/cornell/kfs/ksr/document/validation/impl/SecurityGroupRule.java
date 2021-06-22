be8ae23294ed90fa2e27c3420d81ec78b2702150
db483 <db483@cornell.edu>
2021-06-22 17:38:18 +0100

    KFSPTS-21713 cleanup BR class

--- src/main/java/edu/cornell/kfs/ksr/document/validation/impl/SecurityGroupRule.java
+++ src/main/java/edu/cornell/kfs/ksr/document/validation/impl/SecurityGroupRule.java
@@ -1,21 +1,21 @@
 package edu.cornell.kfs.ksr.document.validation.impl;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 import org.kuali.kfs.kns.document.MaintenanceDocument;
 import org.kuali.kfs.kns.maintenance.rules.MaintenanceDocumentRuleBase;
 import org.kuali.kfs.krad.bo.PersistableBusinessObject;
+import org.kuali.kfs.krad.service.BusinessObjectService;
+import org.kuali.kfs.krad.util.GlobalVariables;
+import org.kuali.kfs.sys.context.SpringContext;
 
+import edu.cornell.kfs.ksr.KsrConstants;
 import edu.cornell.kfs.ksr.businessobject.SecurityGroup;
+import edu.cornell.kfs.ksr.businessobject.SecurityGroupTab;
 
-/**
- * ====
- * CU Customization:
- * Remediated this class as needed for Rice 2.x compatibility.
- * ====
- * 
- * Used to validate the SecurityGroup and SecurityGroupTab list
- * 
- * @author rSmart Development Team
- */
 public class SecurityGroupRule extends MaintenanceDocumentRuleBase {
 
 	/**
@@ -29,140 +29,141 @@ public class SecurityGroupRule extends MaintenanceDocumentRuleBase {
 		boolean success = true;
 
 		SecurityGroup securityGroup = (SecurityGroup) document.getDocumentDataObject();
-//		SecurityGroupTab tempTab = (SecurityGroupTab) line;
-//		if (securityGroup.getSecurityGroupTabs() != null) {
-//			success = validateSecurityGroupTabs(document);
-//			if (success) {
-//				Iterator<SecurityGroupTab> it = securityGroup.getSecurityGroupTabs().iterator();
-//				while (it.hasNext()) {
-//					SecurityGroupTab tab = it.next();
-//
-//					if (tab.getTabOrder().equals(tempTab.getTabOrder())) {
-//						GlobalVariables.getMessageMap().putError(KsrConstants.SECURITY_GROUP_TAB_ORDER, KsrConstants.ERROR_SECURITY_GROUP_TAB_ORDER_UNIQUE);
-//						success = false;
-//					}
-//					if (tab.getTabName().equals(tempTab.getTabName())) {
-//						GlobalVariables.getMessageMap().putError(KsrConstants.SECURITY_GROUP_TAB_NAME, KsrConstants.ERROR_SECURITY_GROUP_TAB_NAME_UNIQUE);
-//						success = false;
-//					}
-//				}
-//			}
-//		}
+		SecurityGroupTab tempTab = (SecurityGroupTab) line;
+		if (securityGroup.getSecurityGroupTabs() != null) {
+			success = validateSecurityGroupTabs(document);
+			if (success) {
+				Iterator<SecurityGroupTab> it = securityGroup.getSecurityGroupTabs().iterator();
+				while (it.hasNext()) {
+					SecurityGroupTab tab = it.next();
+
+					if (tab.getTabOrder().equals(tempTab.getTabOrder())) {
+						GlobalVariables.getMessageMap().putError(KsrConstants.SECURITY_GROUP_TAB_ORDER, KsrConstants.ERROR_SECURITY_GROUP_TAB_ORDER_UNIQUE);
+						success = false;
+					}
+					if (tab.getTabName().equals(tempTab.getTabName())) {
+						GlobalVariables.getMessageMap().putError(KsrConstants.SECURITY_GROUP_TAB_NAME, KsrConstants.ERROR_SECURITY_GROUP_TAB_NAME_UNIQUE);
+						success = false;
+					}
+				}
+			}
+		}
 
 		return super.processCustomAddCollectionLineBusinessRules(document, collectionName, line)
 				&& success;
 	}
 
-//	/**
-//	 * Determines if there is at least one SecurityGroupTab in the list
-//	 * 
-//	 * @param tabs
-//	 *            - A list of tabs that a SecurityGroup is associated with.
-//	 * @return true if there is at least one SecurityGroupTab in the list
-//	 */
-//	public boolean hasAtLeastOneActiveTab(List<SecurityGroupTab> tabs) {
-//		if (tabs == null) {
-//			return false;
-//		}
-//		else if (tabs.size() > 0) {
-//			Iterator<SecurityGroupTab> it = tabs.iterator();
-//			while (it.hasNext()) {
-//				SecurityGroupTab tab = it.next();
-//				if (tab.isActive()) {
-//					return true;
-//				}
-//			}
-//		}
-//
-//		return false;
-//	}
-//
-//	/**
-//	 * Validates the maint. doc before it is written to the db.
-//	 * 
-//	 * @see org.kuali.rice.kns.maintenance.rules.MaintenanceDocumentRuleBase#dataDictionaryValidate(org.kuali.rice.kns.document.MaintenanceDocument)
-//	 */
-//	@Override
-//	protected boolean dataDictionaryValidate(MaintenanceDocument document) {
-//		boolean success = true;
-//		SecurityGroup securityGroup = (SecurityGroup) document.getDocumentDataObject();
-//		if (!hasAtLeastOneActiveTab(securityGroup.getSecurityGroupTabs())) {
-//			GlobalVariables.getMessageMap().putErrorForSectionId(KsrConstants.SECTION_SECURITY_TABS, KsrConstants.ERROR_SECURITY_GROUP_TAB_MISSING, new String());
-//			success = false;
-//		}
-//
-//		// Check the Security Group Name against the DB
-//		Map<String, Object> hashMap = new HashMap<String, Object>();
-//		hashMap.put(KsrConstants.SECURITY_GROUP_NAME, securityGroup.getSecurityGroupName());
-//
-//		SecurityGroup temp = (SecurityGroup) KRADServiceLocator.getBusinessObjectService().findByPrimaryKey(SecurityGroup.class, hashMap);
-//
-//		if (temp != null) {
-//			if (securityGroup.getSecurityGroupId() != null) {
-//				if (temp.getSecurityGroupName().equals(securityGroup.getSecurityGroupName())
-//						&& (!temp.getSecurityGroupId().equals(securityGroup.getSecurityGroupId()))) {
-//					GlobalVariables.getMessageMap().putError(KsrConstants.KSR_DOCUMENT_MAINTANABLE
-//							+ "." + KsrConstants.SECURITY_GROUP_NAME, KsrConstants.ERROR_SECURITY_GROUP_NAME_UNIQUE);
-//					success = false;
-//				}
-//			}
-//			else {
-//				GlobalVariables.getMessageMap().putError(KsrConstants.KSR_DOCUMENT_MAINTANABLE
-//						+ "." + KsrConstants.SECURITY_GROUP_NAME, KsrConstants.ERROR_SECURITY_GROUP_NAME_UNIQUE);
-//				success = false;
-//			}
-//
-//		}
-//
-//		success = validateSecurityGroupTabs(document);
-//
-//		return (super.dataDictionaryValidate(document) && success);
-//	}
-//
-//	/**
-//	 * Determines uniqueness of each SecurityGroupTab in the maint. doc.
-//	 * 
-//	 * @param document
-//	 *            - the current maint. doc
-//	 * @return true if there are no duplicate tab orders or names
-//	 */
-//	public boolean validateSecurityGroupTabs(MaintenanceDocument document) {
-//		boolean successTabOrder = true;
-//		boolean successTabName = true;
-//
-//		List<SecurityGroupTab> securityGroupTabs = ((SecurityGroup) document.getDocumentDataObject()).getSecurityGroupTabs();
-//		if (securityGroupTabs != null) {
-//			for (int i = 0; i < securityGroupTabs.size(); i++) {
-//				SecurityGroupTab tab = securityGroupTabs.get(i);
-//
-//				if ((tab.getTabOrder() == null) && (tab.getTabName() == null)) {
-//					securityGroupTabs.remove(i);
-//				}
-//				else if (tab.getTabOrder() == null) { // Reset to old value
-//					tab.setTabOrder(((SecurityGroup) document.getOldMaintainableObject().getDataObject()).getSecurityGroupTabs().get(i).getTabOrder());
-//				}
-//				if (tab.getTabName() == null) {
-//					tab.setTabName(((SecurityGroup) document.getOldMaintainableObject().getDataObject()).getSecurityGroupTabs().get(i).getTabName());
-//				}
-//
-//				for (int j = i + 1; j < securityGroupTabs.size(); j++) {
-//					SecurityGroupTab tempTab = securityGroupTabs.get(j);
-//					if (successTabOrder
-//							&& tab.getTabOrder().equals(tempTab.getTabOrder())) {
-//						GlobalVariables.getMessageMap().putErrorForSectionId(KsrConstants.SECTION_SECURITY_TABS, KsrConstants.ERROR_SECURITY_GROUP_TAB_ORDER_UNIQUE, new String());
-//						successTabOrder = false;
-//					}
-//					if (successTabName
-//							&& tab.getTabName().equals(tempTab.getTabName())) {
-//						GlobalVariables.getMessageMap().putErrorForSectionId(KsrConstants.SECTION_SECURITY_TABS, KsrConstants.ERROR_SECURITY_GROUP_TAB_NAME_UNIQUE, new String());
-//						successTabName = false;
-//					}
-//
-//				}
-//			}
-//		}
-//
-//		return successTabOrder && successTabName;
-//	}
+	/**
+	 * Determines if there is at least one SecurityGroupTab in the list
+	 * 
+	 * @param tabs
+	 *            - A list of tabs that a SecurityGroup is associated with.
+	 * @return true if there is at least one SecurityGroupTab in the list
+	 */
+	public boolean hasAtLeastOneActiveTab(List<SecurityGroupTab> tabs) {
+		if (tabs == null) {
+			return false;
+		}
+		else if (tabs.size() > 0) {
+			Iterator<SecurityGroupTab> it = tabs.iterator();
+			while (it.hasNext()) {
+				SecurityGroupTab tab = it.next();
+				if (tab.isActive()) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Validates the maint. doc before it is written to the db.
+	 * 
+	 * @see org.kuali.rice.kns.maintenance.rules.MaintenanceDocumentRuleBase#dataDictionaryValidate(org.kuali.rice.kns.document.MaintenanceDocument)
+	 */
+	@Override
+	protected boolean dataDictionaryValidate(MaintenanceDocument document) {
+		boolean success = true;
+		SecurityGroup securityGroup = (SecurityGroup) document.getDocumentDataObject();
+		if (!hasAtLeastOneActiveTab(securityGroup.getSecurityGroupTabs())) {
+			GlobalVariables.getMessageMap().putErrorForSectionId(KsrConstants.SECTION_SECURITY_TABS, KsrConstants.ERROR_SECURITY_GROUP_TAB_MISSING, new String());
+			success = false;
+		}
+
+		// Check the Security Group Name against the DB
+		Map<String, Object> hashMap = new HashMap<String, Object>();
+		hashMap.put(KsrConstants.SECURITY_GROUP_NAME, securityGroup.getSecurityGroupName());
+
+		BusinessObjectService businessObjectService = SpringContext.getBean(BusinessObjectService.class);
+		SecurityGroup temp = (SecurityGroup) businessObjectService.findByPrimaryKey(SecurityGroup.class, hashMap);
+
+		if (temp != null) {
+			if (securityGroup.getSecurityGroupId() != null) {
+				if (temp.getSecurityGroupName().equals(securityGroup.getSecurityGroupName())
+						&& (!temp.getSecurityGroupId().equals(securityGroup.getSecurityGroupId()))) {
+					GlobalVariables.getMessageMap().putError(KsrConstants.KSR_DOCUMENT_MAINTANABLE
+							+ "." + KsrConstants.SECURITY_GROUP_NAME, KsrConstants.ERROR_SECURITY_GROUP_NAME_UNIQUE);
+					success = false;
+				}
+			}
+			else {
+				GlobalVariables.getMessageMap().putError(KsrConstants.KSR_DOCUMENT_MAINTANABLE
+						+ "." + KsrConstants.SECURITY_GROUP_NAME, KsrConstants.ERROR_SECURITY_GROUP_NAME_UNIQUE);
+				success = false;
+			}
+
+		}
+
+		success = validateSecurityGroupTabs(document);
+
+		return (super.dataDictionaryValidate(document) && success);
+	}
+
+	/**
+	 * Determines uniqueness of each SecurityGroupTab in the maint. doc.
+	 * 
+	 * @param document
+	 *            - the current maint. doc
+	 * @return true if there are no duplicate tab orders or names
+	 */
+	public boolean validateSecurityGroupTabs(MaintenanceDocument document) {
+		boolean successTabOrder = true;
+		boolean successTabName = true;
+
+		List<SecurityGroupTab> securityGroupTabs = ((SecurityGroup) document.getDocumentDataObject()).getSecurityGroupTabs();
+		if (securityGroupTabs != null) {
+			for (int i = 0; i < securityGroupTabs.size(); i++) {
+				SecurityGroupTab tab = securityGroupTabs.get(i);
+
+				if ((tab.getTabOrder() == null) && (tab.getTabName() == null)) {
+					securityGroupTabs.remove(i);
+				}
+				else if (tab.getTabOrder() == null) { // Reset to old value
+					tab.setTabOrder(((SecurityGroup) document.getOldMaintainableObject().getDataObject()).getSecurityGroupTabs().get(i).getTabOrder());
+				}
+				if (tab.getTabName() == null) {
+					tab.setTabName(((SecurityGroup) document.getOldMaintainableObject().getDataObject()).getSecurityGroupTabs().get(i).getTabName());
+				}
+
+				for (int j = i + 1; j < securityGroupTabs.size(); j++) {
+					SecurityGroupTab tempTab = securityGroupTabs.get(j);
+					if (successTabOrder
+							&& tab.getTabOrder().equals(tempTab.getTabOrder())) {
+						GlobalVariables.getMessageMap().putErrorForSectionId(KsrConstants.SECTION_SECURITY_TABS, KsrConstants.ERROR_SECURITY_GROUP_TAB_ORDER_UNIQUE, new String());
+						successTabOrder = false;
+					}
+					if (successTabName
+							&& tab.getTabName().equals(tempTab.getTabName())) {
+						GlobalVariables.getMessageMap().putErrorForSectionId(KsrConstants.SECTION_SECURITY_TABS, KsrConstants.ERROR_SECURITY_GROUP_TAB_NAME_UNIQUE, new String());
+						successTabName = false;
+					}
+
+				}
+			}
+		}
+
+		return successTabOrder && successTabName;
+	}
 
 }
