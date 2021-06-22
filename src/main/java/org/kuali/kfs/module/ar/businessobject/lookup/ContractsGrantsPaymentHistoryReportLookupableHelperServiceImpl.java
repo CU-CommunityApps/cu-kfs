e42e426d288a8ec462f5a81370077498c78c1ba6
Jay Hulslander <jdh34@cornell.edu>
2021-06-22 09:15:14 -0400

    Revert "removing logging"
    This reverts commit c67fa709bf06e09c221b9e877dedfb084ad4533c.

--- src/main/java/org/kuali/kfs/module/ar/businessobject/lookup/ContractsGrantsPaymentHistoryReportLookupableHelperServiceImpl.java
+++ src/main/java/org/kuali/kfs/module/ar/businessobject/lookup/ContractsGrantsPaymentHistoryReportLookupableHelperServiceImpl.java
@@ -19,6 +19,8 @@
 package org.kuali.kfs.module.ar.businessobject.lookup;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.joda.time.DateTime;
 import org.kuali.kfs.kns.web.struts.form.LookupForm;
 import org.kuali.kfs.kns.web.ui.ResultRow;
@@ -51,6 +53,8 @@ import java.util.Map;
 public class ContractsGrantsPaymentHistoryReportLookupableHelperServiceImpl extends
         ContractsGrantsReportLookupableHelperServiceImplBase {
     
+    private static final Logger LOG = LogManager.getLogger();
+
     protected DateTimeService dateTimeService;
     protected DocumentService documentService;
     protected FinancialSystemDocumentService financialSystemDocumentService;
@@ -81,6 +85,7 @@ public class ContractsGrantsPaymentHistoryReportLookupableHelperServiceImpl exte
     @Override
     public Collection<ContractsGrantsPaymentHistoryReport> performLookup(LookupForm lookupForm,
             Collection<ResultRow> resultTable, boolean bounded) {
+        LOG.info("performLookup, back-port FINP 7396");
         Map<String, String> lookupFormFields = lookupForm.getFieldsForLookup();
 
         setBackLocation(lookupForm.getFieldsForLookup().get(KRADConstants.BACK_LOCATION));
