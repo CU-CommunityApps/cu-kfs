dcc9472afed08e8e2d3ded62fae71abf1155c5c9
db483 <db483@cornell.edu>
2021-06-29 10:39:38 +0100

    KFSPTS-2713 add code review changes

--- src/main/java/edu/cornell/kfs/ksr/document/validation/impl/SecurityGroupRule.java
+++ src/main/java/edu/cornell/kfs/ksr/document/validation/impl/SecurityGroupRule.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.kuali.kfs.kns.document.MaintenanceDocument;
 import org.kuali.kfs.kns.maintenance.rules.MaintenanceDocumentRuleBase;
 import org.kuali.kfs.krad.bo.PersistableBusinessObject;
