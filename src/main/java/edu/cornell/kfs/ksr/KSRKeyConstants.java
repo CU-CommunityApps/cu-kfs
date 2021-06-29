ffd37ccf845ca01fd52b7d5fd96a307996bb54ad
db483 <db483@cornell.edu>
2021-06-29 18:36:16 +0100

    KFSPTS-21713 Add Security Group (#1136)

---
+++ src/main/java/edu/cornell/kfs/ksr/KSRKeyConstants.java
@@ -0,0 +1,11 @@
+package edu.cornell.kfs.ksr;
+
+public class KSRKeyConstants {
+    
+    // Security Group errors
+    public static final String ERROR_SECURITY_GROUP_NAME_UNIQUE = "error.ksr.securitygroup.name.notunique";
+    public static final String ERROR_SECURITY_GROUP_TAB_ORDER_UNIQUE = "error.ksr.securitytab.tab.order.notunique";
+    public static final String ERROR_SECURITY_GROUP_TAB_NAME_UNIQUE = "error.ksr.securitytab.tab.name.notunique";
+    public static final String ERROR_SECURITY_GROUP_TAB_MISSING = "error.ksr.security.tab.missing";
+
+}
