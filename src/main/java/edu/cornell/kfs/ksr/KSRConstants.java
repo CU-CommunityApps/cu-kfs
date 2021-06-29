73799f3442f6413dde46d18608c94d8c914f053e
db483 <db483@cornell.edu>
2021-06-29 18:36:16 +0100

    KFSPTS-21713 Add Security Group (#1136)

---
+++ src/main/java/edu/cornell/kfs/ksr/KSRConstants.java
@@ -0,0 +1,24 @@
+package edu.cornell.kfs.ksr;
+
+public class KSRConstants {
+	public static final String KSR_DOCUMENT_MAINTAINABLE = "document.newMaintainableObject";
+	
+	public static final String KSR_NAMESPACE = "KFS-SR";
+
+	// Security Group sections
+	public static final String SECTION_SECURITY_TABS = "SecurityGroupTabs";
+
+	// Security Group properties
+	public static final String SECURITY_GROUP_NAME = "securityGroupName";
+	
+	// Security Group Tab properties
+	public static final String SECURITY_GROUP_TAB_NAME = "tabName";
+	public static final String SECURITY_GROUP_TAB_ID = "tabId";
+	public static final String SECURITY_GROUP_TAB_ORDER = "tabOrder";
+
+	// Sequence names
+	public static final String SECURITY_GROUP_SEQ_NAME = "KRSR_SEC_GRP_ID_SEQ";
+	public static final String SECURITY_GROUP_TAB_SEQ_NAME = "KRSR_SEC_GRP_TB_ID_SEQ";
+    
+    public static final String SECURITY_PROVISIONING_URL_NAME = "Edit Provisioning";
+}
