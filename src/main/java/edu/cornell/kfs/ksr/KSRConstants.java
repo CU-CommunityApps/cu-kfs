12e4e6f4fe73c4088e1598b7eae469b616a07c5d
db483 <db483@cornell.edu>
2021-07-02 13:42:44 +0100

    KFSPTS-21714 add more changes

--- src/main/java/edu/cornell/kfs/ksr/KSRConstants.java
+++ src/main/java/edu/cornell/kfs/ksr/KSRConstants.java
@@ -15,10 +15,35 @@ public class KSRConstants {
 	public static final String SECURITY_GROUP_TAB_NAME = "tabName";
 	public static final String SECURITY_GROUP_TAB_ID = "tabId";
 	public static final String SECURITY_GROUP_TAB_ORDER = "tabOrder";
+	
+	// Security Group sections
+    public static final String SECURITY_PROVISIONING_GROUP = "securityProvisioningGroup";
+    public static final String SECURITY_PROVISIONING_GROUPS = "securityProvisioningGroups";
+    public static final String DEPENDENT_ROLES = "dependentRoles";
+    
+    // Security Provisioning Group parameters
+    public static final String SECURITY_PROVISIONING_GROUP_ADD_AUTH_ROLE_ID = "additionalAuthorizerRoleId";
+    public static final String SECURITY_PROVISIONING_GROUP_CENT_AUTH_ROLE_ID = "centralAuthorizerRoleId";
+    public static final String SECURITY_PROVISIONING_GROUP_DIST_AUTH_ROLE_ID = "distributedAuthorizerRoleId";
+    public static final String SECURITY_PROVISIONING_GROUP_ADD_AUTH_ROLE_NAME = "additionalAuthorizerRoleName";
+    public static final String SECURITY_PROVISIONING_GROUP_CENT_AUTH_ROLE_NAME = "centralAuthorizerRoleName";
+    public static final String SECURITY_PROVISIONING_GROUP_DIST_AUTH_ROLE_NAME = "distributedAuthorizerRoleName";
+    public static final String SECURITY_PROVISIONING_GROUP_ROLE_NAME = "roleName";
+    public static final String SECURITY_PROVISIONING_GROUP_SECURITY_PROVISIONING_GROUP_NAME = "securityGroup.securityGroupName";
+    public static final String PROVISIONING_ROLE_ID = "roleId";
+    public static final String PROVISIONING_ROLE_TAB_ORDER = "roleTabOrder";
 
 	// Sequence names
 	public static final String SECURITY_GROUP_SEQ_NAME = "KRSR_SEC_GRP_ID_SEQ";
 	public static final String SECURITY_GROUP_TAB_SEQ_NAME = "KRSR_SEC_GRP_TB_ID_SEQ";
+	public static final String SECURITY_PROVISIONING_GROUP_SEQ_NAME = "KRSR_SEC_PRV_ID_SEQ";
     
     public static final String SECURITY_PROVISIONING_URL_NAME = "Edit Provisioning";
+    
+    // Security Provisioning Group labels
+    public static final String SECURITY_PROVISIONING_GROUP_ADD_AUTH_LBL = "Additional Authorizer";
+    public static final String SECURITY_PROVISIONING_GROUP_CENT_AUTH_LBL = "Central Authorizer";
+    public static final String SECURITY_PROVISIONING_GROUP_DIST_AUTH_LBL = "Distributed Authorizer";
+    public static final String SECURITY_PROVISIONING_GROUP_DEP_ROLE_LBL = "Dependent Role";
+    public static final String SECURITY_PROVISIONING_GROUP_ROLE_LBL = "Role (to be granted)";
 }
--- src/main/java/edu/cornell/kfs/ksr/KSRKeyConstants.java
+++ src/main/java/edu/cornell/kfs/ksr/KSRKeyConstants.java
@@ -8,4 +8,14 @@ public class KSRKeyConstants {
     public static final String ERROR_SECURITY_GROUP_TAB_NAME_UNIQUE = "error.ksr.securitytab.tab.name.notunique";
     public static final String ERROR_SECURITY_GROUP_TAB_MISSING = "error.ksr.security.tab.missing";
 
+    // Security Provisioning Group errors
+    public static final String ERROR_SECURITY_PROVISIONING_GROUP_AUTH_UNIQUE = "error.ksr.securityprovisioninggroup.authorizer.notunique";
+    public static final String ERROR_SECURITY_PROVISIONING_GROUP_ROLE_INVALID = "error.ksr.securityprovisioninggroup.role.invalid";
+    public static final String ERROR_SECURITY_PROVISIONING_GROUP_ROLE_UNIQUE = "error.ksr.securityprovisioninggroup.role.notunique";
+    public static final String ERROR_SECURITY_PROVISIONING_GROUP_ROLE_DERIVED = "error.ksr.securityprovisioninggroup.role.derived";
+    public static final String ERROR_SECURITY_PROVISIONING_GROUP_TAB_ORDER_UNIQUE = "error.ksr.securityprovisioninggroup.tabOrder.notunique";
+    public static final String ERROR_SECURITY_PROVISIONING_GROUP_DEPENDENT_ROLE_UNIQUE = "error.ksr.securityprovisioninggroup.dependentRole.notunique";
+    public static final String ERROR_SECURITY_PROVISIONING_GROUP_DEPENDENT_ROLE_BLANK = "error.ksr.securityprovisioninggroup.dependentRole.blank";
+    public static final String ERROR_SECURITY_PROVISIONING_GROUP_CIRCULAR_REFERENCE = "error.ksr.securityprovisioninggroup.role.circular.reference";
+    public static final String ERROR_SECURITY_PROVISIONING_GROUP_DEPENDENT_ROLE_MATCH = "error.ksr.securityprovisioninggroup.dependentRole.match";
 }
