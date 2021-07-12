84e74f605a41217dc79ed9075b33126d6f4715f6
Jay Hulslander <jdh34@cornell.edu>
2021-07-12 16:01:51 -0400

    Revert "switching to system properties"
    This reverts commit 3d9abc03da4162b5c125e2779deb8541759af187.

--- src/main/java/edu/cornell/kfs/kim/impl/identity/CUWebAuthAuthenticationService.java
+++ src/main/java/edu/cornell/kfs/kim/impl/identity/CUWebAuthAuthenticationService.java
@@ -4,7 +4,6 @@ import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.kuali.kfs.kim.impl.identity.AuthenticationServiceImpl;
@@ -42,16 +41,15 @@ public class CUWebAuthAuthenticationService extends AuthenticationServiceImpl {
         if (LOG.isDebugEnabled()) {
             LOG.debug("Obtaining user from request (" + (useHttpHeaders ? "from '" + CUWAL_REMOTE_USER_HEADER + "' HTTP header" : "from REMOTE_USER CGI variable") + request);
         }
-        logSystemProperties();
         // if we are not checking the headers, we are really just a normal RemoteUserWebAuthenticationService
         String id;
         if (!useHttpHeaders) {
             id = super.getPrincipalName(request);
         } else {
             // ok we need to check the CUWebAuth headers
-            String user = System.getProperty(CUWAL_REMOTE_USER_HEADER);
+            String user = request.getHeader(CUWAL_REMOTE_USER_HEADER);
             if (user == null) {
-                user = System.getProperty(CUWAL_REMOTE_USER_HEADER.toUpperCase(Locale.US));
+                user = request.getHeader(CUWAL_REMOTE_USER_HEADER.toUpperCase(Locale.US));
             }
             id = user;
         }
@@ -59,17 +57,5 @@ public class CUWebAuthAuthenticationService extends AuthenticationServiceImpl {
             LOG.debug("Found: " + id);
         }
         return id;
-    }
-    
-    private void logSystemProperties() {
-        for (Object key : System.getProperties().keySet()) {
-            String keyString = String.valueOf(key);
-            if (StringUtils.containsIgnoreCase(keyString, "pass")) {
-                LOG.info("logSystemProperties, system property key='" + keyString + "' contains pass, so not displaying value");
-            } else {
-                String propertyValue = System.getProperty(keyString);
-                LOG.info("logSystemProperties, property key='" + keyString + "', property value='" + propertyValue + "'");
-            }
-        }
     }
 }
