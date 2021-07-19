5ce7185ec51cbfe4c311e6a3b752a6060ce39e3a
cah292 <cah292@users.noreply.github.com>
2021-07-19 14:11:03 -0400

    KFSPTS-21559 Add KFS version of ClamAV scanning (#1147)

---
+++ src/main/java/edu/cornell/kfs/krad/antivirus/util/ClamAVUtils.java
@@ -0,0 +1,37 @@
+package edu.cornell.kfs.krad.antivirus.util;
+
+import org.apache.commons.lang3.StringUtils;
+import org.kuali.kfs.sys.KFSConstants;
+
+import edu.cornell.kfs.krad.CUKRADConstants.ClamAVResponses;
+import edu.cornell.kfs.krad.antivirus.service.ScanResult.Status;
+
+public final class ClamAVUtils {
+
+    public static Status determineScanStatusFromScanResultMessage(String result) {
+        switch (StringUtils.defaultString(result)) {
+            case ClamAVResponses.RESPONSE_OK :
+                return Status.PASSED;
+            
+            case ClamAVResponses.RESPONSE_SIZE_EXCEEDED :
+                return Status.ERROR;
+            
+            case ClamAVResponses.RESPONSE_ERROR_WRITING_FILE :
+                return Status.ERROR;
+            
+            default :
+                return StringUtils.endsWith(result, ClamAVResponses.FOUND_SUFFIX) ? Status.FAILED : Status.ERROR;
+        }
+    }
+
+    public static String extractSignatureFromScanResultMessage(String result) {
+        if (!StringUtils.startsWith(result, ClamAVResponses.STREAM_PREFIX)
+                || !StringUtils.endsWith(result, ClamAVResponses.FOUND_SUFFIX)) {
+            return KFSConstants.EMPTY_STRING;
+        }
+        String signature = StringUtils.substringAfter(result, ClamAVResponses.STREAM_PREFIX);
+        signature = StringUtils.substringBeforeLast(signature, ClamAVResponses.FOUND_SUFFIX);
+        return StringUtils.trimToEmpty(signature);
+    }
+
+}
