7754fb16b9086a7143dd642acf8ea2e1595ef441
cah292 <cah292@users.noreply.github.com>
2021-07-19 14:11:03 -0400

    KFSPTS-21559 Add KFS version of ClamAV scanning (#1147)

--- src/main/java/edu/cornell/kfs/krad/service/impl/CuAttachmentServiceImpl.java
+++ src/main/java/edu/cornell/kfs/krad/service/impl/CuAttachmentServiceImpl.java
@@ -1,8 +1,8 @@
 package edu.cornell.kfs.krad.service.impl;
 
-import edu.cornell.kfs.antivirus.service.AntiVirusService;
-import edu.cornell.kfs.antivirus.service.ScanResult;
 import edu.cornell.kfs.krad.dao.CuAttachmentDao;
+import edu.cornell.kfs.krad.antivirus.service.ScanResult;
+import edu.cornell.kfs.krad.antivirus.service.AntiVirusService;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
