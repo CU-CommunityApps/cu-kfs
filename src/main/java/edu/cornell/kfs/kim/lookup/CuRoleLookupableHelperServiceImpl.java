ddb79a90fc459823b6626fcde1650b1279f0e0a0
db483 <db483@cornell.edu>
2021-07-26 14:35:13 +0100

    KFSPTS-22142 Add local customization to fix Role lookup from a child/collection object on a maintenance doc (#1152)

---
+++ src/main/java/edu/cornell/kfs/kim/lookup/CuRoleLookupableHelperServiceImpl.java
@@ -0,0 +1,20 @@
+package edu.cornell.kfs.kim.lookup;
+
+import java.util.List;
+import java.util.Map;
+
+import org.kuali.kfs.kim.lookup.RoleLookupableHelperServiceImpl;
+import org.kuali.kfs.krad.bo.BusinessObject;
+import org.kuali.kfs.krad.util.KRADConstants;
+
+/**
+ * KFSPTS-22142 Add local customization to fix Role lookup from a child/collection object on a maintenance doc
+ */
+public class CuRoleLookupableHelperServiceImpl extends RoleLookupableHelperServiceImpl {
+
+    @Override
+    public List<? extends BusinessObject> getSearchResults(Map<String, String> fieldValues) {
+        fieldValues.remove(KRADConstants.REFERENCES_TO_REFRESH);
+        return super.getSearchResults(fieldValues);
+    }
+}
