e35e8775d7db3f774fe6e53fa1b3963b29eae651
db483 <db483@cornell.edu>
2021-08-05 14:44:23 +0100

    KFSPTS-21714 move Security Provisioning to KFS (#1154)

---
+++ src/main/java/edu/cornell/kfs/ksr/businessobject/SecurityProvisioningGroupDependentRoles.java
@@ -0,0 +1,45 @@
+package edu.cornell.kfs.ksr.businessobject;
+
+import org.kuali.kfs.kim.impl.role.Role;
+import org.kuali.kfs.krad.bo.PersistableBusinessObjectBase;
+
+public class SecurityProvisioningGroupDependentRoles extends PersistableBusinessObjectBase implements KsrObjectWithRoles {
+
+    private static final long serialVersionUID = 8290338935892653260L;
+
+    private Long provisioningId;
+    private String roleId;
+
+    private Role role;
+
+    public Long getProvisioningId() {
+        return provisioningId;
+    }
+
+    public void setProvisioningId(Long provisioningId) {
+        this.provisioningId = provisioningId;
+    }
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId;
+    }
+
+    public Role getRole() {
+        initializeRoleIfNecessary(roleId, role, this::setRole);
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getRoleName() {
+        Role roleImpl = getRole();
+        return getFormattedRoleName(roleImpl);
+    }
+
+}
