b237e5b96c8e49a994f65032f3db6489c4f1ce28
db483 <db483@cornell.edu>
2021-08-13 14:02:09 +0100

    KFSPTS-21715 add Security Request wizard (#1160)

---
+++ src/main/java/edu/cornell/kfs/ksr/service/impl/SecurityRequestDocumentServiceImpl.java
@@ -0,0 +1,37 @@
+package edu.cornell.kfs.ksr.service.impl;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.kuali.kfs.krad.service.BusinessObjectService;
+
+import edu.cornell.kfs.ksr.KSRPropertyConstants;
+import edu.cornell.kfs.ksr.businessobject.SecurityGroup;
+import edu.cornell.kfs.ksr.service.SecurityRequestDocumentService;
+
+public class SecurityRequestDocumentServiceImpl implements SecurityRequestDocumentService {
+    private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(SecurityRequestDocumentServiceImpl.class);
+
+    private BusinessObjectService businessObjectService;
+
+    @Override
+    public List<SecurityGroup> getActiveSecurityGroups() {
+        Map<String, Object> hashMap = new HashMap<String, Object>();
+        hashMap.put(KSRPropertyConstants.SECURITY_GROUP_ACTIVE_INDICATOR, true);
+
+        List<SecurityGroup> groupList = (List<SecurityGroup>) businessObjectService.findMatchingOrderBy(SecurityGroup.class, hashMap,
+                KSRPropertyConstants.SECURITY_GROUP_NAME, true);
+
+        return groupList;
+    }
+
+    public BusinessObjectService getBusinessObjectService() {
+        return businessObjectService;
+    }
+
+    public void setBusinessObjectService(BusinessObjectService businessObjectService) {
+        this.businessObjectService = businessObjectService;
+    }
+
+}
