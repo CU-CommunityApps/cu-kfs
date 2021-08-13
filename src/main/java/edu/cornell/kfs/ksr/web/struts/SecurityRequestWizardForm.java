a8a0bbb81b0b83e89763876678bc95de705e6b06
db483 <db483@cornell.edu>
2021-08-13 14:02:09 +0100

    KFSPTS-21715 add Security Request wizard (#1160)

---
+++ src/main/java/edu/cornell/kfs/ksr/web/struts/SecurityRequestWizardForm.java
@@ -0,0 +1,54 @@
+package edu.cornell.kfs.ksr.web.struts;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
+import org.kuali.kfs.sys.document.web.struts.FinancialSystemTransactionalDocumentFormBase;
+
+import edu.cornell.kfs.ksr.KSRConstants;
+import edu.cornell.kfs.ksr.businessobject.SecurityGroup;
+
+public class SecurityRequestWizardForm extends FinancialSystemTransactionalDocumentFormBase {
+    private static final long serialVersionUID = 8896771325196119964L;
+
+    private SecurityGroup securityGroup;
+    private List<SecurityGroup> securityGroups;
+
+    public SecurityRequestWizardForm() {
+        super();
+    }
+
+    /**
+     * Overriding to allow the process wizard call (since the wizard form is not stored in session)
+     * 
+     * @see org.kuali.kfs.kns.web.struts.form.KualiForm#shouldMethodToCallParameterBeUsed(java.lang.String, java.lang.String,
+     *      javax.servlet.http.HttpServletRequest)
+     */
+    @Override
+    public boolean shouldMethodToCallParameterBeUsed(String methodToCallParameterName, String methodToCallParameterValue, HttpServletRequest request) {
+        if (StringUtils.equals(methodToCallParameterValue, KSRConstants.PROCESS_WIZARD_METHOD_NAME)) {
+            return true;
+        }
+
+        return super.shouldMethodToCallParameterBeUsed(methodToCallParameterName, methodToCallParameterValue, request);
+    }
+
+    public SecurityGroup getSecurityGroup() {
+        return securityGroup;
+    }
+
+    public void setSecurityGroup(SecurityGroup securityGroup) {
+        this.securityGroup = securityGroup;
+    }
+
+    public void setSecurityGroups(List<SecurityGroup> securityGroups) {
+        this.securityGroups = securityGroups;
+    }
+
+    public List<SecurityGroup> getSecurityGroups() {
+        return securityGroups;
+    }
+
+}
