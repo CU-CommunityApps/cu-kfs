084a8960a548bdef9502f752215f42eb5bc59ee0
Jay Hulslander <jdh34@cornell.edu>
2021-08-26 09:34:16 -0400

    KFSPTS-22016 add workday valid account-sub account-sub object code export job (#1162)

---
+++ src/main/java/edu/cornell/kfs/coa/batch/service/impl/CreateWorkdayOpenAccountsCsvServiceImpl.java
@@ -0,0 +1,136 @@
+package edu.cornell.kfs.coa.batch.service.impl;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.kuali.rice.core.api.datetime.DateTimeService;
+
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
+
+import edu.cornell.kfs.coa.batch.CuCoaBatchConstants;
+import edu.cornell.kfs.coa.batch.CuCoaBatchConstants.WorkdayOpenAccountDetailDTOCsvColumn;
+import edu.cornell.kfs.coa.batch.businessobject.WorkdayOpenAccountDetailDTO;
+import edu.cornell.kfs.coa.batch.dataaccess.WorkdayOpenAccountDao;
+import edu.cornell.kfs.coa.batch.service.CreateWorkdayOpenAccountsCsvService;
+import edu.cornell.kfs.sys.CUKFSConstants;
+import edu.cornell.kfs.sys.util.EnumConfiguredMappingStrategy;
+
+public class CreateWorkdayOpenAccountsCsvServiceImpl implements CreateWorkdayOpenAccountsCsvService {
+    private static final Logger LOG = LogManager.getLogger();
+    
+    protected String csvOpenAccountsExportDirectory;
+    protected String csvOpenAccountsFileCreationDirectory;
+    protected DateTimeService dateTimeService;
+    protected WorkdayOpenAccountDao workdayOpenAccountDao;
+    
+    @Override
+    public void createWorkdayOpenAccountsCsv() throws IOException {
+        List<WorkdayOpenAccountDetailDTO> details = workdayOpenAccountDao.getWorkdayOpenAccountDetails();
+        if (CollectionUtils.isNotEmpty(details)) {
+            LOG.info("createWorkdayOpenAccountsCsv, number of open account detail lines to write: " + details.size());
+            writeOpenAccountsToCsvFile(details);
+        } else {
+            LOG.error("createWorkdayOpenAccountsCsv, no open accounts were found, this shouldn't happen");
+            throw new RuntimeException("No open accounts were found.");
+        }
+        
+    }
+    
+    private void writeOpenAccountsToCsvFile(List<WorkdayOpenAccountDetailDTO> details) {
+        String csvFileName = generateCsvOutputFileName();
+        String fullyQualifiedCreationDirectoryFileName = fullyQualifyFileNameToCreationDirectory(csvFileName);
+        LOG.info("writeOpenAccountsToCsvFile: fullyQualifiedOutputFile = " + fullyQualifiedCreationDirectoryFileName);
+        
+        try (FileOutputStream fileOutputStream = new FileOutputStream(fullyQualifiedCreationDirectoryFileName);
+                OutputStreamWriter writer = new OutputStreamWriter(fileOutputStream, StandardCharsets.UTF_8);
+                BufferedWriter bufferedWriter = new BufferedWriter(writer);) {
+            StatefulBeanToCsv<WorkdayOpenAccountDetailDTO> csvWriter = buildCsvWriter(bufferedWriter);
+            csvWriter.write(details);
+        } catch (CsvDataTypeMismatchException | CsvRequiredFieldEmptyException | IOException e) {
+            LOG.error("writeOpenAccountsToCsvFile, Error writing to file " + fullyQualifiedCreationDirectoryFileName, e);
+            throw new RuntimeException(e);
+        }
+
+        try {
+            String fullyQualifiedExportDirectoryFileName = fullyQualifyFileNameToExportDirectory(csvFileName);
+            LOG.info("writeOpenAccountsToCsvFile, Moving data file from creation directory to fullyQualifiedExportDirectoryFileName = "
+                            + fullyQualifiedExportDirectoryFileName);
+            File outputFile = new File(fullyQualifiedCreationDirectoryFileName);
+            moveCreatedFileToExportDirectory(outputFile, fullyQualifiedExportDirectoryFileName);
+            LOG.info("writeOpenAccountsToCsvFile, file was successfully moved to export directory.");
+        } catch (IOException ie) {
+            LOG.error("writeOpenAccountsToCsvFile, had a problem moving the output file.", ie);
+            throw new RuntimeException(ie);
+        }
+
+    }
+    
+    private StatefulBeanToCsv<WorkdayOpenAccountDetailDTO> buildCsvWriter(BufferedWriter bufferedWriter) {
+        EnumConfiguredMappingStrategy<WorkdayOpenAccountDetailDTO, WorkdayOpenAccountDetailDTOCsvColumn> mappingStrategy = new EnumConfiguredMappingStrategy<>(
+                WorkdayOpenAccountDetailDTOCsvColumn.class, WorkdayOpenAccountDetailDTOCsvColumn::getHeaderLabel,
+                WorkdayOpenAccountDetailDTOCsvColumn::getWorkdayOpenAccountDetailPropertyName);
+        mappingStrategy.setType(WorkdayOpenAccountDetailDTO.class);
+
+        StatefulBeanToCsv<WorkdayOpenAccountDetailDTO> csvWriter = new StatefulBeanToCsvBuilder<WorkdayOpenAccountDetailDTO>(
+                bufferedWriter).withMappingStrategy(mappingStrategy).build();
+        return csvWriter;
+    }
+    
+    private String generateCsvOutputFileName() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US);
+        StringBuilder filename = new StringBuilder(CuCoaBatchConstants.WorkdayOpenAccountsFileCreationConstants.OUTPUT_FILE_NAME);
+        filename.append("_").append(sdf.format(dateTimeService.getCurrentDate())).append(CUKFSConstants.FILE_EXTENSIONS.CSV_FILE_EXTENSION);
+        return filename.toString();
+    }
+    
+    private String fullyQualifyFileNameToCreationDirectory(String csvFileName) {
+        StringBuilder filename = new StringBuilder(csvOpenAccountsFileCreationDirectory);
+        filename.append(csvFileName);
+        return filename.toString();
+    }
+    
+    private String fullyQualifyFileNameToExportDirectory(String csvFileName) {
+        StringBuilder filename = new StringBuilder(csvOpenAccountsExportDirectory);
+        filename.append(csvFileName);
+        return filename.toString();
+    }
+    
+    private void moveCreatedFileToExportDirectory (File outputFile, String fullyQualifiedTargetFile) throws IOException {
+        Path sourceFilePath = outputFile.toPath();
+        File exportFile = new File(fullyQualifiedTargetFile);
+        Path exportFilePath = exportFile.toPath();
+        java.nio.file.Files.move(sourceFilePath, exportFilePath, StandardCopyOption.ATOMIC_MOVE);
+    }
+
+    public void setCsvOpenAccountsExportDirectory(String csvOpenAccountsExportDirectory) {
+        this.csvOpenAccountsExportDirectory = csvOpenAccountsExportDirectory;
+    }
+
+    public void setCsvOpenAccountsFileCreationDirectory(String csvOpenAccountsFileCreationDirectory) {
+        this.csvOpenAccountsFileCreationDirectory = csvOpenAccountsFileCreationDirectory;
+    }
+
+    public void setDateTimeService(DateTimeService dateTimeService) {
+        this.dateTimeService = dateTimeService;
+    }
+
+    public void setWorkdayOpenAccountDao(WorkdayOpenAccountDao workdayOpenAccountDao) {
+        this.workdayOpenAccountDao = workdayOpenAccountDao;
+    }
+
+}
---
+++ src/test/java/edu/cornell/kfs/coa/batch/dataaccess/impl/WorkdayOpenAccountDaoJdbcTest.java
@@ -0,0 +1,64 @@
+package edu.cornell.kfs.coa.batch.dataaccess.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.kuali.kfs.sys.service.UniversityDateService;
+import org.mockito.Mockito;
+
+public class WorkdayOpenAccountDaoJdbcTest {
+    
+    private WorkdayOpenAccountDaoJdbc workdayOpenAccountDao;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        workdayOpenAccountDao = new WorkdayOpenAccountDaoJdbc();
+        workdayOpenAccountDao.setUniversityDateService(buildMockUniversityDateService());
+    }
+    
+    private UniversityDateService buildMockUniversityDateService() {
+        UniversityDateService dateService = Mockito.mock(UniversityDateService.class);
+        Mockito.when(dateService.getCurrentFiscalYear()).thenReturn(2022);
+        return dateService;
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        workdayOpenAccountDao = null;
+    }
+
+    @Test
+    void testBuildFullOpenAccountSql() {
+        String actualSql = workdayOpenAccountDao.buildFullOpenAccountSql();
+        String expectedSql = "SELECT CAT.FIN_COA_CD, CAT.ACCOUNT_NBR, CAT.ACCOUNT_NM, CSF.SUB_FUND_GRP_WAGE_IND, CAT.SUB_FUND_GRP_CD,"
+                + " CAT.FIN_HGH_ED_FUNC_CD, CAT.ACCT_EFFECT_DT,  CAT.ACCT_CLOSED_IND, CAT.ACCT_TYP_CD, '' AS SUB_ACCT_NBR, "
+                + "'' AS SUB_ACCT_NM, '' AS SUB_ACCT_ACTV_CD, '' AS FIN_OBJECT_CD, '' AS FIN_SUB_OBJ_CD, '' AS FIN_SUB_OBJ_CD_NM "
+                + "FROM KFS.CA_ACCOUNT_T CAT "
+                + "JOIN KFS.CA_SUB_FUND_GRP_T CSF ON CAT.SUB_FUND_GRP_CD = CSF.SUB_FUND_GRP_CD "
+                + "WHERE CAT.ACCT_CLOSED_IND = 'N' AND CSF.SUB_FUND_GRP_WAGE_IND = 'Y' AND CAT.FIN_COA_CD = 'IT'  "
+                + "UNION "
+                + "SELECT CAT.FIN_COA_CD, CAT.ACCOUNT_NBR, CAT.ACCOUNT_NM, CSF.SUB_FUND_GRP_WAGE_IND, CAT.SUB_FUND_GRP_CD, "
+                + "CAT.FIN_HGH_ED_FUNC_CD, CAT.ACCT_EFFECT_DT,  CAT.ACCT_CLOSED_IND, CAT.ACCT_TYP_CD, CSA.SUB_ACCT_NBR, "
+                + "CSA.SUB_ACCT_NM, CSA.SUB_ACCT_ACTV_CD, '' AS FIN_OBJECT_CD, '' AS FIN_SUB_OBJ_CD, '' AS FIN_SUB_OBJ_CD_NM "
+                + "FROM KFS.CA_ACCOUNT_T CAT "
+                + "JOIN KFS.CA_SUB_FUND_GRP_T CSF ON CAT.SUB_FUND_GRP_CD = CSF.SUB_FUND_GRP_CD "
+                + "JOIN KFS.CA_SUB_ACCT_T CSA ON CAT.FIN_COA_CD = CSA.FIN_COA_CD AND CAT.ACCOUNT_NBR = CSA.ACCOUNT_NBR "
+                + "WHERE CAT.ACCT_CLOSED_IND = 'N' AND CSF.SUB_FUND_GRP_WAGE_IND = 'Y' AND CAT.FIN_COA_CD = 'IT' AND CSA.SUB_ACCT_ACTV_CD = 'Y'  "
+                + "UNION "
+                + "SELECT CAT.FIN_COA_CD, CAT.ACCOUNT_NBR, CAT.ACCOUNT_NM, CSF.SUB_FUND_GRP_WAGE_IND, CAT.SUB_FUND_GRP_CD, "
+                + "CAT.FIN_HGH_ED_FUNC_CD, CAT.ACCT_EFFECT_DT,  CAT.ACCT_CLOSED_IND, CAT.ACCT_TYP_CD, '' AS SUB_ACCT_NBR, '' AS SUB_ACCT_NM, "
+                + "'' AS SUB_ACCT_ACTV_CD, CSO.FIN_OBJECT_CD, CSO.FIN_SUB_OBJ_CD, CSO.FIN_SUB_OBJ_CD_NM "
+                + "FROM KFS.CA_ACCOUNT_T CAT "
+                + "JOIN KFS.CA_SUB_FUND_GRP_T CSF ON CAT.SUB_FUND_GRP_CD = CSF.SUB_FUND_GRP_CD "
+                + "JOIN KFS.CA_SUB_OBJECT_CD_T CSO ON CAT.FIN_COA_CD = CSO.FIN_COA_CD AND CAT.ACCOUNT_NBR = CSO.ACCOUNT_NBR "
+                + "JOIN KFS.LD_LABOR_OBJ_T COC ON CAT.FIN_COA_CD = COC.FIN_COA_CD AND CSO.FIN_OBJECT_CD = COC.FIN_OBJECT_CD AND "
+                + "CSO.UNIV_FISCAL_YR = COC.UNIV_FISCAL_YR "
+                + "WHERE CAT.ACCT_CLOSED_IND = 'N' AND CSF.SUB_FUND_GRP_WAGE_IND = 'Y' AND CAT.FIN_COA_CD = 'IT' AND "
+                + "CSO.UNIV_FISCAL_YR = 2022 AND COC.ACTV_IND = 'Y'  "
+                + "ORDER BY FIN_COA_CD, ACCOUNT_NBR, SUB_ACCT_NBR, FIN_SUB_OBJ_CD";
+        assertEquals(expectedSql, actualSql);
+    }
+
+}
