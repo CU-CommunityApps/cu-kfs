0e3be83d5677adc795cc24f382c3fba5cf44dd2d
Andrew Dickson <33495813+ajd299@users.noreply.github.com>
2021-08-26 15:48:20 -0400

    KFSPTS-22312 Tax Add 3 Fields to Transaction Detail Report (#1167)

--- src/main/java/edu/cornell/kfs/tax/businessobject/TransactionDetail.java
+++ src/main/java/edu/cornell/kfs/tax/businessobject/TransactionDetail.java
@@ -2,6 +2,7 @@ package edu.cornell.kfs.tax.businessobject;
 
 import org.kuali.rice.core.api.util.type.KualiDecimal;
 import org.kuali.kfs.krad.bo.TransientBusinessObjectBase;
+import org.kuali.rice.core.api.util.type.KualiInteger;
 
 /**
  * Convenience lightweight BO to allow for ORM tools to map to the
@@ -65,6 +66,10 @@ public class TransactionDetail extends TransientBusinessObjectBase {
     private String form1042SBox;
     private String form1042SOverriddenBox;
     private String paymentReasonCode;
+
+    private KualiInteger disbursementNbr;
+    private String paymentStatusCode;
+    private String disbursementTypeCode;
 
     public String getTransactionDetailId() {
         return transactionDetailId;
@@ -490,4 +495,27 @@ public class TransactionDetail extends TransientBusinessObjectBase {
         this.paymentReasonCode = paymentReasonCode;
     }
 
+    public KualiInteger getDisbursementNbr() {
+        return disbursementNbr;
+    }
+
+    public void setDisbursementNbr(KualiInteger disbursementNbr) {
+        this.disbursementNbr = disbursementNbr;
+    }
+
+    public String getPaymentStatusCode() {
+        return paymentStatusCode;
+    }
+
+    public void setPaymentStatusCode(String paymentStatusCode) {
+        this.paymentStatusCode = paymentStatusCode;
+    }
+
+    public String getDisbursementTypeCode() {
+        return disbursementTypeCode;
+    }
+
+    public void setDisbursementTypeCode(String disbursementTypeCode) {
+        this.disbursementTypeCode = disbursementTypeCode;
+    }
 }
