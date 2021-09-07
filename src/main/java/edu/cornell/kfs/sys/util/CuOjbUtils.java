cdea61e5a4c1513e62ca061de57d93739e9604c3
cah292 <cah292@cornell.edu>
2021-09-07 17:38:11 -0400

    KFSPTS-21560 Changes from PR feedback

--- src/main/java/edu/cornell/kfs/sys/util/CuOjbUtils.java
+++ src/main/java/edu/cornell/kfs/sys/util/CuOjbUtils.java
@@ -3,7 +3,7 @@ package edu.cornell.kfs.sys.util;
 import java.util.Iterator;
 import java.util.Objects;
 import java.util.Spliterator;
-import java.util.function.Consumer;
+import java.util.Spliterators;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
@@ -13,52 +13,14 @@ public final class CuOjbUtils {
         return buildStreamForQueryResults(Object[].class, reportQueryIterator);
     }
 
+    @SuppressWarnings("unchecked")
     public static <T> Stream<T> buildStreamForQueryResults(Class<T> queryResultType, Iterator<?> queryIterator) {
         Objects.requireNonNull(queryResultType, "queryResultType cannot be null");
         Objects.requireNonNull(queryIterator, "queryIterator cannot be null");
-        return StreamSupport.stream(() -> new QuerySpliterator<>(queryResultType, queryIterator), 0, false);
-    }
-
-    private static class QuerySpliterator<T> implements Spliterator<T> {
-        private Class<T> queryResultType;
-        private Iterator<?> queryIterator;
-
-        private QuerySpliterator(Class<T> queryResultType, Iterator<?> queryIterator) {
-            this.queryResultType = queryResultType;
-            this.queryIterator = queryIterator;
-        }
-
-        @Override
-        public boolean tryAdvance(Consumer<? super T> action) {
-            if (queryIterator.hasNext()) {
-                action.accept(queryResultType.cast(queryIterator.next()));
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        @Override
-        public void forEachRemaining(Consumer<? super T> action) {
-            while (queryIterator.hasNext()) {
-                action.accept(queryResultType.cast(queryIterator.next()));
-            }
-        }
-
-        @Override
-        public Spliterator<T> trySplit() {
-            return null;
-        }
-
-        @Override
-        public long estimateSize() {
-            return Long.MAX_VALUE;
-        }
-
-        @Override
-        public int characteristics() {
-            return 0;
-        }
+        Spliterator<Object> querySpliterator = Spliterators.spliteratorUnknownSize(
+                (Iterator<Object>) queryIterator, 0);
+        return StreamSupport.stream(() -> querySpliterator, 0, false)
+                .map(queryResultType::cast);
     }
 
 }
