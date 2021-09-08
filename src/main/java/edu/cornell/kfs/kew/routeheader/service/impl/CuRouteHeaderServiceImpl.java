e6a66380bd5a06906b8745282db21bb289633f5c
cah292 <cah292@users.noreply.github.com>
2021-09-08 05:35:20 -0400

    KFSPTS-21560 Update CU RouteHeaderService for KEW upgrade (#1174)

---
+++ src/main/java/edu/cornell/kfs/kew/routeheader/service/impl/CuRouteHeaderServiceImpl.java
@@ -0,0 +1,33 @@
+package edu.cornell.kfs.kew.routeheader.service.impl;
+
+import java.sql.Timestamp;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.kuali.kfs.kew.routeheader.service.impl.RouteHeaderServiceImpl;
+
+import edu.cornell.kfs.kew.routeheader.dao.CuDocumentRouteHeaderDAO;
+import edu.cornell.kfs.kew.routeheader.service.CuRouteHeaderService;
+
+public class CuRouteHeaderServiceImpl extends RouteHeaderServiceImpl implements CuRouteHeaderService {
+
+    @Override
+    public Map<String, Timestamp> getFinalizedDatesForDocumentType(String documentTypeName, Timestamp startDate,
+            Timestamp endDate) {
+        if (StringUtils.isBlank(documentTypeName)) {
+            throw new IllegalArgumentException("documentTypeName cannot be blank");
+        } else if (startDate == null) {
+            throw new IllegalArgumentException("startDate cannot be null");
+        } else if (endDate == null) {
+            throw new IllegalArgumentException("endDate cannot be null");
+        } else if (startDate.compareTo(endDate) > 0) {
+            throw new IllegalArgumentException("startDate cannot be later than endDate");
+        }
+        return getCuRouteHeaderDAO().getFinalizedDatesForDocumentType(documentTypeName, startDate, endDate);
+    }
+
+    private CuDocumentRouteHeaderDAO getCuRouteHeaderDAO() {
+        return (CuDocumentRouteHeaderDAO) getRouteHeaderDAO();
+    }
+
+}
