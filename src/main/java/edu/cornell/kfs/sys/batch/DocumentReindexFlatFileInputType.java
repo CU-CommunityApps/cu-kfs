dbd858eb847ecd6532026c76bd228c4aabfdc51f
cah292 <cah292@users.noreply.github.com>
2021-09-15 15:29:34 -0400

    KFSPTS-22105 Fix Document Reindexer Job (#1177)

--- src/main/java/edu/cornell/kfs/sys/batch/DocumentReindexFlatFileInputType.java
+++ src/main/java/edu/cornell/kfs/sys/batch/DocumentReindexFlatFileInputType.java
@@ -2,17 +2,13 @@ package edu.cornell.kfs.sys.batch;
 
 import java.io.File;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.kuali.kfs.sys.exception.ParseException;
 
+import edu.cornell.kfs.sys.CUKFSConstants;
 import edu.cornell.kfs.sys.CUKFSKeyConstants;
 
 public class DocumentReindexFlatFileInputType extends CuBatchInputFileTypeBase {
 
-	private static final Logger LOG = LogManager.getLogger(DocumentReindexFlatFileInputType.class);
-	private static final String FILE_NAME_PREFIX = "documentReindex";
-	
 	@Override
 	public String getFileTypeIdentifier() {
 		return "documentReindexFlatFileInputType";
@@ -20,7 +16,7 @@ public class DocumentReindexFlatFileInputType extends CuBatchInputFileTypeBase {
 
 	@Override
 	public String getFileName(String principalName, Object parsedFileContents, String fileUserIdentifuer) {
-		return FILE_NAME_PREFIX;
+		return CUKFSConstants.DOCUMENT_REINDEX_FILE_NAME_PREFIX;
 	}
 
 	@Override
