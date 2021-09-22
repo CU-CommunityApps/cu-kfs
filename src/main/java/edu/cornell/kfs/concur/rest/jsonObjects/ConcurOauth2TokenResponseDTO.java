1346859b7e534aecc3e082345f2bcdcafef895dc
Jay Hulslander <jdh34@cornell.edu>
2021-09-22 09:27:52 -0400

    adding ConcurOauth2TokenResponseDTO

---
+++ src/main/java/edu/cornell/kfs/concur/rest/jsonObjects/ConcurOauth2TokenResponseDTO.java
@@ -0,0 +1,77 @@
+package edu.cornell.kfs.concur.rest.jsonObjects;
+
+public class ConcurOauth2TokenResponseDTO {
+    
+    private int expires_in;
+    private String scope;
+    private String token_type;
+    private String access_token;
+    private String refresh_token;
+    private int refresh_expires_in;
+    private String geolocation;
+    private String id_token;
+    
+    public int getExpires_in() {
+        return expires_in;
+    }
+    
+    public void setExpires_in(int expires_in) {
+        this.expires_in = expires_in;
+    }
+    
+    public String getScope() {
+        return scope;
+    }
+    
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+    
+    public String getToken_type() {
+        return token_type;
+    }
+    
+    public void setToken_type(String token_type) {
+        this.token_type = token_type;
+    }
+    
+    public String getAccess_token() {
+        return access_token;
+    }
+    
+    public void setAccess_token(String access_token) {
+        this.access_token = access_token;
+    }
+    
+    public String getRefresh_token() {
+        return refresh_token;
+    }
+    
+    public void setRefresh_token(String refresh_token) {
+        this.refresh_token = refresh_token;
+    }
+    
+    public int getRefresh_expires_in() {
+        return refresh_expires_in;
+    }
+    
+    public void setRefresh_expires_in(int refresh_expires_in) {
+        this.refresh_expires_in = refresh_expires_in;
+    }
+    
+    public String getGeolocation() {
+        return geolocation;
+    }
+    
+    public void setGeolocation(String geolocation) {
+        this.geolocation = geolocation;
+    }
+    
+    public String getId_token() {
+        return id_token;
+    }
+    
+    public void setId_token(String id_token) {
+        this.id_token = id_token;
+    }
+}
---
+++ src/test/java/edu/cornell/kfs/concur/eventnotification/rest/jsonObjects/ConcurDTOJsonTest.java
@@ -0,0 +1,71 @@
+package edu.cornell.kfs.concur.eventnotification.rest.jsonObjects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import edu.cornell.kfs.concur.rest.jsonObjects.ConcurOauth2TokenResponseDTO;
+import edu.cornell.kfs.sys.util.CUJsonUtils;
+
+public class ConcurDTOJsonTest {
+    private static final Logger LOG = LogManager.getLogger();
+    
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        objectMapper = CUJsonUtils.buildObjectMapperUsingDefaultTimeZone();
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        objectMapper = null;
+    }
+
+    @Test
+    void testCreateConcurOauth2TokenResponseDTOFromJsonFile() throws JsonParseException, JsonMappingException, IOException {
+        File jsonFile = new File(ConcurDTOJsonTestConstants.OAUTH2_TOKEN_RESPONSE_FILE_NAME); 
+        ConcurOauth2TokenResponseDTO dto = objectMapper.readValue(jsonFile, ConcurOauth2TokenResponseDTO.class);
+        assertEquals(ConcurDTOJsonTestConstants.Oauth2TokenResponseValue.ACCESS_TOKEN, dto.getAccess_token());
+        assertEquals(ConcurDTOJsonTestConstants.Oauth2TokenResponseValue.EXPIRES_IN, dto.getExpires_in());
+        assertEquals(ConcurDTOJsonTestConstants.Oauth2TokenResponseValue.GEOLOCATION, dto.getGeolocation());
+        assertEquals(ConcurDTOJsonTestConstants.Oauth2TokenResponseValue.ID_TOKEN, dto.getId_token());
+        assertEquals(ConcurDTOJsonTestConstants.Oauth2TokenResponseValue.REFRESH_EXPIRES_IN, dto.getRefresh_expires_in());
+        assertEquals(ConcurDTOJsonTestConstants.Oauth2TokenResponseValue.REFRESH_TOKEN, dto.getRefresh_token());
+        assertEquals(ConcurDTOJsonTestConstants.Oauth2TokenResponseValue.SCOPE, dto.getScope());
+        assertEquals(ConcurDTOJsonTestConstants.Oauth2TokenResponseValue.TOKEN_TYPE, dto.getToken_type());
+    }
+    
+    @Test
+    void testCreateJsonStringFromConcurOauth2TokenResponseDTO() throws JsonProcessingException {
+        ConcurOauth2TokenResponseDTO dto = new ConcurOauth2TokenResponseDTO();
+        dto.setAccess_token("access");
+        dto.setExpires_in(321);
+        dto.setGeolocation("https://www.cornell.edu");
+        dto.setId_token("id");
+        dto.setRefresh_expires_in(987654321);
+        dto.setRefresh_token("refresh");
+        dto.setScope("scope");
+        dto.setToken_type("type");
+        
+        String expectedJson = "{\"expires_in\":321,\"scope\":\"scope\",\"token_type\":\"type\",\"access_token\":\"access\",\"refresh_token\":\"refresh\",\"refresh_expires_in\":987654321,\"geolocation\":\"https://www.cornell.edu\",\"id_token\":\"id\"}";
+        
+        String actualJson = objectMapper.writeValueAsString(dto);
+        LOG.info("testCreateJsonStringFromConcurOauth2TokenResponseDTO, generated JSON: " + actualJson);
+        
+        assertEquals(expectedJson, actualJson);
+    }
+
+}
