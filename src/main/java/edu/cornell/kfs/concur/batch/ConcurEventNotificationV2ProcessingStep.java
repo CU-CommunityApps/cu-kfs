b939f00da8d0e093c58693386944e28283eda189
Jay Hulslander <jdh34@cornell.edu>
2021-09-23 08:53:18 -0400

    add webservice call

--- src/main/java/edu/cornell/kfs/concur/batch/ConcurEventNotificationV2ProcessingStep.java
+++ src/main/java/edu/cornell/kfs/concur/batch/ConcurEventNotificationV2ProcessingStep.java
@@ -4,12 +4,16 @@ import java.util.Date;
 
 import org.kuali.kfs.sys.batch.AbstractStep;
 
+import edu.cornell.kfs.concur.service.ConcurAccessTokenV2Service;
+
 public class ConcurEventNotificationV2ProcessingStep extends AbstractStep {
+    
+    protected ConcurAccessTokenV2Service concurAccessTokenV2Service;
 
     @Override
     public boolean execute(String jobName, Date jobRunDate) throws InterruptedException {
-        // TODO Auto-generated method stub
-        return false;
+        String accessToken = concurAccessTokenV2Service.retrieveNewAccessBearerToken();
+        return true;
     }
 
 }
