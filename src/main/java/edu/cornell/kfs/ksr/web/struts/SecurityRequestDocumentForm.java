ebb9075e032ba91577e8f9ad33a30e74e9ceec82
db483 <db483@cornell.edu>
2021-09-28 08:40:40 +0100

    KFSPTS-21716 formatting

--- src/main/java/edu/cornell/kfs/ksr/web/struts/SecurityRequestDocumentForm.java
+++ src/main/java/edu/cornell/kfs/ksr/web/struts/SecurityRequestDocumentForm.java
@@ -21,11 +21,11 @@ public class SecurityRequestDocumentForm extends FinancialSystemTransactionalDoc
         return KSRConstants.SECURITY_REQUEST_DOC_TYPE_NAME;
     }
 
-    @SuppressWarnings({"rawtypes","unchecked"})
+    @SuppressWarnings({ "rawtypes", "unchecked" })
     public void setTabRoleIndexes(final List tabRoleIndexes) {
         this.tabRoleIndexes = tabRoleIndexes;
     }
-    
+
     @SuppressWarnings("rawtypes")
     public List getTabRoleIndexes() {
         return tabRoleIndexes;
@@ -33,34 +33,34 @@ public class SecurityRequestDocumentForm extends FinancialSystemTransactionalDoc
 
     public static class TabRoleIndexes implements java.io.Serializable {
 
-    	private static final long serialVersionUID = 6981150675755290581L;
-    	
-    	private Long tabId;
-    	private String tabName;
-        private List<Integer> roleRequestIndexes;
+        private static final long serialVersionUID = 6981150675755290581L;
 
+        private Long tabId;
+        private String tabName;
+        private List<Integer> roleRequestIndexes;
+        
         public Long getTabId() {
             return tabId;
         }
-
+        
         public void setTabId(Long tabId) {
             this.tabId = tabId;
         }
-
+        
         public String getTabName() {
             return tabName;
         }
-
+        
         public void setTabName(String tabName) {
             this.tabName = tabName;
         }
-
+        
         @SuppressWarnings("rawtypes")
         public List getRoleRequestIndexes() {
             return roleRequestIndexes;
         }
-
-        @SuppressWarnings({"rawtypes","unchecked"})
+        
+        @SuppressWarnings({ "rawtypes", "unchecked" })
         public void setRoleRequestIndexes(List roleRequestIndexes) {
             this.roleRequestIndexes = (List<Integer>) roleRequestIndexes;
         }
