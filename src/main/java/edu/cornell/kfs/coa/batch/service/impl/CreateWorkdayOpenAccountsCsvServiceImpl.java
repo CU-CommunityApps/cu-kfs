071a451e414b159de76e978aed6baa6fb8018a86
cah292 <cah292@users.noreply.github.com>
2021-09-29 04:35:26 -0400

    KFSPTS-22565 Fix changes from 2021-09-28 rebase (#1183)

--- src/main/java/edu/cornell/kfs/coa/batch/service/impl/CreateWorkdayOpenAccountsCsvServiceImpl.java
+++ src/main/java/edu/cornell/kfs/coa/batch/service/impl/CreateWorkdayOpenAccountsCsvServiceImpl.java
@@ -15,7 +15,7 @@ import java.util.Locale;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.kuali.rice.core.api.datetime.DateTimeService;
+import org.kuali.kfs.core.api.datetime.DateTimeService;
 
 import com.opencsv.bean.StatefulBeanToCsv;
 import com.opencsv.bean.StatefulBeanToCsvBuilder;
