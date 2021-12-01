f19983cee23dd066de447ce25016a0425369df5a
db483 <db483@cornell.edu>
2021-12-01 15:17:48 +0000

    KFSPTS-21620 remove customizations related to PERMIT groups that are not used in KFS (#1218)

--- src/main/java/edu/cornell/kfs/kim/service/impl/CuUiDocumentServiceImpl.java
+++ src/main/java/edu/cornell/kfs/kim/service/impl/CuUiDocumentServiceImpl.java
@@ -1,53 +1,18 @@
 package edu.cornell.kfs.kim.service.impl;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
-import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.kuali.kfs.kim.api.KimConstants;
 import org.kuali.kfs.kim.bo.ui.KimDocumentRoleMember;
-import org.kuali.kfs.kim.bo.ui.PersonDocumentGroup;
-import org.kuali.kfs.kim.document.IdentityManagementGroupDocument;
-import org.kuali.kfs.kim.document.IdentityManagementPersonDocument;
 import org.kuali.kfs.kim.document.IdentityManagementRoleDocument;
-import org.kuali.kfs.kim.impl.group.GroupMember;
 import org.kuali.kfs.kim.impl.role.Role;
 import org.kuali.kfs.kim.impl.role.RoleMember;
 import org.kuali.kfs.kim.service.impl.UiDocumentServiceImpl;
 
-import edu.cornell.kfs.sys.CUKFSConstants;
-
 public class CuUiDocumentServiceImpl extends UiDocumentServiceImpl {
-
-    /**
-     * Overridden to exclude PERMIT groups from membership updates by the Person Document.
-     */
-    @Override
-    protected List<GroupMember> populateGroupMembers(
-            IdentityManagementPersonDocument identityManagementPersonDocument) {
-        List<GroupMember> groupMembers = super.populateGroupMembers(identityManagementPersonDocument);
-        Set<String> groupIdWhitelist = getIdsOfUpdatableGroups(identityManagementPersonDocument);
-        return groupMembers.stream()
-                .filter(groupMember -> groupIdWhitelist.contains(groupMember.getGroupId()))
-                .collect(Collectors.toList());
-    }
-
-    protected Set<String> getIdsOfUpdatableGroups(IdentityManagementPersonDocument identityManagementPersonDocument) {
-        if (CollectionUtils.isEmpty(identityManagementPersonDocument.getGroups())) {
-            return Collections.emptySet();
-        }
-        return identityManagementPersonDocument.getGroups().stream()
-                .filter(group -> !StringUtils.equalsIgnoreCase(
-                        CUKFSConstants.LEGACY_PERMIT_NAMESPACE, group.getNamespaceCode()))
-                .map(PersonDocumentGroup::getGroupId)
-                .collect(Collectors.toSet());
-    }
 
     /**
      * Overridden to allow for loading unmodified role members even when there are no delegations,
@@ -78,18 +43,6 @@ public class CuUiDocumentServiceImpl extends UiDocumentServiceImpl {
 
         identityManagementRoleDocument.setMembers(loadRoleMembers(identityManagementRoleDocument, members));
         loadMemberRoleRspActions(identityManagementRoleDocument);
-    }
-
-    /**
-     * Overridden to prevent users from updating PERMIT groups via the Group Document.
-     */
-    @Override
-    public void saveGroup(IdentityManagementGroupDocument identityManagementGroupDocument) {
-        if (StringUtils.equalsIgnoreCase(
-                CUKFSConstants.LEGACY_PERMIT_NAMESPACE, identityManagementGroupDocument.getGroupNamespace())) {
-            throw new UnsupportedOperationException("Cannot modify PERMIT groups using the Group Document");
-        }
-        super.saveGroup(identityManagementGroupDocument);
     }
 
 }
