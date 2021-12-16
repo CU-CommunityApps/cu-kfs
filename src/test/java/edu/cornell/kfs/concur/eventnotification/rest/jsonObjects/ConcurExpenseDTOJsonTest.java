0275c8536691f90f63557ae0a66895cc416d868d
Jay Hulslander <jdh34@cornell.edu>
2021-12-16 16:09:25 -0500

    add more unit test checks

--- src/test/java/edu/cornell/kfs/concur/eventnotification/rest/jsonObjects/ConcurExpenseDTOJsonTest.java
+++ src/test/java/edu/cornell/kfs/concur/eventnotification/rest/jsonObjects/ConcurExpenseDTOJsonTest.java
@@ -4,12 +4,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.joda.time.DateTime;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.kuali.rice.core.api.util.type.KualiDecimal;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -20,6 +23,12 @@ import edu.cornell.kfs.concur.rest.jsonObjects.ConcurExpenseV3ListingDTO;
 import edu.cornell.kfs.sys.util.CUJsonUtils;
 
 class ConcurExpenseDTOJsonTest {
+    private static final Logger LOG = LogManager.getLogger();
+    private ObjectMapper objectMapper;
+    
+    private static final String EXPENSE_REPORT_JSON_EXAMPLE_FILE = "src/test/resources/edu/cornell/kfs/concur/rest/jsonObjects/fixture/expenseV3/v3_expense_report.json";
+    private static final String EXPENSE_LISTING_JSON_EXAMPLE_FILE = "src/test/resources/edu/cornell/kfs/concur/rest/jsonObjects/fixture/expenseV3/v3_expense_listing.json";
+    
     private static final String REPORT_2_NAME = "Expense Report 2";
     private static final String REPORT_1_NAME = "Expense Report 1";
     private static final String ACCOUNT_EXO4769 = "EXO4769";
@@ -28,12 +37,16 @@ class ConcurExpenseDTOJsonTest {
     private static final String ACCOUNT_G224700 = "G224700";
     private static final String CHART_IT = "IT";
     private static final String REPORTID1 = "reportid1";
-    private static final Logger LOG = LogManager.getLogger();
-    private ObjectMapper objectMapper;
+    private static final Date REPORT_1_LAST_MODIFIED_DATE = buildDateFromString("2020-02-06T18:30:11.247");
+    private static final KualiDecimal REPORT_1_AMOUNT_DUE_EMPLOYEE = new KualiDecimal(5.00000000);
+    private static final Date REPORT_2_CREATE_DATE = buildDateFromString("2020-03-05T18:21:54.143");
+    private static final KualiDecimal REPORT_2_TOTAL = new KualiDecimal(1800.00000000);
     
-    private static final String EXPENSE_REPORT_JSON_EXAMPLE_FILE = "src/test/resources/edu/cornell/kfs/concur/rest/jsonObjects/fixture/expenseV3/v3_expense_report.json";
-    private static final String EXPENSE_LISTING_JSON_EXAMPLE_FILE = "src/test/resources/edu/cornell/kfs/concur/rest/jsonObjects/fixture/expenseV3/v3_expense_listing.json";
-
+    private static Date buildDateFromString(String stringDate) {
+        DateTime jodaDate = new DateTime(stringDate);
+        return new Date(jodaDate.getMillis());
+    }
+    
     @BeforeEach
     public void setUp() throws Exception {
         objectMapper = CUJsonUtils.buildObjectMapperUsingDefaultTimeZone();
@@ -51,6 +64,8 @@ class ConcurExpenseDTOJsonTest {
         LOG.info("testConcurV3ExpenseReportDTO, dto: " + dto.toString());
         assertEquals(CHART_IT, dto.getChart().getCode());
         assertEquals(ACCOUNT_G224700, dto.getAccount().getCode());
+        assertEquals(REPORT_1_LAST_MODIFIED_DATE, dto.getLastModifiedDate());
+        assertEquals(REPORT_1_AMOUNT_DUE_EMPLOYEE, dto.getAmountDueEmployee());
     }
     
     @Test
@@ -64,6 +79,8 @@ class ConcurExpenseDTOJsonTest {
         assertEquals(REPORTID1, firstExpenseItem.getId());
         assertEquals(CHART_IT, firstExpenseItem.getChart().getCode());
         assertEquals(ACCOUNT_G224700, firstExpenseItem.getAccount().getCode());
+        assertEquals(REPORT_1_LAST_MODIFIED_DATE, firstExpenseItem.getLastModifiedDate());
+        assertEquals(REPORT_1_AMOUNT_DUE_EMPLOYEE, firstExpenseItem.getAmountDueEmployee());
         
         ConcurExpenseV3ListItemDTO secondExpenseItem = dto.getItems().stream()
                 .filter(item -> item.getName().equals(REPORT_2_NAME))
@@ -71,6 +88,8 @@ class ConcurExpenseDTOJsonTest {
         assertEquals(REPORTID2, secondExpenseItem.getId());
         assertEquals(CHART_EX, secondExpenseItem.getChart().getCode());
         assertEquals(ACCOUNT_EXO4769, secondExpenseItem.getAccount().getCode());
+        assertEquals(REPORT_2_TOTAL, secondExpenseItem.getTotal());
+        assertEquals(REPORT_2_CREATE_DATE, secondExpenseItem.getCreateDate());
         
     }
 
--- src/test/java/edu/cornell/kfs/concur/eventnotification/rest/jsonObjects/ConcurRequestDTOJsonTest.java
+++ src/test/java/edu/cornell/kfs/concur/eventnotification/rest/jsonObjects/ConcurRequestDTOJsonTest.java
@@ -4,9 +4,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.joda.time.DateTime;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,19 +24,27 @@ import edu.cornell.kfs.concur.rest.jsonObjects.ConcurRequestV4ReportDTO;
 import edu.cornell.kfs.sys.util.CUJsonUtils;
 
 class ConcurRequestDTOJsonTest {
-    
     private static final Logger LOG = LogManager.getLogger();
     private ObjectMapper objectMapper;
     
     private static final String TRAVEL_REPORT_JSON_EXAMPLE_FILE = "src/test/resources/edu/cornell/kfs/concur/rest/jsonObjects/fixture/travelV4/v4_travel_report.json";
     private static final String TRAVEL_LIST_JSON_EXAMPLE_FILE = "src/test/resources/edu/cornell/kfs/concur/rest/jsonObjects/fixture/travelV4/v4_travel_request_listing.json";
+    
     private static final String REPORT_ID = "reportId";
     private static final String PURPOSE1 = "purpose1";
     private static final String DOE = "Doe";
     private static final String EZRA = "Ezra";
     private static final KualiDecimal REPORT_EXPECTED_TOTAL_REMAINING_AMOUNT = new KualiDecimal(1290.0);
     private static final KualiDecimal REPORT_EXPECTED_TOTAL_APPROVED_AMOUNT = new KualiDecimal(879.07);
+    private static final Date REPORT_CREATION_DATE = buildDateFromString("2021-11-24T15:36:59.000Z");
     private static final Integer LISTING_EXPECTED_TOTAL_COUNT = 5;
+    public static final Date EZRA_REPORT_END_DATE = buildDateFromString("2021-12-15");
+    public static final KualiDecimal EZRA_TOTAL_POSTED_AMOUNT = new KualiDecimal("1290.00000000");
+    
+    private static Date buildDateFromString(String stringDate) {
+        DateTime jodaDate = new DateTime(stringDate);
+        return new Date(jodaDate.getMillis());
+    }
 
     @BeforeEach
     public void setUp() throws Exception {
@@ -56,6 +66,7 @@ class ConcurRequestDTOJsonTest {
         assertEquals(DOE, dto.getApprover().getLastName());
         assertEquals(REPORT_EXPECTED_TOTAL_REMAINING_AMOUNT, dto.getTotalRemainingAmount().getValue());
         assertEquals(REPORT_EXPECTED_TOTAL_APPROVED_AMOUNT, dto.getTotalApprovedAmount().getValue());
+        assertEquals(REPORT_CREATION_DATE, dto.getCreationDate());
     }
     
     @Test
@@ -69,6 +80,8 @@ class ConcurRequestDTOJsonTest {
                 .filter(item -> item.getBusinessPurpose().equals(PURPOSE1))
                 .findFirst().orElseThrow();
         assertEquals(EZRA, purposeOneItem.getApprover().getFirstName());
+        assertEquals(EZRA_REPORT_END_DATE, purposeOneItem.getEndDate());
+        assertEquals(EZRA_TOTAL_POSTED_AMOUNT, purposeOneItem.getTotalPostedAmount().getValue());
     }
 
 }
