0628549c608b47adc9820584cd5283ab402c9c96
Jay Hulslander <jdh34@cornell.edu>
2022-02-07 15:29:25 -0500

    KFSPTS-23305 additional logging to figure out concur issue

--- src/main/java/edu/cornell/kfs/concur/rest/authentication/ConcurAuthenticationFilter.java
+++ src/main/java/edu/cornell/kfs/concur/rest/authentication/ConcurAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package edu.cornell.kfs.concur.rest.authentication;
 
 import java.io.IOException;
+import java.util.Enumeration;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -14,6 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.kuali.kfs.core.api.config.property.ConfigContext;
 import org.kuali.kfs.sys.context.SpringContext;
 
 import edu.cornell.kfs.concur.ConcurConstants;
@@ -31,6 +33,7 @@ public class ConcurAuthenticationFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         LOG.debug("doFilter() started");
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        logRequst(httpServletRequest);
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
         String authorizationHeader = httpServletRequest.getHeader(ConcurConstants.AUTHORIZATION_PROPERTY);
 
@@ -47,8 +50,27 @@ public class ConcurAuthenticationFilter implements Filter {
                 return;
             }
         }
-        LOG.debug("doFilter(): Concur authentication was successful");
+        LOG.info("doFilter(): Concur authentication was successful");
         filterChain.doFilter(servletRequest, response);
+    }
+    
+    private void logRequst(HttpServletRequest request) {
+        LOG.info("logRequst, entering");
+        boolean isProd = ConfigContext.getCurrentContextConfig().isProductionEnvironment();
+        if (!isProd) {
+            for (String attributeKey : request.getParameterMap().keySet()) {
+                LOG.info("logRequst, attributeKey: '" + attributeKey + "' and attribute value: '" + request.getParameterMap().get(attributeKey) + "'");
+            }
+            
+            Enumeration<String> headerNames = request.getHeaderNames();
+            while (headerNames.hasMoreElements()) {
+                String headerKey = headerNames.nextElement();
+                String headerValue = request.getHeader(headerKey);
+                LOG.info("logRequst, headerKey: '" + headerKey + "' and header value: '" + headerValue + "'");
+            }
+        } else {
+            LOG.info("logRequst, this is production, so we aren't dooing this logging.");
+        }
     }
 
     @Override
