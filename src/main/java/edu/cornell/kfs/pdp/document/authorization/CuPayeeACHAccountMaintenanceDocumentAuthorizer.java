9813e4a0209766aea850091fcc752b1185804bd1
Andrew Dickson <ajd299@cornell.edu>
2022-04-21 16:04:15 -0400

    SuperUser and Blanket Approve

--- src/main/java/edu/cornell/kfs/pdp/document/authorization/CuPayeeACHAccountMaintenanceDocumentAuthorizer.java
+++ src/main/java/edu/cornell/kfs/pdp/document/authorization/CuPayeeACHAccountMaintenanceDocumentAuthorizer.java
@@ -3,6 +3,8 @@ package edu.cornell.kfs.pdp.document.authorization;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.kuali.kfs.kew.api.WorkflowDocument;
+import org.kuali.kfs.kew.service.KEWServiceLocator;
 import org.kuali.kfs.kim.api.identity.Person;
 import org.kuali.kfs.kns.service.KNSServiceLocator;
 import org.kuali.kfs.krad.document.Document;
@@ -19,28 +21,26 @@ public class CuPayeeACHAccountMaintenanceDocumentAuthorizer extends PayeeACHAcco
     public boolean canApprove(Document document, Person user) {
 
         try {
-            if (StringUtils.equalsIgnoreCase(user.getPrincipalId(), KFSConstants.SYSTEM_USER)) {
+            WorkflowDocument workflowDocument = document.getDocumentHeader().getWorkflowDocument();
+
+            if (StringUtils.equalsIgnoreCase(user.getPrincipalName(), KFSConstants.SYSTEM_USER)) {
                 return true;
-            } else if (canCurrentUserBlanketApprove(document)) {
+            } else if (canBlanketApprove(document, user)) {
+                return true;
+            } else if (KEWServiceLocator.getDocumentTypeService().isSuperUserForDocumentTypeId(user.getPrincipalId(), workflowDocument.getDocumentTypeId())) {
                 return true;
             }
 
-            String documentInitiatorPrincipalId = document.getDocumentHeader().getWorkflowDocument().getInitiatorPrincipalId();
+            String documentInitiatorPrincipalId = workflowDocument.getInitiatorPrincipalId();
             if (StringUtils.equalsIgnoreCase(user.getPrincipalId(), documentInitiatorPrincipalId)) {
                 return false;
             }
         } catch (Exception ex) {
-            LOG.error("canApprove" + ex.getMessage(), ex);
+            LOG.error("canApprove " + ex.getMessage(), ex);
             return false;
         }
 
         return super.canApprove(document, user);
-    }
-
-    private boolean canCurrentUserBlanketApprove(Document document) {
-        DocumentPresentationController documentPresentationController = KNSServiceLocator.getDocumentHelperService().getDocumentPresentationController(document);
-        Set<String> documentActions = documentPresentationController.getDocumentActions(document);
-        return documentActions.contains(KRADConstants.KUALI_ACTION_CAN_BLANKET_APPROVE);
     }
 
 }
