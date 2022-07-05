4bff3161df8200edd97cc78f157c9e490715a232
Nancy Kimble <nkk4@cornell.edu>
2022-07-05 15:03:41 -0400

    KFSPTS-23855: Remove unused modification to KFS re Incoming Wires.

--- src/main/java/edu/cornell/kfs/fp/CuFPParameterConstants.java
+++ src/main/java/edu/cornell/kfs/fp/CuFPParameterConstants.java
@@ -1,56 +1,7 @@
 package edu.cornell.kfs.fp;
 
-/**
- Portions Modified 04/2016 and Copyright Cornell University
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU Affero General Public License as
- published by the Free Software Foundation, either version 3 of the
- License, or (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU Affero General Public License for more details.
-
- You should have received a copy of the GNU Affero General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/**
- Copyright Indiana University
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU Affero General Public License as
- published by the Free Software Foundation, either version 3 of the
- License, or (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU Affero General Public License for more details.
-
- You should have received a copy of the GNU Affero General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 public class CuFPParameterConstants {
-
-    public static class AchIncome {
-        public static final String ACH_INCOME_SUMMARY_EMAIL_SUBJECT = "ACH_INCOME_SUMMARY_EMAIL_SUBJECT";
-        public static final String ACH_INCOME_SUMMARY_FROM_EMAIL_ADDRESS = "ACH_INCOME_SUMMARY_FROM_EMAIL_ADDRESS";
-        public static final String ACH_INCOME_SUMMARY_TO_EMAIL_ADDRESSES = "ACH_INCOME_SUMMARY_TO_EMAIL_ADDRESSES";
-    }
-
-    public static class AdvanceDepositDocument {
-        public static final String DETAIL_REFERENCE_NUMBER = "DETAIL_REFERENCE_NUMBER";
-        public static final String DETAIL_DESCRIPTION = "DETAIL_DESCRIPTION";
-        public static final String BANK_CODE = "BANK_CODE";
-        public static final String DOCUMENT_DESCRIPTION = "DOCUMENT_DESCRIPTION";
-        public static final String CHART = "CHART";
-        public static final String OBJECT_CODE = "OBJECT_CODE";
-        public static final String ACCOUNT = "ACCOUNT";
-    }
-    
+   
     public static class CorporateBilledCorporatePaidDocument {
         public static final String CBCP_COMPONENT_NAME = "CorporateBilledCorporatePaidDocument";
         public static final String CBCP_ACCOUNTING_DEFAULT_IND_PARAMETER_NAME = "CBCP_ACCOUNTING_DEFAULT_IND";
