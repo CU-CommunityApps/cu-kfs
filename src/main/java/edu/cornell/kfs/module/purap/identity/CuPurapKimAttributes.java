597bb6134cbfad39c1b0dbb98d3d7c5dc4f91d2a
cah292 <cah292@users.noreply.github.com>
2022-07-29 09:36:32 -0400

    KFSPTS-25158 Fix display of Jaggaer roles in KIM (#1306)

---
+++ src/main/java/edu/cornell/kfs/module/purap/identity/CuPurapKimAttributes.java
@@ -0,0 +1,20 @@
+package edu.cornell.kfs.module.purap.identity;
+
+import org.kuali.kfs.module.purap.identity.PurapKimAttributes;
+
+public class CuPurapKimAttributes extends PurapKimAttributes {
+    private static final long serialVersionUID = 5268027448509178843L;
+
+    public static final String JAGGAER_ROLE = "jaggaerRole";
+
+    protected String jaggaerRole;
+
+    public String getJaggaerRole() {
+        return jaggaerRole;
+    }
+
+    public void setJaggaerRole(String jaggaerRole) {
+        this.jaggaerRole = jaggaerRole;
+    }
+
+}
