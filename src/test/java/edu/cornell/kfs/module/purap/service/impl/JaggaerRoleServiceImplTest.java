5b4e4f4153d8fa6456858e3209d5dd6f6bcbd5d8
Jay Hulslander <jdh34@cornell.edu>
2022-08-01 08:58:43 -0400

    completed unit tests

--- src/test/java/edu/cornell/kfs/module/purap/service/impl/JaggaerRoleServiceImplTest.java
+++ src/test/java/edu/cornell/kfs/module/purap/service/impl/JaggaerRoleServiceImplTest.java
@@ -2,6 +2,11 @@ package edu.cornell.kfs.module.purap.service.impl;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 import org.apache.commons.lang3.StringUtils;
@@ -19,13 +24,18 @@ import org.kuali.kfs.sys.KFSConstants;
 import org.mockito.Mockito;
 
 import edu.cornell.kfs.module.purap.CUPurapConstants;
+import edu.cornell.kfs.module.purap.CUPurapConstants.JaggaerRoleSet;
 import edu.cornell.kfs.module.purap.JaggaerConstants;
+import edu.cornell.kfs.module.purap.businessobject.JaggaerRoleLinkMapping;
 import edu.cornell.kfs.module.purap.service.JaggaerRoleLinkMappingService;
 
 class JaggaerRoleServiceImplTest {
     
     private JaggaerRoleServiceImpl jaggaerRoleService;
     private Person user;
+    
+    private static final String JAGGAER_ROLE_HAS = "jaggar role has";
+    private static final String JAGGAER_ROLE_DOES_NOT_HAVE = "jaggar role does not have";
 
     @BeforeEach
     void setUp() throws Exception {
@@ -38,11 +48,6 @@ class JaggaerRoleServiceImplTest {
         Person person = Mockito.mock(Person.class);
         Mockito.when(person.getPrincipalId()).thenReturn("123456789");
         return person;
-    }
-    
-    private JaggaerRoleLinkMappingService buildMockJaggaerRoleLinkMappingService() {
-        JaggaerRoleLinkMappingService service = Mockito.mock(JaggaerRoleLinkMappingService.class);
-        return service;
     }
     
     private PermissionService buildMockPermissionService(boolean isBuyer, boolean isOffice, boolean isLab, boolean isFacilities) {
@@ -64,7 +69,18 @@ class JaggaerRoleServiceImplTest {
                 KFSConstants.OptionalModuleNamespaces.PURCHASING_ACCOUNTS_PAYABLE,
                 CUPurapConstants.B2B_SHOPPER_FACILITIES_PERMISSION)).thenReturn(isFacilities);
         
+        addMockRolePermissionCheck(permissionService, JAGGAER_ROLE_HAS, true);
+        addMockRolePermissionCheck(permissionService, JAGGAER_ROLE_DOES_NOT_HAVE, false);
+        
         return permissionService;
+    }
+    
+    private void addMockRolePermissionCheck(PermissionService permissionService, String roleName, boolean hasPermission) {
+        Map<String, String> permissionDetails = new HashMap<>();
+        permissionDetails.put(JaggaerConstants.JAGGAER_ATTRIBUTE_VALUE_KEY, roleName);
+        Mockito.when(permissionService.hasPermissionByTemplate(user.getPrincipalId(),
+                JaggaerConstants.JAGGAER_NAMESPACE,
+                JaggaerConstants.JAGGAER_PERMISSION_TEMPLATE_NAME, permissionDetails)).thenReturn(hasPermission);
     }
 
     @AfterEach
@@ -156,6 +172,64 @@ class JaggaerRoleServiceImplTest {
           Arguments.of(false, false, true, CUPurapConstants.SCIQUEST_ROLE_FACILITIES),
           Arguments.of(false, false, false, CUPurapConstants.SCIQUEST_ROLE_UNRESTRICTED)
         );
+    }
+    
+    
+    @ParameterizedTest
+    @MethodSource("provideforTestGetJaggaerRoles")
+    void testGetJaggaerRoles(boolean isBuyer, boolean isOffice, boolean isLab, boolean isFacilities, List<String> expectedRoles) {
+        jaggaerRoleService.setJaggaerRoleLinkMappingService(buildMockJaggaerRoleLinkMappingService());
+        jaggaerRoleService.setPermissionService(buildMockPermissionService(isBuyer, isOffice, isLab, isFacilities));
+        List<String> actualRoles = jaggaerRoleService.getJaggaerRoles(user, JaggaerRoleSet.ADMINISTRATOR);
+        assertEquals(expectedRoles.size(), actualRoles.size());
+        for (String role : actualRoles) {
+            assertTrue(expectedRoles.contains(role));
+        }
+    }
+    
+    private static Stream<Arguments> provideforTestGetJaggaerRoles() {
+        return Stream.of(
+          Arguments.of(true, true, false, false, List.of(JAGGAER_ROLE_HAS, 
+                  JaggaerConstants.JAGGAER_ROLE_NAME_VIEW_ONLY, CUPurapConstants.SCIQUEST_ROLE_BUYER,
+                  CUPurapConstants.SCIQUEST_ROLE_OFFICE)),
+          Arguments.of(false, true, false, false, List.of(JAGGAER_ROLE_HAS, 
+                  JaggaerConstants.JAGGAER_ROLE_NAME_VIEW_ONLY, CUPurapConstants.SCIQUEST_ROLE_SHOPPER,
+                  CUPurapConstants.SCIQUEST_ROLE_OFFICE)),
+          Arguments.of(false, true, true, true, List.of(JAGGAER_ROLE_HAS, 
+                  JaggaerConstants.JAGGAER_ROLE_NAME_VIEW_ONLY, CUPurapConstants.SCIQUEST_ROLE_SHOPPER,
+                  CUPurapConstants.SCIQUEST_ROLE_OFFICE)),
+          Arguments.of(true, false, true, false, List.of(JAGGAER_ROLE_HAS, 
+                  JaggaerConstants.JAGGAER_ROLE_NAME_VIEW_ONLY, CUPurapConstants.SCIQUEST_ROLE_BUYER,
+                  CUPurapConstants.SCIQUEST_ROLE_LAB)),
+          Arguments.of(true, false, false, true, List.of(JAGGAER_ROLE_HAS, 
+                  JaggaerConstants.JAGGAER_ROLE_NAME_VIEW_ONLY, CUPurapConstants.SCIQUEST_ROLE_BUYER,
+                  CUPurapConstants.SCIQUEST_ROLE_FACILITIES)),
+          Arguments.of(true, false, false, false, List.of(JAGGAER_ROLE_HAS, 
+                  JaggaerConstants.JAGGAER_ROLE_NAME_VIEW_ONLY, CUPurapConstants.SCIQUEST_ROLE_BUYER,
+                  CUPurapConstants.SCIQUEST_ROLE_UNRESTRICTED))
+        );
+    }
+    
+    private JaggaerRoleLinkMappingService buildMockJaggaerRoleLinkMappingService() {
+        JaggaerRoleLinkMappingService service = Mockito.mock(JaggaerRoleLinkMappingService.class);
+        List<String> roles = List.of(JAGGAER_ROLE_HAS, JAGGAER_ROLE_DOES_NOT_HAVE, 
+                JaggaerConstants.JAGGAER_ROLE_NAME_VIEW_ONLY, CUPurapConstants.SCIQUEST_ROLE_BUYER,
+                CUPurapConstants.SCIQUEST_ROLE_SHOPPER, CUPurapConstants.SCIQUEST_ROLE_OFFICE,
+                CUPurapConstants.SCIQUEST_ROLE_LAB, CUPurapConstants.SCIQUEST_ROLE_FACILITIES,
+                CUPurapConstants.SCIQUEST_ROLE_UNRESTRICTED);
+        Mockito.when(service.getJaggaerLinkRoles(Mockito.any())).thenReturn(buildLinkRoleMappingCollection(roles));
+        return service;
+    }
+    
+    private Collection<JaggaerRoleLinkMapping> buildLinkRoleMappingCollection(List<String> roles) {
+        Collection<JaggaerRoleLinkMapping> links = new ArrayList<>();
+        for (String role : roles) {
+            JaggaerRoleLinkMapping link = new JaggaerRoleLinkMapping();
+            link.setActive(true);
+            link.setJaggaerRoleName(role);
+            links.add(link);
+        }
+        return links;
     }
 
 }
