9d0589985b67fa0e0d0af053e3c2c0f783d0afc2
Jay Hulslander <jdh34@cornell.edu>
2022-08-18 10:20:38 -0400

    move constants

--- src/main/java/edu/cornell/kfs/module/purap/batch/dataaccess/impl/JaggaerUploadDaoJdbc.java
+++ src/main/java/edu/cornell/kfs/module/purap/batch/dataaccess/impl/JaggaerUploadDaoJdbc.java
@@ -16,30 +16,12 @@ import edu.cornell.kfs.module.purap.CUPurapConstants.JaggaerLegalStructure;
 import edu.cornell.kfs.module.purap.batch.dataaccess.JaggaerUploadDao;
 import edu.cornell.kfs.module.purap.businessobject.lookup.JaggaerContractAddressUploadDto;
 import edu.cornell.kfs.module.purap.businessobject.lookup.JaggaerContractPartyUploadDto;
-import edu.cornell.kfs.pmw.batch.PaymentWorksConstants;
+import edu.cornell.kfs.vnd.CUVendorConstants.FIELD_NAMES;
 
 import org.springframework.jdbc.core.RowMapper;
 
 public class JaggaerUploadDaoJdbc extends PlatformAwareDaoBaseJdbc implements JaggaerUploadDao {
     private static final Logger LOG = LogManager.getLogger();
-    
-    private static final String VNDR_HDR_GNRTD_ID = "VNDR_HDR_GNRTD_ID";
-    private static final String VNDR_DTL_ASND_ID = "VNDR_DTL_ASND_ID";
-    private static final String VNDR_NM = "VNDR_NM";
-    private static final String VNDR_CORP_CTZN_CNTRY_CD = "VNDR_CORP_CTZN_CNTRY_CD";
-    private static final String VNDR_OWNR_CD = "VNDR_OWNR_CD";
-    private static final String VNDR_URL_ADDR = "VNDR_URL_ADDR";
-    
-    private static final String VNDR_ADDR_GNRTD_ID = "VNDR_ADDR_GNRTD_ID";
-    private static final String VNDR_ADDR_TYP_CD = "VNDR_ADDR_TYP_CD";
-    private static final String VNDR_DFLT_ADDR_IND = "VNDR_DFLT_ADDR_IND";
-    private static final String VNDR_CNTRY_CD = "VNDR_CNTRY_CD";
-    private static final String VNDR_LN1_ADDR = "VNDR_LN1_ADDR";
-    private static final String VNDR_LN2_ADDR = "VNDR_LN2_ADDR";
-    private static final String VNDR_CTY_NM = "VNDR_CTY_NM";
-    private static final String VNDR_ST_CD = "VNDR_ST_CD";
-    private static final String VNDR_ADDR_INTL_PROV_NM = "VNDR_ADDR_INTL_PROV_NM";
-    private static final String VNDR_ZIP_CD = "VNDR_ZIP_CD";
 
     @Override
     public List<JaggaerContractPartyUploadDto> findJaggaerContractParty(JaggaerContractUploadProcessingMode processingMode, String processingDate) {
@@ -48,20 +30,20 @@ public class JaggaerUploadDaoJdbc extends PlatformAwareDaoBaseJdbc implements Ja
                 JaggaerContractPartyUploadDto dto = new JaggaerContractPartyUploadDto();
                 dto.setRowType(JaggaerContractPartyUploadRowType.PARTY);
                 dto.setOverrideDupError(false);
-                dto.setERPNumber(buildVendorNumber(resultSet.getString(VNDR_HDR_GNRTD_ID), resultSet.getString(VNDR_DTL_ASND_ID)));
+                dto.setERPNumber(buildVendorNumber(resultSet.getString(FIELD_NAMES.VNDR_HDR_GNRTD_ID), resultSet.getString(FIELD_NAMES.VNDR_DTL_ASND_ID)));
                 dto.setSciQuestID(StringUtils.EMPTY);
-                dto.setContractPartyName(resultSet.getString(VNDR_NM));
+                dto.setContractPartyName(resultSet.getString(FIELD_NAMES.VNDR_NM));
                 dto.setDoingBusinessAs(StringUtils.EMPTY);
                 dto.setOtherNames(StringUtils.EMPTY);
-                dto.setCountryOfOrigin(convertToISOCountry(resultSet.getString(VNDR_CORP_CTZN_CNTRY_CD)));
+                dto.setCountryOfOrigin(convertToISOCountry(resultSet.getString(FIELD_NAMES.VNDR_CORP_CTZN_CNTRY_CD)));
                 dto.setActive(StringUtils.EMPTY);
                 dto.setContractPartyType(JaggaerContractPartyType.SUPPLIER);
                 dto.setPprimary(StringUtils.EMPTY);
-                dto.setLegalStructure(JaggaerLegalStructure.findJaggaerLegalStructureByKFSOwnershipCode(resultSet.getString(VNDR_OWNR_CD)));
+                dto.setLegalStructure(JaggaerLegalStructure.findJaggaerLegalStructureByKFSOwnershipCode(resultSet.getString(FIELD_NAMES.VNDR_OWNR_CD)));
                 dto.setTaxIDType(StringUtils.EMPTY);
                 dto.setTaxIdentificationNumber(StringUtils.EMPTY);
                 dto.setVATRegistrationNumber(StringUtils.EMPTY);
-                dto.setWebsiteURL(resultSet.getString(VNDR_URL_ADDR));
+                dto.setWebsiteURL(resultSet.getString(FIELD_NAMES.VNDR_URL_ADDR));
                 return dto;
             };
             return this.getJdbcTemplate().query(buildVendorSql(processingMode, processingDate), rowMapper);
@@ -77,23 +59,30 @@ public class JaggaerUploadDaoJdbc extends PlatformAwareDaoBaseJdbc implements Ja
             RowMapper<JaggaerContractAddressUploadDto> rowMapper = (resultSet, rowNumber) -> {
                 JaggaerContractAddressUploadDto dto = new JaggaerContractAddressUploadDto();
                 dto.setRowType(JaggaerContractPartyUploadRowType.ADDRESS);
-                dto.setAddressID(resultSet.getString(VNDR_ADDR_GNRTD_ID));
+                dto.setAddressID(resultSet.getString(FIELD_NAMES.VNDR_ADDR_GNRTD_ID));
                 dto.setSciQuestID(StringUtils.EMPTY);
-                dto.setAddressType(JaggaerAddressType.findJaggaerAddressTypeFromKfsAddressTypeCode(resultSet.getString(VNDR_ADDR_TYP_CD)));
-                dto.setPrimaryType(resultSet.getString(VNDR_DFLT_ADDR_IND));
+                dto.setAddressType(JaggaerAddressType.findJaggaerAddressTypeFromKfsAddressTypeCode(resultSet.getString(FIELD_NAMES.VNDR_ADDR_TYP_CD)));
+                dto.setPrimaryType(resultSet.getString(FIELD_NAMES.VNDR_DFLT_ADDR_IND));
                 dto.setActive(StringUtils.EMPTY);
-                dto.setCountry(convertToISOCountry(resultSet.getString(VNDR_CNTRY_CD)));
-                dto.setStreetLine1(resultSet.getString(VNDR_LN1_ADDR));
-                dto.setStreetLine2(resultSet.getString(VNDR_LN2_ADDR));
+                dto.setCountry(convertToISOCountry(resultSet.getString(FIELD_NAMES.VNDR_CNTRY_CD)));
+                dto.setStreetLine1(resultSet.getString(FIELD_NAMES.VNDR_LN1_ADDR));
+                dto.setStreetLine2(resultSet.getString(FIELD_NAMES.VNDR_LN2_ADDR));
                 dto.setStreetLine3(StringUtils.EMPTY);
-                dto.setCity(resultSet.getString(VNDR_CTY_NM));
-                dto.setState(resultSet.getString(VNDR_ST_CD));
-                dto.setPostalCode(resultSet.getString(VNDR_ZIP_CD));
+                dto.setCity(resultSet.getString(FIELD_NAMES.VNDR_CTY_NM));
+                
+                String state = resultSet.getString(FIELD_NAMES.VNDR_ST_CD);
+                if (StringUtils.isBlank(state)) {
+                    state = resultSet.getString(FIELD_NAMES.VNDR_ADDR_INTL_PROV_NM);
+                    LOG.info("findJaggaerContractAddress, found an international province (REMOVE this log statement): " + state);
+                }
+                dto.setState(state);
+                
+                dto.setPostalCode(resultSet.getString(FIELD_NAMES.VNDR_ZIP_CD));
                 dto.setPhone(StringUtils.EMPTY);
                 dto.setTollFreeNumber(StringUtils.EMPTY);
                 dto.setFax(StringUtils.EMPTY);
                 dto.setNotes(StringUtils.EMPTY);
-                dto.setERPNumber(buildVendorNumber(resultSet.getString(VNDR_HDR_GNRTD_ID), resultSet.getString(VNDR_DTL_ASND_ID)));
+                dto.setERPNumber(buildVendorNumber(resultSet.getString(FIELD_NAMES.VNDR_HDR_GNRTD_ID), resultSet.getString(FIELD_NAMES.VNDR_DTL_ASND_ID)));
                 
                 return dto;
             };
@@ -162,7 +151,7 @@ public class JaggaerUploadDaoJdbc extends PlatformAwareDaoBaseJdbc implements Ja
     private String convertToISOCountry(String fIPSCountry) {
         if (StringUtils.isBlank(fIPSCountry)) {
             LOG.debug("convertToISOCountry, empty FIPS country found returning US as default");
-            fIPSCountry = PaymentWorksConstants.PaymentWorksPurchaseOrderCountryFipsOption.UNITED_STATES.fipsCountryCode;
+            fIPSCountry = "US";
         }
         /*
          * @TODO use generic mapping framework
