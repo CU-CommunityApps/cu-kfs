e1149c2605a2d256e61ff1a21285f5e4fd112228
Jay Hulslander <jdh34@cornell.edu>
2022-09-15 15:50:41 -0400

    remove un needed change from cukfsconstants

--- src/main/java/edu/cornell/kfs/sys/CUKFSConstants.java
+++ src/main/java/edu/cornell/kfs/sys/CUKFSConstants.java
@@ -209,5 +209,5 @@ public class CUKFSConstants {
     //** CU Generic ISO-FIPS Country modification items **
     public final static boolean MAPPING_ACTIVE = true;
     public final static boolean MAPPING_INACTIVE = false;
-    
+
 }
