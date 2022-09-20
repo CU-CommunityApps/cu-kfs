bcdf6c0116075cee074694fa9358c2c70cc3c1c4
Daniela Balmus <db483@cornell.edu>
2022-09-20 12:59:42 +0100

    KFSPTS-25320 comment code not working

--- src/test/java/edu/cornell/kfs/sys/web/mock/MockRemoteServerExtension.java
+++ src/test/java/edu/cornell/kfs/sys/web/mock/MockRemoteServerExtension.java
@@ -9,30 +9,32 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.http.HttpHost;
 
-public class MockRemoteServerExtension implements Closeable {
+public class MockRemoteServerExtension {
 
-   // private LocalServer localServer;
-    private Optional<String> serverUrl;
-
-    public MockRemoteServerExtension() {
+/* This is not working after the http client library version upgrade with 11/17/21. This will be fixed in a separate user story */
+// implements Closeable {
+//    private LocalServer localServer;
+//    private Optional<String> serverUrl;
+//
+//    public MockRemoteServerExtension() {
 //        this.localServer = new LocalServer();
 //        this.serverUrl = Optional.empty();
-    }
-
-    public void initialize(MockServiceEndpointBase... endpoints) throws Exception {
-        //initialize(Arrays.asList(endpoints));
-    }
-
-    public void initialize(List<? extends MockServiceEndpointBase> endpoints) throws Exception {
+//    }
+//
+//    public void initialize(MockServiceEndpointBase... endpoints) throws Exception {
+//        initialize(Arrays.asList(endpoints));
+//    }
+//
+//    public void initialize(List<? extends MockServiceEndpointBase> endpoints) throws Exception {
 //        String baseUrl = localServer.configureAndLaunchServer(endpoints);
 //        serverUrl = Optional.of(baseUrl);
 //        for (MockServiceEndpointBase endpoint : endpoints) {
 //            endpoint.onServerInitialized(baseUrl);
 //        }
-    }
-
-    @Override
-    public void close() throws IOException {
+//    }
+//
+//    @Override
+//    public void close() throws IOException {
 //        try {
 //            localServer.shutDown();
 //        } catch (Exception e) {
@@ -41,12 +43,12 @@ public class MockRemoteServerExtension implements Closeable {
 //            localServer = null;
 //            serverUrl = null;
 //        }
-    }
-
-    public Optional<String> getServerUrl() {
-        return serverUrl;
-    }
-
+//    }
+//
+//    public Optional<String> getServerUrl() {
+//        return serverUrl;
+//    }
+//
 //    private static class LocalServer extends LocalServerTestBase {
 //        public String configureAndLaunchServer(List<? extends MockServiceEndpointBase> endpoints) throws Exception {
 //            setUp();
