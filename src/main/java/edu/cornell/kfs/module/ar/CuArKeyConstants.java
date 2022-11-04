e9de6ffc3ac35776aba0ffb2db49d47949aae4cd
nkimble <nkk4@cornell.edu>
2022-11-04 11:41:28 -0400

    KFSPTS-26393: Fix Contracts Grants Invoice Documents Job so that it presents an error when there is an invalid customer address instead of failing the whole job. (#1350)

---
+++ src/main/java/edu/cornell/kfs/module/ar/CuArKeyConstants.java
@@ -0,0 +1,7 @@
+package edu.cornell.kfs.module.ar;
+
+public final class CuArKeyConstants {
+    public static final String CGINVOICE_CREATION_AGENCY_CUSTOMER_MISMATCH = "cginvoice.creation.agency.customer.mismatch";
+    public static final String CGINVOICE_CREATION_CUSTOMER_INVOICING_ADDRESS_MISSING = "cginvoice.creation.customer.invoicing.address.missing";
+   
+}
