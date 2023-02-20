0652a1bedf366499a0d06388aef01a1f76d4bfdb
Jay Hulslander <jdh34@cornell.edu>
2023-02-20 14:54:49 -0500

    additional unit testing

--- src/test/java/edu/cornell/kfs/module/purap/jaggaer/xml/SupplierSyncMessageTest.java
+++ src/test/java/edu/cornell/kfs/module/purap/jaggaer/xml/SupplierSyncMessageTest.java
@@ -36,6 +36,32 @@ class SupplierSyncMessageTest {
 
     @Test
     void test() throws JAXBException, IOException {
+        supplierSyncMessage.setVersion("1.0");
+        Header header = new Header();
+        Authentication auth = new Authentication();
+        auth.setIdentity("Cornell");
+        auth.setSharedSecret("SuperCoolPassword");
+        header.setAuthentication(auth);
+        header.setMessageId("message id");
+        header.setRelatedMessageId("related id");
+        header.setTimestamp("20210218");
+        supplierSyncMessage.setHeader(header);
+        
+        SupplierRequestMessage srm = new SupplierRequestMessage();
+        Supplier supplier = new Supplier();
+        Name name = new Name();
+        name.setvalue("Acme Test Company");
+        supplier.setName(name);
+        srm.getSupplier().add(supplier);
+        
+        Supplier supplier2 = new Supplier();
+        Name name2 = new Name();
+        name2.setvalue("Acme Test Company oart 2");
+        supplier2.setName(name);
+        srm.getSupplier().add(supplier2);
+        
+        supplierSyncMessage.getSupplierRequestMessageOrSupplierResponseMessageOrLookupRequestMessageOrLookupResponseMessage().add(srm);
+        
         String actualResults  = marshalService.marshalObjectToXmlString(supplierSyncMessage);
         LOG.info("actualResults: " + actualResults);
     }
