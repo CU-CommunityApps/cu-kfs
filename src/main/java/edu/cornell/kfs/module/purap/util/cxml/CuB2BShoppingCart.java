3f622a0b1d7ba29a00887b1368e51aefcc09c5c8
Daniela Balmus <db483@cornell.edu>
2023-02-21 10:17:45 +0000

    KFSPTS-27392 jaxb updates

--- src/main/java/edu/cornell/kfs/module/purap/util/cxml/CuB2BShoppingCart.java
+++ src/main/java/edu/cornell/kfs/module/purap/util/cxml/CuB2BShoppingCart.java
@@ -2,8 +2,19 @@ package edu.cornell.kfs.module.purap.util.cxml;
 
 import org.kuali.kfs.module.purap.util.cxml.B2BShoppingCart;
 
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlTransient;
+import jakarta.xml.bind.annotation.XmlType;
+
+import org.kuali.kfs.core.api.impex.xml.XmlConstants;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "cXML", namespace = XmlConstants.B2B_PUNCH_OUT_ORDER_NAMESPACE)
 public class CuB2BShoppingCart extends B2BShoppingCart {
 
+    @XmlTransient
 	private String businessPurpose;
 
 	/**
