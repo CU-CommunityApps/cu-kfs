3edc00d17519cb69ccb6cb56a02181f5fe8a3b40
Andrew Dickson <ajd299@cornell.edu>
2023-05-02 16:33:15 -0400

    Restore newlines

--- src/main/java/edu/cornell/kfs/module/purap/CUPurapKeyConstants.java
+++ src/main/java/edu/cornell/kfs/module/purap/CUPurapKeyConstants.java
@@ -28,8 +28,10 @@ public class CUPurapKeyConstants {
     
     // KFSPTS-2096
     public static final String PURAP_MIX_ITEM_QTY_NONQTY = "error.purap.mix.item.itemtype.nonqty";
+
   //KFSPTS-2527
     public static final String ERROR_DV_OR_REQ_ALREADY_CREATED_FROM_IWNT = "error.document.iwnt.dvOrReqAlreadyCreated";
+
     // KFSUPGRADE-779
     public static final String ERROR_PAYMENTMETHODCODE_MUSTMATCHPREQ = "error.document.creditmemo.paymentmethodcode.mustmatchpreq";
     
