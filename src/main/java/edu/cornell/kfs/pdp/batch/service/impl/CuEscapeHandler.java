1dcaae0d52e101750d7daa1106dd122461e1b29d
cah292 <cah292@cornell.edu>
2023-08-18 16:06:30 -0400

    KFSPTS-26965 Start of ISO format/escape fixes

---
+++ src/main/java/edu/cornell/kfs/pdp/batch/service/impl/CuEscapeHandler.java
@@ -0,0 +1,28 @@
+package edu.cornell.kfs.pdp.batch.service.impl;
+
+import org.kuali.kfs.krad.util.KRADConstants;
+
+import com.prowidesoftware.swift.model.mx.DefaultEscapeHandler;
+
+import edu.cornell.kfs.sys.CUKFSConstants;
+
+public class CuEscapeHandler extends DefaultEscapeHandler {
+
+    private static final String ESCAPED_APOSTROPHE = "&apps;";
+    private static final String ESCAPED_QUOTE = "&quot;";
+
+    @Override
+    public String escape(char[] arr, boolean isAttribute) {
+        StringBuilder result = new StringBuilder(super.escape(arr, isAttribute));
+        replace(result, KRADConstants.SINGLE_QUOTE, ESCAPED_APOSTROPHE);
+        replace(result, CUKFSConstants.DOUBLE_QUOTE, ESCAPED_QUOTE);
+        return result.toString();
+    }
+
+    private void replace(StringBuilder source, String searchValue, String replacementValue) {
+        for (int valIndex = source.indexOf(searchValue); valIndex >= 0; valIndex = source.indexOf(searchValue)) {
+            source.replace(valIndex, valIndex + 1, replacementValue);
+        }
+    }
+
+}
