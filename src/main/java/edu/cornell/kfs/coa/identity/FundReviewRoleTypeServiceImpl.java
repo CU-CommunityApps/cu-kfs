91874d596107e967b003752f0c9d403b95f840e3
Daniela Balmus <db483@cornell.edu>
2023-09-01 15:56:43 +0100

    KFSPTS-29368 add Fund Review node

---
+++ src/main/java/edu/cornell/kfs/coa/identity/FundReviewRoleTypeServiceImpl.java
@@ -0,0 +1,37 @@
+package edu.cornell.kfs.coa.identity;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.kuali.kfs.kim.api.KimConstants;
+import org.kuali.kfs.kim.bo.impl.KimAttributes;
+import org.kuali.kfs.kim.util.KimCommonUtils;
+import org.kuali.kfs.kns.kim.role.RoleTypeServiceBase;
+
+public class FundReviewRoleTypeServiceImpl extends RoleTypeServiceBase {
+    
+    @Override
+    protected boolean performMatch(final Map<String, String> qualification, final Map<String, String> roleQualifier) {
+        if (KimCommonUtils.storedValueNotSpecifiedOrInputValueMatches(roleQualifier, qualification,
+                KimAttributes.FUND_GROUP_CODE)) {
+            final Set<String> potentialParentDocumentTypeNames = new HashSet<>(1);
+            if (roleQualifier.containsKey(KimConstants.AttributeConstants.DOCUMENT_TYPE_NAME)) {
+                potentialParentDocumentTypeNames
+                        .add(roleQualifier.get(KimConstants.AttributeConstants.DOCUMENT_TYPE_NAME));
+            }
+            if (qualification == null || qualification.isEmpty()) {
+                return potentialParentDocumentTypeNames.isEmpty();
+            }
+            return potentialParentDocumentTypeNames.isEmpty()
+                   || qualification.get(KimConstants.AttributeConstants.DOCUMENT_TYPE_NAME).equalsIgnoreCase(
+                            roleQualifier.get(KimConstants.AttributeConstants.DOCUMENT_TYPE_NAME))
+                   || getClosestParentDocumentTypeName(documentTypeService
+                           .getDocumentTypeByName(qualification
+                                   .get(KimConstants.AttributeConstants.DOCUMENT_TYPE_NAME)),
+                                   potentialParentDocumentTypeNames) != null;
+        }
+        return false;
+    }
+
+}
