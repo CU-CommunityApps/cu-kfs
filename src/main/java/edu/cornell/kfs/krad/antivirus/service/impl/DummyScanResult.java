7ad9eeb9369f396d7bd2b81987849b19f7d2563d
Jay Hulslander <jdh34@cornell.edu>
2023-10-12 14:45:22 -0400

    un do dummy results changes

--- src/main/java/edu/cornell/kfs/krad/antivirus/service/impl/DummyScanResult.java
+++ src/main/java/edu/cornell/kfs/krad/antivirus/service/impl/DummyScanResult.java
@@ -3,17 +3,15 @@ package edu.cornell.kfs.krad.antivirus.service.impl;
 import edu.cornell.kfs.krad.antivirus.service.ScanResult;
 
 public class DummyScanResult implements ScanResult {
-    
-    private static final boolean PASS_ANTIVIRUS = true;
 
     @Override
     public String getResult() {
-        return PASS_ANTIVIRUS ? ScanResult.Status.PASSED.toString() : ScanResult.Status.FAILED.toString();
+        return ScanResult.Status.PASSED.toString();
     }
 
     @Override
     public Status getStatus() {
-        return PASS_ANTIVIRUS ? ScanResult.Status.PASSED : ScanResult.Status.FAILED;
+        return ScanResult.Status.PASSED;
     }
 
     @Override
