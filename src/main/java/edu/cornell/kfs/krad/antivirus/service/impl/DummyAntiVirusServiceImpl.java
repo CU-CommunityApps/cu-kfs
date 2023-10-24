d876110f6f896fa61e1f59077cc8e2fcdd01df72
Jay Hulslander <jdh34@cornell.edu>
2023-10-24 15:29:18 -0400

    add helpful logging

--- src/main/java/edu/cornell/kfs/krad/antivirus/service/impl/DummyAntiVirusServiceImpl.java
+++ src/main/java/edu/cornell/kfs/krad/antivirus/service/impl/DummyAntiVirusServiceImpl.java
@@ -3,12 +3,15 @@ package edu.cornell.kfs.krad.antivirus.service.impl;
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import edu.cornell.kfs.krad.antivirus.service.ScanResult;
 import edu.cornell.kfs.krad.antivirus.service.ScanResult.Status;
 import edu.cornell.kfs.krad.antivirus.service.AntiVirusService;
 
 public class DummyAntiVirusServiceImpl implements AntiVirusService {
-    
+    private static final Logger LOG = LogManager.getLogger();
     private Status scanResultStatus;
     
     public DummyAntiVirusServiceImpl() {
@@ -17,6 +20,7 @@ public class DummyAntiVirusServiceImpl implements AntiVirusService {
     
     public DummyAntiVirusServiceImpl(Status scanResultStatus) {
         this.scanResultStatus = scanResultStatus;
+        LOG.debug("DummyAntiVirusServiceImpl, create with virus scan status of {}", scanResultStatus);
     }
 
     @Override
