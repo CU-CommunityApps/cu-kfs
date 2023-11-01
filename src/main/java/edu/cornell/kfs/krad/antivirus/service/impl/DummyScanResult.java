57fa2973f3b0c7eb0e2009cd4c15755475ffe719
Jay Hulslander <jdh34@cornell.edu>
2023-11-01 11:19:21 -0400

    KFSPTS-29871 update DummyAntiVirusServiceImpl configuration (#1515)

--- src/main/java/edu/cornell/kfs/krad/antivirus/service/impl/DummyScanResult.java
+++ src/main/java/edu/cornell/kfs/krad/antivirus/service/impl/DummyScanResult.java
@@ -3,15 +3,21 @@ package edu.cornell.kfs.krad.antivirus.service.impl;
 import edu.cornell.kfs.krad.antivirus.service.ScanResult;
 
 public class DummyScanResult implements ScanResult {
+    
+    private Status scanResultStatus;
+    
+    public DummyScanResult(Status scanResultStatus) {
+        this.scanResultStatus = scanResultStatus;
+    }
 
     @Override
     public String getResult() {
-        return ScanResult.Status.PASSED.toString();
+        return scanResultStatus.toString();
     }
 
     @Override
     public Status getStatus() {
-        return ScanResult.Status.PASSED;
+        return scanResultStatus;
     }
 
     @Override
