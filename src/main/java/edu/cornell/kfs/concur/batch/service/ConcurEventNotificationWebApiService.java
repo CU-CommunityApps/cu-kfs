b43d13c32f5ac71da06a99b937c588dc8af68212
Jay Hulslander <jdh34@cornell.edu>
2023-11-27 13:19:36 -0500

    undo some stuff

--- src/main/java/edu/cornell/kfs/concur/batch/service/ConcurEventNotificationWebApiService.java
+++ src/main/java/edu/cornell/kfs/concur/batch/service/ConcurEventNotificationWebApiService.java
@@ -1,15 +1,14 @@
 package edu.cornell.kfs.concur.batch.service;
 
 import edu.cornell.kfs.concur.batch.ConcurWebRequest;
-import edu.cornell.kfs.concur.exception.ConcurWebserviceException;
 
 public interface ConcurEventNotificationWebApiService {
     
     /*
      * Builds a JSON annotated java POJO from a concur web service endpoint
      */
-    <T> T buildConcurDTOFromEndpoint(String accessToken, String concurEndPoint, Class<T> dtoType, String logMessageDetail) throws ConcurWebserviceException;
+    <T> T buildConcurDTOFromEndpoint(String accessToken, String concurEndPoint, Class<T> dtoType, String logMessageDetail);
 
-    <T> T callConcurEndpoint(String accessToken, ConcurWebRequest<T> webRequest, String logMessageDetail) throws ConcurWebserviceException;
+    <T> T callConcurEndpoint(String accessToken, ConcurWebRequest<T> webRequest, String logMessageDetail);
 
 }
