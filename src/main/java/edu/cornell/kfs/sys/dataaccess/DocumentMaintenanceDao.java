24739b4da8a2d5073967315b7525774fd70d3aec
Andrew Dickson <ajd299@cornell.edu>
2023-11-29 16:03:51 -0500

    KFSPTS-27640 Remove DocumentRequeuerJob

--- src/main/java/edu/cornell/kfs/sys/dataaccess/DocumentMaintenanceDao.java
+++ src/main/java/edu/cornell/kfs/sys/dataaccess/DocumentMaintenanceDao.java
@@ -1,22 +1,8 @@
 package edu.cornell.kfs.sys.dataaccess;
 
-import java.util.Collection;
 import java.util.List;
 
 public interface DocumentMaintenanceDao {
-
-	/**
-	 * Obtains and returns a list of documentIds for documents to requeue.
-	 *
-	 * @return list of documentIds
-	 */
-	Collection<String> getDocumentRequeueValues();
-	
-	/**
-	 * Finds the actions list notes for the documents that will be re-queued.
-	 * @return
-	 */
-	List<ActionItemNoteDetailDto> getActionNotesToBeRequeued();
 
 	/**
 	 * Finds the action list notes for a specific document that will be re-queued.
