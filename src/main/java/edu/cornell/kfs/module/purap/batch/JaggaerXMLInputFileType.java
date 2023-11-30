1540e95629777e9a82a03eacac0af189fd667d58
Andrew Dickson <ajd299@cornell.edu>
2023-11-30 16:41:30 -0500

    Create CuXmlBatchInputFileTypeBase

--- src/main/java/edu/cornell/kfs/module/purap/batch/JaggaerXMLInputFileType.java
+++ src/main/java/edu/cornell/kfs/module/purap/batch/JaggaerXMLInputFileType.java
@@ -3,19 +3,9 @@ package edu.cornell.kfs.module.purap.batch;
 import edu.cornell.kfs.module.purap.CUPurapConstants;
 import edu.cornell.kfs.module.purap.CUPurapKeyConstants;
 import edu.cornell.kfs.module.purap.jaggaer.supplier.xml.SupplierSyncMessage;
-import org.apache.commons.lang.StringUtils;
-import org.kuali.kfs.core.api.datetime.DateTimeService;
-import org.kuali.kfs.sys.KFSConstants;
-import org.kuali.kfs.sys.batch.XmlBatchInputFileTypeBase;
+import edu.cornell.kfs.sys.batch.CuXmlBatchInputFileTypeBase;
 
-import java.io.File;
-
-import static org.kuali.kfs.pdp.PdpConstants.FILE_NAME_PART_DELIMITER;
-
-public class JaggaerXMLInputFileType extends XmlBatchInputFileTypeBase<SupplierSyncMessage> {
-
-    protected String fileNamePrefix;
-    protected DateTimeService dateTimeService;
+public class JaggaerXMLInputFileType extends CuXmlBatchInputFileTypeBase<SupplierSyncMessage> {
 
     @Override
     public String getFileTypeIdentifier() {
@@ -30,45 +20,6 @@ public class JaggaerXMLInputFileType extends XmlBatchInputFileTypeBase<SupplierS
     @Override
     public String getTitleKey() {
         return CUPurapKeyConstants.MESSAGE_BATCH_UPLOAD_TITLE_JAGGAER;
-    }
-
-    @Override
-    public String getAuthorPrincipalName(final File file) {
-        String filename = file.getName();
-        if (StringUtils.isBlank(filename) || !filename.contains(FILE_NAME_PART_DELIMITER) || !filename.contains(fileNamePrefix)) {
-            return StringUtils.EMPTY;
-        }
-
-        return StringUtils.substringBetween(filename, fileNamePrefix, FILE_NAME_PART_DELIMITER);
-    }
-
-    @Override
-    public String getFileName(String principalName, Object parsedFileContents, String fileUserIdentifier) {
-        String fileName = new StringBuilder()
-                .append(fileNamePrefix)
-                .append(principalName)
-                .append(StringUtils.isBlank(fileUserIdentifier) ? StringUtils.EMPTY : FILE_NAME_PART_DELIMITER + fileUserIdentifier)
-                .append(FILE_NAME_PART_DELIMITER)
-                .append(dateTimeService.toDateTimeStringForFilename(dateTimeService.getCurrentDate()))
-                .toString();
-
-        return StringUtils.remove(fileName, KFSConstants.BLANK_SPACE);
-    }
-
-    public String getFileNamePrefix() {
-        return fileNamePrefix;
-    }
-
-    public void setFileNamePrefix(String fileNamePrefix) {
-        this.fileNamePrefix = fileNamePrefix;
-    }
-
-    public DateTimeService getDateTimeService() {
-        return dateTimeService;
-    }
-
-    public void setDateTimeService(DateTimeService dateTimeService) {
-        this.dateTimeService = dateTimeService;
     }
 
 }
