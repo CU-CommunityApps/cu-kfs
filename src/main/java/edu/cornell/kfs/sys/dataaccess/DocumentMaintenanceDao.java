84d76bcedd2806c7dd9a33c135317f60f487ed91
cah292 <cah292@users.noreply.github.com>
2023-12-06 09:58:21 -0500

    KFSPTS-30352 Reinstate action note restoration feature (#1527)

---
+++ src/main/java/edu/cornell/kfs/sys/dataaccess/DocumentMaintenanceDao.java
@@ -0,0 +1,12 @@
+package edu.cornell.kfs.sys.dataaccess;
+
+import java.util.List;
+
+public interface DocumentMaintenanceDao {
+
+    /**
+     * Finds the action list notes for a specific document that will be re-queued.
+     */
+    List<ActionItemNoteDetailDto> getActionNotesToBeRequeuedForDocument(String documentId);
+
+}
