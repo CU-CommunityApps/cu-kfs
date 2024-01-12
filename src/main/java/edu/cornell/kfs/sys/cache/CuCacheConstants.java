fb1131ccc711338b268efbd05cd3dc89cf6e9797
Jay Hulslander <jdh34@cornell.edu>
2024-01-12 13:15:08 -0500

    KFSPTS-30483 remove ehCache - Redis customiztion

--- src/main/java/edu/cornell/kfs/sys/cache/CuCacheConstants.java
+++
@@ -1,9 +0,0 @@
-package edu.cornell.kfs.sys.cache;
-
-public class CuCacheConstants {
-
-    public static final String REDIS_KEY_PREFIX_START_CHARS = "<<";
-    public static final String REDIS_KEY_PREFIX_END_CHARS = ">>|";
-    public static final String REDIS_MESSAGE_TYPE_INVALIDATE = "invalidate";
-
-}
