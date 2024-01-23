d433864d210151c527a5a54fedd8de8c364dad72
Jay Hulslander <jdh34@cornell.edu>
2024-01-23 15:48:46 -0500

    KFSPTS-30483 remove ehCache - Redis customization (#1541)

--- src/main/java/org/kuali/kfs/sys/cache/CacheConfiguration.java
+++ src/main/java/org/kuali/kfs/sys/cache/CacheConfiguration.java
@@ -18,12 +18,6 @@
  */
 package org.kuali.kfs.sys.cache;
 
-import static java.util.Map.entry;
-
-import java.nio.charset.StandardCharsets;
-import java.util.Map;
-import java.util.Set;
-
 import org.kuali.kfs.coa.businessobject.Account;
 import org.kuali.kfs.coa.businessobject.AccountingPeriod;
 import org.kuali.kfs.coa.businessobject.BalanceType;
@@ -67,33 +61,32 @@ import org.kuali.kfs.sys.businessobject.UniversityDate;
 import org.kuali.kfs.sys.service.MenuService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.io.Resource;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 import java.time.Duration;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-import edu.cornell.kfs.sys.cache.ClientTrackingLettuceConnectionFactory;
-import edu.cornell.kfs.sys.cache.CuEhCacheCacheManager;
-import edu.cornell.kfs.sys.cache.EhcacheEventListenerForUpdatingRedis;
-import edu.cornell.kfs.sys.cache.RedisEventListenerLazyInitProxy;
+import static java.util.Map.entry;
 
-/*
- * Cornell Customizations:
- * -- Updated caching to use an Ehcache-and-Redis hybrid setup.
- */
 @Configuration
 @EnableCaching
 @Profile("cache-redis")
 public class CacheConfiguration {
-
+    
+    /*
+     * Cornell Customization 
+     * -- add more cache names 
+     */
     @Bean
     public Set<String> cacheNames() {
         return Set.of(
@@ -142,37 +135,8 @@ public class CacheConfiguration {
     }
 
     @Bean
-    public Set<String> cachesIgnoringRedisEvents() {
-        return Set.of(
-                BatchFile.CACHE_NAME
-        );
-    }
-
-    @Bean
-    public Set<String> cachesToClearOnRedisConnectionChange() {
-        return Set.of(
-                Parameter.CACHE_NAME,
-                Entity.CACHE_NAME,
-                EntityName.CACHE_NAME,
-                EntityPrivacyPreferences.CACHE_NAME,
-                Group.CACHE_NAME,
-                GroupMember.CACHE_NAME,
-                Principal.CACHE_NAME,
-                Permission.CACHE_NAME,
-                PermissionTemplate.CACHE_NAME,
-                Responsibility.CACHE_NAME,
-                ResponsibilityTemplate.CACHE_NAME,
-                Role.CACHE_NAME,
-                RoleMember.CACHE_NAME,
-                RoleMembership.CACHE_NAME,
-                RoleResponsibility.CACHE_NAME
-        );
-    }
-
-    @Bean
     public Map<String, Duration> cacheExpires() {
         // These caches have a TTL value different from the default specified in the redis.default.ttl property
-        // Cornell Note: We are not actively using this Map in our customized caching setup.
         return Map.ofEntries(
                 entry(Account.CACHE_NAME, Duration.ZERO),
                 entry(AccountingPeriod.CACHE_NAME, Duration.ZERO),
@@ -199,17 +163,11 @@ public class CacheConfiguration {
     }
 
     @Bean
-    public RedisEventListenerLazyInitProxy redisEventListener() {
-        return new RedisEventListenerLazyInitProxy();
-    }
-
-    @Bean
     public RedisConnectionFactory connectionFactory(
             @Value("${redis.auth.token.password}") final String redisAuthTokenPassword,
             @Value("${redis.host}") final String redisHost,
             @Value("${redis.port}") final int redisPort,
-            @Value("${redis.use.ssl}") final boolean redisUseSsl,
-            RedisEventListenerLazyInitProxy redisEventListener
+            @Value("${redis.use.ssl}") final boolean redisUseSsl
     ) {
         final RedisStandaloneConfiguration redisStandaloneConfiguration =
                 new RedisStandaloneConfiguration(redisHost, redisPort);
@@ -222,52 +180,35 @@ public class CacheConfiguration {
         }
         final LettuceClientConfiguration lettuceClientConfiguration = lettuceClientConfigurationBuilder.build();
 
-        final ClientTrackingLettuceConnectionFactory lettuceConnectionFactory =
-                new ClientTrackingLettuceConnectionFactory(redisStandaloneConfiguration, lettuceClientConfiguration,
-                        redisEventListener);
+        final LettuceConnectionFactory lettuceConnectionFactory =
+                new LettuceConnectionFactory(redisStandaloneConfiguration, lettuceClientConfiguration);
 
         return lettuceConnectionFactory;
     }
 
     @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
-        final RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(connectionFactory);
-        redisTemplate.setKeySerializer(new StringRedisSerializer(StandardCharsets.UTF_8));
-        redisTemplate.setValueSerializer(new StringRedisSerializer(StandardCharsets.UTF_8));
-        return redisTemplate;
-    }
-
-    @Bean
-    public EhcacheEventListenerForUpdatingRedis ehcacheEventListener(
-            RedisTemplate<String, String> redisTemplate
-    ) {
-        EhcacheEventListenerForUpdatingRedis ehcacheEventListener = new EhcacheEventListenerForUpdatingRedis(
-                redisTemplate);
-        return ehcacheEventListener;
-    }
-
-    @Bean
-    public CuEhCacheCacheManager cacheManager(
-            @Value("${kfs.ehcache.config.location}") Resource ehcacheConfigLocation,
+    public RedisCacheManager cacheManager(
             @Value("${redis.default.ttl}") final Long redisDefaultTtl,
-            RedisEventListenerLazyInitProxy redisEventListener,
-            EhcacheEventListenerForUpdatingRedis ehcacheEventListener
+            final Set<String> cacheNames,
+            final Map<String, Duration> cacheExpires,
+            final RedisConnectionFactory connectionFactory
     ) {
-        EhCacheManagerFactoryBean ehcacheManagerFactory = new EhCacheManagerFactoryBean();
-        ehcacheManagerFactory.setConfigLocation(ehcacheConfigLocation);
-        ehcacheManagerFactory.afterPropertiesSet();
-        
-        CuEhCacheCacheManager cacheManager = new CuEhCacheCacheManager();
-        cacheManager.setCacheManager(ehcacheManagerFactory.getObject());
-        cacheManager.setRedisEventListenerProxy(redisEventListener);
-        cacheManager.setEhcacheEventListener(ehcacheEventListener);
-        cacheManager.setCacheNames(cacheNames());
-        cacheManager.setCachesIgnoringRedisEvents(cachesIgnoringRedisEvents());
-        cacheManager.setCachesToClearOnRedisConnectionChange(cachesToClearOnRedisConnectionChange());
-        cacheManager.setDefaultTimeToLive(redisDefaultTtl);
-        
-        return cacheManager;
-    }
+        final RedisCacheConfiguration cacheDefaults = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(redisDefaultTtl))
+                .disableKeyPrefix();
 
+        final Map<String, RedisCacheConfiguration> cacheConfigurations = cacheExpires.keySet()
+                .stream()
+                .collect(Collectors.toMap(key -> key,
+                        key -> cacheDefaults.entryTtl(cacheExpires.get(key)),
+                        (configuration, cacheConfiguration) -> cacheConfiguration));
+
+        final RedisCacheManager redisCacheManager = RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(cacheDefaults)
+                .initialCacheNames(cacheNames)
+                .withInitialCacheConfigurations(cacheConfigurations)
+                .build();
+
+        return redisCacheManager;
+    }
 }
