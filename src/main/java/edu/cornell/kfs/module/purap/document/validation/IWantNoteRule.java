85f9b043480da62068bd3849d605397bca1131ea
Daniela Balmus <db483@cornell.edu>
2024-01-30 16:01:20 +0000

    KFSPTS-30235 fix check null

--- src/main/java/edu/cornell/kfs/module/purap/document/validation/IWantNoteRule.java
+++ src/main/java/edu/cornell/kfs/module/purap/document/validation/IWantNoteRule.java
@@ -3,6 +3,7 @@ package edu.cornell.kfs.module.purap.document.validation;
 import org.kuali.kfs.kns.document.MaintenanceDocument;
 import org.kuali.kfs.kns.maintenance.rules.MaintenanceDocumentRuleBase;
 import org.kuali.kfs.krad.service.SequenceAccessorService;
+import org.kuali.kfs.krad.util.ObjectUtils;
 import org.kuali.kfs.sys.context.SpringContext;
 
 import edu.cornell.kfs.module.purap.CUPurapConstants;
@@ -15,11 +16,13 @@ public class IWantNoteRule extends MaintenanceDocumentRuleBase {
         boolean isValid = super.processCustomRouteDocumentBusinessRules(document);
         if (isValid) {
             final IWantNote iWantNote = (IWantNote) document.getNewMaintainableObject().getBusinessObject();
-            final SequenceAccessorService sequenceAccessorService = SpringContext
-                    .getBean(SequenceAccessorService.class);
-            final Integer noteId = Integer.valueOf(String.valueOf(sequenceAccessorService
-                    .getNextAvailableSequenceNumber(CUPurapConstants.CU_PUR_IWNT_NTE_TXT_ID_SEQ)));
-            iWantNote.setNoteIdentifier(noteId);
+            if (ObjectUtils.isNull(iWantNote.getNoteIdentifier())) {
+                final SequenceAccessorService sequenceAccessorService = SpringContext
+                        .getBean(SequenceAccessorService.class);
+                final Integer noteId = Integer.valueOf(String.valueOf(sequenceAccessorService
+                        .getNextAvailableSequenceNumber(CUPurapConstants.CU_PUR_IWNT_NTE_TXT_ID_SEQ)));
+                iWantNote.setNoteIdentifier(noteId);
+            }
         }
         return isValid;
     }
