a3d79970e64cee233bf8abe1b6cacb4a809e5b22
cah292 <cah292@users.noreply.github.com>
2024-04-29 10:09:24 -0400

    KFSPTS-28426 Allow SEC view-doc overrides based on route log (#1589)

---
+++ src/main/java/edu/cornell/kfs/sec/CuSecParameterConstants.java
@@ -0,0 +1,8 @@
+package edu.cornell.kfs.sec;
+
+public final class CuSecParameterConstants {
+
+    public static final String DOCUMENT_TYPES_ALLOWING_VIEW_DOCUMENT_ACCESS_FOR_PRIOR_RECIPIENTS =
+            "DOCUMENT_TYPES_ALLOWING_VIEW_DOCUMENT_ACCESS_FOR_PRIOR_RECIPIENTS";
+
+}
