a42515549d3ecd9a039c53d9d8d1e7f8add713c4
Daniela Balmus <db483@cornell.edu>
2024-05-21 18:55:50 +0100

    KFSPTS-30888 add return to SSC functionality

--- src/main/java/edu/cornell/kfs/krad/service/impl/CuDocumentServiceImpl.java
+++ src/main/java/edu/cornell/kfs/krad/service/impl/CuDocumentServiceImpl.java
@@ -1,12 +1,26 @@
 package edu.cornell.kfs.krad.service.impl;
 
 import com.thoughtworks.xstream.core.BaseException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.kuali.kfs.core.framework.persistence.jta.TransactionalNoValidationExceptionRollback;
 import org.kuali.kfs.kew.api.WorkflowDocument;
+import org.kuali.kfs.kew.api.action.DocumentActionParameters;
+import org.kuali.kfs.kew.api.action.DocumentActionResult;
+import org.kuali.kfs.kew.api.action.ReturnPoint;
+import org.kuali.kfs.kew.api.action.WorkflowDocumentActionsService;
 import org.kuali.kfs.kns.document.MaintenanceDocument;
+import org.kuali.kfs.krad.UserSession;
+import org.kuali.kfs.krad.UserSessionUtils;
+import org.kuali.kfs.krad.bo.AdHocRoutePerson;
+import org.kuali.kfs.krad.bo.AdHocRouteWorkgroup;
+import org.kuali.kfs.krad.bo.Note;
 import org.kuali.kfs.krad.document.Document;
 import org.kuali.kfs.krad.service.MaintainableXMLConversionService;
 import org.kuali.kfs.krad.service.impl.DocumentServiceImpl;
+import org.kuali.kfs.krad.util.GlobalVariables;
 
 /**
  * Custom DocumentServiceImpl subclass that performs its own handling
@@ -24,6 +38,7 @@ import org.kuali.kfs.krad.service.impl.DocumentServiceImpl;
 public class CuDocumentServiceImpl extends DocumentServiceImpl {
 
     protected MaintainableXMLConversionService maintainableXMLConversionService;
+    private transient WorkflowDocumentActionsService workflowDocumentActionsService;
 
     /**
      * Overridden to perform just-in-time legacy maintenance XML conversion if necessary.
@@ -68,6 +83,34 @@ public class CuDocumentServiceImpl extends DocumentServiceImpl {
                 && document instanceof MaintenanceDocument
                 && BaseException.class.isAssignableFrom(e.getClass());
     }
+    
+    public Document returnDocumenToPreviousNode(Document document, String annotation, String nodeName) {
+        checkForNulls(document);
+
+        Note note = createNoteFromDocument(document, annotation);
+        document.addNote(note);
+        getNoteService().save(note);
+
+        getDocumentDao().save(document);
+        prepareWorkflowDocument(document);
+        document.getDocumentHeader().getWorkflowDocument().returnToPreviousNode(annotation, nodeName);
+        final UserSession userSession = GlobalVariables.getUserSession();
+        if (userSession != null) {
+            UserSessionUtils.addWorkflowDocument(userSession, document.getDocumentHeader().getWorkflowDocument());
+        }
+        removeAdHocPersonsAndWorkgroups(document);
+        return document;
+    }
+    
+    private void removeAdHocPersonsAndWorkgroups(final Document document) {
+        final List<AdHocRoutePerson> adHocRoutePersons = new ArrayList<>();
+        final List<AdHocRouteWorkgroup> adHocRouteWorkgroups = new ArrayList<>();
+        getBusinessObjectService().delete(document.getAdHocRoutePersons());
+        getBusinessObjectService().delete(document.getAdHocRouteWorkgroups());
+        document.setAdHocRoutePersons(adHocRoutePersons);
+        document.setAdHocRouteWorkgroups(adHocRouteWorkgroups);
+    }
+    
 
     public void setMaintainableXMLConversionService(final MaintainableXMLConversionService maintainableXMLConversionService) {
         this.maintainableXMLConversionService = maintainableXMLConversionService;
