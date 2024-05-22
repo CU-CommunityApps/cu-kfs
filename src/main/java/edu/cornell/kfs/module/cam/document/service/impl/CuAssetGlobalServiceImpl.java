64c5bf38c1dbd8256821a0f414a54c48745b47c0
Jay Hulslander <jdh34@cornell.edu>
2024-05-22 13:46:32 -0400

    KFSPTS-31812 fix asset global doc when asset representative is not set or is non Cornell person

--- src/main/java/edu/cornell/kfs/module/cam/document/service/impl/CuAssetGlobalServiceImpl.java
+++ src/main/java/edu/cornell/kfs/module/cam/document/service/impl/CuAssetGlobalServiceImpl.java
@@ -1,13 +1,19 @@
 package edu.cornell.kfs.module.cam.document.service.impl;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.kuali.kfs.krad.util.ObjectUtils;
+import org.kuali.kfs.module.cam.CamsConstants;
 import org.kuali.kfs.module.cam.businessobject.Asset;
 import org.kuali.kfs.module.cam.businessobject.AssetGlobal;
 import org.kuali.kfs.module.cam.businessobject.AssetGlobalDetail;
+import org.kuali.kfs.module.cam.businessobject.AssetLocation;
 import org.kuali.kfs.module.cam.document.service.impl.AssetGlobalServiceImpl;
 
 import edu.cornell.kfs.module.cam.businessobject.AssetExtension;
 
 public class CuAssetGlobalServiceImpl extends AssetGlobalServiceImpl {
+    private static final Logger LOG = LogManager.getLogger();
 
     // KFSUPGRADE-535
     // if we need to implement service rate ind, then it can be populated from detail to assetext too.
@@ -19,5 +25,41 @@ public class CuAssetGlobalServiceImpl extends AssetGlobalServiceImpl {
         return asset;
 
     }
+    
+    @Override
+    protected void setupAssetLocationOffCampus(final AssetGlobalDetail assetGlobalDetail, final Asset asset) {
+        // We are not checking if it already exists since on a new asset it can't
+        final AssetLocation offCampusAssetLocation = new AssetLocation();
+        offCampusAssetLocation.setCapitalAssetNumber(asset.getCapitalAssetNumber());
+        offCampusAssetLocation.setAssetLocationTypeCode(CamsConstants.AssetLocationTypeCode.OFF_CAMPUS);
+        asset.getAssetLocations().add(offCampusAssetLocation);
 
-}
+        // Set the location fields either way
+        offCampusAssetLocation.setAssetLocationContactName(assetGlobalDetail.getOffCampusName());
+        offCampusAssetLocation.setAssetLocationContactIdentifier(assetGlobalDetail.getRepresentativeUniversalIdentifier());
+        processAssetLocationInstitutionName(assetGlobalDetail, offCampusAssetLocation);
+        offCampusAssetLocation.setAssetLocationStreetAddress(assetGlobalDetail.getOffCampusAddress());
+        offCampusAssetLocation.setAssetLocationCityName(assetGlobalDetail.getOffCampusCityName());
+        offCampusAssetLocation.setAssetLocationStateCode(assetGlobalDetail.getOffCampusStateCode());
+        offCampusAssetLocation.setAssetLocationCountryCode(assetGlobalDetail.getOffCampusCountryCode());
+        offCampusAssetLocation.setAssetLocationZipCode(assetGlobalDetail.getOffCampusZipCode());
+
+        // There is no phone number field on Asset Global... odd...
+        offCampusAssetLocation.setAssetLocationPhoneNumber(null);
+    }
+    
+    /*
+     * CU-Customization
+     * The asset representative might mot be a Cornell person, so we need to handle this situation
+     */
+    private void processAssetLocationInstitutionName(final AssetGlobalDetail assetGlobalDetail,
+            final AssetLocation offCampusAssetLocation) {
+        if (ObjectUtils.isNotNull(assetGlobalDetail.getAssetRepresentative())) {
+            offCampusAssetLocation.setAssetLocationInstitutionName(
+                    assetGlobalDetail.getAssetRepresentative().getPrimaryDepartmentCode());
+        } else {
+            LOG.info("processAssetLocationInstitutionName, the asset representative is not sent, unable to set asset location institution name");
+        }
+    }
+
+}   
