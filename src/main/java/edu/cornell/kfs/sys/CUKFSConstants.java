7948cc031ac54ab26c3b10fa0ff2ccfef5c91d0b
nkimble <nkk4@cornell.edu>
2024-06-26 11:53:03 -0400

    Revert "KFSPTS-31189: Create blacklist parameter and service that disallows attachments on KFS documents with the identified file extensions. (#1618)" (#1621)
    This reverts commit 0d729a4e55083a62d55684ea660fc64ec196b9f7.

--- src/main/java/edu/cornell/kfs/sys/CUKFSConstants.java
+++ src/main/java/edu/cornell/kfs/sys/CUKFSConstants.java
@@ -31,7 +31,6 @@ public class CUKFSConstants {
     public static final String DATE_FORMAT_yyyy_MM_dd_T_HH_mm_ss_SSS_Z = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
     public static final String TIME_ZONE_UTC = "UTC";
     public static final String ANTIVIRUS_FAILED_MESSAGE = "file contents failed virus scan";
-    public static final String DISALLOWED_FILE_EXTENSION_MESSAGE = "Files with this extension are not allowed as attachments : ";
     
     public static final String DECIMAL_FORMAT_0N_NN = "#0.00";
     
