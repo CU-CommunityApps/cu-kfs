766633db058ec73a0446bfe113f384dbbe3ba938
cah292 <cah292@users.noreply.github.com>
2024-09-12 11:30:04 -0400

    KFSPTS-29488 Remove Mellon references from Check Recon (#1647)

--- src/test/java/com/rsmart/kuali/kfs/cr/CrTestConstants.java
+++ src/test/java/com/rsmart/kuali/kfs/cr/CrTestConstants.java
@@ -1,13 +1,11 @@
 package com.rsmart.kuali.kfs.cr;
 
 public class CrTestConstants {
-    public static final String MELLON_BANK_CODE = "DISB";
-    public static final String JPMC_BANK_CODE = "NDWR";
+    public static final String JPMC_BANK_CODE = "JPCD";
     public static final String JPMC_PARAM_PREFIX = "JPMC_";
     public static final String CR_IMPORT_JOB_NAME = "checkReconciliationJob";
 
     public static final class TestParamValues {
-        public static final String ACCOUNT_111_2345 = "111-2345";
         public static final String ACCOUNT_888_7777 = "888-7777";
         public static final String CLEARING_CHART_IT = "IT";
         public static final String CLEARING_ACCOUNT_G987654 = "G987654";
@@ -15,22 +13,6 @@ public class CrTestConstants {
         public static final String SOURCE_FOR_NOT_FOUND_B = "B";
         public static final String NOT_APPLICABLE = "NA";
         public static final String UNUSED_COL_0 = "0";
-
-        public static final String MELLON_CHK_AMT_COL = "4";
-        public static final String MELLON_CHK_DT_COL = "5";
-        public static final String MELLON_CHK_NB_COL = "3";
-        public static final String MELLON_ISSD_DT_COL = "7";
-        public static final String MELLON_PAYEE_ID_COL = "6";
-        public static final String MELLON_PAYEE_NAME_COL = "12";
-        public static final String MELLON_STATUS_COL = "2";
-        public static final String MELLON_HEADER_COL_LENGTHS = "21;10";
-        public static final String MELLON_COL_LENGTHS = "1;1;10;10;6;10;6;5;5;4;22;60;60";
-        public static final String MELLON_DATE_FORMAT = CRConstants.LEGACY_DATE_FORMAT_yyMMdd;
-        public static final String MELLON_CLRD_CODES = "5;1";
-        public static final String MELLON_CNCL_CODES = "8";
-        public static final String MELLON_ISSD_CODES = "2";
-        public static final String MELLON_STOP_CODES = "6;7";
-        public static final String MELLON_VOID_CODES = "8";
 
         public static final String JPMC_CHK_AMT_COL = "7";
         public static final String JPMC_CHK_DT_COL = "11";
