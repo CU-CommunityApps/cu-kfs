d937065e053e9d0a3196615c2c6a402dbd939c3b
Nancy Kimble <nkk4@cornell.edu>
2024-09-16 14:44:44 -0400

    KFSPTS-29347: Fix failing CuPurchasingAccountsPayableModuleServiceImplIntegTest unit tests.

--- src/test/java/edu/cornell/kfs/module/purap/document/service/impl/CuPurchasingAccountsPayableModuleServiceImplIntegTest.java
+++ src/test/java/edu/cornell/kfs/module/purap/document/service/impl/CuPurchasingAccountsPayableModuleServiceImplIntegTest.java
@@ -1,46 +1,36 @@
 package edu.cornell.kfs.module.purap.document.service.impl;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.kuali.kfs.module.purap.CreditMemoStatuses;
-import org.kuali.kfs.module.purap.PurapConstants;
-import org.kuali.kfs.module.purap.document.VendorCreditMemoDocument;
 import org.kuali.kfs.sys.ConfigureContext;
 import org.kuali.kfs.sys.context.IntegTestUtils;
 import org.kuali.kfs.sys.context.KualiIntegTestBase;
 import org.kuali.kfs.sys.fixture.UserNameFixture;
 import org.kuali.kfs.kew.api.exception.WorkflowException;
 
-import edu.cornell.kfs.module.purap.fixture.VendorCreditMemoDocumentFixture;
+import edu.cornell.kfs.module.purap.document.CuVendorCreditMemoDocument;
+import edu.cornell.kfs.module.purap.fixture.CuVendorCreditMemoDocumentFixture;
 
 @ConfigureContext(session = UserNameFixture.mls398)
 public class CuPurchasingAccountsPayableModuleServiceImplIntegTest extends KualiIntegTestBase {
 
-	private static final Logger LOG = LogManager.getLogger();
+    private CuPurchasingAccountsPayableModuleServiceImpl accountsPayableModuleServiceImpl;
 
-	private CuPurchasingAccountsPayableModuleServiceImpl accountsPayableModuleServiceImpl;
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        accountsPayableModuleServiceImpl = (CuPurchasingAccountsPayableModuleServiceImpl) IntegTestUtils.getUnproxiedService("purchasingAccountsPayableModuleService");
+    }
 
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
+    public void testHandlePurchasingBatchCancels_CRCancel() throws WorkflowException{
+        CuVendorCreditMemoDocument creditMemoDocument = CuVendorCreditMemoDocumentFixture.VENDOR_CREDIT_MEMO.createVendorCreditMemoDocument();
+        accountsPayableModuleServiceImpl.handlePurchasingBatchCancels(creditMemoDocument.getDocumentNumber(), creditMemoDocument.getDocumentType(), true, false, true);
+        assertFalse(CreditMemoStatuses.CANCELLED_STATUSES.contains(creditMemoDocument.getApplicationDocumentStatus()));
+    }
 
-		accountsPayableModuleServiceImpl = (CuPurchasingAccountsPayableModuleServiceImpl) IntegTestUtils.getUnproxiedService("purchasingAccountsPayableModuleService");
+    public void testHandlePurchasingBatchCancels_NonCRCancel() throws WorkflowException{
+        CuVendorCreditMemoDocument creditMemoDocument = CuVendorCreditMemoDocumentFixture.VENDOR_CREDIT_MEMO.createVendorCreditMemoDocument();
+        accountsPayableModuleServiceImpl.handlePurchasingBatchCancels(creditMemoDocument.getDocumentNumber(), creditMemoDocument.getDocumentType(), true, false, false);
+        assertTrue(CreditMemoStatuses.CANCELLED_STATUSES.contains(creditMemoDocument.getApplicationDocumentStatus()));
+    }
 
-	}
-	
-	public void testHandlePurchasingBatchCancels_CRCancel() throws WorkflowException{
-		
-		VendorCreditMemoDocument creditMemoDocument = VendorCreditMemoDocumentFixture.VENDOR_CREDIT_MEMO.createVendorCreditMemoDocument();
-		accountsPayableModuleServiceImpl.handlePurchasingBatchCancels(creditMemoDocument.getDocumentNumber(), creditMemoDocument.getDocumentType(), true, false, true);
-		
-		assertFalse(CreditMemoStatuses.CANCELLED_STATUSES.contains(creditMemoDocument.getApplicationDocumentStatus()));
-	}
-	
-	public void testHandlePurchasingBatchCancels_NonCRCancel() throws WorkflowException{
-		
-		VendorCreditMemoDocument creditMemoDocument = VendorCreditMemoDocumentFixture.VENDOR_CREDIT_MEMO.createVendorCreditMemoDocument();
-		accountsPayableModuleServiceImpl.handlePurchasingBatchCancels(creditMemoDocument.getDocumentNumber(), creditMemoDocument.getDocumentType(), true, false, false);
-		
-		assertTrue(CreditMemoStatuses.CANCELLED_STATUSES.contains(creditMemoDocument.getApplicationDocumentStatus()));
-	}
 }
