fc5b9b53166c1631cd3fb8bcccba8b07be26f230
cah292 <cah292@users.noreply.github.com>
2024-10-09 14:33:52 -0400

    KFSPTS-32170 Remove rest of REQS CapAsset tab customization (#1655)

--- src/main/java/edu/cornell/kfs/module/purap/CUPurapAuthorizationConstants.java
+++ src/main/java/edu/cornell/kfs/module/purap/CUPurapAuthorizationConstants.java
@@ -3,11 +3,6 @@ package edu.cornell.kfs.module.purap;
 public class CUPurapAuthorizationConstants {
 
     
-    public static class CURequisitionEditMode  {
-        //KFSPTS-1792
-        public static final String ENABLE_CAPITAL_ASSET = "enableCapitalAsset";
-    }
-    
     public static class CUPaymentRequestEditMode  {
         // KFSPTS-1891
         public static final String EDIT_AMOUNT = "editAmount";
