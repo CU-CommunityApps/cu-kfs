78503247596ea7f0952aa3b91c87c106501e8c0d
Jay Hulslander <jdh34@cornell.edu>
2024-11-07 13:33:09 -0500

    prevent update of stuff in concur while testing changes

--- src/main/java/edu/cornell/kfs/concur/batch/service/impl/ConcurRequestV4ServiceImpl.java
+++ src/main/java/edu/cornell/kfs/concur/batch/service/impl/ConcurRequestV4ServiceImpl.java
@@ -263,8 +263,10 @@ public class ConcurRequestV4ServiceImpl implements ConcurRequestV4Service {
         ConcurWebRequest<ConcurRequestV4ReportDTO> webRequest = buildWebRequestForTravelRequestApproveAction(
                 requestUuid, resultsDTO);
         
-        ConcurRequestV4ReportDTO updatedTravelRequest = concurEventNotificationWebApiService.callConcurEndpoint(
-                accessToken, webRequest, logMessageDetail);
+        /*
+         * 
+         @todo renable this
+        ConcurRequestV4ReportDTO updatedTravelRequest = concurEventNotificationWebApiService.callConcurEndpoint(accessToken, webRequest, logMessageDetail);
         
         try {
             checkStatusOfUpdatedRequest(updatedTravelRequest, requestUuid);
@@ -273,6 +275,8 @@ public class ConcurRequestV4ServiceImpl implements ConcurRequestV4Service {
                     + "for Request UUID " + requestUuid, e);
             updateProcessingResultForInvalidWorkflowResponse(resultsDTO);
         }
+        */
+        LOG.error("updateRequestStatusInConcur. would have updated the status in Concur for " + requestId);
     }
 
     protected boolean shouldUpdateStatusInConcur() {
