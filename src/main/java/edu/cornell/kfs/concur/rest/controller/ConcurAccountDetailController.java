5b2140751fceb325bdbf1891b8c036cef73bfad9
Jay Hulslander <jdh34@cornell.edu>
2024-12-19 15:53:47 -0500

    iterative

--- src/main/java/edu/cornell/kfs/concur/rest/controller/ConcurAccountDetailController.java
+++ src/main/java/edu/cornell/kfs/concur/rest/controller/ConcurAccountDetailController.java
@@ -3,6 +3,6 @@ package edu.cornell.kfs.concur.rest.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ConcurAccountDetailsController {
+public class ConcurAccountDetailController {
 
 }
--- src/main/java/edu/cornell/kfs/concur/rest/jsonObjects/ConcurAccountDetailDto.java
+++ src/main/java/edu/cornell/kfs/concur/rest/jsonObjects/ConcurAccountDetailDto.java
@@ -1,29 +1,59 @@
 package edu.cornell.kfs.concur.rest.jsonObjects;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import org.kuali.kfs.coa.businessobject.Account;
+import org.kuali.kfs.sys.KFSConstants;
+
+import edu.cornell.kfs.coa.businessobject.AccountExtendedAttribute;
 
 public class ConcurAccountDetailDto {
+    
+    private static final SimpleDateFormat SDF = new SimpleDateFormat(KFSConstants.MONTH_DAY_YEAR_DATE_FORMAT, Locale.US);
     
     private boolean active;
     private String chartOfAccountsCode;
     private String accountNumber;
-    private String subFundGroupCode;
-    private String higherEdFunctionCode;
-    private String accountTypeCode;
+    private String accountName;
     private String orgCode;
+    private String accountTypeCode;
+    private String fundGroupCode;
+    private String subFundGroupCode;
+    private String subFundProgramCode;
+    private String fiscalOfficerName;
+    private String accountManagerName;
+    private String accountSupervisorName;
+    private boolean closed;
+    private String accountCreateDate;
+    private String accountExpirateDate;
+    private String higherEdFunctionCode;
+    
+    
     
     public ConcurAccountDetailDto(Account account) {
+        AccountExtendedAttribute accountExtendedAtribute = (AccountExtendedAttribute) account.getExtension();
+        
         this.active = account.isActive();
         this.chartOfAccountsCode = account.getChartOfAccountsCode();
         this.accountNumber = account.getAccountNumber();
-        this.subFundGroupCode = account.getSubFundGroupCode();
-        this.higherEdFunctionCode = account.getFinancialHigherEdFunctionCd();
-        this.accountTypeCode = account.getAccountTypeCode();
+        this.accountName = account.getAccountName();
         this.orgCode = account.getOrganizationCode();
+        this.accountTypeCode = account.getAccountTypeCode();
+        this.fundGroupCode = account.getSubFundGroup().getFundGroupCode();
+        this.subFundGroupCode = account.getSubFundGroupCode();
+        this.subFundProgramCode = accountExtendedAtribute.getProgramCode();
+        this.fiscalOfficerName = account.getAccountFiscalOfficerUser().getName();
+        this.accountManagerName = account.getAccountManagerUser().getName();
+        this.accountSupervisorName = account.getAccountSupervisoryUser().getName();
+        this.closed = account.isClosed();
+        this.accountCreateDate = SDF.format(account.getAccountCreateDate());
+        this.accountExpirateDate = SDF.format(account.getAccountExpirationDate());
+        this.higherEdFunctionCode = account.getFinancialHigherEdFunctionCd();
     }
-
+    
     public boolean isActive() {
         return active;
     }
@@ -48,12 +78,100 @@ public class ConcurAccountDetailDto {
         this.accountNumber = accountNumber;
     }
 
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public String getOrgCode() {
+        return orgCode;
+    }
+
+    public void setOrgCode(String orgCode) {
+        this.orgCode = orgCode;
+    }
+
+    public String getAccountTypeCode() {
+        return accountTypeCode;
+    }
+
+    public void setAccountTypeCode(String accountTypeCode) {
+        this.accountTypeCode = accountTypeCode;
+    }
+
+    public String getFundGroupCode() {
+        return fundGroupCode;
+    }
+
+    public void setFundGroupCode(String fundGroupCode) {
+        this.fundGroupCode = fundGroupCode;
+    }
+
     public String getSubFundGroupCode() {
         return subFundGroupCode;
     }
 
     public void setSubFundGroupCode(String subFundGroupCode) {
         this.subFundGroupCode = subFundGroupCode;
+    }
+
+    public String getSubFundProgramCode() {
+        return subFundProgramCode;
+    }
+
+    public void setSubFundProgramCode(String subFundProgramCode) {
+        this.subFundProgramCode = subFundProgramCode;
+    }
+
+    public String getFiscalOfficerName() {
+        return fiscalOfficerName;
+    }
+
+    public void setFiscalOfficerName(String fiscalOfficerName) {
+        this.fiscalOfficerName = fiscalOfficerName;
+    }
+
+    public String getAccountManagerName() {
+        return accountManagerName;
+    }
+
+    public void setAccountManagerName(String accountManagerName) {
+        this.accountManagerName = accountManagerName;
+    }
+
+    public String getAccountSupervisorName() {
+        return accountSupervisorName;
+    }
+
+    public void setAccountSupervisorName(String accountSupervisorName) {
+        this.accountSupervisorName = accountSupervisorName;
+    }
+
+    public boolean isClosed() {
+        return closed;
+    }
+
+    public void setClosed(boolean closed) {
+        this.closed = closed;
+    }
+
+    public String getAccountCreateDate() {
+        return accountCreateDate;
+    }
+
+    public void setAccountCreateDate(String accountCreateDate) {
+        this.accountCreateDate = accountCreateDate;
+    }
+
+    public String getAccountExpirateDate() {
+        return accountExpirateDate;
+    }
+
+    public void setAccountExpirateDate(String accountExpirateDate) {
+        this.accountExpirateDate = accountExpirateDate;
     }
 
     public String getHigherEdFunctionCode() {
@@ -63,7 +181,7 @@ public class ConcurAccountDetailDto {
     public void setHigherEdFunctionCode(String higherEdFunctionCode) {
         this.higherEdFunctionCode = higherEdFunctionCode;
     }
-    
+
     @Override
     public String toString() {
         return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
