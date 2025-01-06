2daad375679bdab63f51af7c50d537dc91fef647
Jay Hulslander <jdh34@cornell.edu>
2025-01-06 11:14:01 -0500

    KFSPTS-33361 add webservice for concur AI project to get account details (#1675)

---
+++ src/main/java/edu/cornell/kfs/concur/rest/jsonObjects/ConcurAccountDetailDto.java
@@ -0,0 +1,192 @@
+package edu.cornell.kfs.concur.rest.jsonObjects;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+import org.apache.commons.lang3.StringUtils;
+import org.kuali.kfs.coa.businessobject.Account;
+import org.kuali.kfs.sys.KFSConstants;
+
+import edu.cornell.kfs.coa.businessobject.AccountExtendedAttribute;
+
+public class ConcurAccountDetailDto {
+    private static final SimpleDateFormat SDF = new SimpleDateFormat(KFSConstants.MONTH_DAY_YEAR_DATE_FORMAT, Locale.US);
+    
+    private boolean active;
+    private String chartOfAccountsCode;
+    private String accountNumber;
+    private String accountName;
+    private String orgCode;
+    private String accountTypeCode;
+    private String fundGroupCode;
+    private String subFundGroupCode;
+    private String subFundProgramCode;
+    private String fiscalOfficerName;
+    private String accountManagerName;
+    private String accountSupervisorName;
+    private boolean closed;
+    private String accountCreateDate;
+    private String accountExpirateDate;
+    private String higherEdFunctionCode;
+    
+    
+    
+    public ConcurAccountDetailDto(Account account) {
+        AccountExtendedAttribute accountExtendedAtribute = (AccountExtendedAttribute) account.getExtension();
+        
+        this.active = account.isActive();
+        this.chartOfAccountsCode = account.getChartOfAccountsCode();
+        this.accountNumber = account.getAccountNumber();
+        this.accountName = account.getAccountName();
+        this.orgCode = account.getOrganizationCode();
+        this.accountTypeCode = account.getAccountTypeCode();
+        this.fundGroupCode = account.getSubFundGroup().getFundGroupCode();
+        this.subFundGroupCode = account.getSubFundGroupCode();
+        this.subFundProgramCode = accountExtendedAtribute.getProgramCode();
+        this.fiscalOfficerName = account.getAccountFiscalOfficerUser().getName();
+        this.accountManagerName = account.getAccountManagerUser().getName();
+        this.accountSupervisorName = account.getAccountSupervisoryUser().getName();
+        this.closed = account.isClosed();
+        this.accountCreateDate = formatDate(account.getAccountCreateDate());
+        this.accountExpirateDate = formatDate(account.getAccountExpirationDate());
+        this.higherEdFunctionCode = account.getFinancialHigherEdFunctionCd();
+    }
+    
+    private String formatDate(Date date) {
+        if (date != null) {
+            return SDF.format(date);
+        } else {
+            return StringUtils.EMPTY;
+        }
+    }
+    
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getChartOfAccountsCode() {
+        return chartOfAccountsCode;
+    }
+
+    public void setChartOfAccountsCode(String chartOfAccountsCode) {
+        this.chartOfAccountsCode = chartOfAccountsCode;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public String getOrgCode() {
+        return orgCode;
+    }
+
+    public void setOrgCode(String orgCode) {
+        this.orgCode = orgCode;
+    }
+
+    public String getAccountTypeCode() {
+        return accountTypeCode;
+    }
+
+    public void setAccountTypeCode(String accountTypeCode) {
+        this.accountTypeCode = accountTypeCode;
+    }
+
+    public String getFundGroupCode() {
+        return fundGroupCode;
+    }
+
+    public void setFundGroupCode(String fundGroupCode) {
+        this.fundGroupCode = fundGroupCode;
+    }
+
+    public String getSubFundGroupCode() {
+        return subFundGroupCode;
+    }
+
+    public void setSubFundGroupCode(String subFundGroupCode) {
+        this.subFundGroupCode = subFundGroupCode;
+    }
+
+    public String getSubFundProgramCode() {
+        return subFundProgramCode;
+    }
+
+    public void setSubFundProgramCode(String subFundProgramCode) {
+        this.subFundProgramCode = subFundProgramCode;
+    }
+
+    public String getFiscalOfficerName() {
+        return fiscalOfficerName;
+    }
+
+    public void setFiscalOfficerName(String fiscalOfficerName) {
+        this.fiscalOfficerName = fiscalOfficerName;
+    }
+
+    public String getAccountManagerName() {
+        return accountManagerName;
+    }
+
+    public void setAccountManagerName(String accountManagerName) {
+        this.accountManagerName = accountManagerName;
+    }
+
+    public String getAccountSupervisorName() {
+        return accountSupervisorName;
+    }
+
+    public void setAccountSupervisorName(String accountSupervisorName) {
+        this.accountSupervisorName = accountSupervisorName;
+    }
+
+    public boolean isClosed() {
+        return closed;
+    }
+
+    public void setClosed(boolean closed) {
+        this.closed = closed;
+    }
+
+    public String getAccountCreateDate() {
+        return accountCreateDate;
+    }
+
+    public void setAccountCreateDate(String accountCreateDate) {
+        this.accountCreateDate = accountCreateDate;
+    }
+
+    public String getAccountExpirateDate() {
+        return accountExpirateDate;
+    }
+
+    public void setAccountExpirateDate(String accountExpirateDate) {
+        this.accountExpirateDate = accountExpirateDate;
+    }
+
+    public String getHigherEdFunctionCode() {
+        return higherEdFunctionCode;
+    }
+
+    public void setHigherEdFunctionCode(String higherEdFunctionCode) {
+        this.higherEdFunctionCode = higherEdFunctionCode;
+    }
+    
+}
---
+++ src/main/java/edu/cornell/kfs/concur/web/filter/ConcurAIAuthFilter.java
@@ -0,0 +1,89 @@
+package edu.cornell.kfs.concur.web.filter;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.HttpMethod;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.kuali.kfs.sys.context.SpringContext;
+
+import com.google.gson.Gson;
+
+import edu.cornell.kfs.concur.ConcurConstants.ConcurAIConstants;
+import edu.cornell.kfs.sys.service.WebServiceCredentialService;
+
+public class ConcurAIAuthFilter implements Filter {
+    private static final Logger LOG = LogManager.getLogger();
+    private static final Gson gson = new Gson();
+
+    private WebServiceCredentialService webServiceCredentialService;
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+
+        httpServletResponse.setHeader(ConcurAIConstants.ACCESS_CONTROL_HEADER_NAME, HttpMethod.GET);
+
+        checkAuthorization(httpServletRequest, httpServletResponse, chain);
+
+    }
+
+    private void checkAuthorization(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException {
+        try {
+            if (isAuthorized(request)) {
+                chain.doFilter(request, response);
+            } else {
+                LOG.warn("checkAuthorization unauthorized {} {}", request.getMethod(), request.getPathInfo());
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().println(gson.toJson(ConcurAIConstants.UNAUTHORIZED));
+            }
+        } catch (Exception ex) {
+            LOG.error("checkAuthorization", ex);
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().println(gson.toJson(ConcurAIConstants.UNAUTHORIZED));
+        }
+    }
+
+    private boolean isAuthorized(HttpServletRequest request) {
+        byte[] byteArrayEncodedAllowableUserNamePassword = Base64
+                .encodeBase64(getAllowableUserNamePassword().getBytes());
+        String encodedAllowableUserNamePassword = new String(byteArrayEncodedAllowableUserNamePassword,
+                StandardCharsets.UTF_8);
+        String authorizationFromRequest = request.getHeader(ConcurAIConstants.AUTHORIZATION_HEADER_KEY);
+        return StringUtils.equals(authorizationFromRequest,
+                ConcurAIConstants.BASIC_AUTHENTICATION_STARTER + encodedAllowableUserNamePassword);
+
+    }
+
+    private String getAllowableUserNamePassword() {
+        return getWebServiceCredentialService().getWebServiceCredentialValue(
+                ConcurAIConstants.WEBSERVICE_CRED_GROUP_CODE, ConcurAIConstants.WEBSERVICE_CRED_KEY);
+    }
+
+    public WebServiceCredentialService getWebServiceCredentialService() {
+        if (webServiceCredentialService == null) {
+            webServiceCredentialService = SpringContext.getBean(WebServiceCredentialService.class);
+        }
+        return webServiceCredentialService;
+    }
+
+}
