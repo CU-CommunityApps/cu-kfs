eb0830c5738e124eaa208fc8d372a9675630d8fb
cah292 <cah292@cornell.edu>
2025-01-21 18:19:12 -0500

    KFSPTS-33579 More major revisions

--- src/main/java/edu/cornell/kfs/tax/batch/service/TaxTableMetadataLookupService.java
+++ src/main/java/edu/cornell/kfs/tax/batch/service/TaxTableMetadataLookupService.java
@@ -1,12 +1,9 @@
 package edu.cornell.kfs.tax.batch.service;
 
-import edu.cornell.kfs.tax.batch.dto.util.TaxDtoExtractorDefinition;
-import edu.cornell.kfs.tax.batch.dto.util.TaxDtoUpdaterDefinition;
+import edu.cornell.kfs.tax.batch.metadata.TaxDtoMappingDefinition;
 
 public interface TaxTableMetadataLookupService {
 
-    <T> TaxDtoExtractorDefinition<T> getDtoMetadataForSqlExtraction(final Class<T> dtoClass);
-
-    <T> TaxDtoUpdaterDefinition<T> getDtoMetadataForSqlUpdates(final Class<T> dtoClass);
+    <T> TaxDtoMappingDefinition<T> getDatabaseMappingMetadataForDto(final Class<T> dtoClass);
 
 }
