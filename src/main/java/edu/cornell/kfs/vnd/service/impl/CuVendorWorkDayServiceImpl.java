83bb6bc98e55c2fa17b9bdf88e9c31125a47c248
Jay Hulslander <jdh34@cornell.edu>
2025-02-10 13:28:57 -0500

    fix logging of an error

--- src/main/java/edu/cornell/kfs/vnd/service/impl/CuVendorWorkDayServiceImpl.java
+++ src/main/java/edu/cornell/kfs/vnd/service/impl/CuVendorWorkDayServiceImpl.java
@@ -84,7 +84,8 @@ public class CuVendorWorkDayServiceImpl extends DisposableClientServiceImplBase 
                             documentNumber, retryCount, response.getStatus());
                 }
             } catch (Exception e) {
-                LOG.error("findEmployeeBySocialSecurityNumber, for document {} on try {}, got an error", documentNumber, retryCount, e);
+                LOG.error("findEmployeeBySocialSecurityNumber, for document {} on try {}, got an error that wwe can't log as it might contain an SSN",
+                        documentNumber, retryCount);
             }
             retryCount++;
         }
