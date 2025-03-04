1bc2e617817a0e2216ae47dc18eb119f63eb1162
Chad Hagstrom <cah292@cornell.edu>
2025-03-04 16:42:32 -0500

    KFSPTS-32561: Sub-task KFSPTS-34103: Adjust columns in VN-99-03 report file (#1708)

--- src/test/java/edu/cornell/kfs/vnd/batch/service/impl/annotation/VendorComparisonResultRow.java
+++ src/test/java/edu/cornell/kfs/vnd/batch/service/impl/annotation/VendorComparisonResultRow.java
@@ -7,6 +7,12 @@ import java.lang.annotation.Target;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.Validate;
+import org.kuali.kfs.sys.KFSConstants;
+import org.kuali.kfs.vnd.businessobject.VendorDetail;
+import org.kuali.kfs.vnd.businessobject.VendorHeader;
+
 import edu.cornell.kfs.sys.CUKFSConstants;
 
 @Retention(RetentionPolicy.RUNTIME)
@@ -27,6 +33,8 @@ public @interface VendorComparisonResultRow {
 
     String terminationDateGreaterThanProcessingDate();
 
+    String vendorType() default KFSConstants.EMPTY_STRING;
+
     public static final class Converters {
         public static String toCsvRow(final VendorComparisonResultRow resultRow) {
             return Stream.of(
@@ -35,6 +43,25 @@ public @interface VendorComparisonResultRow {
                     resultRow.terminationDateGreaterThanProcessingDate()
             ).collect(Collectors.joining(
                     CUKFSConstants.COMMA_WITH_QUOTES, CUKFSConstants.DOUBLE_QUOTE, CUKFSConstants.DOUBLE_QUOTE));
+        }
+
+        public static boolean canConvertToVendorDetail(final VendorComparisonResultRow resultRow) {
+            return StringUtils.isNoneBlank(resultRow.vendorId(), resultRow.vendorType());
+        }
+
+        public static VendorDetail toVendorDetail(final VendorComparisonResultRow resultRow) {
+            final VendorDetail vendorDetail = new VendorDetail();
+            final VendorHeader vendorHeader = new VendorHeader();
+            final String[] vendorIdParts = StringUtils.split(resultRow.vendorId(), KFSConstants.DASH);
+            Validate.isTrue(vendorIdParts.length == 2, "Vendor ID was not formatted as two dash-delimited numbers");
+
+            vendorHeader.setVendorHeaderGeneratedIdentifier(Integer.valueOf(vendorIdParts[0]));
+            vendorHeader.setVendorTypeCode(resultRow.vendorType());
+            vendorDetail.setVendorHeaderGeneratedIdentifier(vendorHeader.getVendorHeaderGeneratedIdentifier());
+            vendorDetail.setVendorDetailAssignedIdentifier(Integer.valueOf(vendorIdParts[1]));
+            vendorDetail.setVendorHeader(vendorHeader);
+
+            return vendorDetail;
         }
     }
 
