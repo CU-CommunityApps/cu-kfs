96f3ada4745e7da1476dde08551b143c26442d63
Jay Hulslander <jdh34@cornell.edu>
2025-03-31 10:36:10 -0400

    adding some debugging

--- src/main/java/edu/cornell/kfs/sys/batch/service/impl/CuCacheServiceImpl.java
+++ src/main/java/edu/cornell/kfs/sys/batch/service/impl/CuCacheServiceImpl.java
@@ -1,9 +1,13 @@
 package edu.cornell.kfs.sys.batch.service.impl;
 
+import java.util.Optional;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.kuali.kfs.sys.batch.service.impl.CacheServiceImpl;
 import org.springframework.cache.CacheManager;
+import org.springframework.data.redis.cache.RedisCache;
+import org.springframework.data.redis.cache.RedisCacheManager;
 
 public class CuCacheServiceImpl extends CacheServiceImpl {
     private static final Logger LOG = LogManager.getLogger();
@@ -13,8 +17,13 @@ public class CuCacheServiceImpl extends CacheServiceImpl {
     @Override
     public void clearSystemCaches() {
         LOG.debug("clearSystemCaches, cache manager class {}", cacheManager.getClass());
-        for (final String cacheName : cacheManager.getCacheNames()) {
-            LOG.debug("clearSystemCaches, attempting to clear cache for {}", cacheName);
+        RedisCacheManager redisManager = (RedisCacheManager) cacheManager;
+        for (final String cacheName : redisManager.getCacheNames()) {
+            
+            Optional<String> configurationPrefix = ((RedisCache) redisManager.getCache(cacheName)).getCacheConfiguration().getKeyPrefix();
+            String cachePreFix = ((RedisCache) redisManager.getCache(cacheName)).getCacheConfiguration().getKeyPrefixFor(cacheName);
+         
+            LOG.debug("clearSystemCaches, attempting to clear cache for '{}', with configuration prefix '{}' and cachePrefix '{}'", cacheName, configurationPrefix, cachePreFix);
             cacheManager.getCache(cacheName).clear();
         }
     }
