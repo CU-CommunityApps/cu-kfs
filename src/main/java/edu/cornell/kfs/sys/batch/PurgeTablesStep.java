117d47bc348c5ec30b8904551c7c69cd35bfab0b
Nancy Kimble <nkk4@cornell.edu>
2025-04-07 16:26:12 -0400

    KFSPTS-32169: PurgeTablesStep joda changes

--- src/main/java/edu/cornell/kfs/sys/batch/PurgeTablesStep.java
+++ src/main/java/edu/cornell/kfs/sys/batch/PurgeTablesStep.java
@@ -2,17 +2,27 @@ package edu.cornell.kfs.sys.batch;
 
 import java.util.Date;
 
+import org.kuali.kfs.core.api.datetime.DateTimeService;
 import org.kuali.kfs.sys.batch.AbstractStep;
 
 import edu.cornell.kfs.sys.batch.service.TablesPurgeService;
 
 public class PurgeTablesStep extends AbstractStep {
     
+    private DateTimeService dateTimeService;
     private TablesPurgeService tablesPurgeService;
     
     public boolean execute(String jobName, Date jobRunDate) throws InterruptedException {
-        getTablesPurgeService().purgeRecords(jobRunDate);
+        getTablesPurgeService().purgeRecords(getDateTimeService().getLocalDateTime(jobRunDate));
         return true;
+    }
+
+    public DateTimeService getDateTimeService() {
+        return dateTimeService;
+    }
+
+    public void setDateTimeService(DateTimeService dateTimeService) {
+        this.dateTimeService = dateTimeService;
     }
 
     public TablesPurgeService getTablesPurgeService() {
