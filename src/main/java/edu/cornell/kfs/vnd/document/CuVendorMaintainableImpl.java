d1375a3cb6738e977a028bec1fcd8f51113a1c52
Jay Hulslander <jdh34@cornell.edu>
2025-04-16 15:43:29 -0400

    KFSPTS-34133 KFSPTS-34601 add employee id alias to vnedor records

--- src/main/java/edu/cornell/kfs/vnd/document/CuVendorMaintainableImpl.java
+++ src/main/java/edu/cornell/kfs/vnd/document/CuVendorMaintainableImpl.java
@@ -42,6 +42,7 @@ import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.kfs.sys.businessobject.DocumentHeader;
 import org.kuali.kfs.sys.context.SpringContext;
 import org.kuali.kfs.vnd.businessobject.VendorAddress;
+import org.kuali.kfs.vnd.businessobject.VendorAlias;
 import org.kuali.kfs.vnd.businessobject.VendorDetail;
 import org.kuali.kfs.vnd.businessobject.VendorHeader;
 import org.kuali.kfs.vnd.businessobject.VendorSupplierDiversity;
@@ -141,6 +142,7 @@ public class CuVendorMaintainableImpl extends VendorMaintainableImpl {
                     message = MessageFormat.format(getConfigurationService().getPropertyValueAsString(CUVendorKeyConstants.TERMINATED_EMPLOYEE_MESSAGE),
                             getEmployeeTerminationNumberOfDays());
                 }
+                addSearchAliasAndSaveVendorDetailIfNeeded(result);
                 annotateDocument(message);
             }
             LOG.debug("isVendorTaxNumberInWorkday, returning {} for document {}", isActiveOrTerminatedEmployee, getDocumentNumber());
@@ -192,6 +194,50 @@ public class CuVendorMaintainableImpl extends VendorMaintainableImpl {
             return 365;
         }
     }
+    
+    private void addSearchAliasAndSaveVendorDetailIfNeeded(WorkdayKfsVendorLookupRoot root) {
+        if (root != null && root.getResults().get(0) != null) {
+            WorkdayKfsVendorLookupResult result = root.getResults().get(0);
+            String employeeId = result.getEmployeeID();
+            
+            MaintenanceDocument document = (MaintenanceDocument) getDocumentService().getByDocumentHeaderId(getDocumentNumber());
+            VendorDetail vendorDetail = (VendorDetail) document.getNewMaintainableObject();
+            VendorAlias alias = findEmployeeIdAlias(vendorDetail, employeeId);
+            
+            if (alias == null) {
+                LOG.debug("addSearchAliasAndSaveVendorDetailIfNeeded, vendor {}, adding alias named {}", vendorDetail.getVendorNumber(), employeeId);
+                alias = new VendorAlias();
+                alias.setActive(true);
+                alias.setVendorAliasName(employeeId);
+                alias.setNewCollectionRecord(true);
+                alias.setVendorHeaderGeneratedIdentifier(vendorDetail.getVendorHeaderGeneratedIdentifier());
+                alias.setVendorDetailAssignedIdentifier(vendorDetail.getVendorDetailAssignedIdentifier());
+                vendorDetail.getVendorAliases().add(alias);
+                getDocumentService().saveDocument(document);
+            } else {
+                if (alias.isActive()) {
+                    LOG.debug("addSearchAliasAndSaveVendorDetailIfNeeded, vendor {}, already has active alias named {}", vendorDetail.getVendorNumber(), employeeId);
+                } else {
+                    LOG.debug("addSearchAliasAndSaveVendorDetailIfNeeded, vendor {}, updating alias named {} to active", vendorDetail.getVendorNumber(), employeeId);
+                    alias.setActive(true);
+                    getDocumentService().saveDocument(document);
+                }
+            }
+
+        } else {
+            LOG.error("addSearchAliasAndSaveVendorDetailIfNeeded, for document {}, WorkdayKfsVendorLookupRoot was null, or result list was empty, this shouldn't happen", getDocumentNumber());
+        }
+    }
+    
+    private VendorAlias findEmployeeIdAlias(VendorDetail vendorDetail, String employeeId) {
+        for (VendorAlias alias : vendorDetail.getVendorAliases()) {
+            if (StringUtils.equalsAnyIgnoreCase(employeeId, alias.getVendorAliasName())) {
+                return alias;
+            }
+        }
+        return null;
+    }
+
     
     @SuppressWarnings("deprecation")
     private void annotateDocument(final String message) {
