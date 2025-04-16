007aed4b821e5803b862138da8e9181f3d058037
nkimble <nkk4@cornell.edu>
2025-04-16 18:55:10 -0400

    KFSPTS-34628: Remove all pieces of decommissioned batch job CreateClosedAccountCsvJob instead of updating joda to java time libraries. (#1732)

--- src/main/java/edu/cornell/kfs/coa/batch/CuCoaBatchParameterConstants.java
+++ src/main/java/edu/cornell/kfs/coa/batch/CuCoaBatchParameterConstants.java
@@ -1,9 +1,6 @@
 package edu.cornell.kfs.coa.batch;
 
 public class CuCoaBatchParameterConstants {
-    public static final String CREATE_CLOSED_ACCOUNTS_CSV_STEP = "createClosedAccountsCsvStep";
-    public static final String ACCOUNTS_CLOSED_OVER_PAST_DAYS = "ACCOUNTS_CLOSED_OVER_PAST_DAYS";
-    public static final String CLOSED_ACCOUNTS_SEED_FILE_DEFAULT_FROM_DATE = "CLOSED_ACCOUNTS_SEED_FILE_DEFAULT_FROM_DATE";
     public static final String ATTACHMENT_BATCH_SIZE = "ATTACHMENT_BATCH_SIZE";
     public static final String MAX_DOWNLOAD_RETRIES = "MAX_DOWNLOAD_RETRIES";
     public static final String MAX_ATTACHMENT_FAILURES_PER_RUN = "MAX_ATTACHMENT_FAILURES_PER_RUN";
