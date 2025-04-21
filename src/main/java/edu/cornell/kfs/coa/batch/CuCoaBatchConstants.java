9e236c95306abd7e3995c4c5bbb83fd661ae7645
nkimble <nkk4@cornell.edu>
2025-04-21 11:37:09 -0400

    KFSPTS-34628: Remove all pieces of decommissioned batch job CreateClosedAccountCsvJob instead of updating joda to java time libraries. (#1735)

--- src/main/java/edu/cornell/kfs/coa/batch/CuCoaBatchConstants.java
+++ src/main/java/edu/cornell/kfs/coa/batch/CuCoaBatchConstants.java
@@ -4,26 +4,6 @@ import edu.cornell.kfs.fp.CuFPConstants;
 
 public class CuCoaBatchConstants {
     
-    public static class ClosedAccountsFileCreationConstants {
-        
-        public static final int PARAMETER_CLOSED_ACCOUNTS_SEED_FILE_DEFAULT_FROM_DATE_NOT_SET = -2;
-        public static final int PARAMETER_ACCOUNTS_CLOSED_OVER_PAST_DAYS_NOT_SET = -1;
-        public static final int PARAMETER_SET_TO_CREATE_FULL_SEED_FILE = 0;
-                
-        public static final String FROM_DATE = "FROM_DATE";
-        public static final String TO_DATE = "TO_DATE";
-        
-        public static final String FILE_DATA_CONTENT_TYPE_IS = "FILE_DATA_CONTENT_TYPE_IS";
-        
-        public static class FILE_DATA_CONTENT_TYPES {
-            public static final int NO_PARAMETER_FOUND = PARAMETER_ACCOUNTS_CLOSED_OVER_PAST_DAYS_NOT_SET;
-            public static final int SEED = PARAMETER_SET_TO_CREATE_FULL_SEED_FILE;
-            public static final int RANGE = 1;
-        }
-        
-        public static final String OUTPUT_FILE_NAME = "kfsClosedAccounts";
-    }
-    
     public static class WorkdayOpenAccountsFileCreationConstants {
         public static final String OUTPUT_FILE_NAME = "kfsOpenAccountsSubaccountsSubobjectcodes";
     }
--- src/main/java/edu/cornell/kfs/coa/batch/service/CreateClosedAccountsCsvService.java
+++
@@ -1,9 +0,0 @@
-package edu.cornell.kfs.coa.batch.service;
-
-import java.io.IOException;
-
-public interface CreateClosedAccountsCsvService {
-    
-    void createClosedAccountsCsvByParameterPastDays() throws IOException;
-    
-}
