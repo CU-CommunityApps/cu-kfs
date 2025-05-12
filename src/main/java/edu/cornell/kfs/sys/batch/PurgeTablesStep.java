5cecfd67d666e5d1ed0e366fa9e1c7985f809830
Nancy Kimble <nkk4@cornell.edu>
2025-05-12 14:55:09 -0400

    KFSPTS-34622: code review changes

--- src/main/java/edu/cornell/kfs/sys/batch/PurgeTablesStep.java
+++ src/main/java/edu/cornell/kfs/sys/batch/PurgeTablesStep.java
@@ -2,27 +2,17 @@ package edu.cornell.kfs.sys.batch;
 
 import java.util.Date;
 
-import org.kuali.kfs.core.api.datetime.DateTimeService;
 import org.kuali.kfs.sys.batch.AbstractStep;
 
 import edu.cornell.kfs.sys.batch.service.TablesPurgeService;
 
 public class PurgeTablesStep extends AbstractStep {
     
-    private DateTimeService dateTimeService;
     private TablesPurgeService tablesPurgeService;
     
     public boolean execute(String jobName, Date jobRunDate) throws InterruptedException {
         getTablesPurgeService().purgeRecords(getDateTimeService().getLocalDate(jobRunDate));
         return true;
-    }
-
-    public DateTimeService getDateTimeService() {
-        return dateTimeService;
-    }
-
-    public void setDateTimeService(DateTimeService dateTimeService) {
-        this.dateTimeService = dateTimeService;
     }
 
     public TablesPurgeService getTablesPurgeService() {
