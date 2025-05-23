13e032f78ef360d64d69ebb9be880bb09c8c4c06
cah292 <cah292@cornell.edu>
2025-05-23 12:26:48 -0400

    KFSPTS-35018 Remove regex check

--- src/main/webapp/scripts/module/purap/preqInitUtils.js
+++ src/main/webapp/scripts/module/purap/preqInitUtils.js
@@ -1,10 +1,10 @@
 function populateInvoiceReceivedDateIfNecessary(invoiceDateField) {
-    const datePattern = /^\d{1,2}\D\d{1,2}\D\d{2}(\d{2})?$/;
-    if (invoiceDateField && invoiceDateField.value && datePattern.test(invoiceDateField.value)) {
+    const invoiceDate = invoiceDateField && invoiceDateField.value;
+    if (invoiceDate && invoiceDate.trim().length > 0) {
         const invoiceReceivedDateField = document.getElementById("document.invoiceReceivedDate");
         const invoiceReceivedDate = invoiceReceivedDateField && invoiceReceivedDateField.value;
         if (invoiceReceivedDateField && (!invoiceReceivedDate || invoiceReceivedDate.trim().length == 0)) {
-            invoiceReceivedDateField.value = invoiceDateField.value;
+            invoiceReceivedDateField.value = invoiceDate;
         }
     }
 }
