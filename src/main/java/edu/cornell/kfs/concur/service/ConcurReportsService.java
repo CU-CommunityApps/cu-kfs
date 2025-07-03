e4b4e2fc5532275bf47d9d8f2417eb6010466cb5
Andrew Dickson <ajd299@cornell.edu>
2025-07-02 22:36:22 -0400

    Restore bean and remove unused methods in ConcurReportsService

--- src/main/java/edu/cornell/kfs/concur/service/ConcurReportsService.java
+++ src/main/java/edu/cornell/kfs/concur/service/ConcurReportsService.java
@@ -1,18 +1,8 @@
 package edu.cornell.kfs.concur.service;
 
-import edu.cornell.kfs.concur.businessobjects.ConcurReport;
-import edu.cornell.kfs.concur.businessobjects.ValidationResult;
 import edu.cornell.kfs.concur.eventnotification.rest.xmlObjects.ConcurEventNotificationListDTO;
 
 public interface ConcurReportsService {
-    
-    void initializeTemporaryAccessToken();
-    
-    void clearTemporaryAccessToken();
-    
-    ConcurReport extractConcurReport(String reportURI);
-
-    void updateExpenseReportStatusInConcur(String workflowURI, ValidationResult validationResult);
     
     boolean deleteFailedEventQueueItemInConcur(String noticationId);
     
