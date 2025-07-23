d6f8683b00723302adc18e8398b256e5fb629e41
cah292 <cah292@cornell.edu>
2025-07-23 10:42:57 -0400

    KFSPTS-26175 Squashing Daniela's changes into feature branch

--- src/main/java/edu/cornell/kfs/module/ld/businessobject/LaborLedgerBatchFile.java
+++ src/main/java/edu/cornell/kfs/module/ld/businessobject/LaborLedgerBatchFile.java
@@ -10,18 +10,15 @@ import org.kuali.kfs.sys.batch.BatchFile;
 @NAMESPACE(namespace = KFSConstants.OptionalModuleNamespaces.LABOR_DISTRIBUTION)
 public class LaborLedgerBatchFile extends BatchFile {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public LaborLedgerBatchFile() {
-		super();
-	}
 
-	public LaborLedgerBatchFile(String id) throws FileNotFoundException {
-		super(id);
-	}
+    public LaborLedgerBatchFile(String id) throws FileNotFoundException {
+        super(id);
+    }
 
-	public LaborLedgerBatchFile(File file) {
-		super(file);
-	}
+    public LaborLedgerBatchFile(File file) {
+        super(file);
+    }
 
 }
