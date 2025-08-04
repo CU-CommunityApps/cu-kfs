fd19db2e2509ae10e062d100fdaadfb4bfe804c4
Daniela Balmus <db483@cornell.edu>
2025-08-04 16:05:13 +0100

    KFSPTS-35683 add zero arg constructor to CreateDoneBatchFile

--- src/main/java/edu/cornell/kfs/sys/batch/CreateDoneBatchFile.java
+++ src/main/java/edu/cornell/kfs/sys/batch/CreateDoneBatchFile.java
@@ -6,6 +6,10 @@ import java.io.FileNotFoundException;
 import org.kuali.kfs.sys.batch.BatchFile;
 
 public class CreateDoneBatchFile extends BatchFile {
+    
+    public CreateDoneBatchFile() {
+        super();
+    }
 
 	public CreateDoneBatchFile(String id) throws FileNotFoundException {
 		super(id);
