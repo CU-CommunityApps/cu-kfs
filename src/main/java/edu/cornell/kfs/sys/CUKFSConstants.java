fb7ea4892237f02a86aaf72e655190d6f4e97763
Jay Hulslander <jdh34@cornell.edu>
2025-08-15 17:42:48 -0400

    KFSPTS-35705 KFSPTS-25264 display our apologies screen when app spider testing is enabled (#1794)

--- src/main/java/edu/cornell/kfs/sys/CUKFSConstants.java
+++ src/main/java/edu/cornell/kfs/sys/CUKFSConstants.java
@@ -213,6 +213,7 @@ public class CUKFSConstants {
     }
 
     public static final String CU_ALLOW_LOCAL_BATCH_EXECUTION_KEY = "cu.allow.local.batch.execution";
+    public static final String CU_APPSPIDER_ENABLED_KEY = "cu.appspider.enabled";
     public static final String EXCEPTION_MESSAGING_GROUP = "Exception Messaging";
     public static final String EXCEPTION_MESSAGE_JOB_NAME_PREFIX = "Exception_Message_Job ";
     public static final String DELAYED_ASYNCHRONOUS_CALL_GROUP = "Delayed_Asynchronous_Call";
