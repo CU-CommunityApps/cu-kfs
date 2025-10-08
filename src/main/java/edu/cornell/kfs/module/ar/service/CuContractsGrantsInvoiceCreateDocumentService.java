5332e839e2183d70dffa80f11ce648754b46fbd3
Nancy Kimble <nkk4@cornell.edu>
2025-10-08 09:16:42 -0400

    KFSPTS-33340: Update place holders for federal financial report.

--- src/main/java/edu/cornell/kfs/module/ar/service/CuContractsGrantsInvoiceCreateDocumentService.java
+++ src/main/java/edu/cornell/kfs/module/ar/service/CuContractsGrantsInvoiceCreateDocumentService.java
@@ -2,7 +2,11 @@ package edu.cornell.kfs.module.ar.service;
 
 import org.kuali.kfs.module.ar.document.ContractsGrantsInvoiceDocument;
 import org.kuali.kfs.module.ar.service.ContractsGrantsInvoiceCreateDocumentService;
+import org.kuali.kfs.module.cg.businessobject.Award;
 
 public interface CuContractsGrantsInvoiceCreateDocumentService extends ContractsGrantsInvoiceCreateDocumentService {
     public void populateDocumentDescription(ContractsGrantsInvoiceDocument cgInvoiceDocument);
+    
+    // KFSPTS-33340
+    ContractsGrantsInvoiceDocument createCINVForReport(final Award awd);
 }
