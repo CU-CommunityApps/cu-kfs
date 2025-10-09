bd9aa493ed458e2d513bae9845b3840c54ab26f5
Chad Hagstrom <cah292@cornell.edu>
2025-10-09 02:42:36 -0400

    KFSPTS-27185 Fix MIME Type unit tests (#1817)

--- src/test/java/edu/cornell/kfs/fp/batch/service/impl/AccountingXmlDocumentDownloadAttachmentServiceImplTest.java
+++ src/test/java/edu/cornell/kfs/fp/batch/service/impl/AccountingXmlDocumentDownloadAttachmentServiceImplTest.java
@@ -1,91 +1,65 @@
 package edu.cornell.kfs.fp.batch.service.impl;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.apache.commons.lang3.StringUtils;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.kuali.kfs.sys.KFSConstants;
 
+@Execution(ExecutionMode.SAME_THREAD)
 public class AccountingXmlDocumentDownloadAttachmentServiceImplTest {
-    
+
     private AccountingXmlDocumentDownloadAttachmentServiceImpl attachmentService;
 
-    @Before
-    public void setUp() throws Exception {
+    @BeforeEach
+    void setUp() throws Exception {
         attachmentService = new AccountingXmlDocumentDownloadAttachmentServiceImpl();
     }
 
-    @After
-    public void tearDown() throws Exception {
+    @AfterEach
+    void tearDown() throws Exception {
         attachmentService = null;
     }
 
-    @Test
-    public void testFindMimeTypePdf() {
-        String mimeType = attachmentService.findMimeType("test.pdf");
-        assertEquals("application/pdf", mimeType);
+    enum MimeTypeTestCase {
+        PDF("test.pdf", "application/pdf"),
+        PDF_UPPERCASE("TEST.PDF", "application/pdf"),
+        PDF_EXTENSION_ONLY(".pdf", "application/pdf"),
+        DOC("test.doc", "application/msword"),
+        DOC_UPPERCASE("TEST.DOC", "application/msword"),
+        DOCX("test.docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"),
+        XLSX("test.xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"),
+        XLS("test.xls", "application/vnd.ms-excel"),
+        PNG("test.png", "image/png"),
+        TXT("test.txt", "text/plain"),
+        UNRECOGNIZED_TYPE("test.kfs", "kfs"),
+        UNRECOGNIZED_TYPE_UPPERCASE("TEST.KFS", "kfs"),
+        UNRECOGNIZED_TYPE_EXTENSION_ONLY(".kfs", "kfs"),
+        FILENAME_WITHOUT_EXTENSION("test", "test"),
+        FILENAME_WITHOUT_EXTENSION_UPPERCASE("TEST", "test"),
+        EMPTY_FILENAME(StringUtils.EMPTY, StringUtils.EMPTY),
+        BLANK_SPACE_FILENAME(KFSConstants.BLANK_SPACE, StringUtils.EMPTY),
+        NULL_FILENAME(null, StringUtils.EMPTY);
+
+        private final String fileName;
+        private final String expectedMimeType;
+
+        private MimeTypeTestCase(final String fileName, final String expectedMimeType) {
+            this.fileName = fileName;
+            this.expectedMimeType = expectedMimeType;
+        }
     }
-    
-    @Test
-    public void testFindMimeTypePDF() {
-        String mimeType = attachmentService.findMimeType("TEST.PDF");
-        assertEquals("application/pdf", mimeType);
-    }
-    
-    @Test
-    public void testFindMimeTypeDoc() {
-        String mimeType = attachmentService.findMimeType("test.doc");
-        assertEquals("doc", mimeType);
-    }
-    
-    @Test
-    public void testFindMimeTypeDOC() {
-        String mimeType = attachmentService.findMimeType("TEST.DOC");
-        assertEquals("doc", mimeType);
-    }
-    
-    @Test
-    public void testFindMimeTypeXlsx() {
-        String mimeType = attachmentService.findMimeType("test.xlsx");
-        assertEquals("xlsx", mimeType);
-    }
-    
-    @Test
-    public void testFindMimeTypeXls() {
-        String mimeType = attachmentService.findMimeType("test.xls");
-        assertEquals("xls", mimeType);
-    }
-    
-    @Test
-    public void testFindMimeTypeCsv() {
-        String mimeType = attachmentService.findMimeType("test.csv");
-        assertEquals("csv", mimeType);
-    }
-    
-    @Test
-    public void testFindMimeTypeBadFileName() {
-        String mimeType = attachmentService.findMimeType("test");
-        assertEquals("test", mimeType);
-    }
-    
-    @Test
-    public void testFindMimeTypeEmptyFileName() {
-        String mimeType = attachmentService.findMimeType(StringUtils.EMPTY);
-        assertEquals(StringUtils.EMPTY, mimeType);
-    }
-    
-    @Test
-    public void testFindMimeTypeSpaceFileName() {
-        String mimeType = attachmentService.findMimeType(KFSConstants.BLANK_SPACE);
-        assertEquals(StringUtils.EMPTY, mimeType);
-    }
-    
-    @Test
-    public void testFindMimeTypeNullFileName() {
-        String mimeType = attachmentService.findMimeType(null);
-        assertEquals(StringUtils.EMPTY, mimeType);
+
+    @ParameterizedTest
+    @EnumSource(MimeTypeTestCase.class)
+    void testFindMimeType(final MimeTypeTestCase testCase) {
+        final String actualMimeType = attachmentService.findMimeType(testCase.fileName);
+        assertEquals(testCase.expectedMimeType, actualMimeType, "Wrong MIME Type");
     }
 
 }
