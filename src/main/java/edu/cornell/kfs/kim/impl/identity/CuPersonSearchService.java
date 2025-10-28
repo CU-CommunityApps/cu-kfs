9679d69d6d0d57e1fabc36174a59c9b61ba03cfe
Daniela Balmus <db483@cornell.edu>
2025-10-28 10:54:49 +0000

    KFSPTS-36478 add code review changes

--- src/main/java/edu/cornell/kfs/kim/impl/identity/CuPersonSearchService.java
+++ src/main/java/edu/cornell/kfs/kim/impl/identity/CuPersonSearchService.java
@@ -8,17 +8,17 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.kuali.kfs.kim.impl.KIMPropertyConstants;
 import org.kuali.kfs.kim.impl.identity.PersonSearchService;
 import org.kuali.kfs.krad.bo.BusinessObjectBase;
+import org.springframework.util.MultiValueMap;
 
 import edu.cornell.kfs.kim.CuKimPropertyConstants;
 
 public class CuPersonSearchService extends PersonSearchService {
 
     @Override
-    protected Pair<Collection<? extends BusinessObjectBase>, Integer> executeSearch(
-            Class<? extends BusinessObjectBase> businessObjectClass, int skip, int limit, String sortField,
-            boolean sortAscending, Map<String, String> searchProps) {
-        return super.executeSearch(businessObjectClass, skip, limit, transformSortFieldIfNecessary(sortField),
-                sortAscending, searchProps);
+    public Pair<Collection<? extends BusinessObjectBase>, Integer> getSearchResults(
+            Class<? extends BusinessObjectBase> businessObjectClass, MultiValueMap<String, String> fieldValues,
+            int skip, int limit, String sortField, boolean sortAscending) {
+        return super.getSearchResults(businessObjectClass, fieldValues, skip, limit, transformSortFieldIfNecessary(sortField), sortAscending);
     }
 
     private static String transformSortFieldIfNecessary(final String sortField) {
@@ -27,11 +27,9 @@ public class CuPersonSearchService extends PersonSearchService {
         }
 
         String transformedSortField = sortField;
-        if (StringUtils.equalsIgnoreCase(transformedSortField,
+        if (StringUtils.equalsIgnoreCase(sortField,
                 KIMPropertyConstants.Person.NAME + CuKimPropertyConstants.MASKED_IF_NECESSARY_SUFFIX)) {
-            transformedSortField = transformedSortField.replace(
-                    KIMPropertyConstants.Person.NAME + CuKimPropertyConstants.MASKED_IF_NECESSARY_SUFFIX,
-                    KIMPropertyConstants.Person.LAST_NAME);
+            transformedSortField = KIMPropertyConstants.Person.LAST_NAME;
         }
 
         return transformedSortField;
