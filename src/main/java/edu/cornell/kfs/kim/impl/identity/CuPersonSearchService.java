b08d103db483fb77a06552596aecfbbb68254559
Daniela Balmus <db483@cornell.edu>
2025-10-28 16:02:46 +0000

    KFSPTS-36478 fix Person sorting by name (#1824)

---
+++ src/main/java/edu/cornell/kfs/kim/impl/identity/CuPersonSearchService.java
@@ -0,0 +1,38 @@
+package edu.cornell.kfs.kim.impl.identity;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
+import org.kuali.kfs.kim.impl.KIMPropertyConstants;
+import org.kuali.kfs.kim.impl.identity.PersonSearchService;
+import org.kuali.kfs.krad.bo.BusinessObjectBase;
+import org.springframework.util.MultiValueMap;
+
+import edu.cornell.kfs.kim.CuKimPropertyConstants;
+
+public class CuPersonSearchService extends PersonSearchService {
+
+    @Override
+    public Pair<Collection<? extends BusinessObjectBase>, Integer> getSearchResults(
+            Class<? extends BusinessObjectBase> businessObjectClass, MultiValueMap<String, String> fieldValues,
+            int skip, int limit, String sortField, boolean sortAscending) {
+        return super.getSearchResults(businessObjectClass, fieldValues, skip, limit, transformSortFieldIfNecessary(sortField), sortAscending);
+    }
+
+    private static String transformSortFieldIfNecessary(final String sortField) {
+        if (sortField == null) {
+            return null;
+        }
+
+        String transformedSortField = sortField;
+        if (StringUtils.equalsIgnoreCase(sortField,
+                KIMPropertyConstants.Person.NAME + CuKimPropertyConstants.MASKED_IF_NECESSARY_SUFFIX)) {
+            transformedSortField = KIMPropertyConstants.Person.NAME;
+        }
+
+        return transformedSortField;
+    }
+
+}
