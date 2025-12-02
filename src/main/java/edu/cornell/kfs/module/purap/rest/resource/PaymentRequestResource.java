3401790c2a690b973f99aefc430115db66b11377
Jay Hulslander <jdh34@cornell.edu>
2025-12-02 16:11:02 -0500

    setup wireing and struts routing

--- src/main/java/edu/cornell/kfs/module/purap/rest/resource/PaymentRequestResource.java
+++ src/main/java/edu/cornell/kfs/module/purap/rest/resource/PaymentRequestResource.java
@@ -3,22 +3,29 @@ package edu.cornell.kfs.module.purap.rest.resource;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.kuali.kfs.core.api.util.type.KualiDecimal;
+import org.kuali.kfs.sys.KFSConstants;
 import org.kuali.kfs.sys.context.SpringContext;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import edu.cornell.kfs.module.purap.CUPurapConstants;
 import edu.cornell.kfs.module.purap.rest.jsonObjects.PaymentRequestDto;
 import edu.cornell.kfs.module.purap.rest.jsonObjects.PaymentRequestResultsDto;
 import edu.cornell.kfs.module.purap.service.PaymentRequestDtoValidationService;
-import edu.cornell.kfs.module.purap.service.impl.PaymentRequestDtoValidationServiceImpl;
+import edu.cornell.kfs.sys.CUKFSConstants;
+import edu.cornell.kfs.sys.service.ApiAuthenticationService;
+import edu.cornell.kfs.sys.typeadapters.KualiDecimalTypeAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
@@ -26,7 +33,10 @@ import javax.ws.rs.Produces;
 @Produces(MediaType.APPLICATION_JSON)
 public class PaymentRequestResource {
     private static final Logger LOG = LogManager.getLogger();
-    private static final Gson gson = new Gson();
+    private static Gson gson = new GsonBuilder()
+        .setDateFormat(KFSConstants.MONTH_DAY_YEAR_DATE_FORMAT)
+        .registerTypeAdapter(KualiDecimal.class, new KualiDecimalTypeAdapter())
+        .create();
 
     @Context
     protected HttpServletRequest servletRequest;
@@ -35,23 +45,37 @@ public class PaymentRequestResource {
     protected HttpServletResponse servletResponse;
 
     private PaymentRequestDtoValidationService paymentRequestDtoValidationService;
+    private ApiAuthenticationService apiAuthenticationService;
 
     @GET
     public Response describePaymentRequestResource() {
         return Response.ok(CUPurapConstants.PAYMENT_REQUEST_ENDPOINT_DESCRIPTION).build();
     }
 
-    // New POST endpoint for creating PaymentRequestDocument
-    @Path("payment-request")
-    @javax.ws.rs.POST
-    @javax.ws.rs.Consumes(MediaType.APPLICATION_JSON)
-    public Response createPaymentRequestDocument(PaymentRequestDto paymentRequestDto) {
-        LOG.info("Received PaymentRequestDto: {}", paymentRequestDto);
+    @Path("/createpaymentrequest")
+    @POST
+    public Response createPaymentRequestDocument(java.io.InputStream requestBodyStream) {
+        try {
+            java.util.Scanner scanner = new java.util.Scanner(requestBodyStream, "UTF-8").useDelimiter("\\A");
+            String jsonBody = scanner.hasNext() ? scanner.next() : "";
+            PaymentRequestDto paymentRequestDto = gson.fromJson(jsonBody, PaymentRequestDto.class);
 
-        // TODO: You may need to inject/set necessary services on validationService, if not autowired.
-        PaymentRequestResultsDto results = getPaymentRequestDtoValidationService().validatePaymentRequestDto(paymentRequestDto);
+            LOG.info("createPaymentRequestDocument, Received PaymentRequestDto: {}", paymentRequestDto);
 
-        return Response.ok(results).build();
+            String loggedInUser = getApiAuthenticationService().getAuthenticateUser(servletRequest);
+            LOG.info("createPaymentRequestDocument, logged in user: {}", loggedInUser);
+
+            PaymentRequestResultsDto results = getPaymentRequestDtoValidationService().validatePaymentRequestDto(paymentRequestDto);
+            if (results.isValid()) {
+                return Response.ok(gson.toJson(results)).build();
+            } else {
+                return Response.status(Status.BAD_REQUEST).entity(gson.toJson(results)).build();
+            }
+
+        } catch (Exception e) {
+            LOG.error("createPaymentRequestDocument, an error occured", e);
+            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(CUKFSConstants.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     private PaymentRequestDtoValidationService getPaymentRequestDtoValidationService() {
@@ -59,5 +83,12 @@ public class PaymentRequestResource {
             paymentRequestDtoValidationService = SpringContext.getBean(PaymentRequestDtoValidationService.class);
         }
         return paymentRequestDtoValidationService;
+    }
+
+    private ApiAuthenticationService getApiAuthenticationService() {
+        if (apiAuthenticationService == null) {
+            apiAuthenticationService = SpringContext.getBean(ApiAuthenticationService.class);
+        }
+        return apiAuthenticationService;
     }
 }
