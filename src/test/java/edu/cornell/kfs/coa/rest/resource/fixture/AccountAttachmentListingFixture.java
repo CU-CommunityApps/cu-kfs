83acbc5d54c610fcb2b3a10cf55d0e6552cbd6ea
cah292 <cah292@cornell.edu>
2025-12-09 16:11:39 -0500

    KFSPTS-36304 More unit test progress

--- src/test/java/edu/cornell/kfs/coa/rest/resource/fixture/AccountAttachmentListingFixture.java
+++ src/test/java/edu/cornell/kfs/coa/rest/resource/fixture/AccountAttachmentListingFixture.java
@@ -5,6 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.kuali.kfs.coa.businessobject.Account;
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface AccountAttachmentListingFixture {
@@ -13,5 +15,14 @@ public @interface AccountAttachmentListingFixture {
     String accountNumber();
     String accountName();
     AccountAttachmentNoteFixture[] notes();
+
+    public static final class Utils {
+
+        public static Account toAccount(final AccountAttachmentListingFixture fixture) {
+            final Account account = new Account();
+            return account;
+        }
+
+    }
 
 }
