0e9d793ea3013b65f5b6921127e2b287ee6dcbd3
Chad Hagstrom <cah292@cornell.edu>
2026-03-09 09:48:42 -0400

    KFSPTS-37458 Populate Supplier Bank Accounts Tab (#1890)

---
+++ src/main/java/edu/cornell/kfs/vnd/util/VendorAccountFinder.java
@@ -0,0 +1,14 @@
+package edu.cornell.kfs.vnd.util;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import org.kuali.kfs.pdp.businessobject.PayeeACHAccount;
+
+@FunctionalInterface
+public interface VendorAccountFinder {
+
+    Collection<PayeeACHAccount> findAllActiveAccountsForVendor(final Integer vendorHeaderGeneratedIdentifier,
+            final Integer vendorDetailAssignedIdentifier) throws IOException;
+
+}
