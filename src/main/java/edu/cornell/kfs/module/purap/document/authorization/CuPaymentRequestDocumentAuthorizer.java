a87fee774a91347f05e58b8982d69baefaa8b75b
Andrew Dickson <ajd299@cornell.edu>
2026-06-12 11:22:58 -0400

    Added isApprovalRequested

--- src/main/java/edu/cornell/kfs/module/purap/document/authorization/CuPaymentRequestDocumentAuthorizer.java
+++ src/main/java/edu/cornell/kfs/module/purap/document/authorization/CuPaymentRequestDocumentAuthorizer.java
@@ -21,6 +21,7 @@ package edu.cornell.kfs.module.purap.document.authorization;
 import java.util.Set;
 
 import org.apache.commons.lang3.StringUtils;
+import org.kuali.kfs.kew.api.WorkflowDocument;
 import org.kuali.kfs.kim.impl.identity.Person;
 import org.kuali.kfs.krad.document.Document;
 import org.kuali.kfs.module.purap.PaymentRequestStatuses;
@@ -29,20 +30,19 @@ import org.kuali.kfs.module.purap.document.PaymentRequestDocument;
 import org.kuali.kfs.module.purap.document.authorization.PurchasingAccountsPayableTransactionalDocumentAuthorizerBase;
 
 /**
- * CU Customization: KFSPTS-38144 Custom authorizer for PaymentRequestDocument that allows AP staff to edit
+ * CU Customization: KFSPTS-38144 Custom authorizer for PaymentRequestDocument that allows AP Approver to edit
  * vendor address fields when the document is in "Awaiting AP Review" status.
  */
 public class CuPaymentRequestDocumentAuthorizer extends PurchasingAccountsPayableTransactionalDocumentAuthorizerBase {
 
     @Override
-    public Set<String> getDocumentActions(
-            final Document document, final Person user,
+    public Set<String> getDocumentActions(final Document document, final Person user,
             final Set<String> documentActionsFromPresentationController) {
 
-        final Set<String> documentActionsToReturn = super.getDocumentActions(document, user,
-                documentActionsFromPresentationController);
+        final Set<String> documentActionsToReturn = super.getDocumentActions(document, user, documentActionsFromPresentationController);
 
         final PaymentRequestDocument preqDocument = (PaymentRequestDocument) document;
+        
         if (canEditVendorAddressInApReview(preqDocument)) {
             documentActionsToReturn.add(PurapAuthorizationConstants.CAN_EDIT_VENDOR_ADDRESS);
         }
@@ -52,8 +52,13 @@ public class CuPaymentRequestDocumentAuthorizer extends PurchasingAccountsPayabl
 
     protected boolean canEditVendorAddressInApReview(final PaymentRequestDocument preqDocument) {
         final String applicationDocumentStatus = preqDocument.getApplicationDocumentStatus();
-        return StringUtils.equals(PaymentRequestStatuses.APPDOC_AWAITING_ACCOUNTS_PAYABLE_REVIEW, 
-                applicationDocumentStatus);
+        
+        if (!StringUtils.equals(PaymentRequestStatuses.APPDOC_AWAITING_ACCOUNTS_PAYABLE_REVIEW, applicationDocumentStatus)) {
+            return false;
+        }
+        
+        final WorkflowDocument workflowDocument = preqDocument.getDocumentHeader().getWorkflowDocument();
+        return workflowDocument.isApprovalRequested();
     }
 
 }
