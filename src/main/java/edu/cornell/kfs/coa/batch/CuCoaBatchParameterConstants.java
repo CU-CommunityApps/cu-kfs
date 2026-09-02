5c87ebfbfe92eeb504f4b6a0ade1c5c1f7152c97
Jay Hulslander <jdh34@cornell.edu>
2026-09-02 17:34:16 -0400

    KFSPTS-33772 remove batch job to copy legacy acct attachments CO-99-02 (#1958)

--- src/main/java/edu/cornell/kfs/coa/batch/CuCoaBatchParameterConstants.java
+++
@@ -1,7 +0,0 @@
-package edu.cornell.kfs.coa.batch;
-
-public class CuCoaBatchParameterConstants {
-    public static final String ATTACHMENT_BATCH_SIZE = "ATTACHMENT_BATCH_SIZE";
-    public static final String MAX_DOWNLOAD_RETRIES = "MAX_DOWNLOAD_RETRIES";
-    public static final String MAX_ATTACHMENT_FAILURES_PER_RUN = "MAX_ATTACHMENT_FAILURES_PER_RUN";
-}
