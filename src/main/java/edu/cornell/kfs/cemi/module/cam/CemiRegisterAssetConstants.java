5402e006fe20eeb74b7d235c9bcd39453a2e7c97
Daniela Balmus <db483@cornell.edu>
2026-09-03 21:10:09 +0100

    KFSPTS-37934 add more changes

--- src/main/java/edu/cornell/kfs/cemi/module/cam/CemiRegisterAssetConstants.java
+++ src/main/java/edu/cornell/kfs/cemi/module/cam/CemiRegisterAssetConstants.java
@@ -12,8 +12,29 @@ public final class CemiRegisterAssetConstants {
     public static final String REGISTER_ASSET_EXTRACT_FILENAME_PREFIX = "Register_Asset_ITH_";
     public static final String REGISTER_ASSET_EXTRACT_PLAIN_FILENAME = "Register_Asset.xlsx";
     
+    
+    
     public static final class RegisterAssetExtractSheets {
         public static final String REGISTER_ASSET = "Register_Asset";
     }
+    
+    public static final class AccountType {
+        public static final String EN = "EN";
+        public static final String CC = "CC";
+    }
+    
+    public static final class WorkdayCompany{
+        public static final String STATUTORY = "C001";
+        public static final String ENDOWED = "C002";
+    }
+    
+    public static final String NUMERIC_ONE = "1";
+    
+    public static final String COST_CENTER_ID = "Cost_Center_ID";
+    public static final String FUND_ID = "Fund_ID";
+    public static final String PROGRAM_ID = "Program_ID";
+    public static final String CORNELL_UNIVERSITY_ITHACA = "Cornell_University_Ithaca";
+    public static final String STRAIGHT_LINE = "STRAIGHT_LINE";
+    public static final String DEPRECATION_START_DATE = "7/1/2026";
 
 }
