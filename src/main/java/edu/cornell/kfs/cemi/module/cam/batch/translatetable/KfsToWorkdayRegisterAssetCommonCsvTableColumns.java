1112145fe902bd6bc1e5ab9b5294238df62c0580
Daniela Balmus <db483@cornell.edu>
2026-09-10 19:05:21 +0100

    KFSPTS-39168 add Asset Class translation for Register Asset extract (#1961)

---
+++ src/main/java/edu/cornell/kfs/cemi/module/cam/batch/translatetable/KfsToWorkdayRegisterAssetCommonCsvTableColumns.java
@@ -0,0 +1,8 @@
+package edu.cornell.kfs.cemi.module.cam.batch.translatetable;
+
+public enum KfsToWorkdayRegisterAssetCommonCsvTableColumns {
+    LEGACY_CODE,
+    LEGACY_NAME,
+    WORKDAY_REF_ID,
+    CAMPUS
+}
