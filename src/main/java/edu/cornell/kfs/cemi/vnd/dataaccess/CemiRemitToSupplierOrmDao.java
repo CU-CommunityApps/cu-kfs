ef27b169028e3277e1e4c72860265473c0e45d21
cah292 <cah292@cornell.edu>
2026-09-17 14:19:05 -0400

    KFSPTS-38794 Fix issues with Remit To Supplier Extract

--- src/main/java/edu/cornell/kfs/cemi/vnd/dataaccess/CemiRemitToSupplierOrmDao.java
+++ src/main/java/edu/cornell/kfs/cemi/vnd/dataaccess/CemiRemitToSupplierOrmDao.java
@@ -1,16 +1,11 @@
 package edu.cornell.kfs.cemi.vnd.dataaccess;
 
-import java.util.List;
 import java.util.stream.Stream;
-
-import org.kuali.kfs.vnd.businessobject.VendorAddress;
 
 import edu.cornell.kfs.cemi.vnd.batch.businessobject.CemiSupplierFileAddressesTabRowBo;
 
 public interface CemiRemitToSupplierOrmDao {
 
     Stream<CemiSupplierFileAddressesTabRowBo> getAddressesForCemiRemitToSupplierExtractAsCloseableStream();
-
-    List<VendorAddress> getKfsVendorAddresses(final String supplierId, final String supplierJobRunDate);
 
 }
