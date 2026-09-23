5e6a1d88291b3a25dab2ea36b34fe8b02314ace1
Chad Hagstrom <cah292@cornell.edu>
2026-09-23 12:33:26 -0400

    KFSPTS-39277 Fix PO extract handling of dates and Processed docs (#1974)

--- src/main/java/edu/cornell/kfs/cemi/module/purap/batch/service/impl/factory/CemiPurchaseOrderHeaderBoFactory.java
+++ src/main/java/edu/cornell/kfs/cemi/module/purap/batch/service/impl/factory/CemiPurchaseOrderHeaderBoFactory.java
@@ -289,12 +289,13 @@ public class CemiPurchaseOrderHeaderBoFactory {
 
     private String determineDocumentDate() {
         final WorkflowDocument workflowDocument = purchaseOrderDocument.getDocumentHeader().getWorkflowDocument();
-        Validate.validState(workflowDocument.isFinal(), "PO Document Number %s should have been finalized",
+        Validate.validState(workflowDocument.isFinal() || workflowDocument.isProcessed(),
+                "PO Document Number %s should have been in PROCESSED or FINAL status",
                 purchaseOrderDocument.getDocumentNumber());
-        final LocalDateTime dateFinalized = workflowDocument.getDateFinalized();
-        Validate.validState(dateFinalized != null, "PO Document Number %s should have had a finalization date",
+        final LocalDateTime lastApprovedDate = workflowDocument.getDateApproved();
+        Validate.validState(lastApprovedDate != null, "PO Document Number %s should have had a Last Approved Date",
                 purchaseOrderDocument.getDocumentNumber());
-        return CemiPurchaseOrderUtils.formatAsDate(dateFinalized);
+        return CemiPurchaseOrderUtils.formatAsDate(lastApprovedDate);
     }
 
     private String determineFreightAmount() {
